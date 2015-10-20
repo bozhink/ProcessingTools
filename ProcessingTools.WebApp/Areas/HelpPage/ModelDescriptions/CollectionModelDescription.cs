4795bff2eb974c46bc5f356dfbc4858de2f94573
bozhink <bozhink@gmail.com>
2015-10-20 11:08:01 +0300

    ProcessingTools.WebApp added

---
+++ ProcessingTools.WebApp/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace ProcessingTools.WebApp.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ ProcessingTools.WebApp/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ProcessingTools.WebApp.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
