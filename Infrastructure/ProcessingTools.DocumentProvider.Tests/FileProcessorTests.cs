8f22927401d19d863156951a8b6494debef9b5c7
Bozhin Karaivanov <bozhink@gmail.com>
2015-11-20 23:53:35 +0200

    FileProcessorTests added + NUnit update

---
+++ Infrastructure/ProcessingTools.DocumentProvider.Tests/FileProcessorTests.cs
@@ -0,0 +1,16 @@
+﻿namespace ProcessingTools.DocumentProvider.Tests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class FileProcessorTests
+    {
+        [TestMethod]
+        public void FileProcessor_TestMethod1()
+        {
+            var fileProcessor = new FileProcessor();
+            Console.WriteLine(Environment.GetEnvironmentVariable("TEMP"));
+        }
+    }
+}
