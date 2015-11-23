19f05ad8d6722e3c9318b336681e92e28df50d07
Bozhin Karaivanov <bozhink@gmail.com>
2015-11-24 00:28:43 +0200

    CsvObject added

---
+++ Libraries/ProcessingTools.Csv.Serialization.Tests/CsvObjectTests.cs
@@ -0,0 +1,34 @@
+﻿namespace ProcessingTools.Csv.Serialization.Tests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class CsvObjectTests
+    {
+        [TestMethod]
+        public void CsvObject_WithEmptyConstuctor_ShouldCreateValidObject()
+        {
+            var csv = new CsvObject();
+            Assert.IsFalse(string.IsNullOrEmpty(csv.FieldTerminator.ToString()), "FieldTerminator");
+            Assert.IsFalse(string.IsNullOrWhiteSpace(csv.FirstRow.ToString()), "FirstRow");
+            Assert.IsFalse(string.IsNullOrEmpty(csv.RowTerminator.ToString()), "RowTerminator");
+            Assert.IsFalse(string.IsNullOrWhiteSpace(csv.SingleCharEscapeSymbol.ToString()), "SingleCharEscapeSymbol");
+            Assert.IsFalse(string.IsNullOrWhiteSpace(csv.TerminatorEscapeLeftWrapSymbol.ToString()), "TerminatorEscapeLeftWrapSymbol");
+            Assert.IsFalse(string.IsNullOrWhiteSpace(csv.TerminatorEscapeRightWrapSymbol.ToString()), "TerminatorEscapeRightWrapSymbol");
+        }
+
+        [TestMethod]
+        public void CsvObject_ShouldDeserializeSimpleCsv()
+        {
+            const string CsvText = "Name,Year,Description\nJohn Smith,2015,No desription here";
+
+            var csv = new CsvObject();
+            var result = csv.Deserialize(CsvText);
+
+            Assert.AreEqual(2, result.Count, "Invalid number of rows.");
+            Assert.AreEqual(3, result.Dequeue().Count, "Invalid number of columns in first row.");
+            Assert.AreEqual(3, result.Dequeue().Count, "Invalid number of columns in second row.");
+        }
+    }
+}
---
+++ Libraries/ProcessingTools.Csv.Serialization/CsvObject.cs
@@ -0,0 +1,126 @@
+﻿namespace ProcessingTools.Csv.Serialization
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class CsvObject
+    {
+        public CsvObject(
+            char fieldTerminator = ',',
+            char rowTerminator = '\n',
+            int firstRow = 1,
+            char singleCharEscapeSymbol = '\\',
+            char terminatorEscapeLeftWrapSymbol = '"',
+            char terminatorEscapeRightWrapSymbol = '"')
+        {
+            this.FieldTerminator = fieldTerminator;
+            this.RowTerminator = rowTerminator;
+            this.FirstRow = firstRow;
+            this.TerminatorEscapeLeftWrapSymbol = terminatorEscapeLeftWrapSymbol;
+            this.TerminatorEscapeRightWrapSymbol = terminatorEscapeRightWrapSymbol;
+        }
+
+        public char FieldTerminator { get; private set; }
+
+        public char RowTerminator { get; private set; }
+
+        public int FirstRow { get; private set; }
+
+        public char SingleCharEscapeSymbol { get; private set; }
+
+        public char TerminatorEscapeLeftWrapSymbol { get; private set; }
+
+        public char TerminatorEscapeRightWrapSymbol { get; private set; }
+
+        // TODO
+        public Queue<Queue<string>> Deserialize(string text)
+        {
+            char[] textChars = text.ToCharArray();
+
+            Queue<Queue<string>> rows = new Queue<Queue<string>>();
+
+            Queue<string> fields = new Queue<string>();
+            StringBuilder stringBuilder = new StringBuilder();
+
+            bool escapeState = false;
+            for (int i = 0, len = textChars.Length; i < len; ++i)
+            {
+                char ch = textChars[i];
+                if (ch == this.SingleCharEscapeSymbol)
+                {
+                    stringBuilder.Append(ch);
+
+                    if (!(i < len - 1))
+                    {
+                        throw new ApplicationException("Invalid escape of last character of the text.");
+                    }
+
+                    stringBuilder.Append(textChars[++i]);
+                }
+                else if (ch == this.TerminatorEscapeLeftWrapSymbol && ch == this.TerminatorEscapeRightWrapSymbol)
+                {
+                    // Equal TerminatorEscapeLeftWrapSymbol and TerminatorEscapeRightWrapSymbol
+                    stringBuilder.Append(ch);
+                    escapeState = !escapeState;
+                }
+                else if (ch == this.TerminatorEscapeLeftWrapSymbol)
+                {
+                    stringBuilder.Append(ch);
+                    if (!escapeState)
+                    {
+                        escapeState = true;
+                    }
+                }
+                else if (ch == this.TerminatorEscapeRightWrapSymbol)
+                {
+                    stringBuilder.Append(ch);
+                    if (escapeState)
+                    {
+                        escapeState = false;
+                    }
+                }
+                else if (ch == this.FieldTerminator)
+                {
+                    if (escapeState)
+                    {
+                        stringBuilder.Append(ch);
+                    }
+                    else
+                    {
+                        fields.Enqueue(stringBuilder.ToString());
+                        stringBuilder.Clear();
+                    }
+                }
+                else if (ch == this.RowTerminator)
+                {
+                    if (escapeState)
+                    {
+                        stringBuilder.Append(ch);
+                    }
+                    else
+                    {
+                        // The row terminator is actually a field terminator
+                        fields.Enqueue(stringBuilder.ToString());
+                        stringBuilder.Clear();
+
+                        rows.Enqueue(fields);
+                        fields.Clear();
+                    }
+                }
+                else
+                {
+                    stringBuilder.Append(ch);
+                }
+            }
+
+            fields.Enqueue(stringBuilder.ToString());
+            stringBuilder.Clear();
+
+            rows.Enqueue(fields);
+            fields.Clear();
+
+            return rows;
+        }
+    }
+}
