535f355bf7abd8b3aa2c3e4801b142ea338dc136
bozhink <bozhink@gmail.com>
2016-01-13 15:03:15 +0200

    IValidationServiceModel added

---
+++ Services/Services.Common/Models/Contracts/IValidationServiceModel.cs
@@ -0,0 +1,14 @@
+﻿namespace ProcessingTools.Services.Common.Models.Contracts
+{
+    using System;
+    using Types;
+
+    public interface IValidationServiceModel<T>
+    {
+        T ValidatedObject { get; set; }
+
+        ValidationStatus ValidationStatus { get; set; }
+
+        Exception ValidationException { get; set; }
+    }
+}
