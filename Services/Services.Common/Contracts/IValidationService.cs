d1e44ad6a69dbc69f17850c446b685db02982e9e
bozhink <bozhink@gmail.com>
2016-01-13 15:42:17 +0200

    IValidationService added

---
+++ Services/Services.Common/Contracts/IValidationService.cs
@@ -0,0 +1,12 @@
+﻿namespace ProcessingTools.Services.Common.Contracts
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    using Models.Contracts;
+
+    public interface IValidationService<T>
+    {
+        Task<IValidationServiceModel<T>> Validate(IEnumerable<T> items);
+    }
+}
