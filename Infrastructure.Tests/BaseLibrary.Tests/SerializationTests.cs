f660d847b36927f06e379527084b2e0ef66ea424
Bozhin Karaivanov <bozhink@gmail.com>
2016-01-15 23:04:37 +0200

    Serialization sample tests added

---
+++ Infrastructure.Tests/BaseLibrary.Tests/SerializationTests.cs
@@ -0,0 +1,36 @@
+﻿namespace ProcessingTools.BaseLibrary.Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Models;
+    using System.IO;
+    using System.Xml.Serialization;
+    using System.Text;
+
+    [TestClass]
+    public class SerializationTests
+    {
+        [TestMethod]
+        public void SerializeExternalLinkModel_ShouldWork()
+        {
+            var link = new ExternalLinkSerializableModel()
+            {
+                Value = "http://example.com",
+                ExtLinkType = "uri",
+                Href = "http://example.com"
+            };
+
+            XmlSerializer serializer = new XmlSerializer(typeof(ExternalLinkSerializableModel));
+
+            using (MemoryStream stream = new MemoryStream())
+            {
+                serializer.Serialize(stream, link);
+                stream.Position = 0;
+
+                var reader = new StreamReader(stream, new UTF8Encoding());
+                var result = reader.ReadToEnd();
+
+                System.Console.WriteLine(result);
+            }
+        }
+    }
+}
