84e955505f306909dd8d443ab72f14b1afa73f85
bozhink <bozhink@gmail.com>
2016-01-15 11:27:39 +0200

    NlmArchiveConsoleManager added JournalDataContract model.

--- Startup/NlmArchiveConsoleManager/Startup.cs
+++ Startup/NlmArchiveConsoleManager/Startup.cs
@@ -1,8 +1,10 @@
 ﻿namespace ProcessingTools.NlmArchiveConsoleManager
 {
     using System;
+    using System.Configuration;
     using System.IO;
     using System.Linq;
+    using System.Runtime.Serialization.Json;
 
     using Contracts;
     using Loggers;
@@ -13,12 +15,38 @@
     {
         private static ILogger logger = new TextWriterLogger();
 
-        private static IJournal journal = new Journal
-        {
-        };
-
         public static void Main(string[] args)
         {
+            var appSettingsReader = new AppSettingsReader();
+            var journalJsonFileName = appSettingsReader.GetValue("JournalConfigJsonFile", typeof(string)).ToString();
+
+            IJournal journal = null;
+
+            try
+            {
+                using (var journalJsonFileStream = new FileStream(journalJsonFileName, FileMode.Open))
+                {
+                    var serializer = new DataContractJsonSerializer(typeof(JournalDataContract));
+                    var journalJsonObject = (JournalDataContract)serializer.ReadObject(journalJsonFileStream);
+
+                    journal = new Journal
+                    {
+                        AbbreviatedJournalTitle = journalJsonObject.AbbreviatedJournalTitle,
+                        FileNamePattern = journalJsonObject.FileNamePattern,
+                        IssnEPub = journalJsonObject.IssnEPub,
+                        IssnPPub = journalJsonObject.IssnPPub,
+                        JournalId = journalJsonObject.JournalId,
+                        JournalTitle = journalJsonObject.JournalTitle,
+                        PublisherName = journalJsonObject.PublisherName
+                    };
+                }
+            }
+            catch (Exception e)
+            {
+                logger?.Log(e, "Cannot read journal data.");
+                Environment.Exit(1);
+            }
+
             var directories = args.Select(a => Path.GetDirectoryName(a));
 
             foreach (var directoryName in directories)
