abb0d97a4f03500321e6dafe3ba3a7798cea87b4
bozhink <bozhink@gmail.com>
2016-01-18 13:12:28 +0200

    InvokeProcessor: wait async methods

--- Startup/Tagger/FileProcessor.cs
+++ Startup/Tagger/FileProcessor.cs
@@ -12,37 +12,37 @@
 
         protected async Task InvokeProcessor(string message, IValidator validator)
         {
-            await this.InvokeProcessor(message, async () => await validator.Validate());
+            await this.InvokeProcessor(message, () => validator.Validate().Wait());
         }
 
         protected async Task InvokeProcessor(string message, ITagger tagger)
         {
-            await this.InvokeProcessor(message, async () => await tagger.Tag());
+            await this.InvokeProcessor(message, () => tagger.Tag().Wait());
         }
 
         protected async Task InvokeProcessor(string message, IParser parser)
         {
-            await this.InvokeProcessor(message, async () => await parser.Parse());
+            await this.InvokeProcessor(message, () => parser.Parse().Wait());
         }
 
         protected async Task InvokeProcessor(string message, IGenerator generator)
         {
-            await this.InvokeProcessor(message, async () => await generator.Generate());
+            await this.InvokeProcessor(message, () => generator.Generate().Wait());
         }
 
         protected async Task InvokeProcessor(string message, IFormatter formatter)
         {
-            await this.InvokeProcessor(message, async () => await formatter.Format());
+            await this.InvokeProcessor(message, () => formatter.Format().Wait());
         }
 
         protected async Task InvokeProcessor(string message, ICloner cloner)
         {
-            await this.InvokeProcessor(message, async () => await cloner.Clone());
+            await this.InvokeProcessor(message, () => cloner.Clone().Wait());
         }
 
         protected async Task InvokeProcessor(string message, IProcessor processor)
         {
-            await this.InvokeProcessor(message, async () => await processor.Process());
+            await this.InvokeProcessor(message, () => processor.Process().Wait());
         }
     }
 }
