48ee9ca530bab3641c347c8a77e83466e9461f1c
Bozhin Karaivanov <bozhink@gmail.com>
2016-01-23 14:38:22 +0200

    Data.Miners projects added

---
+++ Data.Miners/Data.Miners.Common/Contracts/IDataMiner.cs
@@ -0,0 +1,10 @@
+﻿namespace ProcessingTools.Data.Miners.Common.Contracts
+{
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public interface IDataMiner<T>
+    {
+        Task<IQueryable<T>> Mine(string content);
+    }
+}
