460d76377b5908b5780b375920b12a6824f45dc9
Bozhin Karaivanov <bozhink@gmail.com>
2016-01-23 15:28:06 +0200

    Bio.Data.Miners.Contracts copied.

---
+++ Data.Miners/Bio.Data.Miners/Contracts/IEnvoTermsDataMiner.cs
@@ -0,0 +1,9 @@
+﻿namespace ProcessingTools.Bio.Data.Miners.Contracts
+{
+    using Models.Contracts;
+    using ProcessingTools.Data.Miners.Common.Contracts;
+
+    public interface IEnvoTermsDataMiner : IDataMiner<IEnvoTerm>
+    {
+    }
+}
