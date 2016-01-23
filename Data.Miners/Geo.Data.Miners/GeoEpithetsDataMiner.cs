1093be01eadc697352157f83f39b9976faf22e13
Bozhin Karaivanov <bozhink@gmail.com>
2016-01-23 14:57:38 +0200

    Geo.Harvesters copied to Geo.Data.Miners

---
+++ Data.Miners/Geo.Data.Miners/GeoEpithetsDataMiner.cs
@@ -0,0 +1,15 @@
+﻿namespace ProcessingTools.Geo.Data.Miners
+{
+    using Contracts;
+    using ProcessingTools.Data.Miners.Common.Factories;
+    using ProcessingTools.Geo.Services.Data.Contracts;
+    using ProcessingTools.Geo.Services.Data.Models.Contracts;
+
+    public class GeoEpithetsDataMiner : SimpleServiceStringDataMinerFactory<IGeoEpithetsDataService, IGeoEpithet>, IGeoEpithetsDataMiner
+    {
+        public GeoEpithetsDataMiner(IGeoEpithetsDataService service)
+            : base(service)
+        {
+        }
+    }
+}
---
+++ Data.Miners/Geo.Data.Miners/GeoNamesDataMiner.cs
@@ -0,0 +1,15 @@
+﻿namespace ProcessingTools.Geo.Data.Miners
+{
+    using Contracts;
+    using ProcessingTools.Data.Miners.Common.Factories;
+    using ProcessingTools.Geo.Services.Data.Contracts;
+    using ProcessingTools.Geo.Services.Data.Models.Contracts;
+
+    public class GeoNamesDataMiner : SimpleServiceStringDataMinerFactory<IGeoNamesDataService, IGeoName>, IGeoNamesDataMiner
+    {
+        public GeoNamesDataMiner(IGeoNamesDataService service)
+            : base(service)
+        {
+        }
+    }
+}
