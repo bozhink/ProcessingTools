bc60357382020c07d6a8cf6e24ccb8bdec38afa3
bozhink <bozhink@gmail.com>
2016-01-25 14:16:58 +0200

    IRepository added.

---
+++ Data/Data.Common/Repositories/Contracts/IRepository.cs
@@ -0,0 +1,20 @@
+﻿namespace ProcessingTools.Data.Common.Repositories.Contracts
+{
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public interface IRepository<T>
+    {
+        Task<IQueryable<T>> All();
+
+        Task<T> GetById(object id);
+
+        Task Add(T entity);
+
+        Task Update(T entity);
+
+        Task Delete(T entity);
+
+        Task Delete(object id);
+    }
+}
