a486e31e488c15a803da513b2b054cdd127f4968
bozhink <bozhink@gmail.com>
2016-01-25 16:01:51 +0200

    TaxonomicListRepository prototype added.

---
+++ Data/Bio.Taxonomy.Data/Repositories/TaxonomicListRepository.cs
@@ -0,0 +1,45 @@
+﻿namespace ProcessingTools.Bio.Taxonomy.Data.Repositories
+{
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Contracts;
+
+    public class TaxonomicListRepository<T> : ITaxonomicListRepository<T>
+    {
+        public Task Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IQueryable<T>> All()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(object id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetById(object id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
