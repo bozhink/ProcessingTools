cda18935a3f8a47016d92ec590ae047501175910
bozhink <bozhink@gmail.com>
2016-01-29 12:47:34 +0200

    ParseTreatmentMetaWithAphiaControllerTests added.

---
+++ Startup.Tests/Tagger.Tests/UnitTests/ParseTreatmentMetaWithAphiaControllerTests.cs
@@ -0,0 +1,307 @@
+﻿namespace ProcessingTools.MainProgram.Tests.UnitTests
+{
+    using System;
+    using System.Xml;
+
+    using Controllers;
+    using Moq;
+    using NUnit.Framework;
+    using ProcessingTools.Contracts;
+    using ProcessingTools.Bio.Taxonomy.Services.Data.Contracts;
+
+    [TestFixture]
+    public class ParseTreatmentMetaWithAphiaControllerTests
+    {
+        private const string CallShouldThrowSystemAggregateExceptionMessage = "Call should throw System.AggregateException.";
+        private const string CallShouldThrowSystemArgumentNullExceptionMessage = "Call should throw System.ArgumentNullException.";
+        private const string InnerExceptionShouldBeArgumentNullExceptionMessage = "InnerException should be System.ArgumentNullException.";
+        private const string ContentShouldBeUnchangedMessage = "Content should be unchaged.";
+
+        private XmlDocument document;
+        private XmlNamespaceManager namespaceManager;
+        private ProgramSettings settings;
+        private ILogger logger;
+
+        private IAphiaTaxaClassificationDataService service;
+
+        [SetUp]
+        public void Init()
+        {
+            this.document = new XmlDocument();
+            this.document.LoadXml("<root />");
+
+            this.namespaceManager = new XmlNamespaceManager(this.document.NameTable);
+            this.settings = new ProgramSettings();
+
+            var loggerMock = new Mock<ILogger>();
+            this.logger = loggerMock.Object;
+
+            var serviceMock = new Mock<IAphiaTaxaClassificationDataService>();
+            this.service = serviceMock.Object;
+        }
+
+        [Test]
+        public void ParseTreatmentMetaWithAphiaController_WithDefaultCnstructor_ShouldReturnValidObject()
+        {
+            var controller = new ParseTreatmentMetaWithAphiaController(this.service);
+
+            Assert.IsNotNull(controller, "Controller should not be null.");
+        }
+
+        [Test]
+        public void ParseTreatmentMetaWithAphiaController_WithNullService_ShouldThrowArgumentNullException()
+        {
+
+
+            Assert.Throws<ArgumentNullException>(
+                () =>
+                {
+                    var controller = new ParseTreatmentMetaWithAphiaController(null);
+                },
+                CallShouldThrowSystemArgumentNullExceptionMessage);
+        }
+
+        [Test]
+        public void ParseTreatmentMetaWithAphiaController_WithNullService_ShouldThrowArgumentNullExceptionWithParamName()
+        {
+            try
+            {
+                var controller = new ParseTreatmentMetaWithAphiaController(null);
+            }
+            catch (Exception e)
+            {
+                Assert.AreEqual(typeof(ArgumentNullException), e.GetType(), CallShouldThrowSystemArgumentNullExceptionMessage);
+
+                Assert.AreEqual("service", ((ArgumentNullException)e).ParamName, @"ParamName should be ""service"".");
+            }
+        }
+
+        [Test]
+        public void ParseTreatmentMetaWithAphiaController_RunWithValidParameters_ShouldWork()
+        {
+            var controller = new ParseTreatmentMetaWithAphiaController(this.service);
+
+            string initialContent = this.document.OuterXml;
+
+            controller.Run(this.document.DocumentElement, this.namespaceManager, this.settings, this.logger).Wait();
+
+            string finalContent = this.document.OuterXml;
+
+            Assert.AreEqual(initialContent, finalContent, ContentShouldBeUnchangedMessage);
+        }
+
+        [Test]
+        public void ParseTreatmentMetaWithAphiaController_RunWithNullContextAndValidOtherParameters_ShouldThrowAggregateException()
+        {
+            var controller = new ParseTreatmentMetaWithAphiaController(this.service);
+
+            Assert.Throws<AggregateException>(
+                () => controller.Run(null, this.namespaceManager, this.settings, this.logger).Wait(),
+                CallShouldThrowSystemAggregateExceptionMessage);
+        }
+
+        [Test]
+        public void ParseTreatmentMetaWithAphiaController_RunWithNullContextAndNullNamespaceManagerAndValidOtherParameters_ShouldThrowAggregateException()
+        {
+            var controller = new ParseTreatmentMetaWithAphiaController(this.service);
+
+            Assert.Throws<AggregateException>(
+                () => controller.Run(null, null, this.settings, this.logger).Wait(),
+                CallShouldThrowSystemAggregateExceptionMessage);
+        }
+
+        [Test]
+        public void ParseTreatmentMetaWithAphiaController_RunWithNullContextAndNullProgramSettingsAndValidOtherParameters_ShouldThrowAggregateException()
+        {
+            var controller = new ParseTreatmentMetaWithAphiaController(this.service);
+
+            Assert.Throws<AggregateException>(
+                () => controller.Run(null, this.namespaceManager, null, this.logger).Wait(),
+                CallShouldThrowSystemAggregateExceptionMessage);
+        }
+
+        [Test]
+        public void ParseTreatmentMetaWithAphiaController_RunWithNullContextAndNullLoggerAndValidOtherParameters_ShouldThrowAggregateException()
+        {
+            var controller = new ParseTreatmentMetaWithAphiaController(this.service);
+
+            Assert.Throws<AggregateException>(
+                () => controller.Run(null, this.namespaceManager, this.settings, null).Wait(),
+                CallShouldThrowSystemAggregateExceptionMessage);
+        }
+
+        [Test]
+        public void ParseTreatmentMetaWithAphiaController_RunWithNullContextAndNullNamespaceManagerAndNullProgramSettingsAndValidOtherParameters_ShouldThrowAggregateException()
+        {
+            var controller = new ParseTreatmentMetaWithAphiaController(this.service);
+
+            Assert.Throws<AggregateException>(
+                () => controller.Run(null, null, null, this.logger).Wait(),
+                CallShouldThrowSystemAggregateExceptionMessage);
+        }
+
+        [Test]
+        public void ParseTreatmentMetaWithAphiaController_RunWithNullContextAndNullNamespaceManagerAndNullLoggerAndValidOtherParameters_ShouldThrowAggregateException()
+        {
+            var controller = new ParseTreatmentMetaWithAphiaController(this.service);
+
+            Assert.Throws<AggregateException>(
+                () => controller.Run(null, null, this.settings, null).Wait(),
+                CallShouldThrowSystemAggregateExceptionMessage);
+        }
+
+        [Test]
+        public void ParseTreatmentMetaWithAphiaController_RunWithNullContextAndNullProgramSettingsAndNullLoggerAndValidOtherParameters_ShouldThrowAggregateException()
+        {
+            var controller = new ParseTreatmentMetaWithAphiaController(this.service);
+
+            Assert.Throws<AggregateException>(
+                () => controller.Run(null, this.namespaceManager, null, null).Wait(),
+                CallShouldThrowSystemAggregateExceptionMessage);
+        }
+
+        [Test]
+        public void ParseTreatmentMetaWithAphiaController_RunWithNullParameters_ShouldThrowAggregateException()
+        {
+            var controller = new ParseTreatmentMetaWithAphiaController(this.service);
+
+            Assert.Throws<AggregateException>(
+                () => controller.Run(null, null, null, null).Wait(),
+                CallShouldThrowSystemAggregateExceptionMessage);
+        }
+
+        [Test]
+        public void ParseTreatmentMetaWithAphiaController_RunWithNullContextAndValidOtherParameters_ShouldThrowAggregateExceptionWithInnerArgumentNullException()
+        {
+            var controller = new ParseTreatmentMetaWithAphiaController(this.service);
+
+            try
+            {
+                controller.Run(null, this.namespaceManager, this.settings, this.logger).Wait();
+            }
+            catch (Exception e)
+            {
+                Assert.AreEqual(typeof(AggregateException), e.GetType(), CallShouldThrowSystemAggregateExceptionMessage);
+
+                Exception innerException = e.InnerException;
+                Assert.AreEqual(typeof(ArgumentNullException), innerException.GetType(), InnerExceptionShouldBeArgumentNullExceptionMessage);
+
+                Assert.AreEqual("context", ((ArgumentNullException)innerException).ParamName, @"ParamName should be ""context"".");
+            }
+        }
+
+        [Test]
+        public void ParseTreatmentMetaWithAphiaController_RunWithNullNamespaceManagerAndValidOtherParameters_ShouldThrowAggregateException()
+        {
+            var controller = new ParseTreatmentMetaWithAphiaController(this.service);
+
+            Assert.Throws<AggregateException>(
+                () => controller.Run(this.document.DocumentElement, null, this.settings, this.logger).Wait(),
+                CallShouldThrowSystemAggregateExceptionMessage);
+        }
+
+        [Test]
+        public void ParseTreatmentMetaWithAphiaController_RunWithNullNamespaceManagerAndNullProgramSettingsAndValidOtherParameters_ShouldThrowAggregateException()
+        {
+            var controller = new ParseTreatmentMetaWithAphiaController(this.service);
+
+            Assert.Throws<AggregateException>(
+                () => controller.Run(this.document.DocumentElement, null, null, this.logger).Wait(),
+                CallShouldThrowSystemAggregateExceptionMessage);
+        }
+
+        [Test]
+        public void ParseTreatmentMetaWithAphiaController_RunWithNullNamespaceManagerAndNullLoggerAndValidOtherParameters_ShouldThrowAggregateException()
+        {
+            var controller = new ParseTreatmentMetaWithAphiaController(this.service);
+
+            Assert.Throws<AggregateException>(
+                () => controller.Run(this.document.DocumentElement, null, this.settings, null).Wait(),
+                CallShouldThrowSystemAggregateExceptionMessage);
+        }
+
+        [Test]
+        public void ParseTreatmentMetaWithAphiaController_RunWithNullNamespaceManagerAndNullProgramSettingsAndNullLoggerAndValidOtherParameters_ShouldThrowAggregateException()
+        {
+            var controller = new ParseTreatmentMetaWithAphiaController(this.service);
+
+            Assert.Throws<AggregateException>(
+                () => controller.Run(this.document.DocumentElement, null, null, null).Wait(),
+                CallShouldThrowSystemAggregateExceptionMessage);
+        }
+
+        [Test]
+        public void ParseTreatmentMetaWithAphiaController_RunWithNullNamespaceManagerAndValidOtherParameters_ShouldThrowAggregateExceptionWithInnerArgumentNullException()
+        {
+            var controller = new ParseTreatmentMetaWithAphiaController(this.service);
+
+            try
+            {
+                controller.Run(this.document.DocumentElement, null, this.settings, this.logger).Wait();
+            }
+            catch (Exception e)
+            {
+                Assert.AreEqual(typeof(AggregateException), e.GetType(), CallShouldThrowSystemAggregateExceptionMessage);
+
+                Exception innerException = e.InnerException;
+                Assert.AreEqual(typeof(ArgumentNullException), innerException.GetType(), InnerExceptionShouldBeArgumentNullExceptionMessage);
+
+                Assert.AreEqual("namespaceManager", ((ArgumentNullException)innerException).ParamName, @"ParamName should be ""namespaceManager"".");
+            }
+        }
+
+        [Test]
+        public void ParseTreatmentMetaWithAphiaController_RunWithNullProgramSettingsAndValidOtherParameters_ShouldThrowAggregateException()
+        {
+            var controller = new ParseTreatmentMetaWithAphiaController(this.service);
+
+            Assert.Throws<AggregateException>(
+                () => controller.Run(this.document.DocumentElement, this.namespaceManager, null, this.logger).Wait(),
+                CallShouldThrowSystemAggregateExceptionMessage);
+        }
+
+        [Test]
+        public void ParseTreatmentMetaWithAphiaController_RunWithNullProgramSettingsAndNullLoggerAndValidOtherParameters_ShouldThrowAggregateException()
+        {
+            var controller = new ParseTreatmentMetaWithAphiaController(this.service);
+
+            Assert.Throws<AggregateException>(
+                () => controller.Run(this.document.DocumentElement, this.namespaceManager, null, null).Wait(),
+                CallShouldThrowSystemAggregateExceptionMessage);
+        }
+
+        [Test]
+        public void ParseTreatmentMetaWithAphiaController_RunWithNullProgramSettingsAndValidOtherParameters_ShouldThrowAggregateExceptionWithInnerArgumentNullException()
+        {
+            var controller = new ParseTreatmentMetaWithAphiaController(this.service);
+
+            try
+            {
+                controller.Run(this.document.DocumentElement, this.namespaceManager, null, this.logger).Wait();
+            }
+            catch (Exception e)
+            {
+                Assert.AreEqual(typeof(AggregateException), e.GetType(), CallShouldThrowSystemAggregateExceptionMessage);
+
+                Exception innerException = e.InnerException;
+                Assert.AreEqual(typeof(ArgumentNullException), innerException.GetType(), InnerExceptionShouldBeArgumentNullExceptionMessage);
+
+                Assert.AreEqual("settings", ((ArgumentNullException)innerException).ParamName, @"ParamName should be ""settings"".");
+            }
+        }
+
+        [Test]
+        public void ParseTreatmentMetaWithAphiaController_RunWithNullLoggerAndValidOtherParameters_ShouldWork()
+        {
+            var controller = new ParseTreatmentMetaWithAphiaController(this.service);
+
+            string initialContent = this.document.OuterXml;
+
+            controller.Run(this.document.DocumentElement, this.namespaceManager, this.settings, null).Wait();
+
+            string finalContent = this.document.OuterXml;
+
+            Assert.AreEqual(initialContent, finalContent, ContentShouldBeUnchangedMessage);
+        }
+    }
+}
