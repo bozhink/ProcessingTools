bdcec2e6e281e38f567645f23182cec9f000e9e9
Bozhin Karaivanov <bozhink@gmail.com>
2016-01-30 15:50:21 +0200

    Geo.Data now is seeded by DbContextSeeder.

--- Data/Geo.Data/Migrations/Configuration.cs
+++ Data/Geo.Data/Migrations/Configuration.cs
@@ -3,16 +3,12 @@ namespace ProcessingTools.Geo.Data.Migrations
     using System;
     using System.Configuration;
     using System.Data.Entity.Migrations;
-    using System.IO;
-    using System.Text;
 
     using Models;
+    using ProcessingTools.Data.Common.Seed;
 
     public sealed class Configuration : DbMigrationsConfiguration<GeoDbContext>
     {
-        private const int NumberOfItemsToResetContext = 100;
-        private readonly Encoding encoding = Encoding.UTF8;
-
         private Action<GeoDbContext, string> addOrUpdateGeoEpithet = (context, line) =>
         {
             context.GeoEpithets.AddOrUpdate(new GeoEpithet
@@ -41,33 +37,14 @@ namespace ProcessingTools.Geo.Data.Migrations
             var appSettingsReader = new AppSettingsReader();
             var dataFilesDirectoryPath = appSettingsReader.GetValue("DataFilesDirectoryPath", typeof(string)).ToString();
 
-            this.ImportSimpleObjects(dataFilesDirectoryPath + "/geo-epithets.txt", this.addOrUpdateGeoEpithet);
-            this.ImportSimpleObjects(dataFilesDirectoryPath + "/geo-names.txt", this.addOrUpdateGeoName);
-        }
+            var seeder = new DbContextSeeder<GeoDbContext>();
 
-        private void ImportSimpleObjects(string fileName, Action<GeoDbContext, string> createObject)
-        {
-            using (var stream = new StreamReader(fileName, this.encoding))
-            {
-                var context = new GeoDbContext();
-
-                string line = stream.ReadLine();
-                for (int i = 0; line != null; ++i, line = stream.ReadLine())
-                {
-                    createObject(context, line.Trim(' ', ',', ';', '/', '\\'));
-
-                    if (i % NumberOfItemsToResetContext == 0)
-                    {
-                        context.SaveChanges();
-                        context.Dispose();
-
-                        context = new GeoDbContext();
-                    }
-                }
-
-                context.SaveChanges();
-                context.Dispose();
-            }
+            seeder.ImportSingleLineTextObjectsFromFile(
+                $"{dataFilesDirectoryPath}/geo-epithets.txt",
+                this.addOrUpdateGeoEpithet);
+            seeder.ImportSingleLineTextObjectsFromFile(
+                $"{dataFilesDirectoryPath}/geo-names.txt",
+                this.addOrUpdateGeoName);
         }
     }
 }
