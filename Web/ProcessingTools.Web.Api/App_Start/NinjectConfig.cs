cfb83e59b2cb96d62ae86b6e06cfa6bf4a4ede55
Bozhin Karaivanov <bozhink@gmail.com>
2016-01-30 21:29:35 +0200

    NinjectConfig in ProcessingTools.Web.Api now uses Ninject.Extensions.Conventions.

--- Web/ProcessingTools.Web.Api/App_Start/NinjectConfig.cs
+++ Web/ProcessingTools.Web.Api/App_Start/NinjectConfig.cs
@@ -2,49 +2,15 @@
 {
     using System;
     using System.Web;
+
     using Ninject;
+    using Ninject.Extensions.Conventions;
     using Ninject.Web.Common;
 
     public static class NinjectConfig
     {
         public static Action<IKernel> DependenciesRegistration => kernel =>
         {
-            kernel
-                .Bind<Data.Contracts.IDataDbContext>()
-                .To<Data.DataDbContext>();
-            kernel
-                .Bind(typeof(Data.Repositories.Contracts.IDataRepository<>))
-                .To(typeof(Data.Repositories.DataRepository<>));
-            kernel
-                .Bind<Services.Data.Contracts.IInstitutionsDataService>()
-                .To<Services.Data.Services.InstitutionsDataService>();
-            kernel
-                .Bind<Services.Data.Contracts.IProductsDataService>()
-                .To<Services.Data.Services.ProductsDataService>();
-
-            kernel
-                .Bind<Geo.Data.Contracts.IGeoDbContext>()
-                .To<Geo.Data.GeoDbContext>();
-            kernel
-                .Bind(typeof(Geo.Data.Repositories.Contracts.IGeoDataRepository<>))
-                .To(typeof(Geo.Data.Repositories.GeoDataRepository<>));
-            kernel
-                .Bind<Geo.Services.Data.Contracts.IGeoEpithetsDataService>()
-                .To<Geo.Services.Data.Services.GeoEpithetsDataService>();
-            kernel
-                .Bind<Geo.Services.Data.Contracts.IGeoNamesDataService>()
-                .To<Geo.Services.Data.Services.GeoNamesDataService>();
-
-            kernel
-                .Bind<Bio.Data.Contracts.IBioDbContext>()
-                .To<Bio.Data.BioDbContext>();
-            kernel
-                .Bind(typeof(Bio.Data.Repositories.Contracts.IBioDataRepository<>))
-                .To(typeof(Bio.Data.Repositories.BioDataRepository<>));
-            kernel
-                .Bind<Bio.Services.Data.Contracts.IMorphologicalEpithetsDataService>()
-                .To<Bio.Services.Data.Services.MorphologicalEpithetsDataService>();
-
             kernel
                 .Bind<MediaType.Data.Contracts.IMediaTypesDbContext>()
                 .To<MediaType.Data.MediaTypesDbContext>();
@@ -57,45 +23,104 @@
             ////kernel
             ////    .Bind<MediaType.Services.Data.Contracts.IMediaTypeDataService>()
             ////    .To<MediaType.Services.Data.Services.MediaTypeDataServiceWindowsRegistry>();
-            kernel
-                .Bind<MediaType.Services.Data.Contracts.IMediaTypeDataService>()
-                .To<MediaType.Services.Data.Services.MediaTypeDataService>();
 
-            kernel
-                .Bind<Bio.Environments.Data.Contracts.IBioEnvironmentsDbContext>()
-                .To<Bio.Environments.Data.BioEnvironmentsDbContext>();
-            kernel
-                .Bind(typeof(Bio.Environments.Data.Repositories.Contracts.IBioEnvironmentsRepository<>))
-                .To(typeof(Bio.Environments.Data.Repositories.BioEnvironmentsRepository<>));
-            kernel
-                .Bind<Bio.Environments.Services.Data.Contracts.IEnvoTermsDataService>()
-                .To<Bio.Environments.Services.Data.Services.EnvoTermsDataService>();
+            kernel.Bind(b =>
+            {
+                b.From(Data.Assembly.Assembly.GetType().Assembly)
+                    .SelectAllClasses()
+                    .BindDefaultInterface();
+            });
 
-            kernel
-                .Bind<Common.Providers.Contracts.IRandomProvider>()
-                .To<Common.Providers.RandomProvider>();
+            kernel.Bind(b =>
+            {
+                b.From(Bio.Data.Assembly.Assembly.GetType().Assembly)
+                    .SelectAllClasses()
+                    .BindDefaultInterface();
+            });
 
-            kernel
-                .Bind<Bio.ServiceClient.ExtractHcmr.Contracts.IExtractHcmrDataRequester>()
-                .To<Bio.ServiceClient.ExtractHcmr.ExtractHcmrDataRequester>();
+            kernel.Bind(b =>
+            {
+                b.From(Bio.Environments.Data.Assembly.Assembly.GetType().Assembly)
+                    .SelectAllClasses()
+                    .BindDefaultInterface();
+            });
 
-            kernel
-                .Bind<Bio.Taxonomy.ServiceClient.CatalogueOfLife.Contracts.ICatalogueOfLifeDataRequester>()
-                .To<Bio.Taxonomy.ServiceClient.CatalogueOfLife.CatalogueOfLifeDataRequester>();
+            kernel.Bind(b =>
+            {
+                b.From(Geo.Data.Assembly.Assembly.GetType().Assembly)
+                    .SelectAllClasses()
+                    .BindDefaultInterface();
+            });
 
-            kernel
-                .Bind<Bio.Taxonomy.ServiceClient.Gbif.Contracts.IGbifDataRequester>()
-                .To<Bio.Taxonomy.ServiceClient.Gbif.GbifDataRequester>();
+            kernel.Bind(b =>
+            {
+                b.From(Services.Data.Assembly.Assembly.GetType().Assembly)
+                    .SelectAllClasses()
+                    .BindDefaultInterface();
+            });
 
-            kernel
-                .Bind<Bio.Taxonomy.Services.Data.Contracts.IAphiaTaxaClassificationDataService>()
-                .To<Bio.Taxonomy.Services.Data.Services.AphiaTaxaClassificationDataService>();
-            kernel
-                .Bind<Bio.Taxonomy.Services.Data.Contracts.ICatalogueOfLifeTaxaClassificationDataService>()
-                .To<Bio.Taxonomy.Services.Data.Services.CatalogueOfLifeTaxaClassificationDataService>();
-            kernel
-                .Bind<Bio.Taxonomy.Services.Data.Contracts.IGbifTaxaClassificationDataService>()
-                .To<Bio.Taxonomy.Services.Data.Services.GbifTaxaClassificationDataService>();
+            kernel.Bind(b =>
+            {
+                b.From(Geo.Services.Data.Assembly.Assembly.GetType().Assembly)
+                    .SelectAllClasses()
+                    .BindDefaultInterface();
+            });
+
+            kernel.Bind(b =>
+            {
+                b.From(Bio.Services.Data.Assembly.Assembly.GetType().Assembly)
+                    .SelectAllClasses()
+                    .BindDefaultInterface();
+            });
+
+            kernel.Bind(b =>
+            {
+                b.From(MediaType.Services.Data.Assembly.Assembly.GetType().Assembly)
+                    .SelectAllClasses()
+                    .BindDefaultInterface();
+            });
+
+            kernel.Bind(b =>
+            {
+                b.From(Bio.Environments.Services.Data.Assembly.Assembly.GetType().Assembly)
+                    .SelectAllClasses()
+                    .BindDefaultInterface();
+            });
+
+            kernel.Bind(b =>
+            {
+                b.From(Common.Assembly.Assembly.GetType().Assembly)
+                    .SelectAllClasses()
+                    .BindDefaultInterface();
+            });
+
+            kernel.Bind(b =>
+            {
+                b.From(Bio.ServiceClient.ExtractHcmr.Assembly.Assembly.GetType().Assembly)
+                    .SelectAllClasses()
+                    .BindDefaultInterface();
+            });
+
+            kernel.Bind(b =>
+            {
+                b.From(Bio.Taxonomy.ServiceClient.CatalogueOfLife.Assembly.Assembly.GetType().Assembly)
+                    .SelectAllClasses()
+                    .BindDefaultInterface();
+            });
+
+            kernel.Bind(b =>
+            {
+                b.From(Bio.Taxonomy.ServiceClient.Gbif.Assembly.Assembly.GetType().Assembly)
+                    .SelectAllClasses()
+                    .BindDefaultInterface();
+            });
+
+            kernel.Bind(b =>
+            {
+                b.From(Bio.Taxonomy.Services.Data.Assembly.Assembly.GetType().Assembly)
+                    .SelectAllClasses()
+                    .BindDefaultInterface();
+            });
         };
 
         public static IKernel CreateKernel()
@@ -119,16 +144,6 @@
         private static void RegisterServices(IKernel kernel)
         {
             DependenciesRegistration(kernel);
-
-            ////kernel.Bind(b => b
-            ////    .From(Assemblies.MediaTypeDataServices)
-            ////    .SelectAllClasses()
-            ////    .BindDefaultInterface());
-
-            ////kernel.Bind(b => b
-            ////    .From(Assemblies.TaxonomicDataServices)
-            ////    .SelectAllClasses()
-            ////    .BindDefaultInterface());
         }
     }
 }
