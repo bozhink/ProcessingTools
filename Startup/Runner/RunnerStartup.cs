e88fbb5b1094a0fb8f7db63ad832e46d235ee2f2
Bozhin Karaivanov <bozhink@gmail.com>
2016-01-30 23:38:15 +0200

    Runner clean-up.

--- Startup/Runner/RunnerStartup.cs
+++ Startup/Runner/RunnerStartup.cs
@@ -1,23 +1,9 @@
 ﻿namespace Runner
 {
-    using System;
-    using System.Diagnostics;
-
-    public partial class RunnerStartup
+    public static class RunnerStartup
     {
         public static void Main(string[] args)
         {
-            Stopwatch timer = new Stopwatch();
-            timer.Start();
-
-            InitializeMediaTypeDatabase();
-            InitializeTaxonomyDatabase();
-            InitializeBioEnvironmentsDatabase();
-            InitializeGeoDatabase();
-            InitializeBioDatabase();
-            InitializeDataDatabase();
-
-            Console.WriteLine(timer.Elapsed);
         }
     }
 }
