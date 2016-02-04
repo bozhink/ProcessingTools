50845b5b188b3b7f7b9310ed64e97ad3ecef2f8d
bozhink <bozhink@gmail.com>
2016-02-04 15:22:40 +0200

    GNR xml models parts added.

---
+++ ServiceClients/Bio.Taxonomy.ServiceClient.GlobalNamesResolver/Models/Xml/Hash.cs
@@ -0,0 +1,32 @@
+﻿namespace ProcessingTools.Bio.Taxonomy.ServiceClient.GlobalNamesResolver.Models.Xml
+{
+    using System;
+    using System.Xml.Serialization;
+
+    [Serializable]
+    [XmlType(AnonymousType = true)]
+    [XmlRoot(ElementName = "hash", Namespace = "", IsNullable = false)]
+    public class Hash
+    {
+        [XmlElement("data")]
+        public HashData Data { get; set; }
+
+        [XmlElement("data-sources")]
+        public HashDataSources DataSources { get; set; }
+
+        [XmlElement("id")]
+        public string Id { get; set; }
+
+        [XmlElement("message")]
+        public string Message { get; set; }
+
+        [XmlElement("parameters")]
+        public HashParameters Parameters { get; set; }
+
+        [XmlElement("status")]
+        public string Status { get; set; }
+
+        [XmlElement("url")]
+        public string Url { get; set; }
+    }
+}
---
+++ ServiceClients/Bio.Taxonomy.ServiceClient.GlobalNamesResolver/Models/Xml/HashData.cs
@@ -0,0 +1,17 @@
+﻿namespace ProcessingTools.Bio.Taxonomy.ServiceClient.GlobalNamesResolver.Models.Xml
+{
+    using System;
+    using System.Xml.Serialization;
+
+    [Serializable]
+    [XmlType(AnonymousType = true)]
+    [XmlRoot(ElementName = "data", Namespace = "", IsNullable = false)]
+    public class HashData
+    {
+        [XmlElement("datum")]
+        public HashDataDatum[] Datum { get; set; }
+
+        [XmlAttribute("type")]
+        public string Type { get; set; }
+    }
+}
