f8f2f86dd8d7871c885f2dd2e9b69daf35376c66
Bozhin Karaivanov <bozhink@gmail.com>
2016-02-07 16:44:37 +0200

    Added biorepository-related serializable models in Tagger.

---
+++ Startup/Tagger/Models/BiorepositoryInstitutionSerializableModel.cs
@@ -0,0 +1,15 @@
+﻿namespace ProcessingTools.MainProgram.Models
+{
+    using System.Xml.Serialization;
+
+    [XmlType(AnonymousType = true)]
+    [XmlRoot("institution", Namespace = "", IsNullable = false)]
+    public class BiorepositoryInstitutionSerializableModel
+    {
+        [XmlText]
+        public string Value { get; set; }
+
+        [XmlAttribute("url")]
+        public string Url { get; set; }
+    }
+}
---
+++ Startup/Tagger/Models/BiorepositoryInstitutionalCodeSerializableModel.cs
@@ -0,0 +1,18 @@
+﻿namespace ProcessingTools.MainProgram.Models
+{
+    using System.Xml.Serialization;
+
+    [XmlType(AnonymousType = true)]
+    [XmlRoot("institutional_code", Namespace = "", IsNullable = false)]
+    public class BiorepositoryInstitutionalCodeSerializableModel
+    {
+        [XmlText]
+        public string Value { get; set; }
+
+        [XmlAttribute("description")]
+        public string Description { get; set; }
+
+        [XmlAttribute("url")]
+        public string Url { get; set; }
+    }
+}
