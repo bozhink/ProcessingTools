8338ce019c85637b32f844963d63738a96a34e4e
bozhink <bozhink@gmail.com>
2016-02-11 14:35:34 +0200

    AbbreviationsHarvester updates.

--- Harvesters/Harvesters/Harvesters/AbbreviationsHarvester.cs
+++ Harvesters/Harvesters/Harvesters/AbbreviationsHarvester.cs
@@ -1,6 +1,7 @@
 ﻿namespace ProcessingTools.Harvesters
 {
     using System;
+    using System.Collections.Generic;
     using System.Configuration;
     using System.IO;
     using System.Linq;
@@ -13,7 +14,7 @@
 
     using ProcessingTools.Harvesters.Common.Factories;
     using ProcessingTools.Infrastructure.Transform;
-
+    
     // TODO
     public class AbbreviationsHarvester : GenericHarvesterFactory<IAbbreviationModel>, IAbbreviationsHarvester
     {
@@ -26,13 +27,21 @@
 
             var result = transformer.Evaluate(document.DocumentElement);
 
+            if (result.CanSeek)
+            {
+                result.Seek(0, SeekOrigin.Begin);
+            }
+
             using (var reader = new StreamReader(result))
             {
                 Console.WriteLine(reader.ReadToEnd());
             }
 
 
-            return null;
+            return Task.Run(() =>
+            {
+                return new HashSet<IAbbreviationModel>().AsQueryable();
+            });
         }
     }
 }
