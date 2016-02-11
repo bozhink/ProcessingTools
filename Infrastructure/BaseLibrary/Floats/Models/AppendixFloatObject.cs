b58aadfb68a18496823a0bbba4d75c9d22f0d381
bozhink <bozhink@gmail.com>
2016-02-11 13:22:30 +0200

    AppendixFloatsObject added.

---
+++ Infrastructure/BaseLibrary/Floats/Models/AppendixFloatObject.cs
@@ -0,0 +1,24 @@
+﻿namespace ProcessingTools.BaseLibrary.Floats.Models
+{
+    using Contracts;
+
+    /// <summary>
+    /// Appendix.
+    /// </summary>
+    public class AppendixFloatObject : IFloatObject
+    {
+        public string FloatObjectXPath => $"//app[contains(string(title),'{this.FloatTypeNameInLabel}')]";
+
+        public FloatsReferenceType FloatReferenceType => FloatsReferenceType.Appendix;
+
+        public string FloatTypeNameInLabel => "Append";
+
+        public string MatchCitationPattern => @"(?:Append(?:\.|[a-z]+))";
+
+        public string InternalReferenceType => "app";
+
+        public string ResultantReferenceType => "app";
+
+        public string Description => "Appendix";
+    }
+}
