26ddc7c6f8fb7c13d34fdf5e7e42cd3ef385ae8c
bozhink <bozhink@gmail.com>
2016-02-12 15:15:35 +0200

    TaxaValidationServiceTests added.

---
+++ Services.Tests/Services.Validation.Tests/TaxaValidationServiceTests.cs
@@ -0,0 +1,91 @@
+﻿namespace ProcessingTools.Services.Validation.Tests
+{
+    using System.Linq;
+
+    using Common.Types;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Models;
+
+    [TestClass]
+    public class TaxaValidationServiceTests
+    {
+        [TestMethod]
+        public void TaxaValidationServiceTests_WithDefaultConstructor_ShouldBuildValidObject()
+        {
+            var service = new TaxaValidationService();
+            Assert.IsNotNull(service, "Service should not be null.");
+        }
+
+        [TestMethod]
+        [Ignore]
+        public void TaxaValidationServiceTests_ValidateOfThreeItems_SchouldReturnThreeValidatedItems()
+        {
+            string[] taxa = { "Coleoptera", "Zospeum", "Homo sapiens" };
+
+            var items = taxa.Select(t => new TaxonName
+            {
+                Name = t
+            })
+            .ToArray();
+
+            var service = new TaxaValidationService();
+            var result = service.Validate(items).Result.ToList();
+
+            const int ExpectedNumberOfItems = 3;
+
+            Assert.AreEqual(ExpectedNumberOfItems, result.Count, "The number of returned items should be 3.");
+
+            for (int i = 0; i < ExpectedNumberOfItems; ++i)
+            {
+                Assert.AreEqual(
+                    1,
+                    result.Where(r => r.ValidatedObject.Name == items[i].Name).Count(),
+                    $"Result should contain Item #{i} only once.");
+                Assert.IsTrue(result[i].ValidationStatus == ValidationStatus.Valid, $"Item #{i} schould be valid.");
+                Assert.IsNull(result[i].ValidationException, $"Item #{i} should have null exception.");
+            }
+        }
+
+        [TestMethod]
+        [Ignore]
+        public void TaxaValidationServiceTests_ValidateOfThreeItemsWithOneInvalid_SchouldReturnThreeValidatedItems()
+        {
+            string[] taxa = { "Coleoptera", "Zospeum", "John Smith" };
+
+            var items = taxa.Select(t => new TaxonName
+            {
+                Name = t
+            })
+            .ToArray();
+
+            var service = new TaxaValidationService();
+            var result = service.Validate(items).Result.ToList();
+
+            const int ExpectedNumberOfItems = 3;
+
+            Assert.AreEqual(ExpectedNumberOfItems, result.Count, "The number of returned items should be 3.");
+
+            foreach (var taxon in taxa)
+            {
+                var matchingResults = result.Where(r => r.ValidatedObject.Name == taxon);
+                Assert.AreEqual(
+                    1,
+                    matchingResults.Count(),
+                    $"Result should contain ‘{taxon}’ only once.");
+
+                var matchingResult = matchingResults.First();
+
+                if (taxon == taxa[2])
+                {
+                    Assert.IsTrue(matchingResult.ValidationStatus == ValidationStatus.Invalid, $"‘{taxon}’ schould be valid.");
+                }
+                else
+                {
+                    Assert.IsTrue(matchingResult.ValidationStatus == ValidationStatus.Valid, $"‘{taxon}’ schould be valid.");
+                }
+
+                Assert.IsNull(matchingResult.ValidationException, $"‘{taxon}’ should have null exception.");
+            }
+        }
+    }
+}
