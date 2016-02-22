743f38c1616c15273b492a7163ef8dec0e46898e
bozhink <bozhink@gmail.com>
2016-02-22 10:55:47 +0200

    TypeStatusDataMiner added.

---
+++ Data.Miners/Bio.Data.Miners/Miners/TypeStatusDataMiner.cs
@@ -0,0 +1,15 @@
+﻿namespace ProcessingTools.Bio.Data.Miners.Miners
+{
+    using Contracts;
+    using ProcessingTools.Bio.Services.Data.Contracts;
+    using ProcessingTools.Bio.Services.Data.Models.Contracts;
+    using ProcessingTools.Data.Miners.Common.Factories;
+
+    public class TypeStatusDataMiner : SimpleServiceStringDataMinerFactory<ITypeStatusDataService, ITypeStatus>, ITypeStatusDataMiner
+    {
+        public TypeStatusDataMiner(ITypeStatusDataService service)
+            : base(service)
+        {
+        }
+    }
+}
