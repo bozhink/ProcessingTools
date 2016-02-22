2ada51a2ad73a87ecd1c054e985b9dcaaa0681bb
bozhink <bozhink@gmail.com>
2016-02-22 13:28:18 +0200

    IValidationCacheService.cs added to index

---
+++ Services/Services.Cache/Models/Contracts/IValidationCacheServiceModel.cs
@@ -0,0 +1,14 @@
+﻿namespace ProcessingTools.Services.Cache.Models.Contracts
+{
+    using System;
+
+    using ProcessingTools.Services.Common.Models.Contracts;
+    using ProcessingTools.Services.Common.Types;
+
+    public interface IValidationCacheServiceModel : ISimpleServiceModel
+    {
+        DateTime LastUpdate { get; set; }
+
+        ValidationStatus Status { get; set; }
+    }
+}
