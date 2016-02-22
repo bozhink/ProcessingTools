475827d0e70098ec00b2520c69c7437f92edea75
bozhink <bozhink@gmail.com>
2016-02-22 11:16:31 +0200

    TagTypeStatusController added.

---
+++ Startup/Tagger/Controllers/TagTypeStatusController.cs
@@ -0,0 +1,36 @@
+﻿namespace ProcessingTools.MainProgram.Controllers
+{
+    using System;
+    using System.Xml;
+
+    using Contracts;
+    using Factories;
+    using ProcessingTools.Bio.Data.Miners.Contracts;
+    using ProcessingTools.Data.Miners.Common.Contracts;
+    using ProcessingTools.Infrastructure.Attributes;
+
+    [Description("Tag type status.")]
+    public class TagTypeStatusController : StringTaggerControllerFactory, ITagTypeStatusController
+    {
+        private readonly ITypeStatusDataMiner miner;
+        private readonly XmlElement tagModel;
+
+        public TagTypeStatusController(ITypeStatusDataMiner miner)
+        {
+            if (miner == null)
+            {
+                throw new ArgumentNullException("miner");
+            }
+
+            this.miner = miner;
+
+            XmlDocument document = new XmlDocument();
+            this.tagModel = document.CreateElement("named-content");
+            this.tagModel.SetAttribute("content-type", "dwc:typeStatus");
+        }
+
+        protected override IStringDataMiner Miner => this.miner;
+
+        protected override XmlElement TagModel => this.tagModel;
+    }
+}
