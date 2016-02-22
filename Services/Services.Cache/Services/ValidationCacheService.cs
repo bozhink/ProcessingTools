07bb22030953ed796987334e9443fdee504ec5c2
bozhink <bozhink@gmail.com>
2016-02-22 13:40:31 +0200

    ValidationCacheService added.

---
+++ Services/Services.Cache/Services/ValidationCacheService.cs
@@ -0,0 +1,51 @@
+﻿namespace ProcessingTools.Services.Cache
+{
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Contracts;
+    using Models.Contracts;
+
+    public class ValidationCacheService : IValidationCacheService
+    {
+        public Task Add(string key, IValidationCacheServiceModel entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IQueryable<IValidationCacheServiceModel>> All(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(string key, IValidationCacheServiceModel entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(string key, int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IValidationCacheServiceModel> Get(string key, int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IQueryable<IValidationCacheServiceModel>> Get(string key, int skip, int take)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update(string key, IValidationCacheServiceModel entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
