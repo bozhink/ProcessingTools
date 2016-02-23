118f499918b47b811332529f0f58b5ad56193e2a
bozhink <bozhink@gmail.com>
2016-02-23 09:49:27 +0200

    ISimpleGenericRepository added.

---
+++ Data/Data.Common/Repositories/Contracts/ISimpleGenericRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace ProcessingTools.Data.Common.Repositories.Contracts
+{
+    using Models.Contracts;
+
+    public interface ISimpleGenericRepository<TEntity> : IGenericRepository<string, int, TEntity>
+        where TEntity : IEntity
+    {
+    }
+}
--- Services/Services.Common/Factories/SimpleCacheServiceFactory.cs
+++ Services/Services.Common/Factories/SimpleCacheServiceFactory.cs
@@ -6,10 +6,10 @@
     using ProcessingTools.Data.Common.Repositories.Contracts;
 
     public abstract class SimpleCacheServiceFactory<TDbModel, TServiceModel> : GenericCacheServiceFactory<string, int, TDbModel, TServiceModel>, ISimpleCacheService<TServiceModel>
-        where TDbModel : IGenericEntity<int>
+        where TDbModel : IEntity
         where TServiceModel : ISimpleServiceModel
     {
-        public SimpleCacheServiceFactory(IGenericRepository<string, int, TDbModel> repository)
+        public SimpleCacheServiceFactory(ISimpleGenericRepository<TDbModel> repository)
             : base(repository)
         {
         }
