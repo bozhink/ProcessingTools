4ccc81286ed7e91f55d2d0024ac5001d8fa157e5
Bozhin Karaivanov <bozhink@gmail.com>
2016-02-27 12:58:51 +0200

    AbbreviationsHarvesterTests added.

---
+++ Harvesters.Tests/Harvesters.Tests/AbbreviationsHarvesterTests.cs
@@ -0,0 +1,16 @@
+﻿namespace ProcessingTools.Harvesters.Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class AbbreviationsHarvesterTests
+    {
+        [TestMethod]
+        public void AbbreviationsHarvester_WithDefaultConstructor_ShouldReturnValidObject()
+        {
+            var harvester = new AbbreviationsHarvester();
+
+            Assert.IsNotNull(harvester, "Harvester should not be null");
+        }
+    }
+}
