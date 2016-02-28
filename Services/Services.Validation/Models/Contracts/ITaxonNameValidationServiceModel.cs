b2ecdb20e736b7fdc77f64630a4bf5adcbc9981e
Bozhin Karaivanov <bozhink@gmail.com>
2016-02-28 23:37:56 +0200

    UrlValidationServiceModel and TaxonNameValidationServiceModel are removed.

--- Services/Services.Validation/Models/Contracts/ITaxonNameValidationServiceModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ProcessingTools.Services.Validation.Models.Contracts
-{
-    using ProcessingTools.Services.Common.Models.Contracts;
-
-    public interface ITaxonNameValidationServiceModel : IValidationServiceModel<ITaxonName>
-    {
-    }
-}
--- Services/Services.Validation/Models/Contracts/IUrlValidationServiceModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ProcessingTools.Services.Validation.Models.Contracts
-{
-    using ProcessingTools.Services.Common.Models.Contracts;
-
-    public interface IUrlValidationServiceModel : IValidationServiceModel<IUrl>
-    {
-    }
-}
