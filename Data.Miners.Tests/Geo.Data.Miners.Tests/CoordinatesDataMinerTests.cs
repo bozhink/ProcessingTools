08ad4edac8cc0a1bcc6a99346fb1fbaf9b2fef69
Bozhin Karaivanov <bozhink@gmail.com>
2016-03-08 22:14:20 +0200

    Added CoordinatesDataMiner.

---
+++ Data.Miners.Tests/Geo.Data.Miners.Tests/CoordinatesDataMinerTests.cs
@@ -0,0 +1,30 @@
+﻿namespace ProcessingTools.Geo.Data.Miners.Tests
+{
+    using System.Linq;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class CoordinatesDataMinerTests
+    {
+        [TestMethod]
+        public void CoordinatesDataMiner_WithDefaultConstructor_ShouldReturnValidObject()
+        {
+            var miner = new CoordinatesDataMiner();
+            Assert.IsNotNull(miner, "Miner should not be null.");
+        }
+
+        [TestMethod]
+        public void CoordinatesDataMiner_WithDecimalDegDirectionCoordiantes_ShouldWork()
+        {
+            string content = "24°03.008N, 105°43.147E";
+            var miner = new CoordinatesDataMiner();
+            Assert.IsNotNull(miner, "Miner should not be null.");
+
+            var result = miner.Mine(content).Result.ToList();
+            Assert.IsNotNull(result, "Result should not be null.");
+            Assert.AreEqual(1, result.Count, "Number of items in result should be 1.");
+            Assert.AreEqual(content, result[0], "Content should match.");
+        }
+    }
+}
