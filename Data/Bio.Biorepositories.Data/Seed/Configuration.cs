172bd8ec51037a18dd319f1d0eb69d088bc60c54
Bozhin Karaivanov <bozhink@gmail.com>
2016-03-09 20:56:56 +0200

    Added ProcessingTools.Bio.Biorepositories.Data.Seed.Configuration.

---
+++ Data/Bio.Biorepositories.Data/Seed/Configuration.cs
@@ -0,0 +1,17 @@
+﻿namespace ProcessingTools.Bio.Biorepositories.Data.Seed
+{
+    public class Configuration
+    {
+        public Configuration()
+        {
+        }
+
+        /// <summary>
+        /// Seeds databases with data.
+        /// </summary>
+        public void Seed()
+        {
+            ////
+        }
+    }
+}
