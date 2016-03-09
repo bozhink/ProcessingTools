d48de30b76da3ad3de10ad0081f6c113a1d574cc
Bozhin Karaivanov <bozhink@gmail.com>
2016-03-09 21:43:50 +0200

    ProcessingTools.Bio.Biorepositories.Data.Seed.Configuration.Seed - added deserialization.

--- Data/Bio.Biorepositories.Data/Seed/Configuration.cs
+++ Data/Bio.Biorepositories.Data/Seed/Configuration.cs
@@ -1,5 +1,13 @@
 ﻿namespace ProcessingTools.Bio.Biorepositories.Data.Seed
 {
+    using System;
+    using System.Configuration;
+    using System.IO;
+    using System.Linq;
+
+    using ProcessingTools.Infrastructure.Attributes;
+    using ProcessingTools.Infrastructure.Serialization.Csv;
+
     public class Configuration
     {
         public Configuration()
@@ -11,7 +19,36 @@
         /// </summary>
         public void Seed()
         {
-            ////
+            string dataFilesDirectoryPath = ConfigurationManager.AppSettings["BiorepositoriesSeedCsvDataFiles"];
+
+            var seedModelTypes = ProcessingTools.Bio.Biorepositories.Data.Models.Assembly.Assembly
+                .GetType()
+                .Assembly
+                .GetTypes()
+                .Where(t => !t.IsAbstract && t.IsClass && Attribute.IsDefined(t, typeof(FileNameAttribute)) && Attribute.IsDefined(t, typeof(CsvObjectAttribute)));
+
+            foreach (var seedModelType in seedModelTypes)
+            {
+                Console.WriteLine(seedModelType.FullName);
+
+                var fileNameAttribute = seedModelType.GetCustomAttributes(typeof(FileNameAttribute), false).FirstOrDefault() as FileNameAttribute;
+
+                string fileName = string.Format("{0}/{1}", dataFilesDirectoryPath, fileNameAttribute.Name);
+                if (!File.Exists(fileName))
+                {
+                    throw new ApplicationException($"File {fileName} does not exist.");
+                }
+
+                string csvText = File.ReadAllText(fileName);
+                var serializer = new CsvSerializer();
+                var items = serializer.Deserialize(seedModelType, csvText)?.ToList();
+                if (items == null || items.Count < 1)
+                {
+                    throw new ApplicationException("Deserialized items are not valid.");
+                }
+
+                ////
+            }
         }
     }
 }
