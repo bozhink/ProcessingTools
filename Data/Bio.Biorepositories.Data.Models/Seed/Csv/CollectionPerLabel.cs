d0bfbc33344dbc28d66c2ca4f68250fe1a8f0010
Bozhin Karaivanov <bozhink@gmail.com>
2016-03-09 17:12:02 +0200

    Added ProcessingTools.Bio.Biorepositories.Data.Models.Seed.Csv.CollectionPerLabel.

--- Data/Bio.Biorepositories.Data.Models/Seed/Csv/CollectionPerLabel.cs
+++ Data/Bio.Biorepositories.Data.Models/Seed/Csv/CollectionPerLabel.cs
@@ -5,5 +5,22 @@
     [CsvObject]
     public class CollectionPerLabel
     {
+        [CsvColumn("City/Town")]
+        public string CityTown { get; set; }
+
+        [CsvColumn("Collection Name")]
+        public string CollectionName { get; set; }
+
+        [CsvColumn("Country")]
+        public string Country { get; set; }
+
+        [CsvColumn("Postal/Zip Code")]
+        public string PostalZipCode { get; set; }
+
+        [CsvColumn("Primary Contact")]
+        public string PrimaryContact { get; set; }
+
+        [CsvColumn("State/Province")]
+        public string StateProvince { get; set; }
     }
 }
