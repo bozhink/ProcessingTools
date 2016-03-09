29a94517b65af55c81f40f3ffa70246755c1b10b
Bozhin Karaivanov <bozhink@gmail.com>
2016-03-09 17:12:45 +0200

    Added ProcessingTools.Bio.Biorepositories.Data.Models.Seed.Csv.Collection.

---
+++ Data/Bio.Biorepositories.Data.Models/Seed/Csv/Collection.cs
@@ -0,0 +1,60 @@
+﻿namespace ProcessingTools.Bio.Biorepositories.Data.Models.Seed.Csv
+{
+    using ProcessingTools.Infrastructure.Serialization.Csv;
+
+    [CsvObject]
+    public class Collection
+    {
+        [CsvColumn("Access Eligibility and Rules")]
+        public string AccessEligibilityAndRules { get; set; }
+
+        [CsvColumn("Accession Status")]
+        public string AccessionStatus { get; set; }
+
+        [CsvColumn("Collection Code")]
+        public string CollectionCode { get; set; }
+
+        [CsvColumn("Collection Content Type")]
+        public string CollectionContentType { get; set; }
+
+        [CsvColumn("Collection Description")]
+        public string CollectionDescription { get; set; }
+
+        [CsvColumn("Collection Name")]
+        public string CollectionName { get; set; }
+
+        [CsvColumn("Cool URI")]
+        public string CoolUri { get; set; }
+
+        [CsvColumn("Institution Code")]
+        public string InstitutionCode { get; set; }
+
+        [CsvColumn("Institution Name")]
+        public string InstitutionName { get; set; }
+
+        [CsvColumn("LSID")]
+        public string Lsid { get; set; }
+
+        [CsvColumn("Preservation Type")]
+        public string PreservationType { get; set; }
+
+        [CsvColumn("Primary Contact")]
+        public string PrimaryContact { get; set; }
+
+        [CsvColumn("Status of Collection")]
+        public string StatusOfCollection { get; set; }
+
+        [CsvColumn("URL")]
+        public string Url { get; set; }
+
+        [CsvColumn("URL for collection")]
+        public string UrlForCollection { get; set; }
+
+        [CsvColumn("URL for collection's specimen catalog/database")]
+        public string UrlForCollectionSpecimenCatalog { get; set; }
+
+        [CsvColumn("URL for collection's webservices")]
+        public string UrlForCollectionWebservices { get; set; }
+
+    }
+}
