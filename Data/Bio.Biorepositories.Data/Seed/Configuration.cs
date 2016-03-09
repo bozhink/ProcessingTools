9b97b73194a611df59e9a76672b3ac32b2d81760
Bozhin Karaivanov <bozhink@gmail.com>
2016-03-09 22:33:09 +0200

    ProcessingTools.Bio.Biorepositories.Data.Seed.Configuration is implemented.

--- Data/Bio.Biorepositories.Data/Seed/Configuration.cs
+++ Data/Bio.Biorepositories.Data/Seed/Configuration.cs
@@ -4,51 +4,76 @@
     using System.Configuration;
     using System.IO;
     using System.Linq;
+    using System.Threading.Tasks;
 
+    using Contracts;
+
+    using ProcessingTools.Data.Common.Seed;
     using ProcessingTools.Infrastructure.Attributes;
     using ProcessingTools.Infrastructure.Serialization.Csv;
 
+    using Repositories;
+
     public class Configuration
     {
-        public Configuration()
+        private readonly IBiorepositoriesMongoDatabaseProvider provider;
+        private readonly string dataFilesDirectoryPath;
+
+        public Configuration(IBiorepositoriesMongoDatabaseProvider provider)
         {
+            if (provider == null)
+            {
+                throw new ArgumentNullException("provider");
+            }
+
+            this.provider = provider;
+
+            this.dataFilesDirectoryPath = ConfigurationManager.AppSettings["BiorepositoriesSeedCsvDataFiles"];
         }
 
         /// <summary>
         /// Seeds databases with data.
         /// </summary>
-        public void Seed()
+        public async Task Seed()
         {
-            string dataFilesDirectoryPath = ConfigurationManager.AppSettings["BiorepositoriesSeedCsvDataFiles"];
+            await this.ImportCsvFileToMongo<Models.Seed.Csv.Collection, Models.Bson.Collection>();
+            await this.ImportCsvFileToMongo<Models.Seed.Csv.CollectionLabel, Models.Bson.CollectionLabel>();
+            await this.ImportCsvFileToMongo<Models.Seed.Csv.CollectionPer, Models.Bson.CollectionPer>();
+            await this.ImportCsvFileToMongo<Models.Seed.Csv.CollectionPerLabel, Models.Bson.CollectionPerLabel>();
+            await this.ImportCsvFileToMongo<Models.Seed.Csv.Institution, Models.Bson.Institution>();
+            await this.ImportCsvFileToMongo<Models.Seed.Csv.InstitutionLabel, Models.Bson.InstitutionLabel>();
+            await this.ImportCsvFileToMongo<Models.Seed.Csv.Staff, Models.Bson.Staff>();
+            await this.ImportCsvFileToMongo<Models.Seed.Csv.StaffLabel, Models.Bson.StaffLabel>();
+        }
 
-            var seedModelTypes = ProcessingTools.Bio.Biorepositories.Data.Models.Assembly.Assembly
-                .GetType()
-                .Assembly
-                .GetTypes()
-                .Where(t => !t.IsAbstract && t.IsClass && Attribute.IsDefined(t, typeof(FileNameAttribute)) && Attribute.IsDefined(t, typeof(CsvObjectAttribute)));
-
-            foreach (var seedModelType in seedModelTypes)
+        private async Task ImportCsvFileToMongo<TSeedModel, TEntityModel>()
+            where TSeedModel : class
+            where TEntityModel : class
+        {
+            Type seedModelType = typeof(TSeedModel);
+            var fileNameAttribute = seedModelType.GetCustomAttributes(typeof(FileNameAttribute), false)?.FirstOrDefault() as FileNameAttribute;
+            if (fileNameAttribute == null)
             {
-                Console.WriteLine(seedModelType.FullName);
-
-                var fileNameAttribute = seedModelType.GetCustomAttributes(typeof(FileNameAttribute), false).FirstOrDefault() as FileNameAttribute;
-
-                string fileName = string.Format("{0}/{1}", dataFilesDirectoryPath, fileNameAttribute.Name);
-                if (!File.Exists(fileName))
-                {
-                    throw new ApplicationException($"File {fileName} does not exist.");
-                }
-
-                string csvText = File.ReadAllText(fileName);
-                var serializer = new CsvSerializer();
-                var items = serializer.Deserialize(seedModelType, csvText)?.ToList();
-                if (items == null || items.Count < 1)
-                {
-                    throw new ApplicationException("Deserialized items are not valid.");
-                }
-
-                ////
+                throw new ApplicationException($"Invalid seed model {seedModelType.Name}: There is no FileNameAttribute.");
             }
+
+            string fileName = string.Format("{0}/{1}", this.dataFilesDirectoryPath, fileNameAttribute.Name);
+            if (!File.Exists(fileName))
+            {
+                throw new ApplicationException($"File {fileName} does not exist.");
+            }
+
+            string csvText = File.ReadAllText(fileName);
+            var serializer = new CsvSerializer();
+            var items = serializer.Deserialize<TSeedModel>(csvText)?.ToArray();
+            if (items == null || items.Length < 1)
+            {
+                throw new ApplicationException("Deserialized items are not valid.");
+            }
+
+            var repository = new BiorepositoriesMongoRepository<TEntityModel>(this.provider);
+            var seeder = new SimpleRepositorySeeder<TEntityModel>(repository);
+            await seeder.Seed(items);
         }
     }
 }
