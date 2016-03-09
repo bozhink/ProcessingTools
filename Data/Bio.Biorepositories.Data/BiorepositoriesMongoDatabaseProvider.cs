05e1f05dbda362459e9782fd5f9eba2ac406bb57
Bozhin Karaivanov <bozhink@gmail.com>
2016-03-09 19:07:45 +0200

    Added BiorepositoriesMongoDatabaseProvider.

---
+++ Data/Bio.Biorepositories.Data/BiorepositoriesMongoDatabaseProvider.cs
@@ -0,0 +1,24 @@
+﻿namespace ProcessingTools.Bio.Biorepositories.Data
+{
+    using System.Configuration;
+    using Contracts;
+    using MongoDB.Driver;
+
+    public class BiorepositoriesMongoDatabaseProvider : IBiorepositoriesMongoDatabaseProvider
+    {
+        private readonly string serverUrl;
+        private readonly string databaseName;
+
+        public BiorepositoriesMongoDatabaseProvider()
+        {
+            this.serverUrl = ConfigurationManager.AppSettings["BiorepositoriesMongoDbServerUrl"];
+            this.databaseName = ConfigurationManager.AppSettings["BiorepositoriesMongoDabaseName"];
+        }
+
+        public IMongoDatabase Create()
+        {
+            var client = new MongoClient(this.serverUrl);
+            return client.GetDatabase(this.databaseName);
+        }
+    }
+}
---
+++ Data/Bio.Biorepositories.Data/Contracts/IBiorepositoriesMongoDatabaseProvider.cs
@@ -0,0 +1,8 @@
+﻿namespace ProcessingTools.Bio.Biorepositories.Data.Contracts
+{
+    using ProcessingTools.Data.Common.Mongo.Contracts;
+
+    public interface IBiorepositoriesMongoDatabaseProvider : IMongoDatabaseProvider
+    {
+    }
+}
