93528c6351cb9b4480dd02fcee1e95ab810b4cca
Bozhin Karaivanov <bozhink@gmail.com>
2016-03-09 22:52:09 +0200

    BiorepositoriesDbSeeder is added to DbSeeder.

---
+++ Startup/DbSeeder/Seeders/BiorepositoriesDbSeeder.cs
@@ -0,0 +1,19 @@
+﻿namespace ProcessingTools.DbSeeder.Seeders
+{
+    using System.Threading.Tasks;
+
+    using Contracts;
+
+    using ProcessingTools.Bio.Biorepositories.Data;
+    using ProcessingTools.Bio.Biorepositories.Data.Seed;
+
+    public class BiorepositoriesDbSeeder : IDbSeeder
+    {
+        public async Task Seed()
+        {
+            var provider = new BiorepositoriesMongoDatabaseProvider();
+            var configuration = new Configuration(provider);
+            await configuration.Seed();
+        }
+    }
+}
