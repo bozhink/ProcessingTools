599c518df64e48395ba54409a7361584a7abb7ce
Bozhin Karaivanov <bozhink@gmail.com>
2016-03-09 17:26:59 +0200

    Added ProcessingTools.Bio.Biorepositories.Data.Models.Seed.Csv.StaffLabel.

--- Data/Bio.Biorepositories.Data.Models/Seed/Csv/StaffLabel.cs
+++ Data/Bio.Biorepositories.Data.Models/Seed/Csv/StaffLabel.cs
@@ -5,5 +5,22 @@
     [CsvObject]
     public class StaffLabel
     {
+        [CsvColumn("City/Town")]
+        public string CityTown { get; set; }
+
+        [CsvColumn("Country")]
+        public string Country { get; set; }
+
+        [CsvColumn("Postal/Zip Code")]
+        public string PostalZipCode { get; set; }
+
+        [CsvColumn("Primary Institution")]
+        public string PrimaryInstitution { get; set; }
+
+        [CsvColumn("Staff Member Full Name")]
+        public string StaffMemberFullName { get; set; }
+
+        [CsvColumn("State/Province")]
+        public string StateProvince { get; set; }
     }
 }
