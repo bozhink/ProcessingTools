5aeb58306beaddf0d1a14b33940c86bd1e51ba5c
Bozhin Karaivanov <bozhink@gmail.com>
2016-03-09 17:10:09 +0200

    Added ProcessingTools.Bio.Biorepositories.Data.Models.Seed.Csv.CollectionPer.

--- Data/Bio.Biorepositories.Data.Models/Seed/Csv/CollectionPer.cs
+++ Data/Bio.Biorepositories.Data.Models/Seed/Csv/CollectionPer.cs
@@ -5,5 +5,49 @@
     [CsvObject]
     public class CollectionPer
     {
+        [CsvColumn("Access Eligibility and Rules")]
+        public string AccessEligibilityAndRules { get; set; }
+
+        [CsvColumn("Collection Code")]
+        public string CollectionCode { get; set; }
+
+        [CsvColumn("Collection Content Type")]
+        public string CollectionContentType { get; set; }
+
+        [CsvColumn("Collection Description")]
+        public string CollectionDescription { get; set; }
+
+        [CsvColumn("Collection Name")]
+        public string CollectionName { get; set; }
+
+        [CsvColumn("Cool URI")]
+        public string CoolUri { get; set; }
+
+        [CsvColumn("Institution Name")]
+        public string InstitutionName { get; set; }
+
+        [CsvColumn("LSID")]
+        public string Lsid { get; set; }
+
+        [CsvColumn("Preservation Type")]
+        public string PreservationType { get; set; }
+
+        [CsvColumn("Primary Contact")]
+        public string PrimaryContact { get; set; }
+
+        [CsvColumn("Status of Collection")]
+        public string StatusOfCollection { get; set; }
+
+        [CsvColumn("URL for collection")]
+        public string UrlForCollection { get; set; }
+
+        [CsvColumn("URL for collection's specimen catalog/database")]
+        public string UrlForCollectionSpecimenCatalog { get; set; }
+
+        [CsvColumn("URL for collection's webservices")]
+        public string UrlForCollectionWebservices { get; set; }
+
+        [CsvColumn("URL")]
+        public string Url { get; set; }
     }
 }
