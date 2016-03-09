72c83bde5be2a4f868e2fe1dfc5da5a8e3e9c346
Bozhin Karaivanov <bozhink@gmail.com>
2016-03-09 22:11:13 +0200

    Added SimpleRepositorySeeder.

---
+++ Data/Data.Common/Seed/SimpleRepositorySeeder.cs
@@ -0,0 +1,60 @@
+﻿namespace ProcessingTools.Data.Common.Seed
+{
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using Repositories.Contracts;
+
+    public class SimpleRepositorySeeder<TEntity>
+        where TEntity : class
+    {
+        private IGenericRepository<TEntity> repository;
+
+        public SimpleRepositorySeeder(IGenericRepository<TEntity> repository)
+        {
+            if (repository == null)
+            {
+                throw new ArgumentNullException("repository");
+            }
+
+            this.repository = repository;
+        }
+
+        public async Task Seed<TSeedModel>(params TSeedModel[] data)
+            where TSeedModel : class
+        {
+            if (data == null || data.Length < 1)
+            {
+                throw new ArgumentNullException("data");
+            }
+
+            foreach (var item in data)
+            {
+                var entity = Map<TSeedModel, TEntity>(item);
+                await this.repository.Add(entity);
+            }
+
+            await this.repository.SaveChanges();
+        }
+
+        private Tout Map<Tin, Tout>(Tin item)
+            where Tin : class
+            where Tout : class
+        {
+            Tout result = Activator.CreateInstance<Tout>();
+
+            var propertiesIn = typeof(Tin).GetProperties();
+            var propertiesOut = typeof(Tout).GetProperties();
+            foreach (var propertyIn in propertiesIn)
+            {
+                var propertyOut = propertiesOut
+                    .FirstOrDefault(p => p.Name == propertyIn.Name && p.PropertyType == propertyIn.PropertyType);
+
+                propertyOut?.SetValue(result, propertyIn.GetValue(item));
+            }
+
+            return result;
+        }
+    }
+}
