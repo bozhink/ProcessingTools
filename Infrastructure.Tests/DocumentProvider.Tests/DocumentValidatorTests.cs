7731551633e98ac92d0050727708c2d62a890eea
Bozhin Karaivanov <bozhink@gmail.com>
2016-03-13 17:29:23 +0200

    Added DocumentValidator.

---
+++ Infrastructure.Tests/DocumentProvider.Tests/DocumentValidatorTests.cs
@@ -0,0 +1,19 @@
+﻿namespace ProcessingTools.DocumentProvider.Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class DocumentValidatorTests
+    {
+        [TestMethod]
+        public void DocumentValidator_ValidateSampleXml_ShouldWork()
+        {
+            var document = new TaxPubDocument();
+            document.Xml = "<article><front></front></article>";
+
+            var validator = new DocumentValidator(document);
+
+            validator.Validate().Wait();
+        }
+    }
+}
---
+++ Infrastructure/DocumentProvider/DocumentValidator.cs
@@ -0,0 +1,126 @@
+﻿namespace ProcessingTools.DocumentProvider
+{
+    using System;
+    using System.Configuration;
+    using System.IO;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Xml;
+    using System.Xml.Schema;
+
+    using ProcessingTools.Contracts;
+
+    public class DocumentValidator : IValidator
+    {
+        private const string TaxPubDtdPathKey = "TaxPubDtdPath";
+        private readonly string taxPubDtdPath;
+
+        private ILogger logger;
+        private IDocument document;
+
+        public DocumentValidator(IDocument document)
+            : this(document, null)
+        {
+        }
+
+        public DocumentValidator(IDocument document, ILogger logger)
+        {
+            if (document == null)
+            {
+                throw new ArgumentNullException("document");
+            }
+
+            this.document = document;
+            this.logger = logger;
+
+            var appSettingsReader = new AppSettingsReader();
+            try
+            {
+                string taxPubDtdPath = appSettingsReader.GetValue(TaxPubDtdPathKey, typeof(string)).ToString();
+                if (string.IsNullOrWhiteSpace(taxPubDtdPath) || !File.Exists(taxPubDtdPath))
+                {
+                    throw new ApplicationException("TaxPub DTD Path is invalid.");
+                }
+
+                this.taxPubDtdPath = taxPubDtdPath;
+            }
+            catch (Exception e)
+            {
+                throw new ApplicationException("Invalid application settings.", e);
+            }
+        }
+
+        public async Task Validate()
+        {
+            string fileName = Path.GetTempFileName() + ".xml";
+            this.logger?.Log(fileName);
+
+            await this.WriteXmlFileWithDoctype(fileName);
+
+            await this.ReadXmlFileWithDtdValidation(fileName);
+        }
+
+        private async Task ReadXmlFileWithDtdValidation(string fileName)
+        {
+            var readerSettings = new XmlReaderSettings
+            {
+                Async = true,
+                CheckCharacters = true,
+                CloseInput = true,
+                ConformanceLevel = ConformanceLevel.Document,
+                DtdProcessing = DtdProcessing.Parse,
+                IgnoreComments = false,
+                IgnoreProcessingInstructions = true,
+                IgnoreWhitespace = false,
+                ValidationType = ValidationType.DTD,
+                ValidationFlags = XmlSchemaValidationFlags.ReportValidationWarnings
+            };
+            readerSettings.ValidationEventHandler += new ValidationEventHandler(this.ValidationCallBack);
+
+            var reader = XmlReader.Create(fileName, readerSettings);
+            while (await reader.ReadAsync())
+            {
+            }
+
+            reader.Close();
+        }
+
+        private async Task WriteXmlFileWithDoctype(string fileName)
+        {
+            var writerSettings = new XmlWriterSettings
+            {
+                Async = true,
+                CheckCharacters = true,
+                ConformanceLevel = ConformanceLevel.Document,
+                CloseOutput = true,
+                DoNotEscapeUriAttributes = true,
+                Encoding = Encoding.UTF8,
+                Indent = true,
+                IndentChars = " ",
+                NewLineChars = "\n",
+                NewLineHandling = NewLineHandling.Replace,
+                NewLineOnAttributes = false,
+                NamespaceHandling = NamespaceHandling.OmitDuplicates,
+                OmitXmlDeclaration = false,
+                WriteEndDocumentOnClose = true,
+            };
+
+            var writer = XmlWriter.Create(fileName, writerSettings);
+            await writer.WriteStartDocumentAsync();
+            await writer.WriteDocTypeAsync(
+                "article",
+                "-//TaxonX//DTD Taxonomic Treatment Publishing DTD v0 20100105//EN",
+                this.taxPubDtdPath,
+                null);
+
+            this.document.XmlDocument.DocumentElement.WriteTo(writer);
+            await writer.FlushAsync();
+            writer.Close();
+        }
+
+        private void ValidationCallBack(object sender, ValidationEventArgs e)
+        {
+            this.logger?.Log("Validation Error: {0}", e.Message);
+        }
+    }
+}
