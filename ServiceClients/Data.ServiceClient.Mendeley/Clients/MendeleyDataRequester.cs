b369e039acc8e6a37185f3f499bd7f63f8fe99e2
bozhink <bozhink@gmail.com>
2016-03-21 11:39:08 +0200

    MendeleyDataRequester is added. Data.ServiceClient.Mendeley.Tests is added.

---
+++ ServiceClients/Data.ServiceClient.Mendeley/Clients/MendeleyDataRequester.cs
@@ -0,0 +1,30 @@
+﻿namespace ProcessingTools.Data.ServiceClient.Mendeley
+{
+    using System;
+    using System.Net.Http;
+    using System.Net.Http.Headers;
+    using System.Threading.Tasks;
+    using Contracts;
+
+    public class MendeleyDataRequester : IMendeleyDataRequester
+    {
+        private const string MendeleyApiBaseAddress = "https://api.mendeley.com";
+
+        public async Task GetDocumentInformationByDoi(string doi)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "Your Oauth token");
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/vnd.mendeley-document.1+json"));
+
+                client.BaseAddress = new Uri(MendeleyApiBaseAddress);
+
+                string url = $"/catalog?doi={doi}";
+                var stream = await client.GetStreamAsync(url);
+
+                // TODO: serialization model
+                //// ...
+            }
+        }
+    }
+}
---
+++ ServiceClients/Data.ServiceClient.Mendeley/Contracts/IMendeleyDataRequester.cs
@@ -0,0 +1,9 @@
+﻿namespace ProcessingTools.Data.ServiceClient.Mendeley.Contracts
+{
+    using System.Threading.Tasks;
+
+    public interface IMendeleyDataRequester
+    {
+        Task GetDocumentInformationByDoi(string doi);
+    }
+}
