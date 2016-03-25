17895e0859bca1e035bd5e49de75accc975a548f
Bozhin Karaivanov <bozhink@gmail.com>
2016-03-25 22:01:10 +0200

    Added IDataService.

---
+++ Services/Services.Common/Contracts/IDataService.cs
@@ -0,0 +1,22 @@
+﻿namespace ProcessingTools.Services.Common.Contracts
+{
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public interface IDataService<T>
+    {
+        Task<IQueryable<T>> All();
+
+        Task<IQueryable<T>> Get(int skip, int take);
+
+        Task<IQueryable<T>> Get(object id);
+
+        Task Add(T entity);
+
+        Task Update(T entity);
+
+        Task Delete(T entity);
+
+        Task Delete(object id);
+    }
+}
