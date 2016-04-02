0b9de31e5f0e90bd9642c6f664da525eea3d7563
Bozhin Karaivanov <bozhink@gmail.com>
2016-04-02 10:58:51 +0300

    ITaxaRepository is added.

---
+++ Data/Bio.Taxonomy.Data.Models/Taxon.cs
@@ -0,0 +1,32 @@
+﻿namespace ProcessingTools.Bio.Taxonomy.Data.Models
+{
+    using System.Collections.Generic;
+
+    public class Taxon
+    {
+        private ICollection<string> ranks;
+
+        public Taxon()
+        {
+            this.ranks = new HashSet<string>();
+            this.IsWhiteListed = false;
+        }
+
+        public string Name { get; set; }
+
+        public bool IsWhiteListed { get; set; }
+
+        public virtual ICollection<string> Ranks
+        {
+            get
+            {
+                return this.ranks;
+            }
+
+            set
+            {
+                this.ranks = value;
+            }
+        }
+    }
+}
