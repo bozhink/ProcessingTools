66cb7ca3578accf32d678a5b5f0f55adbc243b9c
Bozhin Karaivanov <bozhink@gmail.com>
2016-04-03 15:54:49 +0300

    Mappings: AutoMapperConfig is renamed to MappingsRegistration.

---
+++ Infrastructure/Extensions/AssemblyExtensions.cs
@@ -0,0 +1,36 @@
+﻿namespace ProcessingTools.Extensions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+
+    public static class AssemblyExtensions
+    {
+        public static IEnumerable<Assembly> GetAssemblies(this IEnumerable<string> assemblies)
+        {
+            if (assemblies == null)
+            {
+                throw new ArgumentNullException(nameof(assemblies));
+            }
+
+            return assemblies.Select(a => Assembly.Load(a));
+        }
+
+        public static IEnumerable<Type> GetExportedTypes(this IEnumerable<Assembly> assemblies)
+        {
+            if (assemblies == null)
+            {
+                throw new ArgumentNullException(nameof(assemblies));
+            }
+
+            var types = new List<Type>();
+            foreach (var assembly in assemblies)
+            {
+                types.AddRange(assembly.GetExportedTypes());
+            }
+
+            return types;
+        }
+    }
+}
