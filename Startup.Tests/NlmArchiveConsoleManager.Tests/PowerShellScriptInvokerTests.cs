635361a8de3621a6f8632de4c31e33c0533529b1
bozhink <bozhink@gmail.com>
2016-04-13 12:58:21 +0300

    PowerShellScriptInvoker is added.

---
+++ Startup.Tests/NlmArchiveConsoleManager.Tests/PowerShellScriptInvokerTests.cs
@@ -0,0 +1,16 @@
+﻿namespace ProcessingTools.NlmArchiveConsoleManager.Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class PowerShellScriptInvokerTests
+    {
+        [TestMethod]
+        public void PowerShellScriptInvoker_Invoke_ShouldGenerateCorrectOutput()
+        {
+            var invoker = new PowerShellScriptInvoker();
+            invoker.Invoke();
+
+        }
+    }
+}
