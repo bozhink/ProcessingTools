93b1937c2ae4f3f01a1a7dc088fadadadd8fbdb9
bozhink <bozhink@gmail.com>
2016-04-19 14:06:15 +0300

    BioDataRepositoryTests are added.

---
+++ Data.Tests/Bio.Data.Repositories.Tests/BioDataRepositoryTests.cs
@@ -0,0 +1,120 @@
+﻿namespace ProcessingTools.Bio.Data.Repositories.Tests
+{
+    using System;
+    using System.Data.Entity;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Moq;
+
+    using ProcessingTools.Bio.Data.Repositories.Contracts;
+    using ProcessingTools.Bio.Data.Repositories.Tests.Models;
+    using ProcessingTools.Bio.Data.Repositories.Tests.Mocks;
+    using ProcessingTools.Data.Common.Entity.Repositories;
+    using ProcessingTools.Data.Common.Repositories.Contracts;
+
+    [TestClass]
+    public class BioDataRepositoryTests
+    {
+        private IBioDbContextProvider contextProvider;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            var contextMock = new ContextMock();
+
+            var contextProviderMock = new Mock<IBioDbContextProvider>();
+            contextProviderMock
+                .Setup(contextProvider => contextProvider.Create())
+                .Returns(contextMock.MockObject.Object);
+
+            this.contextProvider = contextProviderMock.Object;
+        }
+
+        [TestMethod]
+        public void BioDataRepository_WithValidParametersInConstructor_ShouldReturnValidObject()
+        {
+            var repository = new BioDataRepository<Tweet>(this.contextProvider);
+
+            Assert.IsNotNull(repository, "Repository should not be null.");
+
+            Assert.IsInstanceOfType(
+                repository,
+                typeof(IBioDataRepository<Tweet>),
+                $"Repository should be a valid {nameof(IBioDataRepository<Tweet>)} object.");
+
+            Assert.IsInstanceOfType(
+                repository,
+                typeof(IGenericRepository<Tweet>),
+                $"Repository should be a valid {nameof(IGenericRepository<Tweet>)} object.");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void BioDataRepository_WithNullContextFactoryInConstructor_ShouldThrowArgumentNullException()
+        {
+            var repository = new BioDataRepository<Tweet>(null);
+        }
+
+        [TestMethod]
+        public void BioDataRepository_WithNullContextFactoryInConstructor_ShouldThrowArgumentNullExceptionWithCorrectParamName()
+        {
+            try
+            {
+                var repository = new BioDataRepository<Tweet>(null);
+            }
+            catch (ArgumentNullException e)
+            {
+                Assert.AreEqual("contextProvider", e.ParamName, "ParamName should be contextProvider.");
+            }
+        }
+
+        [TestMethod]
+        [Timeout(2000)]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void BioDataRepository_AddNullEntity_ShouldThrowArgumentNullException()
+        {
+            var repository = new BioDataRepository<Tweet>(this.contextProvider);
+            repository.Add(null).Wait();
+        }
+
+        [TestMethod]
+        [Timeout(2000)]
+        public void BioDataRepository_AddNullEntity_ShouldThrowArgumentNullExceptionWithCorrectParamName()
+        {
+            var repository = new BioDataRepository<Tweet>(this.contextProvider);
+
+            try
+            {
+                repository.Add(null).Wait();
+            }
+            catch (ArgumentNullException e)
+            {
+                Assert.AreEqual("entity", e.ParamName, "ParamName should be entity.");
+            }
+        }
+
+        //////[TestMethod]
+        //////[Timeout(2000)]
+        //////public void BioDataRepository_AddValidEntity_ShouldWork()
+        //////{
+        //////    var repository = new BioDataRepository<Tweet>(this.contextProvider);
+        //////    repository.Add(new Tweet
+        //////    {
+        //////        Id = 1,
+        //////        Content = "Content"
+        //////    });
+        //////}
+
+        //////[TestMethod]
+        //////[Timeout(2000)]
+        //////public void BioDataRepository_All_ShouldReturnAllItemsAsQueryable()
+        //////{
+        //////    var repository = new BioDataRepository<Tweet>(this.contextProvider);
+
+        //////    repository.All().Wait();
+        //////    var result = repository.All().Result;
+
+        //////    Assert.IsNotNull(result, "All() should not return Task of null.");
+        //////}
+    }
+}
---
+++ Data.Tests/Bio.Data.Repositories.Tests/Mocks/ContextMock.cs
@@ -0,0 +1,75 @@
+﻿namespace ProcessingTools.Bio.Data.Repositories.Tests.Mocks
+{
+    using Models;
+    using Moq;
+    using ProcessingTools.Bio.Data.Repositories.Contracts;
+    using System.Collections.Generic;
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class ContextMock
+    {
+        private HashSet<Tweet> items;
+
+        public ContextMock()
+        {
+            this.items = new HashSet<Tweet>();
+        }
+
+        public Mock<BioDbContext> MockObject
+        {
+            get
+            {
+                var dbsetMock = new Mock<DbSet<Tweet>>();
+                dbsetMock
+                    .Setup(s => s.Add(It.IsAny<Tweet>()))
+                    .Returns((Tweet t) =>
+                    {
+                        this.items.Add(t);
+                        System.Console.WriteLine(t);
+                        return t;
+                    });
+
+                dbsetMock
+                    .Setup(s => s.Attach(It.IsAny<Tweet>()))
+                    .Returns((Tweet t) => t);
+
+                dbsetMock
+                    .Setup(s => s.Remove(It.IsAny<Tweet>()))
+                    .Returns((Tweet t) =>
+                    {
+                        this.items.Remove(t);
+                        return t;
+                    });
+
+                dbsetMock
+                    .Setup(s => s.Find(It.IsAny<object>()))
+                    .Returns((object id) =>
+                    {
+                        return this.items.FirstOrDefault(t => t.Id == (int)id);
+                    });
+
+                // Mock for DbSet.AsQueriable
+                dbsetMock.As<IQueryable<Tweet>>()
+                    .Setup(m => m.Provider)
+                    .Returns(this.items.AsQueryable().Provider);
+                dbsetMock.As<IQueryable<Tweet>>()
+                    .Setup(m => m.Expression)
+                    .Returns(this.items.AsQueryable().Expression);
+                dbsetMock.As<IQueryable<Tweet>>()
+                    .Setup(m => m.ElementType)
+                    .Returns(this.items.AsQueryable().ElementType);
+                dbsetMock.As<IQueryable<Tweet>>()
+                    .Setup(m => m.GetEnumerator())
+                    .Returns(this.items.AsQueryable().GetEnumerator());
+
+                var contextMock = new Mock<BioDbContext>("Fake connection string");
+                contextMock
+                    .Setup(context => context.Set<Tweet>())
+                    .Returns(dbsetMock.Object);
+
+                return contextMock;
+            }
+        }
+    }
+}
