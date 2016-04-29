fb64b675cf425392db03ca7810d17f4de5f859c6
Bozhin Karaivanov <bozhink@gmail.com>
2016-04-29 22:14:50 +0300

    IBioTaxonomyDataRepository and IBioTaxonomyDataRepositoryProvider are added.

---
+++ Data/Bio.Taxonomy.Data/Repositories/Contracts/IBioTaxonomyDataRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace ProcessingTools.Bio.Taxonomy.Data.Repositories.Contracts
+{
+    using ProcessingTools.Data.Common.Entity.Repositories.Contracts;
+
+    public interface IBioTaxonomyDataRepository<T> : IEntityGenericRepository<T>
+        where T : class
+    {
+    }
+}
