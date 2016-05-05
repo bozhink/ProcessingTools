1344376c4effc4b228a300e9b837db5d857cdab1
bozhink <bozhink@gmail.com>
2016-05-05 14:24:09 +0300

    BiorepositoriesInstitutionalCollectionsDataService inherits BiorepositoriesDataServiceFactory.

--- Services/Bio.Biorepositories.Services.Data/Services/BiorepositoriesInstitutionalCollectionsDataService.cs
+++ Services/Bio.Biorepositories.Services.Data/Services/BiorepositoriesInstitutionalCollectionsDataService.cs
@@ -1,62 +1,29 @@
 ﻿namespace ProcessingTools.Bio.Biorepositories.Services.Data
 {
     using System;
-    using System.Linq;
-    using System.Threading.Tasks;
+    using System.Linq.Expressions;
 
     using Contracts;
+    using Factories;
     using Models;
 
     using ProcessingTools.Bio.Biorepositories.Data.Mongo.Models;
     using ProcessingTools.Bio.Biorepositories.Data.Mongo.Repositories.Contracts;
-    using ProcessingTools.Common.Constants;
-    using ProcessingTools.Common.Exceptions;
-    using ProcessingTools.Extensions;
 
-    public class BiorepositoriesInstitutionalCollectionsDataService : IBiorepositoriesInstitutionalCollectionsDataService
+    public class BiorepositoriesInstitutionalCollectionsDataService : BiorepositoriesDataServiceFactory<Collection, BiorepositoriesCollectionServiceModel>, IBiorepositoriesInstitutionalCollectionsDataService
     {
-        private readonly IBiorepositoriesRepositoryProvider<Collection> repositoryProvider;
-
         public BiorepositoriesInstitutionalCollectionsDataService(IBiorepositoriesRepositoryProvider<Collection> repositoryProvider)
+            : base(repositoryProvider)
         {
-            if (repositoryProvider == null)
-            {
-                throw new ArgumentNullException(nameof(repositoryProvider));
-            }
-
-            this.repositoryProvider = repositoryProvider;
         }
 
-        public async Task<IQueryable<BiorepositoriesCollectionServiceModel>> Get(int skip, int take)
+        protected override Expression<Func<Collection, bool>> Filter => c => c.CollectionCode.Length > 1 && c.CollectionName.Length > 1;
+
+        protected override Expression<Func<Collection, BiorepositoriesCollectionServiceModel>> Project => c => new BiorepositoriesCollectionServiceModel
         {
-            if (skip < 0)
-            {
-                throw new InvalidSkipValuePagingException();
-            }
-
-            if (1 > take || take > DefaultPagingConstants.MaximalItemsPerPageAllowed)
-            {
-                throw new InvalidTakeValuePagingException();
-            }
-
-            var repository = this.repositoryProvider.Create();
-
-            var result = (await repository.All())
-                .Where(i => i.CollectionCode.Length > 1 && i.CollectionName.Length > 1)
-                .OrderBy(i => i.Id)
-                .Skip(skip)
-                .Take(take)
-                .Select(i => new BiorepositoriesCollectionServiceModel
-                {
-                    CollectionCode = i.CollectionCode,
-                    CollectionName = i.CollectionName,
-                    Url = i.Url
-                })
-                .ToList();
-
-            repository.TryDispose();
-
-            return result.AsQueryable();
-        }
+            CollectionCode = c.CollectionCode,
+            CollectionName = c.CollectionName,
+            Url = c.Url
+        };
     }
 }
