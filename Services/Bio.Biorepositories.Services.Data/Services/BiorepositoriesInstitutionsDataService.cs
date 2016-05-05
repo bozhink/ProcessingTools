ccb5b57c86d32402e3c7163ffdf41aa55d513524
bozhink <bozhink@gmail.com>
2016-05-05 14:20:37 +0300

    BiorepositoriesInstitutionsDataService inherits BiorepositoriesDataServiceFactory.

--- Services/Bio.Biorepositories.Services.Data/Services/BiorepositoriesInstitutionsDataService.cs
+++ Services/Bio.Biorepositories.Services.Data/Services/BiorepositoriesInstitutionsDataService.cs
@@ -1,62 +1,29 @@
 ﻿namespace ProcessingTools.Bio.Biorepositories.Services.Data
 {
     using System;
-    using System.Linq;
-    using System.Threading.Tasks;
+    using System.Linq.Expressions;
 
     using Contracts;
+    using Factories;
     using Models;
 
     using ProcessingTools.Bio.Biorepositories.Data.Mongo.Models;
     using ProcessingTools.Bio.Biorepositories.Data.Mongo.Repositories.Contracts;
-    using ProcessingTools.Common.Constants;
-    using ProcessingTools.Common.Exceptions;
-    using ProcessingTools.Extensions;
 
-    public class BiorepositoriesInstitutionsDataService : IBiorepositoriesInstitutionsDataService
+    public class BiorepositoriesInstitutionsDataService : BiorepositoriesDataServiceFactory<Institution, BiorepositoriesInstitutionServiceModel>, IBiorepositoriesInstitutionsDataService
     {
-        private readonly IBiorepositoriesRepositoryProvider<Institution> repositoryProvider;
-
         public BiorepositoriesInstitutionsDataService(IBiorepositoriesRepositoryProvider<Institution> repositoryProvider)
+            : base(repositoryProvider)
         {
-            if (repositoryProvider == null)
-            {
-                throw new ArgumentNullException(nameof(repositoryProvider));
-            }
-
-            this.repositoryProvider = repositoryProvider;
         }
 
-        public async Task<IQueryable<BiorepositoriesInstitutionServiceModel>> Get(int skip, int take)
+        protected override Expression<Func<Institution, bool>> Filter => i => i.InstitutionCode.Length > 1 && i.InstitutionName.Length > 1;
+
+        protected override Expression<Func<Institution, BiorepositoriesInstitutionServiceModel>> Project => i => new BiorepositoriesInstitutionServiceModel
         {
-            if (skip < 0)
-            {
-                throw new InvalidSkipValuePagingException();
-            }
-
-            if (1 > take || take > DefaultPagingConstants.MaximalItemsPerPageAllowed)
-            {
-                throw new InvalidTakeValuePagingException();
-            }
-
-            var repository = this.repositoryProvider.Create();
-
-            var result = (await repository.All())
-                .Where(i => i.InstitutionCode.Length > 1 && i.InstitutionName.Length > 1)
-                .OrderBy(i => i.Id)
-                .Skip(skip)
-                .Take(take)
-                .Select(i => new BiorepositoriesInstitutionServiceModel
-                {
-                    InstitutionalCode = i.InstitutionCode,
-                    NameOfInstitution = i.InstitutionName,
-                    Url = i.Url
-                })
-                .ToList();
-
-            repository.TryDispose();
-
-            return result.AsQueryable();
-        }
+            InstitutionalCode = i.InstitutionCode,
+            NameOfInstitution = i.InstitutionName,
+            Url = i.Url
+        };
     }
 }
