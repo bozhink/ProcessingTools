42109d9422cdd38a653a19449f6229004f89d8c9
bozhink <bozhink@gmail.com>
2016-05-14 15:52:05 +0300

    FakeIRedisList is added to ProcessingTools.Data.Common.Redis.Tests.Fakes, and is plugged to RedisClientProviderMock.

--- Data.Tests/Data.Common.Redis.Tests/Mocks/RedisClientProviderMock.cs
+++ Data.Tests/Data.Common.Redis.Tests/Mocks/RedisClientProviderMock.cs
@@ -1,14 +1,25 @@
 ﻿namespace ProcessingTools.Data.Common.Redis.Tests.Mocks
 {
+    using Fakes;
     using Moq;
     using ProcessingTools.Data.Common.Redis.Contracts;
     using ServiceStack.Redis;
 
     public class RedisClientProviderMock : IRedisClientProvider
     {
+        private readonly FakeIRedisList list;
+
+        public RedisClientProviderMock()
+        {
+            this.list = new FakeIRedisList();
+        }
+
         public IRedisClient Create()
         {
             var redisClientMock = new Mock<IRedisClient>();
+            redisClientMock.Setup(client => client.Lists[It.IsAny<string>()])
+                .Returns(this.list);
+
             return redisClientMock.Object;
         }
     }
