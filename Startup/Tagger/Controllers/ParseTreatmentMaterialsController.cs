7a85d37325b03e4c32eb9d50ccf0b007b4f22665
Bozhin Karaivanov <bozhink@gmail.com>
2016-05-22 23:48:46 +0300

    ProcessingTools.Tagger.Controllers.ParseTreatmentMaterialsController is added.

---
+++ Startup/Tagger/Controllers/ParseTreatmentMaterialsController.cs
@@ -0,0 +1,34 @@
+﻿namespace ProcessingTools.Tagger.Controllers
+{
+    using System;
+    using System.Threading.Tasks;
+    using System.Xml;
+
+    using Contracts;
+    using Factories;
+
+    using ProcessingTools.Attributes;
+    using ProcessingTools.BaseLibrary.Taxonomy.Materials;
+    using ProcessingTools.Contracts;
+
+    [Description("Parse treatment materials.")]
+    public class ParseTreatmentMaterialsController : TaggerControllerFactory, IParseTreatmentMaterialsController
+    {
+        private readonly ITreatmentMaterialsParser parser;
+
+        public ParseTreatmentMaterialsController(ITreatmentMaterialsParser parser)
+        {
+            if (parser == null)
+            {
+                throw new ArgumentNullException(nameof(parser));
+            }
+
+            this.parser = parser;
+        }
+
+        protected override async Task Run(XmlDocument document, XmlNamespaceManager namespaceManager, ProgramSettings settings, ILogger logger)
+        {
+            await this.parser.Parse(document, namespaceManager);
+        }
+    }
+}
