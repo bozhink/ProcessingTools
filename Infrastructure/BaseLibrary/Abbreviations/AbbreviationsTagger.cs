931a149f894abcb7db83cf3ae3ebd2e826224261
bozhink <bozhink@gmail.com>
2016-06-03 14:24:59 +0300

    AbbreviationsTagger: last XPath of tagging is written explicitly.

--- Infrastructure/BaseLibrary/Abbreviations/AbbreviationsTagger.cs
+++ Infrastructure/BaseLibrary/Abbreviations/AbbreviationsTagger.cs
@@ -24,21 +24,22 @@
             return Task.Run(() =>
             {
                 // Do not change this sequence
-                this.TagAbbreviationsInSpecificNodeByXPath("//graphic|//media|//disp-formula-group");
-                this.TagAbbreviationsInSpecificNodeByXPath("//chem-struct-wrap|//fig|//supplementary-material|//table-wrap");
-                this.TagAbbreviationsInSpecificNodeByXPath("//fig-group|//table-wrap-group");
-                this.TagAbbreviationsInSpecificNodeByXPath("//boxed-text");
-                this.TagAbbreviationsInSpecificNodeByXPath("/");
+                this.TagAbbreviationsInSpecificNodeByXPath("//graphic | //media | //disp-formula-group", ".//abbrev");
+                this.TagAbbreviationsInSpecificNodeByXPath("//chem-struct-wrap | //fig | //supplementary-material | //table-wrap", ".//abbrev");
+                this.TagAbbreviationsInSpecificNodeByXPath("//fig-group | //table-wrap-group", ".//abbrev");
+                this.TagAbbreviationsInSpecificNodeByXPath("//boxed-text", ".//abbrev");
+                this.TagAbbreviationsInSpecificNodeByXPath("//alt-title | //article-title | //attrib | //award-id | //comment | //conf-theme | //def-head | //element-citation | //funding-source | //license-p | //meta-value | //mixed-citation | //p | //preformat | //product | //subtitle | //supplement | //td | //term | //term-head | //th | //title | //trans-subtitle | //trans-title | //verse-line", "//abbrev");
             });
         }
 
-        private void TagAbbreviationsInSpecificNode(XmlNode specificNode)
+        private void TagAbbreviationsInSpecificNode(XmlNode specificNode, string abbreviationsXpath)
         {
             var abbreviationsList = new HashSet<Abbreviation>(specificNode
-                .SelectNodes(".//abbrev", this.NamespaceManager)
+                .SelectNodes(abbreviationsXpath, this.NamespaceManager)
                 .Cast<XmlNode>()
                 .Select(x => new Abbreviation(x))
                 .Where(a => !string.IsNullOrWhiteSpace(a.Content))
+                .OrderByDescending(a => a.Content.Length)
                 .ToList());
 
             foreach (Abbreviation abbreviation in abbreviationsList)
@@ -62,12 +63,12 @@
             }
         }
 
-        private void TagAbbreviationsInSpecificNodeByXPath(string selectSpecificNodeXPath)
+        private void TagAbbreviationsInSpecificNodeByXPath(string selectSpecificNodeXPath, string abbreviationsXpath)
         {
             XmlNodeList specificNodes = this.XmlDocument.SelectNodes(selectSpecificNodeXPath, this.NamespaceManager);
             foreach (XmlNode specificNode in specificNodes)
             {
-                this.TagAbbreviationsInSpecificNode(specificNode);
+                this.TagAbbreviationsInSpecificNode(specificNode, abbreviationsXpath);
             }
         }
     }
