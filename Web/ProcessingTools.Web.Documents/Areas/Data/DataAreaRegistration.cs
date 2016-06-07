04d2109be5d8baf31721fb211b00a092c506c4c9
bozhink <bozhink@gmail.com>
2016-06-07 17:44:38 +0300

    ProcessingTools.Web.Documents.Areas.Data is added

---
+++ Web/ProcessingTools.Web.Documents/Areas/Data/DataAreaRegistration.cs
@@ -0,0 +1,28 @@
+﻿namespace ProcessingTools.Web.Documents.Areas.Data
+{
+    using System.Web.Mvc;
+
+    public class DataAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "Data";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "Data_default",
+                "Data/{controller}/{action}/{id}",
+                new
+                {
+                    action = "Index",
+                    id = UrlParameter.Optional
+                }
+            );
+        }
+    }
+}
