94bbe74f333ff72290df0409a0347edaf8b9af08
bozhink <bozhink@gmail.com>
2016-06-08 13:03:06 +0300

    Default-styles.less is added.

--- Web/ProcessingTools.Web.Documents/App_Start/BundleConfig.cs
+++ Web/ProcessingTools.Web.Documents/App_Start/BundleConfig.cs
@@ -24,7 +24,8 @@
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/styles/default-styles.min.css"));
         }
     }
 }
