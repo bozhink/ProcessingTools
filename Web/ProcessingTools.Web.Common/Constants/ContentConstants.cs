980f44675e7b953326737a5240c302a457f4c413
bozhink <bozhink@gmail.com>
2016-06-08 15:10:50 +0300

    ContinentsController: constant strings are extracted from views.

--- Web/ProcessingTools.Web.Common/Constants/ContentConstants.cs
+++ Web/ProcessingTools.Web.Common/Constants/ContentConstants.cs
@@ -3,5 +3,28 @@
     public static class ContentConstants
     {
         public const string CopyrightHolder = "ProcessingTools";
+
+        public const string DefaultSeparatorClassName = "default-separator";
+
+        public const string DefaultCreateActionLinkTitle = "Create New";
+        public const string DefaultEditActionLinkTitle = "Edit";
+        public const string DefaultDetailsActionLinkTitle = "Details";
+        public const string DefaultDeleteActionLinkTitle = "Delete";
+        public const string DefaultBackToListActionLinkTitle = "Back to List";
+
+        public const string DeafultIndexPageTitle = "Index";
+        public const string DeafultIndexPageHeading = "Index";
+
+        public const string DeafultDetailsPageTitle = "Details";
+        public const string DeafultDetailsPageHeading = "Details";
+
+        public const string DeafultCreatePageTitle = "Create";
+        public const string DeafultCreatePageHeading = "Create";
+
+        public const string DeafultDeletePageTitle = "Delete";
+        public const string DeafultDeletePageHeading = "Delete";
+
+        public const string DeafultEditPageTitle = "Edit";
+        public const string DeafultEditPageHeading = "Edit";
     }
 }
