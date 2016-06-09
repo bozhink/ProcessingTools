134c8d3f1ba31cdf36d3389349b69b2c81fb5b13
bozhink <bozhink@gmail.com>
2016-06-09 15:31:48 +0300

    EntityNotFoundException is added.

---
+++ Common/Exceptions/EntityNotFoundException.cs
@@ -0,0 +1,29 @@
+﻿namespace ProcessingTools.Common.Exceptions
+{
+    using System;
+    using System.Runtime.Serialization;
+
+    [Serializable]
+    public class EntityNotFoundException : ApplicationException
+    {
+        public EntityNotFoundException()
+            : base()
+        {
+        }
+
+        public EntityNotFoundException(string message)
+            : base(message)
+        {
+        }
+
+        public EntityNotFoundException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+        public EntityNotFoundException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
+}
--- Common/Exceptions/InvalidTakeValuePagingException.cs
+++ Common/Exceptions/InvalidTakeValuePagingException.cs
@@ -3,6 +3,7 @@
     using System;
     using System.Runtime.Serialization;
 
+    [Serializable]
     public class InvalidTakeValuePagingException : ArgumentException
     {
         public InvalidTakeValuePagingException()
