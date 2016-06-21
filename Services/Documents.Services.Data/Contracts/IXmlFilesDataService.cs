45a87dfbbf69252dd137b8702393505dc0b2b67a
bozhink <bozhink@gmail.com>
2016-06-21 09:04:45 +0300

    IXmlFilesDataService is updated.

--- Services/Documents.Services.Data/Contracts/IXmlFilesDataService.cs
+++ Services/Documents.Services.Data/Contracts/IXmlFilesDataService.cs
@@ -8,12 +8,16 @@
 
     public interface IXmlFilesDataService
     {
-        Task<IQueryable<XmlFileMetadataServiceModel>> All();
+        Task<IQueryable<XmlFileMetadataServiceModel>> All(object userId, object articleId, int pageNumber, int itemsPerPage);
 
-        Task<string> GetXmlFileContent(object id);
+        Task<object> Create(object userId, object articleId, XmlFileMetadataServiceModel fileMetadata, Stream inputStream);
 
-        Task<object> Delete(object id);
+        Task<object> Create(object userId, object articleId, XmlFileDetailsServiceModel file);
 
-        Task<object> Create(XmlFileMetadataServiceModel fileMetadata, Stream inputStream);
+        Task<object> Delete(object userId, object articleId, object fileId);
+
+        Task<XmlFileDetailsServiceModel> Get(object userId, object articleId, object fileId);
+
+        Task<object> Update(object userId, object articleId, XmlFileDetailsServiceModel file);
     }
 }
