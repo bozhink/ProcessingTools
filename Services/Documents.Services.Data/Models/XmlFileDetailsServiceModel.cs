e5e21cada6ae86a728cc4024b912303b7a1c90b1
bozhink <bozhink@gmail.com>
2016-06-24 13:00:17 +0300

    XmlFileDetailsServiceModel is removed.

--- Services/Documents.Services.Data/Models/XmlFileDetailsServiceModel.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ProcessingTools.Documents.Services.Data.Models
-{
-    public class XmlFileDetailsServiceModel : XmlFileMetadataServiceModel
-    {
-        public string Content { get; set; }
-    }
-}
