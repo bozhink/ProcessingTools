f7d18d6080873c253b5dffe7491b57c5e13edda1
bozhink <bozhink@gmail.com>
2016-06-24 09:06:54 +0300

    FileSystem: IFileReader and IFileWriter are added.

---
+++ Infrastructure/FileSystem/Contracts/IFileReader.cs
@@ -0,0 +1,23 @@
+﻿namespace ProcessingTools.FileSystem.Contracts
+{
+    using System.IO;
+
+    public interface IFileReader
+    {
+        /// <summary>
+        /// Reads a file as stream.
+        /// </summary>
+        /// <param name="fileName">Name of the file. Should be full-path name or relative.</param>
+        /// <param name="basePath">Path to directory in which fileName should be.</param>
+        /// <returns>Stream object for fileName.</returns>
+        Stream ReadToStream(string fileName, string basePath = null);
+
+        /// <summary>
+        /// Reads a file to a StreamReader.
+        /// </summary>
+        /// <param name="fileName">Name of the file. Should be full-path name or relative.</param>
+        /// <param name="basePath">Stream object for fileName.</param>
+        /// <returns></returns>
+        StreamReader GetReader(string fileName, string basePath = null);
+    }
+}
---
+++ Infrastructure/FileSystem/Contracts/IFileWriter.cs
@@ -0,0 +1,17 @@
+﻿namespace ProcessingTools.FileSystem.Contracts
+{
+    using System.IO;
+    using System.Threading.Tasks;
+
+    public interface IFileWriter
+    {
+        /// <summary>
+        /// Writes a stream to a file.
+        /// </summary>
+        /// <param name="stream">Stream object to be written to fileName.</param>
+        /// <param name="fileName">Name of the file. Should be full-path name or relative.</param>
+        /// <param name="basePath">Path to directory in which fileName should be.</param>
+        /// <returns>Awaitable task.</returns>
+        Task Write(Stream stream, string fileName, string basePath = null);
+    }
+}
