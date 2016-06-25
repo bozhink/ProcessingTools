25f6e319a57a5ac7573e0c74305a15a3ae9c77d0
Bozhin Karaivanov <bozhink@gmail.com>
2016-06-25 14:54:30 +0300

    PublishersController is added.

---
+++ Web/ProcessingTools.Web.Documents/Areas/Journals/Controllers/PublishersController.cs
@@ -0,0 +1,146 @@
+﻿namespace ProcessingTools.Web.Documents.Areas.Journals.Controllers
+{
+    using System;
+    using System.Data.Entity;
+    using System.Net;
+    using System.Threading.Tasks;
+    using System.Web.Mvc;
+
+    using ProcessingTools.Documents.Data;
+    using ProcessingTools.Documents.Data.Contracts;
+    using ProcessingTools.Documents.Data.Models;
+
+    public class PublishersController : Controller
+    {
+        private readonly DocumentsDbContext db;
+
+        public PublishersController(IDocumentsDbContextProvider contextProvider)
+        {
+            if (contextProvider == null)
+            {
+                throw new ArgumentNullException(nameof(contextProvider));
+            }
+
+            this.db = contextProvider.Create();
+        }
+
+        // GET: Journals/Publishers
+        public async Task<ActionResult> Index()
+        {
+            return this.View(await this.db.Publishers.ToListAsync());
+        }
+
+        // GET: Journals/Publishers/Details/5
+        public async Task<ActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var publisher = await Task.FromResult(this.db.Publishers.Find(id));
+            if (publisher == null)
+            {
+                return this.HttpNotFound();
+            }
+
+            return this.View(publisher);
+        }
+
+        // GET: Journals/Publishers/Create
+        public ActionResult Create()
+        {
+            return this.View();
+        }
+
+        // POST: Journals/Publishers/Create
+        // To protect from over-posting attacks, please enable the specific properties you want to bind to, for
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,Name,AbbreviatedName,CreatedByUserId,DateCreated,DateModified,ModifiedByUserId")] Publisher publisher)
+        {
+            if (ModelState.IsValid)
+            {
+                publisher.Id = Guid.NewGuid();
+                this.db.Publishers.Add(publisher);
+                await this.db.SaveChangesAsync();
+                return this.RedirectToAction(nameof(this.Index));
+            }
+
+            return this.View(publisher);
+        }
+
+        // GET: Journals/Publishers/Edit/5
+        public async Task<ActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var publisher = await Task.FromResult(this.db.Publishers.Find(id));
+            if (publisher == null)
+            {
+                return this.HttpNotFound();
+            }
+
+            return this.View(publisher);
+        }
+
+        // POST: Journals/Publishers/Edit/5
+        // To protect from over-posting attacks, please enable the specific properties you want to bind to, for
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,Name,AbbreviatedName,CreatedByUserId,DateCreated,DateModified,ModifiedByUserId")] Publisher publisher)
+        {
+            if (ModelState.IsValid)
+            {
+                this.db.Entry(publisher).State = EntityState.Modified;
+                await this.db.SaveChangesAsync();
+                return this.RedirectToAction(nameof(this.Index));
+            }
+
+            return this.View(publisher);
+        }
+
+        // GET: Journals/Publishers/Delete/5
+        public async Task<ActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var publisher = await Task.FromResult(this.db.Publishers.Find(id));
+            if (publisher == null)
+            {
+                return this.HttpNotFound();
+            }
+
+            return this.View(publisher);
+        }
+
+        // POST: Journals/Publishers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(Guid id)
+        {
+            var publisher = await Task.FromResult(this.db.Publishers.Find(id));
+            this.db.Publishers.Remove(publisher);
+            await this.db.SaveChangesAsync();
+            return this.RedirectToAction(nameof(this.Index));
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                this.db.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+    }
+}
