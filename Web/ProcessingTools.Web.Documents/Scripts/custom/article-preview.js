356716820ae01440217098218955a2d8baab819b
bozhink <bozhink@gmail.com>
2016-07-07 14:43:22 +0300

    article-preview.js: coordinates list is generated in aside/section.

--- Web/ProcessingTools.Web.Documents/Scripts/custom/article-preview.js
+++ Web/ProcessingTools.Web.Documents/Scripts/custom/article-preview.js
@@ -1,21 +1,60 @@
-﻿(function ($) {
+﻿var articlePreviewActions = (function ($) {
 
-    $('mark').hover(function () {
-        $(this).attr('title', function () {
-            return $(this).attr('elem-name');
-        })
-    }, function () {
-        $(this).removeAttr('title');
-    });
+    function toggleMarkTitle() {
+        $('mark').hover(function () {
+            $(this).attr('title', function () {
+                return $(this).attr('elem-name');
+            })
+        }, function () {
+            $(this).removeAttr('title');
+        });
+    }
 
-    var $asideBox = $('#aside-main-box'),
-        $ul = $('<ul>').attr('class', 'list-group');
-    $('.named-content.geo-json').each(function () {
-        var coordinates = JSON.parse($(this).attr('specific-use'))['coordinates'];
-        $('<li>').attr('class', 'list-group-item').text(coordinates.toString()).appendTo($ul);
-    });
+    function genrateCoordinatesList(targetBoxId) {
+        targetBoxId = '' + targetBoxId;
+        if (targetBoxId.length < 1) {
+            throw 'Invalid target box id!';
+        }
 
-    $('<div>').attr('class', 'panel-heading').text('Coordinates').appendTo($asideBox);
-    $('<div>').attr('class', 'panel-body').append($ul).appendTo($asideBox);
+        if (targetBoxId.substr(0, 1) !== '#') {
+            targetBoxId = '#' + targetBoxId;
+        }
 
-}(jQuery))
+        var $aside = $(targetBoxId),
+            $section = $('<section>').addClass('panel').addClass('panel-default').attr('id', 'coordinates-list'),
+            $list = $('<div>').addClass('list-group');
+
+        $('.named-content.geo-json').each(function () {
+            var $that = $(this),
+                id = $that.attr('id'),
+                coordinates = JSON.parse($that.attr('specific-use'))['coordinates'];
+
+            $('<a>')
+                .addClass('list-group-item')
+                .attr('href', '#' + id)
+                .text(coordinates.toString())
+                .appendTo($list);
+        });
+
+        $('<div>')
+            .addClass('panel-heading')
+            .text('Coordinates')
+            .appendTo($section);
+
+        $('<div>')
+            .addClass('panel-body')
+            .append($list)
+            .appendTo($section);
+
+        $section.appendTo($aside);
+    }
+
+    return {
+        toggleMarkTitle,
+        genrateCoordinatesList
+    }
+
+}(jQuery));
+
+articlePreviewActions.toggleMarkTitle();
+articlePreviewActions.genrateCoordinatesList("#aside-main-box");
