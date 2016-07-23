3eb8d139667776fb65fc5dccad805e692e8e66d4
Bozhin Karaivanov <bozhink@gmail.com>
2016-07-23 17:59:33 +0300

    Project Documents.Services.Data.Tests is added.

---
+++ Services.Tests/Documents.Services.Data.Tests/XmlPresenterTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using ProcessingTools.Documents.Services.Data.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessingTools.Documents.Services.Data.Tests
+{
+    [TestClass]
+    public class XmlPresenterTests
+    {
+        [TestMethod]
+        public void XmlPresenter_WithValidServiceInConstructor_ShouldReturnValidObject()
+        {
+            var serviceMock = new Mock<IDocumentsDataService>();
+            var service = serviceMock.Object;
+
+            var presenter = new XmlPresenter(service);
+            Assert.IsNotNull(presenter, "Object should not be null");
+
+            string fieldName = nameof(service);
+            var field = presenter.GetType().GetField(fieldName, BindingFlags.Instance | BindingFlags.NonPublic);
+            var fieldValue = field?.GetValue(presenter);
+            Assert.IsNotNull(fieldValue, "Service should be initialized.");
+            Assert.AreSame(service, fieldValue, "Service should be set correctly.");
+        }
+    }
+}
