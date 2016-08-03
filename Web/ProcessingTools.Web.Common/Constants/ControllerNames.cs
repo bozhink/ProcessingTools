95f7b13f600874c26f3a5f993695ce5071d26542
bozhink <bozhink@gmail.com>
2016-08-03 10:53:01 +0300

    ControllerNames: added ArticlesControllerName.

--- Web/ProcessingTools.Web.Common/Constants/ControllerNames.cs
+++ Web/ProcessingTools.Web.Common/Constants/ControllerNames.cs
@@ -2,14 +2,15 @@
 {
     public static class ControllerNames
     {
-        public const string HomeControllerName = "Home";
         public const string AccountControllerName = "Account";
+        public const string HomeControllerName = "Home";
         public const string ManageControllerName = "Manage";
 
-        public const string ContinentsControllerName = "Continents";
+        public const string ArticlesControllerName = "Articles";
+        public const string DocumentContentControllerName = "DocumentContent";
         public const string FilesControllerName = "Files";
 
-        public const string DocumentContentControllerName = "DocumentContent";
+        public const string ContinentsControllerName = "Continents";
 
         public const string JournalsControllerName = "Journals";
         public const string PublishersControllerName = "Publishers";
