32a0c5e613260fc503b42f0dff555fdfd65edab9
bozhink <bozhink@gmail.com>
2016-08-11 09:43:21 +0300

    ISearchableRepository and ICountableRepository are added to ProcessingTools.Data.Common.Repositories.Contracts.

---
+++ Data/Data.Common/Repositories/Contracts/ICountableRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace ProcessingTools.Data.Common.Repositories.Contracts
+{
+    using System;
+    using System.Linq.Expressions;
+    using System.Threading.Tasks;
+
+    public interface ICountableRepository<TEntity>
+    {
+        Task<long> Count();
+
+        Task<long> Count(Expression<Func<TEntity, bool>> filter);
+    }
+}
---
+++ Data/Data.Common/Repositories/Contracts/ISearchableRepository.cs
@@ -0,0 +1,35 @@
+﻿namespace ProcessingTools.Data.Common.Repositories.Contracts
+{
+    using System;
+    using System.Linq;
+    using System.Linq.Expressions;
+    using System.Threading.Tasks;
+
+    using ProcessingTools.Common.Constants;
+    using ProcessingTools.Common.Types;
+
+    public interface ISearchableRepository<TEntity>
+    {
+        Task<IQueryable<TEntity>> Find(
+            Expression<Func<TEntity, bool>> filter,
+            Expression<Func<TEntity, object>> sort,
+            SortOrder sortOrder = SortOrder.Ascending,
+            int skip = 0,
+            int take = PagingConstants.DefaultNumberOfTopItemsToSelect);
+
+        Task<IQueryable<T>> Find<T>(
+            Expression<Func<TEntity, bool>> filter,
+            Expression<Func<TEntity, T>> projection,
+            Expression<Func<TEntity, object>> sort,
+            SortOrder sortOrder = SortOrder.Ascending,
+            int skip = 0,
+            int take = PagingConstants.DefaultNumberOfTopItemsToSelect);
+
+        Task<TEntity> FindFirst(
+            Expression<Func<TEntity, bool>> filter);
+
+        Task<T> FindFirst<T>(
+            Expression<Func<TEntity, bool>> filter,
+            Expression<Func<TEntity, T>> projection);
+    }
+}
