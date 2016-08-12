a35614a3d9c5c08915d10f50074e6e1b3865a63a
bozhink <bozhink@gmail.com>
2016-08-12 15:42:19 +0300

    AphiaTaxaRankResolverDataService is added.

---
+++ Services/Data/Bio.Taxonomy.Services.Data/Services/AphiaTaxaRankResolverDataService.cs
@@ -0,0 +1,36 @@
+﻿namespace ProcessingTools.Bio.Taxonomy.Services.Data
+{
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using Contracts;
+    using ProcessingTools.Bio.Taxonomy.Contracts;
+
+    public class AphiaTaxaRankResolverDataService : IAphiaTaxaRankResolverDataService
+    {
+        private readonly IAphiaTaxaClassificationResolverDataService service;
+
+        public AphiaTaxaRankResolverDataService(IAphiaTaxaClassificationResolverDataService service)
+        {
+            if (service == null)
+            {
+                throw new ArgumentNullException(nameof(service));
+            }
+
+            this.service = service;
+        }
+
+        public async Task<IQueryable<ITaxonRank>> Resolve(params string[] scientificNames)
+        {
+            if (scientificNames == null || scientificNames.Length < 1)
+            {
+                throw new ArgumentNullException(nameof(scientificNames));
+            }
+
+            var response = await this.service.Resolve(scientificNames);
+
+            return response.AsQueryable<ITaxonRank>();
+        }
+    }
+}
