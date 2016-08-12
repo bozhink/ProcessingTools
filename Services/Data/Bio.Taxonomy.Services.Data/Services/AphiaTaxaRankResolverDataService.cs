ca36f7bed93dc589d615b59013ebfd7f63afa042
bozhink <bozhink@gmail.com>
2016-08-12 15:52:06 +0300

    TaxaRankResolverOverTaxaClassificationResolverDataServiceFactory and CatalogueOfLifeTaxaRankResolverDataService are added.

--- Services/Data/Bio.Taxonomy.Services.Data/Services/AphiaTaxaRankResolverDataService.cs
+++ Services/Data/Bio.Taxonomy.Services.Data/Services/AphiaTaxaRankResolverDataService.cs
@@ -1,36 +1,15 @@
 ﻿namespace ProcessingTools.Bio.Taxonomy.Services.Data
 {
-    using System;
-    using System.Linq;
-    using System.Threading.Tasks;
-
     using Contracts;
-    using ProcessingTools.Bio.Taxonomy.Contracts;
+    using Factories;
 
-    public class AphiaTaxaRankResolverDataService : IAphiaTaxaRankResolverDataService
+    public class AphiaTaxaRankResolverDataService : TaxaRankResolverOverTaxaClassificationResolverDataServiceFactory, IAphiaTaxaRankResolverDataService
     {
         private readonly IAphiaTaxaClassificationResolverDataService service;
 
         public AphiaTaxaRankResolverDataService(IAphiaTaxaClassificationResolverDataService service)
+            : base(service)
         {
-            if (service == null)
-            {
-                throw new ArgumentNullException(nameof(service));
-            }
-
-            this.service = service;
-        }
-
-        public async Task<IQueryable<ITaxonRank>> Resolve(params string[] scientificNames)
-        {
-            if (scientificNames == null || scientificNames.Length < 1)
-            {
-                throw new ArgumentNullException(nameof(scientificNames));
-            }
-
-            var response = await this.service.Resolve(scientificNames);
-
-            return response.AsQueryable<ITaxonRank>();
         }
     }
 }
