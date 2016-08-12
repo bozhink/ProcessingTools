04ef9fd8d3608f1797c90ce5f7085c48b3b6bbba
bozhink <bozhink@gmail.com>
2016-08-12 09:22:11 +0300

    ITaxonRankRepository, ITaxonRankRepositoryProvider, ITaxonRankSearchableRepository, and are added to ProcessingTools.Bio.Taxonomy.Data.Common.Repositories.Contracts.

---
+++ Data/Bio.Taxonomy.Data.Common/Repositories/Contracts/ITaxonRankRepositoryProvider.cs
@@ -0,0 +1,10 @@
+﻿namespace ProcessingTools.Bio.Taxonomy.Data.Common.Repositories.Contracts
+{
+    using Models.Contracts;
+    using ProcessingTools.Data.Common.Contracts;
+    using ProcessingTools.Data.Common.Repositories.Contracts;
+
+    public interface ITaxonRankRepositoryProvider : IGenericRepositoryProvider<IGenericRepository<ITaxonRankEntity>, ITaxonRankEntity>
+    {
+    }
+}
