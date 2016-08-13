d2540cdffd23768be5d2922ded4a6e60f4b1eb9c
Bozhin Karaivanov <bozhink@gmail.com>
2016-08-13 18:15:54 +0300

    ProcessingTools.Bio.Taxonomy.Data.Common.Models.Contracts.IBlackListEntity is added.

---
+++ Data/Bio.Taxonomy.Data.Common/Models/Contracts/IBlackListEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace ProcessingTools.Bio.Taxonomy.Data.Common.Models.Contracts
+{
+    public interface IBlackListEntity
+    {
+        string Content { get; }
+    }
+}
--- Data/Bio.Taxonomy.Data/Models/BlackListEntity.cs
+++ Data/Bio.Taxonomy.Data/Models/BlackListEntity.cs
@@ -2,8 +2,9 @@
 {
     using System.ComponentModel.DataAnnotations;
     using ProcessingTools.Bio.Taxonomy.Data.Common.Constants;
+    using ProcessingTools.Bio.Taxonomy.Data.Common.Models.Contracts;
 
-    public class BlackListEntity
+    public class BlackListEntity : IBlackListEntity
     {
         [Key]
         public int Id { get; set; }
