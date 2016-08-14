40e3696a79724c6f8c159a77f585db770e77e3b2
Bozhin Karaivanov <bozhink@gmail.com>
2016-08-14 12:29:23 +0300

    ICrudRepository is added.

---
+++ Data/Data.Common/Repositories/Contracts/ICrudRepository.cs
@@ -0,0 +1,19 @@
+﻿namespace ProcessingTools.Data.Common.Repositories.Contracts
+{
+    using System.Threading.Tasks;
+
+    public interface ICrudRepository<TEntity> : IRepository<TEntity>
+    {
+        Task<object> Add(TEntity entity);
+
+        Task<object> Delete(TEntity entity);
+
+        Task<object> Delete(object id);
+
+        Task<TEntity> Get(object id);
+
+        Task<long> SaveChanges();
+
+        Task<object> Update(TEntity entity);
+    }
+}
--- Data/Data.Common/Repositories/Contracts/IGenericRepository.cs
+++ Data/Data.Common/Repositories/Contracts/IGenericRepository.cs
@@ -1,20 +1,6 @@
 ﻿namespace ProcessingTools.Data.Common.Repositories.Contracts
 {
-    using System.Linq;
-    using System.Threading.Tasks;
-
-    public interface IGenericRepository<TEntity> : ICountableRepository<TEntity>, ISearchableRepository<TEntity>, IIterableRepository<TEntity>, IRepository<TEntity>
+    public interface IGenericRepository<TEntity> : ICountableRepository<TEntity>, ISearchableRepository<TEntity>, IIterableRepository<TEntity>, ICrudRepository<TEntity>, IRepository<TEntity>
     {
-        Task<object> Add(TEntity entity);
-
-        Task<object> Delete(TEntity entity);
-
-        Task<object> Delete(object id);
-
-        Task<TEntity> Get(object id);
-
-        Task<long> SaveChanges();
-
-        Task<object> Update(TEntity entity);
     }
 }
