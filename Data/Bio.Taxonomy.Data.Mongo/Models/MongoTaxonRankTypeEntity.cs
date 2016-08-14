d6d247c6823d479a4295faaec42793e498148a29
Bozhin Karaivanov <bozhink@gmail.com>
2016-08-14 10:29:20 +0300

    MongoTaxonRankTypeEntity is added.

---
+++ Data/Bio.Taxonomy.Data.Mongo/Models/MongoTaxonRankTypeEntity.cs
@@ -0,0 +1,41 @@
+﻿namespace ProcessingTools.Bio.Taxonomy.Data.Mongo.Models
+{
+    using MongoDB.Bson;
+    using MongoDB.Bson.Serialization.Attributes;
+    using ProcessingTools.Bio.Taxonomy.Extensions;
+    using ProcessingTools.Bio.Taxonomy.Types;
+    using ProcessingTools.Contracts;
+
+    public class MongoTaxonRankTypeEntity : INameableStringIdentifiable
+    {
+        private TaxonRankType rankType;
+
+        public MongoTaxonRankTypeEntity()
+        {
+            this.rankType = TaxonRankType.Other;
+        }
+
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        [BsonIgnoreIfDefault]
+        public string Id { get; set; }
+
+        [BsonRepresentation(BsonType.String)]
+        public string Name { get; set; }
+
+        [BsonRepresentation(BsonType.Int32)]
+        public TaxonRankType RankType
+        {
+            get
+            {
+                return this.rankType;
+            }
+
+            set
+            {
+                this.rankType = value;
+                this.Name = this.rankType.MapTaxonRankTypeToTaxonRankString();
+            }
+        }
+    }
+}
