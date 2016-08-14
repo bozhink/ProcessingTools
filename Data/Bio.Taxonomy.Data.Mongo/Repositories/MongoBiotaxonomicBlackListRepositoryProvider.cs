ed741114b50c47484f882694e0897320007b9ca6
Bozhin Karaivanov <bozhink@gmail.com>
2016-08-14 14:33:09 +0300

    MongoBiotaxonomicBlackListRepositoryProvider is added.

---
+++ Data/Bio.Taxonomy.Data.Mongo/Repositories/MongoBiotaxonomicBlackListRepositoryProvider.cs
@@ -0,0 +1,30 @@
+﻿namespace ProcessingTools.Bio.Taxonomy.Data.Mongo.Repositories
+{
+    using System;
+
+    using Contracts;
+
+    using ProcessingTools.Bio.Taxonomy.Data.Common.Models.Contracts;
+    using ProcessingTools.Bio.Taxonomy.Data.Mongo.Contracts;
+    using ProcessingTools.Data.Common.Repositories.Contracts;
+
+    public class MongoBiotaxonomicBlackListRepositoryProvider : IMongoBiotaxonomicBlackListRepositoryProvider
+    {
+        private readonly IBiotaxonomyMongoDatabaseProvider provider;
+
+        public MongoBiotaxonomicBlackListRepositoryProvider(IBiotaxonomyMongoDatabaseProvider provider)
+        {
+            if (provider == null)
+            {
+                throw new ArgumentNullException(nameof(provider));
+            }
+
+            this.provider = provider;
+        }
+
+        public ICrudRepository<IBlackListEntity> Create()
+        {
+            return new MongoBiotaxonomicBlackListRepository(this.provider);
+        }
+    }
+}
