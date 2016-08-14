e37c2c651b60f7ebc49bfd018f31e2c3216cab83
Bozhin Karaivanov <bozhink@gmail.com>
2016-08-14 22:02:03 +0300

    BiotaxonomyMongoDatabaseSeeder: seeder for the taxonomic black list is added.

--- Data/Bio.Taxonomy.Data.Seed/Seeders/BiotaxonomyMongoDatabaseSeeder.cs
+++ Data/Bio.Taxonomy.Data.Seed/Seeders/BiotaxonomyMongoDatabaseSeeder.cs
@@ -8,23 +8,30 @@
 
     using MongoDB.Driver;
 
+    using ProcessingTools.Bio.Taxonomy.Data.Common.Repositories.Contracts;
     using ProcessingTools.Bio.Taxonomy.Data.Mongo.Contracts;
     using ProcessingTools.Bio.Taxonomy.Data.Mongo.Models;
     using ProcessingTools.Bio.Taxonomy.Data.Mongo.Repositories.Contracts;
     using ProcessingTools.Bio.Taxonomy.Data.Xml.Repositories.Contracts;
     using ProcessingTools.Bio.Taxonomy.Types;
     using ProcessingTools.Data.Common.Mongo.Factories;
+    using ProcessingTools.Extensions;
 
     public class BiotaxonomyMongoDatabaseSeeder : IBiotaxonomyMongoDatabaseSeeder
     {
         private readonly IBiotaxonomyMongoDatabaseProvider biotaxonomyMongoDatabaseProvider;
         private readonly IMongoTaxonRankRepositoryProvider mongoTaxonRankRepositoryProvider;
-        private readonly IXmlTaxonRankRepositoryProvider xmlTaxonRankRepositoryProvider;
+        private readonly ITaxonRankRepositoryProvider taxonRankRepositoryProvider;
+
+        private readonly IMongoBiotaxonomicBlackListRepositoryProvider mongoBiotaxonomicBlackListRepositoryProvider;
+        private readonly IBiotaxonomicBlackListIterableRepositoryProvider biotaxonomicBlackListIterableRepositoryProvider;
 
         public BiotaxonomyMongoDatabaseSeeder(
             IBiotaxonomyMongoDatabaseProvider biotaxonomyMongoDatabaseProvider,
             IMongoTaxonRankRepositoryProvider mongoTaxonRankRepositoryProvider,
-            IXmlTaxonRankRepositoryProvider xmlTaxonRankRepositoryProvider)
+            ITaxonRankRepositoryProvider taxonRankRepositoryProvider,
+            IMongoBiotaxonomicBlackListRepositoryProvider mongoBiotaxonomicBlackListRepositoryProvider,
+            IBiotaxonomicBlackListIterableRepositoryProvider biotaxonomicBlackListIterableRepositoryProvider)
         {
             if (biotaxonomyMongoDatabaseProvider == null)
             {
@@ -36,32 +43,49 @@
                 throw new ArgumentNullException(nameof(mongoTaxonRankRepositoryProvider));
             }
 
-            if (xmlTaxonRankRepositoryProvider == null)
+            if (taxonRankRepositoryProvider == null)
             {
-                throw new ArgumentNullException(nameof(xmlTaxonRankRepositoryProvider));
+                throw new ArgumentNullException(nameof(taxonRankRepositoryProvider));
+            }
+
+            if (mongoBiotaxonomicBlackListRepositoryProvider == null)
+            {
+                throw new ArgumentNullException(nameof(mongoBiotaxonomicBlackListRepositoryProvider));
+            }
+
+            if (biotaxonomicBlackListIterableRepositoryProvider == null)
+            {
+                throw new ArgumentNullException(nameof(biotaxonomicBlackListIterableRepositoryProvider));
             }
 
             this.biotaxonomyMongoDatabaseProvider = biotaxonomyMongoDatabaseProvider;
             this.mongoTaxonRankRepositoryProvider = mongoTaxonRankRepositoryProvider;
-            this.xmlTaxonRankRepositoryProvider = xmlTaxonRankRepositoryProvider;
+            this.taxonRankRepositoryProvider = taxonRankRepositoryProvider;
+            this.mongoBiotaxonomicBlackListRepositoryProvider = mongoBiotaxonomicBlackListRepositoryProvider;
+            this.biotaxonomicBlackListIterableRepositoryProvider = biotaxonomicBlackListIterableRepositoryProvider;
         }
 
         public async Task Seed()
         {
             await this.SeedTaxonRankTypeCollection();
             await this.SeedTaxonRankCollection();
+            await this.SeedBlackListCollection();
         }
 
         private async Task SeedTaxonRankCollection()
         {
             var mongoTaxonRankRepository = this.mongoTaxonRankRepositoryProvider.Create();
-            var xmlTaxonRankRepository = this.xmlTaxonRankRepositoryProvider.Create();
-            var entities = await xmlTaxonRankRepository.All();
+
+            var repository = this.taxonRankRepositoryProvider.Create();
+            var entities = await repository.All();
 
             foreach (var entity in entities)
             {
                 await mongoTaxonRankRepository.Add(entity);
             }
+
+            repository.TryDispose();
+            mongoTaxonRankRepository.TryDispose();
         }
 
         private async Task SeedTaxonRankTypeCollection()
@@ -85,6 +109,21 @@
                     IsOrdered = false,
                     BypassDocumentValidation = false
                 });
+        }
+
+        private async Task SeedBlackListCollection()
+        {
+            var mongoBiotaxonomicBlackListRepository = this.mongoBiotaxonomicBlackListRepositoryProvider.Create();
+            var repository = this.biotaxonomicBlackListIterableRepositoryProvider.Create();
+
+            var entities = await repository.All();
+            foreach (var entity in entities)
+            {
+                await mongoBiotaxonomicBlackListRepository.Add(entity);
+            }
+
+            repository.TryDispose();
+            mongoBiotaxonomicBlackListRepository.TryDispose();
         }
     }
 }
