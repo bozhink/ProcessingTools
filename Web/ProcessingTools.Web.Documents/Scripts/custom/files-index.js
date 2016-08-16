63dbede53f0c6686f4d4dd2d1623b01db7b41c99
bozhink <bozhink@gmail.com>
2016-08-16 10:02:00 +0300

    FilesController.Index: page-Up/Down, home and end are now binded to pagination.

--- Web/ProcessingTools.Web.Documents/Scripts/custom/files-index.js
+++ Web/ProcessingTools.Web.Documents/Scripts/custom/files-index.js
@@ -1,21 +1,51 @@
 ﻿(function (window, document) {
     'use strict';
 
-    function keydownEventHandler(event) {
-        var e = event || window.event,
+    function redirect(anchorId) {
+        var id = anchorId || null,
             anchorElement,
             href;
-
-        // U
-        if (e.which === 85) {
-            anchorElement = document.getElementById('upload-file-link');
+        if (id) {
+            anchorElement = document.getElementById(id);
             if (anchorElement) {
                 href = anchorElement.href || null;
                 if (href) {
                     document.location = href;
                 }
             }
+        }
+    }
 
+    function keydownEventHandler(event) {
+        var e = event || window.event;
+
+        // U
+        if (e.which === 85) {
+            redirect('upload-file-link');
+            return false;
+        }
+
+        // Page Up
+        if (e.which === 33) {
+            redirect('page-next');
+            return false;
+        }
+
+        // Page Down
+        if (e.which === 34) {
+            redirect('page-prev');
+            return false;
+        }
+
+        // End
+        if (e.which === 35) {
+            redirect('page-last');
+            return false;
+        }
+
+        // Home
+        if (e.which === 36) {
+            redirect('page-first');
             return false;
         }
     }
