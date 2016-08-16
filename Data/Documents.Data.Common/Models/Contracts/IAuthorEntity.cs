a90b29dfbfcf357c02943a25f423aaab39752323
bozhink <bozhink@gmail.com>
2016-08-16 13:55:26 +0300

    IAuthorEntity is completed.

--- Data/Documents.Data.Common/Models/Contracts/IAuthorEntity.cs
+++ Data/Documents.Data.Common/Models/Contracts/IAuthorEntity.cs
@@ -1,10 +1,20 @@
 ﻿namespace ProcessingTools.Documents.Data.Common.Models.Contracts
 {
-    using System;
     using System.Collections.Generic;
     using ProcessingTools.Contracts;
 
     public interface IAuthorEntity : IGuidIdentifiable, IModelWithUserInformation
     {
+        string Surname { get; }
+
+        string GivenNames { get; }
+
+        string Prefix { get; }
+
+        string Suffix { get; }
+
+        ICollection<IAffiliationEntity> Affiliations { get; }
+
+        ICollection<IArticleEntity> Articles { get; }
     }
 }
