4ff43b5f942772e6544c10b05d7d5c6af84ef344
bozhink <bozhink@gmail.com>
2016-08-16 15:24:36 +0300

    IPublishersRepository is added.

---
+++ Data/Documents.Data.Common/Repositories/Contracts/IPublishersRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace ProcessingTools.Documents.Data.Common.Repositories.Contracts
+{
+    using System.Threading.Tasks;
+
+    using Models.Contracts;
+    using ProcessingTools.Data.Common.Repositories.Contracts;
+
+    public interface IPublishersRepository : ICountableRepository<IPublisherEntity>, ICrudRepository<IPublisherEntity>, IIterableRepository<IPublisherEntity>
+    {
+        Task<object> AddAddress(IAddressEntity address);
+
+        Task<object> RemoveAddress(object addressId);
+    }
+}
