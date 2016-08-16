93599d557b0fdd4e1b2ebf17e74c25aa0a688a14
bozhink <bozhink@gmail.com>
2016-08-16 15:43:38 +0300

    IAddressableRepository is added and IPublishersRepository and IInstitutionsRepository now inherit it.

--- Data/Documents.Data.Common/Repositories/Contracts/IInstitutionsRepository.cs
+++ Data/Documents.Data.Common/Repositories/Contracts/IInstitutionsRepository.cs
@@ -1,14 +1,9 @@
 ﻿namespace ProcessingTools.Documents.Data.Common.Repositories.Contracts
 {
-    using System.Threading.Tasks;
-
     using Models.Contracts;
     using ProcessingTools.Data.Common.Repositories.Contracts;
 
-    public interface IInstitutionsRepository : ICountableIterableCrudRepository<IInstitutionEntity>
+    public interface IInstitutionsRepository : IAddressableRepository, ICountableIterableCrudRepository<IInstitutionEntity>
     {
-        Task<object> AddAddress(IAddressEntity address);
-
-        Task<object> RemoveAddress(object addressId);
     }
 }
--- Data/Documents.Data.Common/Repositories/Contracts/IPublishersRepository.cs
+++ Data/Documents.Data.Common/Repositories/Contracts/IPublishersRepository.cs
@@ -1,14 +1,9 @@
 ﻿namespace ProcessingTools.Documents.Data.Common.Repositories.Contracts
 {
-    using System.Threading.Tasks;
-
     using Models.Contracts;
     using ProcessingTools.Data.Common.Repositories.Contracts;
 
-    public interface IPublishersRepository : ICountableIterableCrudRepository<IPublisherEntity>
+    public interface IPublishersRepository : IAddressableRepository, ICountableIterableCrudRepository<IPublisherEntity>
     {
-        Task<object> AddAddress(IAddressEntity address);
-
-        Task<object> RemoveAddress(object addressId);
     }
 }
