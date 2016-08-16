f7b2358ade4de8734285f8cc2ea7c062aaf20ec7
bozhink <bozhink@gmail.com>
2016-08-16 10:41:34 +0300

    files-index.js: e.stopPropagation();e.preventDefault(); is added.

--- Web/ProcessingTools.Web.Documents/Scripts/custom/files-index.js
+++ Web/ProcessingTools.Web.Documents/Scripts/custom/files-index.js
@@ -21,30 +21,40 @@
 
         // U
         if (e.which === 85) {
+            e.stopPropagation();
+            e.preventDefault();
             redirect('upload-file-link');
             return false;
         }
 
         // Page Up
         if (e.which === 33) {
+            e.stopPropagation();
+            e.preventDefault();
             redirect('page-next');
             return false;
         }
 
         // Page Down
         if (e.which === 34) {
+            e.stopPropagation();
+            e.preventDefault();
             redirect('page-prev');
             return false;
         }
 
         // End
         if (e.which === 35) {
+            e.stopPropagation();
+            e.preventDefault();
             redirect('page-last');
             return false;
         }
 
         // Home
         if (e.which === 36) {
+            e.stopPropagation();
+            e.preventDefault();
             redirect('page-first');
             return false;
         }
