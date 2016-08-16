5fe564969aa64f4dc9919d22e93d853e2029ad7f
bozhink <bozhink@gmail.com>
2016-08-16 14:21:23 +0300

    IPerson is added and IAuthorEntity now inherits it.

--- Data/Documents.Data.Common/Models/Contracts/IAuthorEntity.cs
+++ Data/Documents.Data.Common/Models/Contracts/IAuthorEntity.cs
@@ -3,16 +3,8 @@
     using System.Collections.Generic;
     using ProcessingTools.Contracts;
 
-    public interface IAuthorEntity : IGuidIdentifiable, IModelWithUserInformation
+    public interface IAuthorEntity : IPerson, IGuidIdentifiable, IModelWithUserInformation
     {
-        string Surname { get; }
-
-        string GivenNames { get; }
-
-        string Prefix { get; }
-
-        string Suffix { get; }
-
         ICollection<IAffiliationEntity> Affiliations { get; }
 
         ICollection<IArticleEntity> Articles { get; }
---
+++ Infrastructure/Contracts/IPerson.cs
@@ -0,0 +1,13 @@
+﻿namespace ProcessingTools.Contracts
+{
+    public interface IPerson
+    {
+        string GivenNames { get; }
+
+        string Prefix { get; }
+
+        string Suffix { get; }
+
+        string Surname { get; }
+    }
+}
