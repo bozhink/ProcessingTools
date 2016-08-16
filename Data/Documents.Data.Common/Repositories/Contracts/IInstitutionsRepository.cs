fcd370c32106518e3ae084b712916a56e66cc985
bozhink <bozhink@gmail.com>
2016-08-16 15:41:16 +0300

    IInstitutionsRepository is added.

---
+++ Data/Documents.Data.Common/Repositories/Contracts/IInstitutionsRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace ProcessingTools.Documents.Data.Common.Repositories.Contracts
+{
+    using System.Threading.Tasks;
+
+    using Models.Contracts;
+    using ProcessingTools.Data.Common.Repositories.Contracts;
+
+    public interface IInstitutionsRepository : ICountableIterableCrudRepository<IInstitutionEntity>
+    {
+        Task<object> AddAddress(IAddressEntity address);
+
+        Task<object> RemoveAddress(object addressId);
+    }
+}
