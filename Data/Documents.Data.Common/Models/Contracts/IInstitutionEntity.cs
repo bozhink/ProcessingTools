34a10d6447208c299f21422e666b3064c507dd8e
bozhink <bozhink@gmail.com>
2016-08-16 13:57:51 +0300

    IInstitutionEntity is completed.

--- Data/Documents.Data.Common/Models/Contracts/IInstitutionEntity.cs
+++ Data/Documents.Data.Common/Models/Contracts/IInstitutionEntity.cs
@@ -1,6 +1,14 @@
 ﻿namespace ProcessingTools.Documents.Data.Common.Models.Contracts
 {
-    public interface IInstitutionEntity
+    using System.Collections.Generic;
+    using ProcessingTools.Contracts;
+
+    public interface IInstitutionEntity : IGuidIdentifiable, IModelWithUserInformation
     {
+        string Name { get; set; }
+
+        string AbbreviatedName { get; set; }
+
+        ICollection<IAddressEntity> Addresses { get; }
     }
 }
