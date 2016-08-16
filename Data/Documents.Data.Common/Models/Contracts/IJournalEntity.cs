95a9ca2c4deca58bc9ae7406674a761878290543
bozhink <bozhink@gmail.com>
2016-08-16 13:41:33 +0300

    IJournalEntity is completed.

--- Data/Documents.Data.Common/Models/Contracts/IJournalEntity.cs
+++ Data/Documents.Data.Common/Models/Contracts/IJournalEntity.cs
@@ -6,5 +6,20 @@
 
     public interface IJournalEntity : IGuidIdentifiable, IModelWithUserInformation
     {
+        string Name { get; }
+
+        string AbbreviatedName { get; }
+
+        string JournalId { get; }
+
+        string PrintIssn { get; }
+
+        string ElectronicIssn { get; }
+
+        Guid PublisherId { get; }
+
+        IPublisherEntity Publisher { get; }
+
+        ICollection<IArticleEntity> Articles { get; }
     }
 }
