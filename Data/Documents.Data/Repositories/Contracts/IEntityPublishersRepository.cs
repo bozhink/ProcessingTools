44bc95d2247fe4a2549c2917ea22899f1e6411e2
bozhink <bozhink@gmail.com>
2016-08-17 11:54:19 +0300

    IEntityPublishersRepository now inherits IEntityCountableIterableCrudRepository.

--- Data/Documents.Data/Repositories/Contracts/IEntityPublishersRepository.cs
+++ Data/Documents.Data/Repositories/Contracts/IEntityPublishersRepository.cs
@@ -1,8 +1,10 @@
 ﻿namespace ProcessingTools.Documents.Data.Repositories.Contracts
 {
+    using ProcessingTools.Data.Common.Entity.Repositories.Contracts;
+    using ProcessingTools.Documents.Data.Common.Models.Contracts;
     using ProcessingTools.Documents.Data.Common.Repositories.Contracts;
 
-    public interface IEntityPublishersRepository : IPublishersRepository
+    public interface IEntityPublishersRepository : IPublishersRepository, IEntityCountableIterableCrudRepository<IPublisherEntity>
     {
     }
 }
