b408abe0ad3009cec902340a564b448fcee9387d
bozhink <bozhink@gmail.com>
2016-08-17 11:35:44 +0300

    IEntityCrudRepository is added.

--- Data/Data.Common.Entity/Repositories/EntityCrudRepository.cs
+++ Data/Data.Common.Entity/Repositories/EntityCrudRepository.cs
@@ -4,11 +4,12 @@
     using System.Data.Entity;
     using System.Threading.Tasks;
 
+    using Contracts;
+
     using ProcessingTools.Common.Validation;
     using ProcessingTools.Data.Common.Entity.Contracts;
-    using ProcessingTools.Data.Common.Repositories.Contracts;
 
-    public class EntityCrudRepository<TContext, TEntity> : EntityRepository<TContext, TEntity>, ICrudRepository<TEntity>, IDisposable
+    public class EntityCrudRepository<TContext, TEntity> : EntityRepository<TContext, TEntity>, IEntityCrudRepository<TEntity>, IDisposable
         where TContext : DbContext
         where TEntity : class
     {
