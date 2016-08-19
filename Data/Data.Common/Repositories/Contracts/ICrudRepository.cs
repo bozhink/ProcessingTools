50fbcd14d5d46b7ee417d889e7d7214fb20b5b1b
Bozhin Karaivanov <bozhink@gmail.com>
2016-08-19 21:42:45 +0300

    IUpdatableRepository is added.

--- Data/Data.Common/Repositories/Contracts/ICrudRepository.cs
+++ Data/Data.Common/Repositories/Contracts/ICrudRepository.cs
@@ -3,20 +3,10 @@
     using System.Threading.Tasks;
     using Expressions.Contracts;
 
-    public interface ICrudRepository<TEntity> : IRepository<TEntity>
+    public interface ICrudRepository<TEntity> : IUpdatableRepository<TEntity>, IRepository<TEntity>
     {
-        Task<object> Add(TEntity entity);
-
-        Task<object> Delete(TEntity entity);
-
-        Task<object> Delete(object id);
 
         Task<TEntity> Get(object id);
-
-        Task<long> SaveChanges();
-
-        Task<object> Update(TEntity entity);
-
-        Task<object> Update(object id, IUpdateExpression<TEntity> update);
+        
     }
 }
---
+++ Data/Data.Common/Repositories/Contracts/IUpdatableRepository.cs
@@ -0,0 +1,20 @@
+﻿namespace ProcessingTools.Data.Common.Repositories.Contracts
+{
+    using System.Threading.Tasks;
+    using Expressions.Contracts;
+
+    public interface IUpdatableRepository<T> : IRepository<T>
+    {
+        Task<object> Add(T entity);
+
+        Task<object> Delete(T entity);
+
+        Task<object> Delete(object id);
+
+        Task<long> SaveChanges();
+
+        Task<object> Update(T entity);
+
+        Task<object> Update(object id, IUpdateExpression<T> update);
+    }
+}
