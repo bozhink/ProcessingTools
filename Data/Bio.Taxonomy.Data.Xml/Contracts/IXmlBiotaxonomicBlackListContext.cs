8e4bcd32cad56556a7cac4d6c3f7feeaec5c471f
bozhink <bozhink@gmail.com>
2016-08-22 11:25:05 +0300

    IXmlBiotaxonomicBlackListContext: unused usings are removed.

--- Data/Bio.Taxonomy.Data.Xml/Contracts/IXmlBiotaxonomicBlackListContext.cs
+++ Data/Bio.Taxonomy.Data.Xml/Contracts/IXmlBiotaxonomicBlackListContext.cs
@@ -1,7 +1,5 @@
 ﻿namespace ProcessingTools.Bio.Taxonomy.Data.Xml.Contracts
 {
-    using System.Linq;
-    using System.Threading.Tasks;
     using ProcessingTools.Bio.Taxonomy.Data.Common.Models.Contracts;
     using ProcessingTools.Data.Common.File.Contracts;
 
