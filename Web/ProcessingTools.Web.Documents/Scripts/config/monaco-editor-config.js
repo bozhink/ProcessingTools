198e70f8b36f67a4a2bddbfab6c9d7c9fc29f219
bozhink <bozhink@gmail.com>
2016-08-23 13:08:51 +0300

    monaco-editor-config.js: added resize event.

--- Web/ProcessingTools.Web.Documents/Scripts/config/monaco-editor-config.js
+++ Web/ProcessingTools.Web.Documents/Scripts/config/monaco-editor-config.js
@@ -1,22 +1,33 @@
 ﻿function MonacoEditor(window, document) {
     'use strict';
 
+    var require = window.require,
+        editor = window.editor || null;
+
     function initEditor(containerId, content) {
         content = '' + content;
 
-        window.require.config({
+        require.config({
             paths: {
                 'vs': '../../../node_modules/monaco-editor/min/vs'
             }
         });
 
-        window.require(['vs/editor/editor.main'], function () {
-            window.editor = window.monaco.editor.create(document.getElementById(containerId), {
+        require(['vs/editor/editor.main'], function () {
+            editor = window.monaco.editor.create(document.getElementById(containerId), {
                 value: content,
                 language: 'xml'
                 //, theme: 'vs-dark'
             });
+
+            window.editor = editor;
         });
+
+        window.addEventListener('resize', function () {
+            if (editor) {
+                editor.layout();
+            }
+        }, false);
     }
 
     return {
