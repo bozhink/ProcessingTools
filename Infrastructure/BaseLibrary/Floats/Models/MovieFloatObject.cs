d735bd417076677e631312921f31ad88ab3517cd
bozhink <bozhink@gmail.com>
2016-09-01 10:00:18 +0300

    MovieFloatObject is added to ProcessingTools.BaseLibrary.Floats.Models.

---
+++ Infrastructure/BaseLibrary/Floats/Models/MovieFloatObject.cs
@@ -0,0 +1,24 @@
+﻿namespace ProcessingTools.BaseLibrary.Floats.Models
+{
+    using Contracts;
+
+    /// <summary>
+    /// Movie.
+    /// </summary>
+    public class MovieFloatObject : IFloatObject
+    {
+        public string FloatObjectXPath => $"//fig[contains(string(label),'{this.FloatTypeNameInLabel}')]";
+
+        public FloatsReferenceType FloatReferenceType => FloatsReferenceType.Figure;
+
+        public string FloatTypeNameInLabel => "Movie";
+
+        public string MatchCitationPattern => @"(?:Movies?)";
+
+        public string InternalReferenceType => "movie";
+
+        public string ResultantReferenceType => "fig";
+
+        public string Description => "Movie";
+    }
+}
