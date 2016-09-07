e5718751276f64e3d46d5ee62b578284b2167f33
Bozhin Karaivanov <bozhink@gmail.com>
2016-09-07 23:29:32 +0300

    Elmah is added to ProcessingTools.Web.Documents.

--- Web/ProcessingTools.Web.Documents/App_Start/RouteConfig.cs
+++ Web/ProcessingTools.Web.Documents/App_Start/RouteConfig.cs
@@ -7,6 +7,7 @@
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
+            routes.IgnoreRoute("elmah.axd");
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
