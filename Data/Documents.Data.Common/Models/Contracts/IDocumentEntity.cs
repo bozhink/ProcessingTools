140c1270134b1d6bcca9618db970845c9c2d6ac1
bozhink <bozhink@gmail.com>
2016-09-08 16:40:39 +0300

    IFileEntity is added to ProcessingTools.Documents.Data.Common.Models.Contracts.

--- Data/Documents.Data.Common/Models/Contracts/IDocumentEntity.cs
+++ Data/Documents.Data.Common/Models/Contracts/IDocumentEntity.cs
@@ -3,20 +3,33 @@
     using System;
     using ProcessingTools.Contracts;
 
+    // TODO: separation with IFileEntity
     public interface IDocumentEntity : IGuidIdentifiable, IModelWithUserInformation
     {
         Guid ArticleId { get; }
 
+        // TODO: add
+        // Guid FileId { get; }
+
+        // TODO: add
+        // string Description { get; }
+
+        // TODO: remove
         long ContentLength { get; }
 
+        // TODO: remove
         string ContentType { get; }
 
+        // TODO: remove
         string FileExtension { get; }
 
+        // TODO: remove
         string FileName { get; }
 
+        // TODO: remove
         long OriginalContentLength { get; }
 
+        // TODO: remove
         string OriginalFileName { get; }
     }
 }
