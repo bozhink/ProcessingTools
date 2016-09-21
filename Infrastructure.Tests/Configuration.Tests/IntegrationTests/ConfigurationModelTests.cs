c671988d091531f2541c823b495dd05554b7344a
bozhink <bozhink@gmail.com>
2016-09-21 16:23:33 +0300

    ConfigurationModelTests: added test for the IConfigurationModel properties.

--- Infrastructure.Tests/Configuration.Tests/IntegrationTests/ConfigurationModelTests.cs
+++ Infrastructure.Tests/Configuration.Tests/IntegrationTests/ConfigurationModelTests.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.IO;
+    using System.Linq;
     using System.Reflection;
 
     using Newtonsoft.Json;
@@ -59,6 +60,40 @@
 
             Assert.AreEqual("RedisServerAddress", model.Settings[0].Key, "Invalid Key-value in the setting object.");
             Assert.AreEqual("redis://localhost:6379", model.Settings[0].Value, "Invalid Value-value in the setting object.");
+        }
+
+        [Test(Description = "ConfigurationModel: deserialization of a valid configuration JSON file as IConfigurationModel should work.", Author = "Bozhin Karaivanov", TestOf = typeof(ConfigurationModel))]
+        public void ConfigurationModel_DeserializationOfValidConfigurationJsonFileAsIConfigurationModel_ShouldWork()
+        {
+            var fileName = "configuration.json";
+            var directory = this.GetDirectoryOfTestAssembly;
+            var filePath = Path.Combine(directory, Path.Combine("Files", fileName));
+
+            Assert.IsTrue(File.Exists(filePath), "The configuration JSON file should be present.");
+
+            var json = File.ReadAllText(filePath);
+            Assert.IsFalse(string.IsNullOrWhiteSpace(json), "The configuration JSON file should not be empty.");
+
+            var obj = JsonConvert.DeserializeObject<ConfigurationModel>(json);
+            Assert.IsNotNull(obj, "De-serialized model object should not be null.");
+            Assert.IsInstanceOf<IConfigurationModel>(obj, $"De-serialized model object should be instance of {typeof(IConfigurationModel).FullName}.");
+
+            var model = obj as IConfigurationModel;
+
+            const int NumberOfFileEntries = 2;
+            const int NumberOfSettingEntries = 1;
+
+            Assert.AreEqual(NumberOfFileEntries, model.Files.Count(), $"Number of file entries should be {NumberOfFileEntries}");
+            Assert.AreEqual(NumberOfSettingEntries, model.Settings.Count(), $"Number of setting entries should be {NumberOfSettingEntries}");
+
+            Assert.AreEqual("NlmInitialFormatXslPath", model.Files.ElementAt(0).Key, "Invalid Key-value in the first file object.");
+            Assert.AreEqual("Xsl/format.nlm.initial.xsl", model.Files.ElementAt(0).Value, "Invalid Value-value in the first file object.");
+
+            Assert.AreEqual("SystemInitialFormatXslPath", model.Files.ElementAt(1).Key, "Invalid Key-value in the second file object.");
+            Assert.AreEqual("Xsl/format.system.initial.xsl", model.Files.ElementAt(1).Value, "Invalid Value-value in the second file object.");
+
+            Assert.AreEqual("RedisServerAddress", model.Settings.ElementAt(0).Key, "Invalid Key-value in the setting object.");
+            Assert.AreEqual("redis://localhost:6379", model.Settings.ElementAt(0).Value, "Invalid Value-value in the setting object.");
         }
     }
 }
