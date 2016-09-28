53af6128aad827a8f7fc5e678291b4e5a8997530
bozhink <bozhink@gmail.com>
2016-09-28 09:22:34 +0300

    Project ProcessingTools.Constants is added.

---
+++ Infrastructure/Constants/Configuration/ConnectionStringsKeys.cs
@@ -0,0 +1,15 @@
+﻿namespace ProcessingTools.Constants.Configuration
+{
+    public static class ConnectionStringsKeys
+    {
+        public const string BioDatabaseConnectionKey = "BioDatabaseConnection";
+        public const string BioEnvironmentsDatabaseConnectionKey = "BioEnvironmentsDatabaseConnection";
+        public const string BiorepositoriesMongoConnectionKey = "BiorepositoriesMongoConnection";
+        public const string BioTaxonomyDatabaseConnectionKey = "BioTaxonomyDatabaseConnection";
+        public const string BiotaxonomyMongoConnectionKey = "BiotaxonomyMongoConnection";
+        public const string DataDatabseConnectionKey = "DataDatabseConnection";
+        public const string GeoDatabseConnectionKey = "GeoDatabseConnection";
+        public const string MediatypesDatabaseConnectionKey = "MediatypesDatabaseConnection";
+        public const string RedisConnectionStringKey = "RedisConnectionString";
+    }
+}
