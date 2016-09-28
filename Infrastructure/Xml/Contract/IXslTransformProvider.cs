a9f51e634fd759d85f8447d5b13e6eb0915f5136
bozhink <bozhink@gmail.com>
2016-09-28 13:33:50 +0300

    IXslTransformer and IXslTransformProvider are added.

---
+++ Infrastructure/Xml/Contract/IXslTransformProvider.cs
@@ -0,0 +1,9 @@
+﻿namespace ProcessingTools.Xml.Contract
+{
+    using System.Xml.Xsl;
+
+    public interface IXslTransformProvider
+    {
+        XslCompiledTransform GetXslTransform();
+    }
+}
---
+++ Infrastructure/Xml/Contract/IXslTransformer.cs
@@ -0,0 +1,21 @@
+﻿namespace ProcessingTools.Xml.Contract
+{
+    using System.IO;
+    using System.Threading.Tasks;
+    using System.Xml;
+
+    public interface IXslTransformer
+    {
+        Stream TransformToStream(string xml);
+
+        Stream TransformToStream(XmlNode node);
+
+        Stream TransformToStream(XmlReader reader);
+
+        Task<string> Transform(string xml);
+
+        Task<string> Transform(XmlNode node);
+
+        Task<string> Transform(XmlReader reader);
+    }
+}
