ab49988b301141f83259fec6e06cc9e538a27e91
Bozhin Karaivanov <bozhink@gmail.com>
2016-10-05 23:38:12 +0300

    Twitter is added to Startup.Auth.cs

--- Web/ProcessingTools.Web.Documents/App_Start/Startup.Auth.cs
+++ Web/ProcessingTools.Web.Documents/App_Start/Startup.Auth.cs
@@ -1,11 +1,14 @@
 ﻿namespace ProcessingTools.Web.Documents
 {
     using System;
+    using System.Configuration;
     using Microsoft.AspNet.Identity;
     using Microsoft.AspNet.Identity.Owin;
     using Microsoft.Owin;
+    using Microsoft.Owin.Security;
     using Microsoft.Owin.Security.Cookies;
     using Microsoft.Owin.Security.Google;
+    using Microsoft.Owin.Security.Twitter;
     using Owin;
     using ProcessingTools.Api.Data;
     using ProcessingTools.Api.Data.Models;
@@ -52,8 +55,22 @@
             ////    clientSecret: "");
 
             ////app.UseTwitterAuthentication(
-            ////   consumerKey: "",
-            ////   consumerSecret: "");
+            ////   consumerKey: ConfigurationManager.AppSettings["TwitterConsumerKey"],
+            ////   consumerSecret: ConfigurationManager.AppSettings["TwitterConsumerSecret"]);
+            app.UseTwitterAuthentication(new TwitterAuthenticationOptions
+            {
+                ConsumerKey = ConfigurationManager.AppSettings["TwitterConsumerKey"],
+                ConsumerSecret = ConfigurationManager.AppSettings["TwitterConsumerSecret"],
+                BackchannelCertificateValidator = new CertificateSubjectKeyIdentifierValidator(new[]
+                {
+                    "A5EF0B11CEC04103A34A659048B21CE0572D7D47", // VeriSign Class 3 Secure Server CA - G2
+                    "0D445C165344C1827E1D20AB25F40163D8BE79A5", // VeriSign Class 3 Secure Server CA - G3
+                    "7FD365A7C2DDECBBF03009F34339FA02AF333133", // VeriSign Class 3 Public Primary Certification Authority - G5
+                    "39A55D933676616E73A761DFA16A7E59CDE66FAD", // Symantec Class 3 Secure Server CA - G4
+                    "5168FF90AF0207753CCCD9656462A212B859723B", //DigiCert SHA2 High Assurance Server C‎A 
+                    "B13EC36903F8BF4701D498261A0802EF63642BC3" //DigiCert High Assurance EV Root CA
+                })
+            });
 
             ////app.UseFacebookAuthentication(
             ////   appId: "",
