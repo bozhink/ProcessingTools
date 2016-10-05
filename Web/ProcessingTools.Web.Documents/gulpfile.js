d656fcd8109d276a2cece89b445c5b7121c3469b
bozhink <bozhink@gmail.com>
2016-10-05 12:51:04 +0300

    gulpfile.js is added. data-set-tests.js: new tests are added.

---
+++ Web/ProcessingTools.Web.Documents/gulpfile.js
@@ -0,0 +1,19 @@
+﻿/*
+This file in the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+var gulp = require('gulp'),
+    mocha = require('gulp-mocha');
+
+gulp.task('test', function () {
+    gulp.src('./static/code/tests/*.js')
+        .pipe(mocha())
+        .on('error', function () {
+            this.emit('end');
+        });
+});
+
+gulp.task('watch', function () {
+    gulp.watch('./static/code/**/*.js', ['test']);
+});
--- Web/ProcessingTools.Web.Documents/static/code/tests/data-set-tests.js
+++ Web/ProcessingTools.Web.Documents/static/code/tests/data-set-tests.js
@@ -43,6 +43,173 @@
                 expect(dataSet.removeAll).to.be.a('function');
             });
         });
-    });
 
+        describe('Add items to DataSet', function () {
+            it('Expect addition of single valid item to DataSet to add it correctly with valid id', function () {
+                var item = {
+                    a: 1,
+                    getHash: () => 1
+                };
+
+                var dataSet = new DataSet();
+
+                expect(function () {
+                    dataSet.add(item);
+                }).to.not.throw();
+
+                expect(dataSet.data.length).to.equal(1);
+
+                var addedItem = dataSet.data[0];
+                expect(addedItem.a).to.equal(item.a);
+
+                expect(addedItem).to.have.property('id');
+            });
+
+            it('Expect addition of two valid items with different hash to DataSet to add them correctly with valid id', function () {
+                var item1 = {
+                    a: 1,
+                    getHash: () => 1
+                }, item2 = {
+                    a: 2,
+                    getHash: () => 2
+                };
+
+                var dataSet = new DataSet();
+
+                expect(function () {
+                    dataSet.add(item1);
+                }).to.not.throw();
+
+                expect(function () {
+                    dataSet.add(item2);
+                }).to.not.throw();
+
+                expect(dataSet.data.length).to.equal(2);
+
+                var addedItem1 = dataSet.data[0];
+                expect(addedItem1.a).to.equal(item1.a);
+
+                var addedItem2 = dataSet.data[1];
+                expect(addedItem2.a).to.equal(item2.a);
+
+                expect(addedItem1).to.have.property('id');
+                expect(addedItem2).to.have.property('id');
+
+                expect(addedItem1.id).not.to.be.equal(addedItem2.id);
+            });
+
+            it('Expect addition of two valid items with equal hash to DataSet to add only the first one', function () {
+                var item1 = {
+                    a: 1,
+                    getHash: () => 1
+                }, item2 = {
+                    a: 2,
+                    getHash: () => 1
+                };
+
+                var dataSet = new DataSet();
+
+                expect(function () {
+                    dataSet.add(item1);
+                }).to.not.throw();
+
+                expect(function () {
+                    dataSet.add(item2);
+                }).to.not.throw();
+
+                expect(dataSet.data.length).to.equal(1);
+
+                var addedItem = dataSet.data[0];
+                expect(addedItem.a).to.equal(item1.a);
+
+                expect(addedItem).to.have.property('id');
+            });
+
+            it('Expect addition of single item with no getHash() to DataSet to throw and not to add it', function () {
+                var item = {
+                    a: 1
+                };
+
+                var dataSet = new DataSet();
+
+                expect(function () {
+                    dataSet.add(item);
+                }).to.throw('Item to add should have function "getHash"');
+
+                expect(dataSet.data.length).to.equal(0);
+            });
+
+            it('Expect addition of one valid item and one item with no getHash() to DataSet to throw and to add only the first one', function () {
+                var item1 = {
+                    a: 1,
+                    getHash: () => 1
+                }, item2 = {
+                    a: 2
+                };
+
+                var dataSet = new DataSet();
+
+                expect(function () {
+                    dataSet.add(item1);
+                }).to.not.throw();
+
+                expect(function () {
+                    dataSet.add(item2);
+                }).to.throw('Item to add should have function "getHash"');
+
+                expect(dataSet.data.length).to.equal(1);
+
+                var addedItem = dataSet.data[0];
+                expect(addedItem.a).to.equal(item1.a);
+
+                expect(addedItem).to.have.property('id');
+            });
+
+            it('Expect addition of one item with no getHash() and one valid item to DataSet to throw and to add only the second one', function () {
+                var item1 = {
+                    a: 1
+                }, item2 = {
+                    a: 2,
+                    getHash: () => 2
+                };
+
+                var dataSet = new DataSet();
+
+                expect(function () {
+                    dataSet.add(item1);
+                }).to.throw('Item to add should have function "getHash"');
+
+                expect(function () {
+                    dataSet.add(item2);
+                }).to.not.throw();
+
+                expect(dataSet.data.length).to.equal(1);
+
+                var addedItem = dataSet.data[0];
+                expect(addedItem.a).to.equal(item2.a);
+
+                expect(addedItem).to.have.property('id');
+            });
+
+            it('Expect addition of two items with no getHash() to DataSet to throw and not to add them', function () {
+                var item1 = {
+                    a: 1
+                }, item2 = {
+                    a: 2
+                };
+
+                var dataSet = new DataSet();
+
+                expect(function () {
+                    dataSet.add(item1);
+                }).to.throw('Item to add should have function "getHash"');
+
+                expect(function () {
+                    dataSet.add(item2);
+                }).to.throw('Item to add should have function "getHash"');
+
+                expect(dataSet.data.length).to.equal(0);
+            });
+        });
+    });
 }());
