fad49f5a3d3651dd33aa76e13e546ca5432123c7
Bozhin Karaivanov <bozhink@gmail.com>
2016-10-09 23:00:24 +0300

    SingleFileProcessor is split in two partial classes.

--- Desktop/Tagger/SingleFileProcessor.cs
+++ Desktop/Tagger/SingleFileProcessor.cs
@@ -3,13 +3,11 @@
     using System;
     using System.Collections.Concurrent;
     using System.Diagnostics;
-    using System.IO;
     using System.Linq;
     using System.Threading.Tasks;
     using System.Xml;
 
     using Contracts;
-    using Extensions;
     using Ninject;
     using ProcessingTools.BaseLibrary;
     using ProcessingTools.Common.Constants;
@@ -17,7 +15,7 @@
     using ProcessingTools.Contracts.Types;
     using ProcessingTools.DocumentProvider;
 
-    public class SingleFileProcessor
+    public partial class SingleFileProcessor
     {
         private ConcurrentQueue<Task> tasks;
 
@@ -236,84 +234,6 @@
                 this.logger?.Log(e, string.Empty);
                 throw;
             }
-        }
-
-        private Task InvokeProcessor(string message, Action action)
-        {
-            return Task.Run(() =>
-            {
-                Stopwatch timer = new Stopwatch();
-                timer.Start();
-                this.logger?.Log(message);
-
-                try
-                {
-                    action.Invoke();
-                }
-                catch (Exception e)
-                {
-                    this.logger?.Log(e, string.Empty);
-                }
-
-                this.PrintElapsedTime(timer);
-            });
-        }
-
-        private async Task InvokeProcessor<TController>(IKernel kernel)
-            where TController : ITaggerController
-        {
-            await this.InvokeProcessor<TController>(this.document.XmlDocument.DocumentElement, kernel);
-        }
-
-        private async Task InvokeProcessor<TController>(XmlNode context, IKernel kernel)
-            where TController : ITaggerController
-        {
-            var controller = kernel.Get<TController>();
-
-            string message = controller.GetDescriptionMessageForController();
-
-            await this.InvokeProcessor(
-                message,
-                () =>
-                {
-                    controller.Run(context, this.document.NamespaceManager, this.settings, this.logger).Wait();
-                });
-        }
-
-        private async Task InvokeProcessor(Type controllerType, IKernel kernel)
-        {
-            // Do not wait validation controllers to return.
-            var validationController = controllerType.GetInterfaces()?.FirstOrDefault(t => t == typeof(INotAwaitableController));
-            if (validationController != null)
-            {
-                // Validation controllers should not overwrite the content of this.document.XmlDocument,
-                // and here this content is copied in a new DOM object.
-                XmlDocument document = new XmlDocument
-                {
-                    PreserveWhitespace = true
-                };
-
-                document.LoadXml(this.document.Xml);
-                this.tasks.Enqueue(this.InvokeProcessor(controllerType, document.DocumentElement, kernel));
-            }
-            else
-            {
-                await this.InvokeProcessor(controllerType, this.document.XmlDocument.DocumentElement, kernel);
-            }
-        }
-
-        private async Task InvokeProcessor(Type controllerType, XmlNode context, IKernel kernel)
-        {
-            var controller = kernel.Get(controllerType) as ITaggerController;
-
-            string message = controller.GetDescriptionMessageForController();
-
-            await this.InvokeProcessor(
-                message,
-                () =>
-                {
-                    controller.Run(context, this.document.NamespaceManager, this.settings, this.logger).Wait();
-                });
         }
 
         private Task MainProcessing(XmlNode context, IKernel kernel)
---
+++ Desktop/Tagger/SingleFileProcessorHelpers.cs
@@ -0,0 +1,94 @@
+﻿namespace ProcessingTools.Tagger
+{
+    using System;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using System.Xml;
+
+    using Contracts;
+    using Extensions;
+
+    using Ninject;
+
+    public partial class SingleFileProcessor
+    {
+        private Task InvokeProcessor(string message, Action action)
+        {
+            return Task.Run(() =>
+            {
+                Stopwatch timer = new Stopwatch();
+                timer.Start();
+                this.logger?.Log(message);
+
+                try
+                {
+                    action.Invoke();
+                }
+                catch (Exception e)
+                {
+                    this.logger?.Log(e, string.Empty);
+                }
+
+                this.PrintElapsedTime(timer);
+            });
+        }
+
+        private async Task InvokeProcessor<TController>(IKernel kernel)
+            where TController : ITaggerController
+        {
+            await this.InvokeProcessor<TController>(this.document.XmlDocument.DocumentElement, kernel);
+        }
+
+        private async Task InvokeProcessor<TController>(XmlNode context, IKernel kernel)
+            where TController : ITaggerController
+        {
+            var controller = kernel.Get<TController>();
+
+            string message = controller.GetDescriptionMessageForController();
+
+            await this.InvokeProcessor(
+                message,
+                () =>
+                {
+                    controller.Run(context, this.document.NamespaceManager, this.settings, this.logger).Wait();
+                });
+        }
+
+        private async Task InvokeProcessor(Type controllerType, IKernel kernel)
+        {
+            // Do not wait validation controllers to return.
+            var validationController = controllerType.GetInterfaces()?.FirstOrDefault(t => t == typeof(INotAwaitableController));
+            if (validationController != null)
+            {
+                // Validation controllers should not overwrite the content of this.document.XmlDocument,
+                // and here this content is copied in a new DOM object.
+                XmlDocument document = new XmlDocument
+                {
+                    PreserveWhitespace = true
+                };
+
+                document.LoadXml(this.document.Xml);
+                this.tasks.Enqueue(this.InvokeProcessor(controllerType, document.DocumentElement, kernel));
+            }
+            else
+            {
+                await this.InvokeProcessor(controllerType, this.document.XmlDocument.DocumentElement, kernel);
+            }
+        }
+
+        private async Task InvokeProcessor(Type controllerType, XmlNode context, IKernel kernel)
+        {
+            var controller = kernel.Get(controllerType) as ITaggerController;
+
+            string message = controller.GetDescriptionMessageForController();
+
+            await this.InvokeProcessor(
+                message,
+                () =>
+                {
+                    controller.Run(context, this.document.NamespaceManager, this.settings, this.logger).Wait();
+                });
+        }
+    }
+}
