5911f1f23ab430cf4d2410d107338354289705b5
Bozhin Karaivanov <bozhink@gmail.com>
2016-10-09 18:43:03 +0300

    ProcessingTools.Layout.Processors.Transformers: FormatNlmToSystemTransformer and FormatSystemToNlmTransformer are added.

---
+++ Services/Processors/Layout.Processors/Transformers/FormatNlmToSystemTransformer.cs
@@ -0,0 +1,15 @@
+﻿namespace ProcessingTools.Layout.Processors.Transformers
+{
+    using Contracts.Transformers;
+
+    using ProcessingTools.Xml.Contracts.Providers;
+    using ProcessingTools.Xml.Transformers;
+
+    public class FormatNlmToSystemTransformer : XslTransformer<IFormatNlmToSystemXslTransformProvider>, IFormatNlmToSystemTransformer
+    {
+        public FormatNlmToSystemTransformer(IFormatNlmToSystemXslTransformProvider xslTransformProvider)
+            : base(xslTransformProvider)
+        {
+        }
+    }
+}
---
+++ Services/Processors/Layout.Processors/Transformers/FormatSystemToNlmTransformer.cs
@@ -0,0 +1,15 @@
+﻿namespace ProcessingTools.Layout.Processors.Transformers
+{
+    using Contracts.Transformers;
+
+    using ProcessingTools.Xml.Contracts.Providers;
+    using ProcessingTools.Xml.Transformers;
+
+    public class FormatSystemToNlmTransformer : XslTransformer<IFormatSystemToNlmXslTransformProvider>, IFormatSystemToNlmTransformer
+    {
+        public FormatSystemToNlmTransformer(IFormatSystemToNlmXslTransformProvider xslTransformProvider)
+            : base(xslTransformProvider)
+        {
+        }
+    }
+}
