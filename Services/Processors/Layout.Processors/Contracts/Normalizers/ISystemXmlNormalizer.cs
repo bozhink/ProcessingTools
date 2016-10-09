ae155a132972d1b6c765b39831d3fcde43524314
Bozhin Karaivanov <bozhink@gmail.com>
2016-10-09 18:32:24 +0300

    ProcessingTools.Layout.Processors.Contracts.Normalizers: INlmXmlNormalizer and ISystemXmlNormalizer are added.

---
+++ Services/Processors/Layout.Processors/Contracts/Normalizers/ISystemXmlNormalizer.cs
@@ -0,0 +1,11 @@
+﻿namespace ProcessingTools.Layout.Processors.Contracts.Normalizers
+{
+    using ProcessingTools.Contracts;
+
+    /// <summary>
+    /// Provides normalization to system xml.
+    /// </summary>
+    public interface ISystemXmlNormalizer : IXmlContextNormalizer
+    {
+    }
+}
