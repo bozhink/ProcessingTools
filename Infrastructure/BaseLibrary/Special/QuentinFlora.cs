3a755313387f7d4a2fb1beb6fac369cd5f27e511
Bozhin Karaivanov <bozhink@gmail.com>
2016-10-12 22:38:32 +0300

    QuentinFlora: regex-es are replaced with DOM operations.

--- Infrastructure/BaseLibrary/Special/QuentinFlora.cs
+++ Infrastructure/BaseLibrary/Special/QuentinFlora.cs
@@ -1,82 +1,85 @@
 ﻿namespace ProcessingTools.BaseLibrary.Special
 {
-    using System.Text.RegularExpressions;
+    using System;
+    using System.Linq;
+    using System.Xml;
+    using ProcessingTools.Contracts;
+    using ProcessingTools.Extensions;
 
-    using ProcessingTools.DocumentProvider;
-
-    public class QuentinFlora : TaxPubDocument
+    public class QuentinFlora
     {
-        public QuentinFlora(string xml)
-            : base(xml)
+        public void InitialFormat(IDocument document)
         {
-        }
-
-        public void InitialFormat()
-        {
-            string xml = this.Xml;
-
-            xml = Regex.Replace(xml, "(<notes>|<latinname>)\\s+", "$1");
-            xml = Regex.Replace(xml, "\\s+(</notes>|</latinname>)", "$1");
-
-            // Paste paragraphs in notes-tags
-            for (Match m = Regex.Match(xml, "<notes>[\\s\\S]*?</notes>"); m.Success; m = m.NextMatch())
+            if (document == null)
             {
-                string replace = Regex.Replace(m.Value, "(?<=<notes>)([\\s\\S]*?)(?=</notes>)", "<p>$1</p>");
-                replace = Regex.Replace(replace, "\n", "</p><p>");
-                xml = Regex.Replace(xml, Regex.Escape(m.Value), replace);
+                throw new ArgumentNullException(nameof(document));
             }
 
-            xml = Regex.Replace(xml, "(<p>)\\s+", "$1");
-            xml = Regex.Replace(xml, "\\s+(</p>)", "$1");
-
-            this.Xml = xml;
+            document.SelectNodes("//latinname").AsParallel().ForAll(this.ClearWhitespacingAction);
+            document.SelectNodes("//notes").AsParallel().ForAll(this.ClearWhitespacingAction);
+            document.SelectNodes("//notes").AsParallel().ForAll(n =>
+            {
+                n.InnerXml = $"<p>{n.InnerXml}</p>";
+            });
+            document.SelectNodes("//p").AsParallel().ForAll(this.ClearWhitespacingAction);
         }
 
-        public void Split1()
+        public void Split1(IDocument document)
         {
-            string xml = this.Xml;
-
-            for (Match m = Regex.Match(xml, "<sec type=\"family\">[\\s\\S]*?</sec>"); m.Success; m = m.NextMatch())
+            if (document == null)
             {
-                string replace = m.Value;
-                replace = Regex.Replace(replace, "(<genus_header>[\\s\\S]*?)(?=<genus_header>)", "<sec type=\"genus\">\n$1\n</sec>\n");
-                replace = Regex.Replace(replace, "(</sec>\\s*|</family_header>\\s*)(<genus_header>[\\s\\S]*?)(?=</sec>)", "$1<sec type=\"genus\">\n$2\n</sec>\n");
-
-                xml = Regex.Replace(xml, Regex.Escape(m.Value), replace);
+                throw new ArgumentNullException(nameof(document));
             }
 
-            this.Xml = xml;
+            document.SelectNodes("//sec[@type='family']")
+                .AsParallel()
+                .ForAll(n =>
+                {
+                    n.InnerXml = n.InnerXml
+                        .RegexReplace("(<genus_header>[\\s\\S]*?)(?=<genus_header>)", "<sec type=\"genus\">\n$1\n</sec>\n")
+                        .RegexReplace("(</sec>\\s*|</family_header>\\s*)(<genus_header>[\\s\\S]*?)(?=</sec>)", "$1<sec type=\"genus\">\n$2\n</sec>\n");
+                });
         }
 
-        public void Split2()
+        public void Split2(IDocument document)
         {
-            this.Xml = Regex.Replace(this.Xml, "(<family_header>[\\s\\S]*?)(?=<family_header>)", "<sec type=\"family\">\n$1\n</sec>\n");
-            this.Xml = Regex.Replace(this.Xml, "(</sec>\\s*)(<family_header>[\\s\\S]*?)(?=</sec>)", "$1<sec type=\"family\">\n$2\n</sec>\n");
-        }
-
-        public void FinalFormat()
-        {
-            string xml = this.Xml;
-
-            xml = Regex.Replace(xml, "(<p>|<title>)\\s+", "$1");
-            xml = Regex.Replace(xml, "\\s+(</p>|</title>)", "$1");
-
-            for (Match m = Regex.Match(xml, "(<p>|<title>)[\\s\\S]*?(</p>|</title>)"); m.Success; m = m.NextMatch())
+            if (document == null)
             {
-                string replace = Regex.Replace(m.Value, "\\s+", " ");
-                xml = Regex.Replace(xml, Regex.Escape(m.Value), replace);
+                throw new ArgumentNullException(nameof(document));
             }
 
-            xml = Regex.Replace(xml, "\\s*(</?content>|</?ballon_content>)\\s*", "$1");
-            xml = Regex.Replace(xml, "(</div>)\\s+(<div)", "$1$2");
-            xml = Regex.Replace(xml, "(<tp:taxon-name>)\\s+", "$1");
-            xml = Regex.Replace(xml, "\\s+(</tp:taxon-name>)", "$1");
-            xml = Regex.Replace(xml, "\\s+(<br/>)\\s+", "$1");
-            xml = Regex.Replace(xml, "(</span>)\\s*<br/>\\s*(</ballon_content>)", "$1$2");
+            document.Xml = document.Xml
+                .RegexReplace(@"(<family_header>[\s\S]*?)(?=<family_header>)", "<sec type=\"family\">\n$1\n</sec>\n")
+                .RegexReplace(@"(</sec>\s*)(<family_header>[\s\S]*?)(?=</sec>)", "$1<sec type=\"family\">\n$2\n</sec>\n");
+        }
+
+        public void FinalFormat(IDocument document)
+        {
+            if (document == null)
+            {
+                throw new ArgumentNullException(nameof(document));
+            }
+
+            document.SelectNodes("//tp:taxon-name").AsParallel().ForAll(this.ClearWhitespacingAction);
+            document.SelectNodes("//p").AsParallel().ForAll(this.ClearWhitespacingAction);
+            document.SelectNodes("//title").AsParallel().ForAll(this.ClearWhitespacingAction);
+
+            string xml = document.Xml
+                .RegexReplace("\\s*(</?content>|</?ballon_content>)\\s*", "$1")
+                .RegexReplace("(</div>)\\s+(<div)", "$1$2")
+                .RegexReplace("\\s+(<br/>)\\s+", "$1")
+                .RegexReplace("(</span>)\\s*<br/>\\s*(</ballon_content>)", "$1$2");
 
             ////xml = Regex.Replace(xml, "(</ballon_wrapper>)\\s*(<ballon_wrapper)", "$1<span class=\"space\"/> $2");
 
-            this.Xml = xml;
+            document.Xml = xml;
         }
+
+        private Action<XmlNode> ClearWhitespacingAction => n =>
+        {
+            n.InnerXml = n.InnerXml
+                .RegexReplace(@"\s+", " ")
+                .Trim();
+        };
     }
 }
