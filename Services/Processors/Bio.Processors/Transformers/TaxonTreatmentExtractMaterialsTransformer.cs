f707daa0f51cbea314407d581013bb6fdeec7b45
Bozhin Karaivanov <bozhink@gmail.com>
2016-10-13 22:08:45 +0300

    ProcessingTools.Bio.Processors.Transformers.TaxonTreatmentExtractMaterialsTransformer is added.

---
+++ Services/Processors/Bio.Processors/Transformers/TaxonTreatmentExtractMaterialsTransformer.cs
@@ -0,0 +1,15 @@
+﻿namespace ProcessingTools.Bio.Processors.Transformers
+{
+    using Contracts.Transformers;
+
+    using ProcessingTools.Xml.Contracts.Providers;
+    using ProcessingTools.Xml.Transformers;
+
+    public class TaxonTreatmentExtractMaterialsTransformer : XslTransformer<ITaxonTreatmentExtractMaterialsXslTransformProvider>, ITaxonTreatmentExtractMaterialsTransformer
+    {
+        public TaxonTreatmentExtractMaterialsTransformer(ITaxonTreatmentExtractMaterialsXslTransformProvider xslTransformProvider)
+            : base(xslTransformProvider)
+        {
+        }
+    }
+}
