d0c815d6db88eb3897c07e406a8346b699dd9941
Bozhin Karaivanov <bozhink@gmail.com>
2016-10-23 22:09:51 +0300

    ProcessingTools.Xml.Providers.GetAbbreviationsXQueryTransformProvider is added.

---
+++ Infrastructure/Xml/Providers/GetAbbreviationsXQueryTransformProvider.cs
@@ -0,0 +1,20 @@
+﻿namespace ProcessingTools.Xml.Providers
+{
+    using System.Configuration;
+
+    using Abstracts;
+    using Contracts.Cache;
+    using Contracts.Providers;
+
+    using ProcessingTools.Constants.Configuration;
+
+    public class GetAbbreviationsXQueryTransformProvider : AbstractXQueryTransformProvider, IGetAbbreviationsXQueryTransformProvider
+    {
+        public GetAbbreviationsXQueryTransformProvider(IXQueryTransformCache cache)
+            : base(cache)
+        {
+        }
+
+        protected override string XQueryFileName => ConfigurationManager.AppSettings[AppSettingsKeys.AbbreviationsXQueryFilePathKey];
+    }
+}
