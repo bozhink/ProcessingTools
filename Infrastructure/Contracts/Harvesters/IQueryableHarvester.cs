bf8686d8bacd58861f92f7b80ef2cf83946cbc5c
Bozhin Karaivanov <bozhink@gmail.com>
2016-10-23 14:34:19 +0300

    IQueryableHarvester is added.

---
+++ Infrastructure/Contracts/Harvesters/IQueryableHarvester.cs
@@ -0,0 +1,8 @@
+﻿namespace ProcessingTools.Contracts.Harvesters
+{
+    using System.Linq;
+
+    public interface IQueryableHarvester<TContext, TResult> : IHarvester<TContext, IQueryable<TResult>>
+    {
+    }
+}
