de1a2b101a7adcf620f4d8e680e990cc95661cf8
Bozhin Karaivanov <bozhink@gmail.com>
2016-10-23 14:39:00 +0300

    IGenericQueryableXmlHarvester is added.

---
+++ Infrastructure/Contracts/Harvesters/IGenericQueryableXmlHarvester.cs
@@ -0,0 +1,9 @@
+﻿namespace ProcessingTools.Contracts.Harvesters
+{
+    using System.Linq;
+    using System.Xml;
+
+    public interface IGenericQueryableXmlHarvester<T> : IQueryableHarvester<XmlNode, T>, IGenericXmlHarvester<IQueryable<T>>
+    {
+    }
+}
