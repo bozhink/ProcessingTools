592734e2d50d7ce11553d33fcabb960c5b87c10a
Bozhin Karaivanov <bozhink@gmail.com>
2016-10-23 22:13:47 +0300

    ProcessingTools.Harvesters.Transformers.GetAbbreviationsTransformer is added.

---
+++ Services/Harvesters/Harvesters/Transformers/GetAbbreviationsTransformer.cs
@@ -0,0 +1,15 @@
+﻿namespace ProcessingTools.Harvesters.Transformers
+{
+    using Contracts.Transformers;
+
+    using ProcessingTools.Xml.Contracts.Providers;
+    using ProcessingTools.Xml.Transformers;
+
+    public class GetAbbreviationsTransformer : XQueryTransformer<IGetAbbreviationsXQueryTransformProvider>, IGetAbbreviationsTransformer
+    {
+        public GetAbbreviationsTransformer(IGetAbbreviationsXQueryTransformProvider xqueryTransformProvider)
+            : base(xqueryTransformProvider)
+        {
+        }
+    }
+}
