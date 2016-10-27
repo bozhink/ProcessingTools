9fa41364d405bfbffb5914e0b09b427869d3f63a
bozhink <bozhink@gmail.com>
2016-10-27 16:06:50 +0300

    ProcessingTools.Tagger.Controllers.Generics.GenericDocumentTaggerController is added.

---
+++ Desktop/Tagger/Controllers/Generics/GenericDocumentTaggerController.cs
@@ -0,0 +1,38 @@
+﻿namespace ProcessingTools.Tagger.Controllers.Generics
+{
+    using System;
+    using System.Threading.Tasks;
+    using Contracts;
+    using ProcessingTools.Contracts;
+
+    public class GenericDocumentTaggerController<TTagger> : ITaggerController
+        where TTagger : IDocumentTagger
+    {
+        private readonly TTagger tagger;
+
+        public GenericDocumentTaggerController(TTagger tagger)
+        {
+            if (tagger == null)
+            {
+                throw new ArgumentNullException(nameof(tagger));
+            }
+
+            this.tagger = tagger;
+        }
+
+        public async Task<object> Run(IDocument document, IProgramSettings settings)
+        {
+            if (document == null)
+            {
+                throw new ArgumentNullException(nameof(document));
+            }
+
+            if (settings == null)
+            {
+                throw new ArgumentNullException(nameof(settings));
+            }
+
+            return await this.tagger.Tag(document);
+        }
+    }
+}
