7cefba3f1ffdbfcd64af504beab14b9943478ed8
Bozhin Karaivanov <bozhink@gmail.com>
2016-10-28 21:37:14 +0300

    ITextContentHarvester is plugged to GenericStringMinerTagger

--- Services/Processors/Processors/Generics/GenericStringMinerTagger.cs
+++ Services/Processors/Processors/Generics/GenericStringMinerTagger.cs
@@ -8,22 +8,32 @@
     using ProcessingTools.Constants.Schema;
     using ProcessingTools.Contracts;
     using ProcessingTools.Data.Miners.Common.Contracts;
+    using ProcessingTools.Harvesters.Contracts.Content;
     using ProcessingTools.Layout.Processors.Contracts.Taggers;
-    using ProcessingTools.Xml.Extensions;
 
     public class GenericStringMinerTagger<TMiner, TTagModelProvider> : IDocumentTagger
         where TMiner : IStringDataMiner
         where TTagModelProvider : IXmlTagModelProvider
     {
         private readonly TMiner miner;
+        private readonly ITextContentHarvester contentHarvester;
         private readonly IStringTagger tagger;
         private readonly TTagModelProvider tagModelProvider;
 
-        public GenericStringMinerTagger(TMiner miner, IStringTagger tagger, TTagModelProvider tagModelProvider)
+        public GenericStringMinerTagger(
+            TMiner miner,
+            ITextContentHarvester contentHarvester,
+            IStringTagger tagger,
+            TTagModelProvider tagModelProvider)
         {
             if (miner == null)
             {
                 throw new ArgumentNullException(nameof(miner));
+            }
+
+            if (contentHarvester == null)
+            {
+                throw new ArgumentNullException(nameof(contentHarvester));
             }
 
             if (tagger == null)
@@ -37,6 +47,7 @@
             }
 
             this.miner = miner;
+            this.contentHarvester = contentHarvester;
             this.tagger = tagger;
             this.tagModelProvider = tagModelProvider;
         }
@@ -50,7 +61,7 @@
 
             var tagModel = this.tagModelProvider.TagModel(document.XmlDocument);
 
-            var textContent = document.XmlDocument.GetTextContent();
+            var textContent = await this.contentHarvester.Harvest(document.XmlDocument.DocumentElement);
             var data = await this.miner.Mine(textContent);
 
             await this.tagger.Tag(document, data, tagModel, XPathConstants.SelectContentNodesXPath);
