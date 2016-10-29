e8cf3ddc44582d05bf8360e42390f8efa431cdae
Bozhin Karaivanov <bozhink@gmail.com>
2016-10-29 18:20:48 +0300

    ProcessingTools.Tagger.Controllers.TagReferencesController - set this.tagger.ReferencesGetReferencesXmlPath

--- Desktop/Tagger/Controllers/TagReferencesController.cs
+++ Desktop/Tagger/Controllers/TagReferencesController.cs
@@ -1,17 +1,43 @@
 ﻿namespace ProcessingTools.Tagger.Controllers
 {
+    using System;
+    using System.Threading.Tasks;
+
     using Contracts;
-    using Generics;
 
     using ProcessingTools.Attributes;
+    using ProcessingTools.Contracts;
     using ProcessingTools.Processors.Contracts.References;
 
     [Description("Tag references.")]
-    public class TagReferencesController : GenericXmlContextTaggerController<IReferencesTagger>, ITagReferencesController
+    public class TagReferencesController : ITagReferencesController
     {
+        private readonly IReferencesTagger tagger;
+
         public TagReferencesController(IReferencesTagger tagger)
-            : base(tagger)
         {
+            if (tagger == null)
+            {
+                throw new ArgumentNullException(nameof(tagger));
+            }
+
+            this.tagger = tagger;
+        }
+
+        public Task<object> Run(IDocument document, IProgramSettings settings)
+        {
+            if (document == null)
+            {
+                throw new ArgumentNullException(nameof(document));
+            }
+
+            if (settings == null)
+            {
+                throw new ArgumentNullException(nameof(settings));
+            }
+
+            this.tagger.ReferencesGetReferencesXmlPath = settings.ReferencesGetReferencesXmlPath;
+            return this.tagger.Tag(document.XmlDocument.DocumentElement);
         }
     }
 }
