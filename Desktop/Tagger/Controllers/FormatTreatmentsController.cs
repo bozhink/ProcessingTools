50dc716e45803d94284edc496f76fcdbdfb48eff
Bozhin Karaivanov <bozhink@gmail.com>
2016-10-29 15:46:57 +0300

    ProcessingTools.Tagger.Controllers.FormatTreatmentsController inherits GenericDocumentFormatterController

--- Desktop/Tagger/Controllers/FormatTreatmentsController.cs
+++ Desktop/Tagger/Controllers/FormatTreatmentsController.cs
@@ -1,42 +1,17 @@
 ﻿namespace ProcessingTools.Tagger.Controllers
 {
-    using System;
-    using System.Threading.Tasks;
-
     using Contracts;
+    using Generics;
 
     using ProcessingTools.Attributes;
     using ProcessingTools.Bio.Taxonomy.Processors.Contracts.Formatters;
-    using ProcessingTools.Contracts;
 
     [Description("Format treatments.")]
-    public class FormatTreatmentsController : IFormatTreatmentsController
+    public class FormatTreatmentsController : GenericDocumentFormatterController<ITreatmentFormatter>, IFormatTreatmentsController
     {
-        private readonly ITreatmentFormatter formatter;
-
         public FormatTreatmentsController(ITreatmentFormatter formatter)
+            : base(formatter)
         {
-            if (formatter == null)
-            {
-                throw new ArgumentNullException(nameof(formatter));
-            }
-
-            this.formatter = formatter;
-        }
-
-        public async Task<object> Run(IDocument document, IProgramSettings settings)
-        {
-            if (document == null)
-            {
-                throw new ArgumentNullException(nameof(document));
-            }
-
-            if (settings == null)
-            {
-                throw new ArgumentNullException(nameof(settings));
-            }
-
-            return await this.formatter.Format(document);
         }
     }
 }
