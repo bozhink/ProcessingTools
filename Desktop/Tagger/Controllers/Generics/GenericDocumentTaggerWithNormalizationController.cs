26ee96b4c47c99dda8397483608ea12df819bc3a
Bozhin Karaivanov <bozhink@gmail.com>
2016-10-29 16:56:32 +0300

    TagLowerTaxaController and TagHigherTaxaController: parent class is extracted.

---
+++ Desktop/Tagger/Controllers/Generics/GenericDocumentTaggerWithNormalizationController.cs
@@ -0,0 +1,49 @@
+﻿namespace ProcessingTools.Tagger.Controllers.Generics
+{
+    using System;
+    using System.Threading.Tasks;
+    using Contracts;
+    using ProcessingTools.Contracts;
+    using ProcessingTools.Layout.Processors.Contracts.Normalizers;
+
+    public class GenericDocumentTaggerWithNormalizationController<TTagger> : ITaggerController
+        where TTagger : IDocumentTagger
+    {
+        private readonly TTagger tagger;
+        private readonly IDocumentNormalizer documentNormalizer;
+
+        public GenericDocumentTaggerWithNormalizationController(TTagger tagger, IDocumentNormalizer documentNormalizer)
+        {
+            if (tagger == null)
+            {
+                throw new ArgumentNullException(nameof(tagger));
+            }
+
+            if (documentNormalizer == null)
+            {
+                throw new ArgumentNullException(nameof(documentNormalizer));
+            }
+
+            this.tagger = tagger;
+            this.documentNormalizer = documentNormalizer;
+        }
+
+        public async Task<object> Run(IDocument document, IProgramSettings settings)
+        {
+            if (document == null)
+            {
+                throw new ArgumentNullException(nameof(document));
+            }
+
+            if (settings == null)
+            {
+                throw new ArgumentNullException(nameof(settings));
+            }
+
+            var result = await this.tagger.Tag(document);
+            await this.documentNormalizer.NormalizeToSystem(document);
+
+            return result;
+        }
+    }
+}
--- Desktop/Tagger/Controllers/TagLowerTaxaController.cs
+++ Desktop/Tagger/Controllers/TagLowerTaxaController.cs
@@ -1,53 +1,18 @@
 ﻿namespace ProcessingTools.Tagger.Controllers
 {
-    using System;
-    using System.Threading.Tasks;
-
     using Contracts;
+    using Generics;
 
     using ProcessingTools.Attributes;
     using ProcessingTools.Bio.Taxonomy.Processors.Contracts.Taggers;
-    using ProcessingTools.Contracts;
     using ProcessingTools.Layout.Processors.Contracts.Normalizers;
 
     [Description("Tag lower taxa.")]
-    public class TagLowerTaxaController : ITagLowerTaxaController
+    public class TagLowerTaxaController : GenericDocumentTaggerWithNormalizationController<ILowerTaxaTagger>, ITagLowerTaxaController
     {
-        private readonly ILowerTaxaTagger tagger;
-        private readonly IDocumentNormalizer documentNormalizer;
-
         public TagLowerTaxaController(ILowerTaxaTagger tagger, IDocumentNormalizer documentNormalizer)
+            : base(tagger, documentNormalizer)
         {
-            if (tagger == null)
-            {
-                throw new ArgumentNullException(nameof(tagger));
-            }
-
-            if (documentNormalizer == null)
-            {
-                throw new ArgumentNullException(nameof(documentNormalizer));
-            }
-
-            this.tagger = tagger;
-            this.documentNormalizer = documentNormalizer;
-        }
-
-        public async Task<object> Run(IDocument document, IProgramSettings settings)
-        {
-            if (document == null)
-            {
-                throw new ArgumentNullException(nameof(document));
-            }
-
-            if (settings == null)
-            {
-                throw new ArgumentNullException(nameof(settings));
-            }
-
-            var result = await this.tagger.Tag(document);
-            await this.documentNormalizer.NormalizeToSystem(document);
-
-            return result;
         }
     }
 }
