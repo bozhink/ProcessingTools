93c069cbc321eb1ff3e3a8f9a161dbc745981f0e
Bozhin Karaivanov <bozhink@gmail.com>
2016-10-29 15:45:19 +0300

    ProcessingTools.Tagger.Controllers.Generics.GenericDocumentFormatterController is added.

---
+++ Desktop/Tagger/Controllers/Generics/GenericDocumentFormatterController.cs
@@ -0,0 +1,38 @@
+﻿namespace ProcessingTools.Tagger.Controllers.Generics
+{
+    using System;
+    using System.Threading.Tasks;
+    using Contracts;
+    using ProcessingTools.Contracts;
+
+    public class GenericDocumentFormatterController<TFormatter> : ITaggerController
+        where TFormatter : IDocumentFormatter
+    {
+        private readonly TFormatter formatter;
+
+        public GenericDocumentFormatterController(TFormatter formatter)
+        {
+            if (formatter == null)
+            {
+                throw new ArgumentNullException(nameof(formatter));
+            }
+
+            this.formatter = formatter;
+        }
+
+        public async Task<object> Run(IDocument document, IProgramSettings settings)
+        {
+            if (document == null)
+            {
+                throw new ArgumentNullException(nameof(document));
+            }
+
+            if (settings == null)
+            {
+                throw new ArgumentNullException(nameof(settings));
+            }
+
+            return await this.formatter.Format(document);
+        }
+    }
+}
