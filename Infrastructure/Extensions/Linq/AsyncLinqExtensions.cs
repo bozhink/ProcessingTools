eda1bd3263edb97264d023c6f86b3acdffb519a7
Bozhin Karaivanov <bozhink@gmail.com>
2016-10-30 17:29:31 +0200

    ProcessingTools.Extensions.Linq.AsyncLinqExtensions CountAsync is added.

--- Infrastructure/Extensions/Linq/AsyncLinqExtensions.cs
+++ Infrastructure/Extensions/Linq/AsyncLinqExtensions.cs
@@ -1,41 +1,83 @@
 ﻿namespace ProcessingTools.Extensions.Linq
 {
+    using System;
     using System.Collections.Generic;
     using System.Linq;
+    using System.Linq.Expressions;
     using System.Threading.Tasks;
 
     public static class AsyncLinqExtensions
     {
-        public static Task<T> FirstAsync<T>(this IEnumerable<T> items) => Task.Run(() => items.First());
+        public static Task<int> CountAsync<TSource>(this IEnumerable<TSource> source) => Task.Run(() => source.Count());
 
-        public static Task<T> FirstAsync<T>(this IQueryable<T> items) => Task.Run(() => items.First());
+        public static Task<int> CountAsync<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate) => Task.Run(() => source.Count(predicate));
 
-        public static Task<T> FirstOrDefaultAsync<T>(this IEnumerable<T> items) => Task.Run(() => items.FirstOrDefault());
+        public static Task<int> CountAsync<TSource>(this IQueryable<TSource> source) => Task.Run(() => source.Count());
 
-        public static Task<T> FirstOrDefaultAsync<T>(this IQueryable<T> items) => Task.Run(() => items.FirstOrDefault());
+        public static Task<int> CountAsync<TSource>(this IQueryable<TSource> source, Expression<Func<TSource, bool>> predicate) => Task.Run(() => source.Count(predicate));
 
-        public static Task<T> LastAsync<T>(this IEnumerable<T> items) => Task.Run(() => items.Last());
+        public static Task<TSource> FirstAsync<TSource>(this IEnumerable<TSource> source) => Task.Run(() => source.First());
 
-        public static Task<T> LastAsync<T>(this IQueryable<T> items) => Task.Run(() => items.Last());
+        public static Task<TSource> FirstAsync<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate) => Task.Run(() => source.First(predicate));
 
-        public static Task<T> LastOrDefaultAsync<T>(this IEnumerable<T> items) => Task.Run(() => items.LastOrDefault());
+        public static Task<TSource> FirstAsync<TSource>(this IQueryable<TSource> source) => Task.Run(() => source.First());
 
-        public static Task<T> LastOrDefaultAsync<T>(this IQueryable<T> items) => Task.Run(() => items.LastOrDefault());
+        public static Task<TSource> FirstAsync<TSource>(this IQueryable<TSource> source, Expression<Func<TSource, bool>> predicate) => Task.Run(() => source.First(predicate));
 
-        public static Task<T> SingleAsync<T>(this IEnumerable<T> items) => Task.Run(() => items.Single());
+        public static Task<TSource> FirstOrDefaultAsync<TSource>(this IEnumerable<TSource> source) => Task.Run(() => source.FirstOrDefault());
 
-        public static Task<T> SingleAsync<T>(this IQueryable<T> items) => Task.Run(() => items.Single());
+        public static Task<TSource> FirstOrDefaultAsync<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate) => Task.Run(() => source.FirstOrDefault(predicate));
 
-        public static Task<T> SingleOrDefaultAsync<T>(this IEnumerable<T> items) => Task.Run(() => items.SingleOrDefault());
+        public static Task<TSource> FirstOrDefaultAsync<TSource>(this IQueryable<TSource> source) => Task.Run(() => source.FirstOrDefault());
 
-        public static Task<T> SingleOrDefaultAsync<T>(this IQueryable<T> items) => Task.Run(() => items.SingleOrDefault());
+        public static Task<TSource> FirstOrDefaultAsync<TSource>(this IQueryable<TSource> source, Expression<Func<TSource, bool>> predicate) => Task.Run(() => source.FirstOrDefault(predicate));
 
-        public static Task<T[]> ToArrayAsync<T>(this IEnumerable<T> items) => Task.Run(() => items.ToArray());
+        public static Task<TSource> LastAsync<TSource>(this IEnumerable<TSource> source) => Task.Run(() => source.Last());
 
-        public static Task<T[]> ToArrayAsync<T>(this IQueryable<T> items) => Task.Run(() => items.ToArray());
+        public static Task<TSource> LastAsync<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate) => Task.Run(() => source.Last(predicate));
 
-        public static Task<List<T>> ToListAsync<T>(this IEnumerable<T> items) => Task.Run(() => items.ToList());
+        public static Task<TSource> LastAsync<TSource>(this IQueryable<TSource> source) => Task.Run(() => source.Last());
 
-        public static Task<List<T>> ToListAsync<T>(this IQueryable<T> items) => Task.Run(() => items.ToList());
+        public static Task<TSource> LastAsync<TSource>(this IQueryable<TSource> source, Expression<Func<TSource, bool>> predicate) => Task.Run(() => source.Last(predicate));
+
+        public static Task<TSource> LastOrDefaultAsync<TSource>(this IEnumerable<TSource> source) => Task.Run(() => source.LastOrDefault());
+
+        public static Task<TSource> LastOrDefaultAsync<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate) => Task.Run(() => source.LastOrDefault(predicate));
+
+        public static Task<TSource> LastOrDefaultAsync<TSource>(this IQueryable<TSource> source) => Task.Run(() => source.LastOrDefault());
+
+        public static Task<TSource> LastOrDefaultAsync<TSource>(this IQueryable<TSource> source, Expression<Func<TSource, bool>> predicate) => Task.Run(() => source.LastOrDefault(predicate));
+
+        public static Task<long> LongCountAsync<TSource>(this IEnumerable<TSource> source) => Task.Run(() => source.LongCount());
+
+        public static Task<long> LongCountAsync<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate) => Task.Run(() => source.LongCount(predicate));
+
+        public static Task<long> LongCountAsync<TSource>(this IQueryable<TSource> source) => Task.Run(() => source.LongCount());
+
+        public static Task<long> LongCountAsync<TSource>(this IQueryable<TSource> source, Expression<Func<TSource, bool>> predicate) => Task.Run(() => source.LongCount(predicate));
+
+        public static Task<TSource> SingleAsync<TSource>(this IEnumerable<TSource> source) => Task.Run(() => source.Single());
+
+        public static Task<TSource> SingleAsync<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate) => Task.Run(() => source.Single(predicate));
+
+        public static Task<TSource> SingleAsync<TSource>(this IQueryable<TSource> source) => Task.Run(() => source.Single());
+
+        public static Task<TSource> SingleAsync<TSource>(this IQueryable<TSource> source, Expression<Func<TSource, bool>> predicate) => Task.Run(() => source.Single(predicate));
+
+        public static Task<TSource> SingleOrDefaultAsync<TSource>(this IEnumerable<TSource> source) => Task.Run(() => source.SingleOrDefault());
+
+        public static Task<TSource> SingleOrDefaultAsync<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate) => Task.Run(() => source.SingleOrDefault(predicate));
+
+        public static Task<TSource> SingleOrDefaultAsync<TSource>(this IQueryable<TSource> source) => Task.Run(() => source.SingleOrDefault());
+
+        public static Task<TSource> SingleOrDefaultAsync<TSource>(this IQueryable<TSource> source, Expression<Func<TSource, bool>> predicate) => Task.Run(() => source.SingleOrDefault(predicate));
+
+        public static Task<TSource[]> ToArrayAsync<TSource>(this IEnumerable<TSource> source) => Task.Run(() => source.ToArray());
+
+        public static Task<TSource[]> ToArrayAsync<TSource>(this IQueryable<TSource> source) => Task.Run(() => source.ToArray());
+
+        public static Task<List<TSource>> ToListAsync<TSource>(this IEnumerable<TSource> source) => Task.Run(() => source.ToList());
+
+        public static Task<List<TSource>> ToListAsync<TSource>(this IQueryable<TSource> source) => Task.Run(() => source.ToList());
     }
 }
