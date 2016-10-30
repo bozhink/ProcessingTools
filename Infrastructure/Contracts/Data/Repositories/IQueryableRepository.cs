a1992def8cd8ac8f140915c6bead538b312f45bc
Bozhin Karaivanov <bozhink@gmail.com>
2016-10-30 17:33:07 +0200

    ProcessingTools.Contracts.Data.Repositories.IQueryableRepository is added.

---
+++ Infrastructure/Contracts/Data/Repositories/IQueryableRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace ProcessingTools.Contracts.Data.Repositories
+{
+    using System.Linq;
+
+    public interface IQueryableRepository<T> : IRepository<T>
+    {
+        IQueryable<T> Query { get; }
+    }
+}
