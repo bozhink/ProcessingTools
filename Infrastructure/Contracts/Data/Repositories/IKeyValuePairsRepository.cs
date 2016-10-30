648fae754898681e2d5a45945bbd356bec1ba7ed
Bozhin Karaivanov <bozhink@gmail.com>
2016-10-30 20:31:28 +0200

    ProcessingTools.Contracts.Data.Repositories.IKeyValuePairsRepository is added.

---
+++ Infrastructure/Contracts/Data/Repositories/IKeyValuePairsRepository.cs
@@ -0,0 +1,17 @@
+﻿namespace ProcessingTools.Contracts.Data.Repositories
+{
+    using System.Threading.Tasks;
+
+    public interface IKeyValuePairsRepository<TKey, TValue> : IRepository<TValue>
+    {
+        Task<object> Add(TKey key, TValue value);
+
+        Task<TValue> Get(TKey key);
+
+        Task<object> Remove(TKey key);
+
+        Task<object> Update(TKey key, TValue value);
+
+        Task<object> Upsert(TKey key, TValue value);
+    }
+}
