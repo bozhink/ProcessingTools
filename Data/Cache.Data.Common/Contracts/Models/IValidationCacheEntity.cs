3920cc347a55163d4b5f1530b4478fbd061c7fca
bozhink <bozhink@gmail.com>
2016-11-02 14:08:15 +0200

    Project ProcessingTools.Cache.Data.Common is added.

---
+++ Data/Cache.Data.Common/Contracts/Models/IValidationCacheEntity.cs
@@ -0,0 +1,14 @@
+﻿namespace ProcessingTools.Cache.Data.Common.Contracts.Models
+{
+    using System;
+    using ProcessingTools.Enumerations;
+
+    public interface IValidationCacheEntity
+    {
+        string Content { get; }
+
+        DateTime LastUpdate { get; }
+
+        ValidationStatus Status { get; }
+    }
+}
---
+++ Data/Cache.Data.Common/Contracts/Repositories/IValidationCacheDataRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace ProcessingTools.Cache.Data.Common.Contracts.Repositories
+{
+    using Models;
+    using ProcessingTools.Contracts.Data.Repositories;
+
+    public interface IValidationCacheDataRepository : IStringKeyCollectionValuePairsRepository<IValidationCacheEntity>
+    {
+    }
+}
