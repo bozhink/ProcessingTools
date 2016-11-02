cd4965e8701c1942877b416891770c1509e9c263
bozhink <bozhink@gmail.com>
2016-11-02 14:37:12 +0200

    Project ProcessingTools.Cache.Data.Redis is added.

---
+++ Data/Cache.Data.Redis/Contracts/Repositories/IRedisValidationCacheDataRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace ProcessingTools.Cache.Data.Redis.Contracts.Repositories
+{
+    using ProcessingTools.Cache.Data.Common.Contracts.Repositories;
+
+    public interface IRedisValidationCacheDataRepository : IValidationCacheDataRepository
+    {
+    }
+}
---
+++ Data/Cache.Data.Redis/Repositories/RedisValidationCacheDataRepository.cs
@@ -0,0 +1,59 @@
+﻿namespace ProcessingTools.Cache.Data.Redis.Repositories
+{
+    using System;
+    using System.Threading.Tasks;
+    using Contracts.Repositories;
+    using Models;
+    using ProcessingTools.Cache.Data.Common.Contracts.Models;
+    using ProcessingTools.Data.Common.Redis.Contracts;
+    using ProcessingTools.Data.Common.Redis.Repositories;
+
+    public class RedisValidationCacheDataRepository : RedisKeyCollectionValuePairsRepository<IValidationCacheEntity>, IRedisValidationCacheDataRepository
+    {
+        public RedisValidationCacheDataRepository(IRedisClientProvider provider)
+            : base(provider)
+        {
+        }
+
+        private Func<IValidationCacheEntity, ValidationCacheEntity> MapToEntity => e => new ValidationCacheEntity
+        {
+            Content = e.Content,
+            LastUpdate = e.LastUpdate,
+            Status = e.Status
+        };
+
+        public override Task<object> Add(string key, IValidationCacheEntity value)
+        {
+            if (string.IsNullOrWhiteSpace(key))
+            {
+                throw new ArgumentNullException(nameof(key));
+            }
+
+            if (value == null)
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+
+            var entity = this.MapToEntity(value);
+
+            return base.Add(key, entity);
+        }
+
+        public override Task<object> Remove(string key, IValidationCacheEntity value)
+        {
+            if (string.IsNullOrWhiteSpace(key))
+            {
+                throw new ArgumentNullException(nameof(key));
+            }
+
+            if (value == null)
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+
+            var entity = this.MapToEntity(value);
+
+            return base.Remove(key, entity);
+        }
+    }
+}
