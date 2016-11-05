67281151d09ab00752148222b7a3ea04826464e9
Bozhin Karaivanov <bozhink@gmail.com>
2016-11-06 01:06:09 +0200

    ProcessingTools.Bio.Data.Entity.IBioDbContext is added.

--- Data/Bio.Data.Entity/BioDbContext.cs
+++ Data/Bio.Data.Entity/BioDbContext.cs
@@ -2,9 +2,11 @@
 {
     using System.Data.Entity;
     using System.Data.Entity.ModelConfiguration.Conventions;
+    using Contracts;
     using Models;
+    using ProcessingTools.Data.Common.Entity.Contracts;
 
-    public class BioDbContext : DbContext
+    public class BioDbContext : DbContext, IBioDbContext
     {
         public BioDbContext(string connectionString)
             : base(connectionString)
@@ -14,6 +16,8 @@
         public IDbSet<MorphologicalEpithet> MorphologicalEpithets { get; set; }
 
         public IDbSet<TypeStatus> TypesStatuses { get; set; }
+
+        IDbSet<T> IDbContext.Set<T>() => this.Set<T>();
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
---
+++ Data/Bio.Data.Entity/Contracts/IBioDbContext.cs
@@ -0,0 +1,13 @@
+﻿namespace ProcessingTools.Bio.Data.Entity.Contracts
+{
+    using System.Data.Entity;
+    using Models;
+    using ProcessingTools.Data.Common.Entity.Contracts;
+
+    public interface IBioDbContext : IDbContext
+    {
+        IDbSet<MorphologicalEpithet> MorphologicalEpithets { get; set; }
+
+        IDbSet<TypeStatus> TypesStatuses { get; set; }
+    }
+}
