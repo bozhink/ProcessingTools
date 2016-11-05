482db772752daad21cf3fe433749a669db19b2e7
Bozhin Karaivanov <bozhink@gmail.com>
2016-11-05 10:23:39 +0200

    Exceptions: StreamCannotBeReadException and FileNameIsNullOrWhitespaceException are added

---
+++ Infrastructure/Exceptions/FileNameIsNullOrWhitespaceException.cs
@@ -0,0 +1,31 @@
+﻿namespace ProcessingTools.Exceptions
+{
+    using System;
+    using System.Runtime.Serialization;
+
+    [Serializable]
+    public class FileNameIsNullOrWhitespaceException : Exception
+    {
+        private const string DefaultMessage = "File name is null or whitespace";
+
+        public FileNameIsNullOrWhitespaceException()
+            : base(message: DefaultMessage)
+        {
+        }
+
+        public FileNameIsNullOrWhitespaceException(string message)
+            : base(message: message)
+        {
+        }
+
+        public FileNameIsNullOrWhitespaceException(string message, Exception innerException)
+            : base(message: message, innerException: innerException)
+        {
+        }
+
+        public FileNameIsNullOrWhitespaceException(SerializationInfo info, StreamingContext context)
+            : base(info: info, context: context)
+        {
+        }
+    }
+}
---
+++ Infrastructure/Exceptions/StreamCannotBeReadException.cs
@@ -0,0 +1,31 @@
+﻿namespace ProcessingTools.Exceptions
+{
+    using System;
+    using System.Runtime.Serialization;
+
+    [Serializable]
+    public class StreamCannotBeReadException : Exception
+    {
+        private const string DefaultMessage = "Stream can not be read";
+
+        public StreamCannotBeReadException()
+            : base(message: DefaultMessage)
+        {
+        }
+
+        public StreamCannotBeReadException(string message)
+            : base(message: message)
+        {
+        }
+
+        public StreamCannotBeReadException(string message, Exception innerException)
+            : base(message: message, innerException: innerException)
+        {
+        }
+
+        public StreamCannotBeReadException(SerializationInfo info, StreamingContext context)
+            : base(info: info, context: context)
+        {
+        }
+    }
+}
