813ece0f041648e4621ee2ab4f67a73f21d3e070
Bozhin Karaivanov <bozhink@gmail.com>
2016-11-06 01:10:06 +0200

    ProcessingTools.Bio.Taxonomy.Data.Entity.Contracts.IBioTaxonomyDbContext is added.

--- Data/Bio.Taxonomy.Data.Entity/BioTaxonomyDbContext.cs
+++ Data/Bio.Taxonomy.Data.Entity/BioTaxonomyDbContext.cs
@@ -2,20 +2,24 @@
 {
     using System.Data.Entity;
     using System.Data.Entity.ModelConfiguration.Conventions;
+    using Contracts;
     using Models;
+    using ProcessingTools.Data.Common.Entity.Contracts;
 
-    public class BioTaxonomyDbContext : DbContext
+    public class BioTaxonomyDbContext : DbContext, IBioTaxonomyDbContext
     {
         public BioTaxonomyDbContext(string connectionString)
             : base(connectionString)
         {
         }
 
-        public IDbSet<TaxonRank> TaxonRanks { get; set; }
+        public IDbSet<BlackListEntity> BlackListedItems { get; set; }
 
         public IDbSet<TaxonName> TaxonNames { get; set; }
 
-        public IDbSet<BlackListEntity> BlackListedItems { get; set; }
+        public IDbSet<TaxonRank> TaxonRanks { get; set; }
+
+        IDbSet<T> IDbContext.Set<T>() => this.Set<T>();
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
