ca112cdf4c73cab01b8573edfb008c9063ce6c3d
Bozhin Karaivanov <bozhink@gmail.com>
2016-11-06 00:26:28 +0200

    ProcessingTools.Data.Common.Entity.Contracts.IDbContext is added.

---
+++ Data/Data.Common.Entity/Contracts/IDbContext.cs
@@ -0,0 +1,20 @@
+﻿namespace ProcessingTools.Data.Common.Entity.Contracts
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Infrastructure;
+    using System.Threading.Tasks;
+
+    public interface IDbContext : IDisposable
+    {
+        DbEntityEntry<T> Entry<T>(T entity)
+            where T : class;
+
+        int SaveChanges();
+
+        Task<int> SaveChangesAsync();
+
+        IDbSet<T> Set<T>()
+            where T : class;
+    }
+}
