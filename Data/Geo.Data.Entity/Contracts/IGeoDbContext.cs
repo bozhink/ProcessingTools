6bf2f124ccda02fd878aa7e8d0d44b886e5e71d0
Bozhin Karaivanov <bozhink@gmail.com>
2016-11-06 01:22:13 +0200

    ProcessingTools.Geo.Data.Entity.Contracts.IGeoDbContext is added

---
+++ Data/Geo.Data.Entity/Contracts/IGeoDbContext.cs
@@ -0,0 +1,23 @@
+﻿namespace ProcessingTools.Geo.Data.Entity.Contracts
+{
+    using System.Data.Entity;
+    using Models;
+    using ProcessingTools.Data.Common.Entity.Contracts;
+
+    public interface IGeoDbContext : IDbContext
+    {
+        IDbSet<City> Cities { get; set; }
+
+        IDbSet<Continent> Continents { get; set; }
+
+        IDbSet<Country> Countries { get; set; }
+
+        IDbSet<GeoEpithet> GeoEpithets { get; set; }
+
+        IDbSet<GeoName> GeoNames { get; set; }
+
+        IDbSet<PostCode> PostCodes { get; set; }
+
+        IDbSet<State> States { get; set; }
+    }
+}
--- Data/Geo.Data.Entity/GeoDbContext.cs
+++ Data/Geo.Data.Entity/GeoDbContext.cs
@@ -2,28 +2,32 @@
 {
     using System.Data.Entity;
     using System.Data.Entity.ModelConfiguration.Conventions;
+    using Contracts;
     using Models;
+    using ProcessingTools.Data.Common.Entity.Contracts;
 
-    public class GeoDbContext : DbContext
+    public class GeoDbContext : DbContext, IGeoDbContext
     {
         public GeoDbContext(string connectionString)
             : base(connectionString)
         {
         }
 
+        public IDbSet<City> Cities { get; set; }
+
         public IDbSet<Continent> Continents { get; set; }
 
         public IDbSet<Country> Countries { get; set; }
 
-        public IDbSet<State> States { get; set; }
-
-        public IDbSet<City> Cities { get; set; }
-
-        public IDbSet<PostCode> PostCodes { get; set; }
+        public IDbSet<GeoEpithet> GeoEpithets { get; set; }
 
         public IDbSet<GeoName> GeoNames { get; set; }
 
-        public IDbSet<GeoEpithet> GeoEpithets { get; set; }
+        public IDbSet<PostCode> PostCodes { get; set; }
+
+        public IDbSet<State> States { get; set; }
+
+        IDbSet<T> IDbContext.Set<T>() => this.Set<T>();
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
