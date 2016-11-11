0ae09e24e0ecbc25099283b75f74490664d2a0ec
Bozhin Karaivanov <bozhink@gmail.com>
2016-11-11 22:38:16 +0200

    ProcessingTools.FileSystem.Contracts: IFileReader and IFileWriter are removed

--- Infrastructure/FileSystem/Contracts/IXmlFileReaderWriter.cs
+++ Infrastructure/FileSystem/Contracts/IXmlFileReaderWriter.cs
@@ -1,12 +1,31 @@
 ﻿namespace ProcessingTools.FileSystem.Contracts
 {
+    using System.IO;
+    using System.Threading.Tasks;
     using System.Xml;
 
-    public interface IXmlFileReaderWriter : IFileReader, IFileWriter
+    public interface IXmlFileReaderWriter
     {
         XmlReaderSettings ReaderSettings { get; set; }
 
         XmlWriterSettings WriterSettings { get; set; }
+
+        /// <summary>
+        /// Deletes file.
+        /// </summary>
+        /// <param name="fileName">Name of the output file. Should be full-path name or relative.</param>
+        /// <param name="basePath">Path to directory in which fileName should be.</param>
+        /// <returns>Awaitable task.</returns>
+        Task Delete(string fileName, string basePath = null);
+
+        /// <summary>
+        /// Creates new unique file name in given full path.
+        /// </summary>
+        /// <param name="fileName">Name of the output file. Should be full-path name or relative.</param>
+        /// <param name="basePath">Path to directory in which fileName should be.</param>
+        /// <param name="length">Length of the file name part of the new file path.</param>
+        /// <returns>Unique file path based on the <paramref name="path"/>.</returns>
+        Task<string> GetNewFilePath(string fileName, string basePath, int length);
 
         /// <summary>
         /// Reads a file to a XmlReader.
@@ -15,5 +34,22 @@
         /// <param name="basePath">Stream object for fileName.</param>
         /// <returns>XmlReader object for fileName.</returns>
         XmlReader GetXmlReader(string fileName, string basePath = null);
+
+        /// <summary>
+        /// Reads a file as stream.
+        /// </summary>
+        /// <param name="fileName">Name of the input file. Should be full-path name or relative.</param>
+        /// <param name="basePath">Path to directory in which fileName should be.</param>
+        /// <returns>Stream object for fileName.</returns>
+        Stream ReadToStream(string fileName, string basePath = null);
+
+        /// <summary>
+        /// Writes a stream to a file.
+        /// </summary>
+        /// <param name="stream">Stream object to be written to fileName.</param>
+        /// <param name="fileName">Name of the output file. Should be full-path name or relative.</param>
+        /// <param name="basePath">Path to directory in which fileName should be.</param>
+        /// <returns>Content length of the written file.</returns>
+        Task<long> Write(Stream stream, string fileName, string basePath = null);
     }
 }
