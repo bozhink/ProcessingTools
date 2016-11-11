c2bd33bda4d7624c4a10064d33ec6efac28d1ef0
Bozhin Karaivanov <bozhink@gmail.com>
2016-11-11 22:15:32 +0200

    ProcessingTools.Contracts.Files.Generators.IFileNameGenerator is added.

---
+++ Infrastructure/Contracts/Files/Generators/IFileNameGenerator.cs
@@ -0,0 +1,11 @@
+﻿namespace ProcessingTools.Contracts.Files.Generators
+{
+    using System.Threading.Tasks;
+
+    public interface IFileNameGenerator
+    {
+        Task<string> Generate(string baseFileFullName, int maximalFileNameLength, bool returnFullName = true);
+
+        Task<string> Generate(string directoryPath, string baseFileName, int maximalFileNameLength, bool returnFullName = false);
+    }
+}
