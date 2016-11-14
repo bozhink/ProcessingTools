2193bb3cba380dffed25d553195fda2d8adea247
Bozhin Karaivanov <bozhink@gmail.com>
2016-11-14 23:05:27 +0200

    ProcessingTools.Contracts: IDeserializer and IGenericDeserializer are added

---
+++ Infrastructure/Contracts/IGenericDeserializer.cs
@@ -0,0 +1,10 @@
+﻿namespace ProcessingTools.Contracts
+{
+    using System.IO;
+    using System.Threading.Tasks;
+
+    public interface IGenericDeserializer<T>
+    {
+        Task<T> Deserialize(Stream stream);
+    }
+}
