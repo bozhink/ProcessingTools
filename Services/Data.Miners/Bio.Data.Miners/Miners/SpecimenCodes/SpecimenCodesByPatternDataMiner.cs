d390c6872b566e568e95b04810a08983c9da3ee4
bozhink <bozhink@gmail.com>
2016-11-17 10:01:58 +0200

    ProcessingTools.Bio.Data.Miners.SpecimenCodes.SpecimenCodesByPatternDataMiner is added.

---
+++ Services/Data.Miners/Bio.Data.Miners/Miners/SpecimenCodes/SpecimenCodesByPatternDataMiner.cs
@@ -0,0 +1,45 @@
+﻿namespace ProcessingTools.Bio.Data.Miners.SpecimenCodes
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+    using System.Threading.Tasks;
+    using Contracts.Models.SpecimenCodes;
+    using Contracts.SpecimenCodes;
+    using Models.SpecimenCodes;
+    using ProcessingTools.Extensions;
+    using ProcessingTools.Extensions.Linq;
+
+    public class SpecimenCodesByPatternDataMiner : ISpecimenCodesByPatternDataMiner
+    {
+        private readonly IDictionary<string, string> patterns;
+
+        public SpecimenCodesByPatternDataMiner()
+        {
+            this.patterns = new Dictionary<string, string>
+            {
+                { @"\bBOLD:[A-Z]{3,3}[0-9]{4,4}\b", "BOLD BIN" }
+            };
+        }
+
+        public async Task<IEnumerable<ISpecimenCode>> Mine(string content)
+        {
+            if (string.IsNullOrWhiteSpace(content))
+            {
+                return new ISpecimenCode[] { };
+            }
+
+            var data = await this.patterns.AsParallel()
+                .SelectMany(p => Regex.Match(content, p.Key).ToIEnumerable()
+                    .Select(m => new SpecimenCodeResponseModel
+                    {
+                        Content = m,
+                        ContentType = p.Value
+                    }))
+                .Distinct()
+                .ToListAsync();
+
+            return data;
+        }
+    }
+}
---
+++ Services/Data.Miners/Bio.Data.Miners/Models/SpecimenCodes/SpecimenCodeResponseModel.cs
@@ -0,0 +1,43 @@
+﻿namespace ProcessingTools.Bio.Data.Miners.Models.SpecimenCodes
+{
+    using Contracts.Models.SpecimenCodes;
+
+    internal class SpecimenCodeResponseModel : ISpecimenCode
+    {
+        private string content;
+        private string contentType;
+        private int hashCode;
+
+        public string Content
+        {
+            get
+            {
+                return content;
+            }
+
+            set
+            {
+                content = value;
+                this.hashCode = (this.content + this.contentType).GetHashCode();
+            }
+        }
+
+        public string ContentType
+        {
+            get
+            {
+                return contentType;
+            }
+
+            set
+            {
+                contentType = value;
+                this.hashCode = (this.content + this.contentType).GetHashCode();
+            }
+        }
+
+        public override bool Equals(object obj) => this.GetHashCode() == obj.GetHashCode();
+
+        public override int GetHashCode() => this.hashCode;
+    }
+}
