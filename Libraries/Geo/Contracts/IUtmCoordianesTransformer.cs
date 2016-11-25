4c7261209acee7ecc5c4d70de2409b7605626297
bozhink <bozhink@gmail.com>
2016-11-25 16:39:24 +0200

    ProcessingTools.Geo.UtmCoordianesTransformer is added.

---
+++ Libraries/Geo/Contracts/IUtmCoordianesTransformer.cs
@@ -0,0 +1,9 @@
+﻿namespace ProcessingTools.Geo.Contracts
+{
+    public interface IUtmCoordianesTransformer
+    {
+        double[] TransformDecimal2Utm(double latitude, double longitude, string utmZone);
+
+        double[] TransformUtm2Decimal(double utmEasting, double utmNorthing, string utmZone);
+    }
+}
