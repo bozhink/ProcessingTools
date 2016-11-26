58beb5d0a8e655a6fbee0704d0aefed696345fba
Bozhin Karaivanov <bozhink@gmail.com>
2016-11-26 23:11:50 +0200

    ProcessingTools.Mediatypes.Data.Entity.Contracts.Repositories.IMediatypesRepositoryProvider is removed

--- Data/Mediatypes/Mediatypes.Data.Entity/Contracts/Repositories/IMediaTypesRepositoryProvider.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ProcessingTools.Mediatypes.Data.Entity.Contracts.Repositories
-{
-    using ProcessingTools.Contracts.Data.Repositories;
-
-    public interface IMediatypesRepositoryProvider<T> : ISearchableCountableCrudRepositoryProvider<T>
-        where T : class
-    {
-    }
-}
