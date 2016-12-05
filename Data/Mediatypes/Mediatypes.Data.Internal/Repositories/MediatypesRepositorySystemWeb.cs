9e91ca1a9ccec35712f83cd43290445aa27d23ad
bozhink <bozhink@gmail.com>
2016-12-05 10:10:13 +0200

    ProcessingTools.Mediatypes.Data.Internal.Repositories.MediatypesRepositorySystemWeb is added.

---
+++ Data/Mediatypes/Mediatypes.Data.Internal/Repositories/MediatypesRepositorySystemWeb.cs
@@ -0,0 +1,16 @@
+﻿namespace ProcessingTools.Mediatypes.Data.Internal.Repositories
+{
+    using System.Web;
+    using Abstractions.Repositories;
+    using ProcessingTools.Mediatypes.Data.Common.Contracts.Repositories;
+
+    public class MediatypesRepositorySystemWeb : AbstractMediatypesRepository, IMediatypesRepository
+    {
+        protected override string GetMediatype(string fileExtension)
+        {
+            string fileName = "/prefix/file." + fileExtension.Trim(new char[] { '.', ' ' });
+            string mediatype = MimeMapping.GetMimeMapping(fileName);
+            return mediatype;
+        }
+    }
+}
