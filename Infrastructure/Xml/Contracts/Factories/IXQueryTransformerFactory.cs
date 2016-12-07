2ad44cdf8010e5848dfa50dae15a6e4ebddc1521
Bozhin Karaivanov <bozhink@gmail.com>
2016-12-07 22:38:06 +0200

    ProcessingTools.Xml.Contracts.Factories.IXQueryTransformerFactory is added.

---
+++ Infrastructure/Xml/Contracts/Factories/IXQueryTransformerFactory.cs
@@ -0,0 +1,9 @@
+﻿namespace ProcessingTools.Xml.Contracts.Factories
+{
+    using ProcessingTools.Xml.Contracts.Transformers;
+
+    public interface IXQueryTransformerFactory
+    {
+        IXQueryTransformer CreateTransformer(string xqueryFileName);
+    }
+}
