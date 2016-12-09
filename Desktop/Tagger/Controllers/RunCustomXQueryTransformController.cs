55c8ded9f4f9cf522b07e54fd5c039bb81c55352
bozhink <bozhink@gmail.com>
2016-12-09 10:43:25 +0200

    ProcessingTools.Tagger.Controllers.RunCustomXQueryTransformController is added.

---
+++ Desktop/Tagger/Controllers/RunCustomXQueryTransformController.cs
@@ -0,0 +1,51 @@
+﻿namespace ProcessingTools.Tagger.Controllers
+{
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Contracts;
+    using Contracts.Controllers;
+    using ProcessingTools.Attributes;
+    using ProcessingTools.Contracts;
+    using ProcessingTools.Processors.Contracts;
+
+    public class RunCustomXQueryTransformController : IRunCustomXQueryTransformController
+    {
+        private readonly IDocumentXQueryProcessor processor;
+
+        public RunCustomXQueryTransformController(IDocumentXQueryProcessor processor)
+        {
+            if (processor == null)
+            {
+                throw new ArgumentNullException(nameof(processor));
+            }
+
+            this.processor = processor;
+        }
+
+        public async Task<object> Run(IDocument document, IProgramSettings settings)
+        {
+            if (document == null)
+            {
+                throw new ArgumentNullException(nameof(document));
+            }
+
+            if (settings == null)
+            {
+                throw new ArgumentNullException(nameof(settings));
+            }
+
+            int numberOfFileNames = settings.FileNames.Count();
+            if (numberOfFileNames < 3)
+            {
+                throw new ApplicationException("The name of the XQuey file should be set.");
+            }
+
+            this.processor.XQueryFileFullName = settings.FileNames.ElementAt(2);
+
+            await this.processor.Process(document);
+
+            return true;
+        }
+    }
+}
