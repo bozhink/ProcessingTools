eb206927911645c97452b37e7e349f9d078d2399
bozhink <bozhink@gmail.com>
2016-12-12 15:34:25 +0200

    ProcessingTools.Data.Common.Mongo.Repositories.MongoCrudRepository is removed.

--- Data/Common/Data.Common.Mongo/Repositories/MongoGenericRepository.cs
+++ Data/Common/Data.Common.Mongo/Repositories/MongoGenericRepository.cs
@@ -5,14 +5,34 @@
     using System.Threading.Tasks;
     using Contracts;
     using Contracts.Repositories;
+    using MongoDB.Bson.Serialization.Attributes;
     using ProcessingTools.Common.Validation;
+    using ProcessingTools.Data.Common.Extensions;
 
-    public class MongoGenericRepository<TEntity> : MongoCrudRepository<TEntity>, IMongoGenericRepository<TEntity>
+    public class MongoGenericRepository<TEntity> : MongoCrudRepository<TEntity, TEntity>, IMongoGenericRepository<TEntity>
         where TEntity : class
     {
         public MongoGenericRepository(IMongoDatabaseProvider provider)
             : base(provider)
         {
+        }
+
+        public override async Task<object> Add(TEntity entity)
+        {
+            DummyValidator.ValidateEntity(entity);
+
+            await this.Collection.InsertOneAsync(entity);
+            return entity;
+        }
+
+        public override async Task<object> Update(TEntity entity)
+        {
+            DummyValidator.ValidateEntity(entity);
+
+            var id = entity.GetIdValue<BsonIdAttribute>();
+            var filter = this.GetFilterById(id);
+            var result = await this.Collection.ReplaceOneAsync(filter, entity);
+            return result;
         }
 
         public virtual async Task<long> Count()
