045060d91edfe22200f9535a33e2d057f44de8d5
bozhink <bozhink@gmail.com>
2016-12-13 15:41:10 +0200

    ProcessingTools.Processors.Processors.Validation.CrossReferencesValidator is added.

---
+++ Desktop/Tagger/Controllers/ValidateCrossReferencesController.cs
@@ -0,0 +1,16 @@
+﻿namespace ProcessingTools.Tagger.Controllers
+{
+    using Contracts.Controllers;
+    using Generics;
+    using ProcessingTools.Attributes;
+    using ProcessingTools.Processors.Contracts.Validation;
+
+    [Description("Validate cross-references.")]
+    public class ValidateCrossReferencesController : GenericDocumentValidatorController<ICrossReferencesValidator>, IValidateCrossReferencesController
+    {
+        public ValidateCrossReferencesController(ICrossReferencesValidator validator)
+            : base(validator)
+        {
+        }
+    }
+}
