e38255e6b95a72fbde91961d1cbe60eef963991f
Bozhin Karaivanov <bozhink@gmail.com>
2016-12-16 23:46:52 +0200

    ProcessingTools.Contracts.Data.Repositories.IInsertableRepository is added.

---
+++ Infrastructure/Contracts/Data/Repositories/IRemovableRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace ProcessingTools.Contracts.Data.Repositories
+{
+    using System.Threading.Tasks;
+
+    public interface IRemovableRepository<T> : IRepository<T>
+    {
+        Task<object> Remove(object id);
+
+        Task<object> Remove(T entity);
+    }
+}
