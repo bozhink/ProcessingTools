839d882e82bca85dd3bad155cc25d592e68c054c
Bozhin Karaivanov <bozhink@gmail.com>
2016-12-16 22:57:28 +0200

    ProcessingTools.Services.Common.Factories.DataServiceBaseFactory is removed.

--- Services/Services.Common/Contracts/IDataService.cs
+++ Services/Services.Common/Contracts/IDataService.cs
@@ -3,8 +3,16 @@
     using System.Linq;
     using System.Threading.Tasks;
 
-    public interface IDataService<TServiceModel> : IDataServiceBase<TServiceModel>
+    public interface IDataService<TServiceModel>
     {
         Task<IQueryable<TServiceModel>> All();
+
+        Task<object> Add(TServiceModel model);
+
+        Task<object> Delete(object id);
+
+        Task<TServiceModel> Get(object id);
+
+        Task<object> Update(TServiceModel model);
     }
 }
