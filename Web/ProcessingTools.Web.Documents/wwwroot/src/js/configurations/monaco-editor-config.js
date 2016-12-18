2d2044044c24f01ab6866c87aae45097f935b56d
Bozhin Karaivanov <bozhink@gmail.com>
2016-12-18 15:31:10 +0200

    monaco-editor-config is exported

--- Web/ProcessingTools.Web.Documents/wwwroot/src/js/configurations/monaco-editor-config.js
+++ Web/ProcessingTools.Web.Documents/wwwroot/src/js/configurations/monaco-editor-config.js
@@ -1,130 +1,125 @@
-﻿(function(window, document) {
-    'use strict';
-    var app, configurations;
+﻿'use strict';
 
-    window.app = window.app || {};
-    app = window.app;
+const
+    DEFAULT_LANGUAGE = 'xml',
+    DEFAULT_THEME = 'vs';
 
-    app.configurations = app.configurations || {};
-    configurations = app.configurations;
+var modes = null,
+    themes = [{
+        themeId: 'vs',
+        display: 'Visual Studio',
+        selected: true
+    }, {
+        themeId: 'vs-dark',
+        display: 'Visual Studio Dark'
+    }, {
+        themeId: 'hc-black',
+        display: 'High Contrast Dark'
+    }];
 
-    configurations.MonacoEditor = function() {
-        const
-            DEFAULT_LANGUAGE = 'xml',
-            DEFAULT_THEME = 'vs';
+function getModes(monaco) {
+    return (function () {
+        var modesIds = monaco.languages.getLanguages().map(function (lang) {
+            return lang.id;
+        });
 
-        var require = window.require,
-            editor = window.editor || null,
-            modes = null,
-            themes = [{
-                themeId: 'vs',
-                display: 'Visual Studio',
-                selected: true
-            }, {
-                themeId: 'vs-dark',
-                display: 'Visual Studio Dark'
-            }, {
-                themeId: 'hc-black',
-                display: 'High Contrast Dark'
-            }];
+        modesIds.sort();
 
-        function initEditor(containerId, pathToNodeModules, language, theme, content) {
-            content = content || '';
-            language = language || DEFAULT_LANGUAGE;
-            theme = theme || DEFAULT_THEME;
+        return modesIds.map(function (modeId) {
+            var item = {
+                modeId: modeId
+            };
 
-            return new Promise(function(resolve, reject) {
-                try {
-                    require.config({
-                        paths: {
-                            'vs': pathToNodeModules + '/monaco-editor/min/vs'
-                        }
-                    });
+            if (modeId === DEFAULT_LANGUAGE) {
+                item.selected = true;
+            }
 
-                    require(['vs/editor/editor.main'], function() {
-                        var monaco = window.monaco || null;
+            return item;
+        });
+    }());
+}
 
-                        if (!modes) {
-                            modes = (function() {
-                                var modesIds = monaco.languages.getLanguages().map(function(lang) {
-                                    return lang.id;
-                                });
+function createEditor(monaco, containerElement, language, theme, content) {
+    content = content || '';
+    language = language || DEFAULT_LANGUAGE;
+    theme = theme || DEFAULT_THEME;
 
-                                modesIds.sort();
+    return monaco.editor.create(containerElement, {
+        value: content.toString(),
+        language: language,
+        theme: theme
+    });
+}
 
-                                return modesIds.map(function(modeId) {
-                                    var item = {
-                                        modeId: modeId
-                                    };
+export function MonacoEditorConfig(window, require, monaco) {
 
-                                    if (modeId === DEFAULT_LANGUAGE) {
-                                        item.selected = true;
-                                    }
+    function initEditor(containerElement, pathToNodeModules, language, theme, content) {
+        return new Promise(function (resolve, reject) {
+            var editor;
 
-                                    return item;
-                                });
-                            }());
-                        }
-
-                        editor = monaco.editor.create(document.getElementById(containerId), {
-                            value: content.toString(),
-                            language: language,
-                            theme: theme
-                        });
-
-                        window.editor = editor;
-
-                        window.addEventListener('resize', function() {
-                            if (editor) {
-                                editor.layout();
-                            }
-                        }, false);
-
-                        if (resolve) {
-                            resolve({
-                                modes: modes,
-                                themes: themes
-                            });
-                        }
-                    });
-                } catch (e) {
-                    if (reject) {
-                        reject(e);
+            try {
+                require.config({
+                    paths: {
+                        'vs': pathToNodeModules + '/monaco-editor/min/vs'
                     }
+                });
+
+                require(['vs/editor/editor.main'], function () {
+                    modes = getModes(monaco);
+
+                    editor = createEditor(monaco, containerElement, language, theme, content);
+
+                    window.addEventListener('resize', function () {
+                        if (editor) {
+                            editor.layout();
+                        }
+                    }, false);
+
+                    if (resolve) {
+                        resolve({
+                            editor: editor,
+                            modes: modes,
+                            themes: themes
+                        });
+                    }
+                });
+            } catch (e) {
+                if (reject) {
+                    reject(e);
                 }
+            }
+        });
+    }
+
+    function changeMode(editor, mode) {
+        var oldModel,
+            newModel,
+            content,
+            language = mode ? mode.modeId || DEFAULT_LANGUAGE : DEFAULT_LANGUAGE;
+
+        if (editor) {
+            content = editor.getValue();
+            oldModel = editor.getModel();
+            newModel = window.monaco.editor.createModel(content, language);
+            editor.setModel(newModel);
+            if (oldModel) {
+                oldModel.dispose();
+            }
+        }
+    }
+
+    function changeTheme(editor, theme) {
+        var newTheme = theme ? theme.themeId || DEFAULT_THEME : DEFAULT_THEME;
+        if (editor) {
+            editor.updateOptions({
+                'theme': newTheme
             });
         }
-
-        function changeMode(mode) {
-            var oldModel,
-                newModel,
-                content,
-                language = mode ? mode.modeId || DEFAULT_LANGUAGE : DEFAULT_LANGUAGE;
-
-            if (editor) {
-                content = editor.getValue();
-                oldModel = editor.getModel();
-                newModel = window.monaco.editor.createModel(content, language);
-                editor.setModel(newModel);
-                if (oldModel) {
-                    oldModel.dispose();
-                }
-            }
-        }
-
-        function changeTheme(theme) {
-            var newTheme = theme ? theme.themeId || DEFAULT_THEME : DEFAULT_THEME;
-            if (editor) {
-                editor.updateOptions({
-                    'theme': newTheme
-                });
-            }
-        }
-
-        return {
-            init: initEditor,
-            changeMode: changeMode,
-            changeTheme: changeTheme
-        };
     }
-}(window, document));
+
+    return {
+        init: initEditor,
+        changeMode: changeMode,
+        changeTheme: changeTheme
+    };
+}
