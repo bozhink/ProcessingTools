53d7cf9fd6b0bb2049baa965f067de077132976c
Bozhin Karaivanov <bozhink@gmail.com>
2016-12-24 17:21:42 +0200

    search-string-service.js is exported

--- Web/ProcessingTools.Web.Documents/wwwroot/src/js/apps/bio-data-app.js
+++ Web/ProcessingTools.Web.Documents/wwwroot/src/js/apps/bio-data-app.js
@@ -1,7 +1,8 @@
 /* globals angular */
 
 var DataSet = require('../data/data-set'),
-    JsonRequester = require('../services/ng-json-requester');
+    JsonRequester = require('../services/ng-json-requester'),
+    SearchStringService = require('../services/search-string-service');
 
 var app = angular.module('bioDataApp', [])
     .service('DataSet', [
@@ -10,4 +11,8 @@ var app = angular.module('bioDataApp', [])
     .factory('JsonRequester', [
         '$http',
         JsonRequester
+    ])
+    .factory('SearchStringService', [
+        'JsonRequester',
+        SearchStringService
     ]);
--- Web/ProcessingTools.Web.Documents/wwwroot/src/js/services/search-string-service.js
+++ Web/ProcessingTools.Web.Documents/wwwroot/src/js/services/search-string-service.js
@@ -1,38 +1,28 @@
-﻿(function (window) {
-    'use strict';
-    var app, services;
+﻿'use strict';
 
-    window.app = window.app || {};
-    app = window.app;
+module.exports = function SearchStringService(jsonRequester) {
+    if (!jsonRequester) {
+        throw 'JsonRequester should not be null';
+    }
 
-    app.services = app.services || {};
-    services = app.services;
-
-    services.SearchStringService = function SearchStringService(jsonRequester) {
-        if (!jsonRequester) {
-            throw 'JsonRequester should not be null';
+    function search(url, searchString) {
+        if (!url || !searchString) {
+            throw 'Invalid input parameter';
         }
 
-        function search(url, searchString) {
-            var request;
-            if (!url || !searchString) {
-                throw 'Invalid input parameter';
-            }
-
-            searchString = searchString.trim();
-            if (searchString.length < 1) {
-                throw 'Search string should not be empty';
-            }
-
-            return jsonRequester.post(url, {
-                data: {
-                    searchString: searchString
-                }
-            });
+        searchString = searchString.trim();
+        if (searchString.length < 1) {
+            throw 'Search string should not be empty';
         }
 
-        return {
-            search: search
-        };
+        return jsonRequester.post(url, {
+            data: {
+                searchString: searchString
+            }
+        });
+    }
+
+    return {
+        search: search
     };
-}(window));
+};
