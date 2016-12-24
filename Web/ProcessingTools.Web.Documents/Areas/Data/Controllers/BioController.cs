3166e94084bde9aa038625b8c2f0d9da5353037c
Bozhin Karaivanov <bozhink@gmail.com>
2016-12-24 16:03:28 +0200

    ProcessingTools.Web.Documents.Areas.Data.Controllers.BioController is added

---
+++ Web/ProcessingTools.Web.Documents/Areas/Data/Controllers/BioController.cs
@@ -0,0 +1,21 @@
+﻿namespace ProcessingTools.Web.Documents.Areas.Data.Controllers
+{
+    using System.Web.Mvc;
+
+    public class BioController : Controller
+    {
+        // GET: Data/Bio
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return this.View();
+        }
+
+        // GET: Data/Bio/Help
+        [HttpGet]
+        public ActionResult Help()
+        {
+            return this.View();
+        }
+    }
+}
