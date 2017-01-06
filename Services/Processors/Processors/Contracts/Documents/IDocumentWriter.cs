04243a31d7155596a52164c1d73267103918fd8a
Bozhin Karaivanov <bozhink@gmail.com>
2017-01-06 14:17:01 +0200

    ProcessingTools.Processors.Contracts.Documents.IDocumentWriter is added

---
+++ Services/Processors/Processors/Contracts/Documents/IDocumentWriter.cs
@@ -0,0 +1,10 @@
+﻿namespace ProcessingTools.Processors.Contracts.Documents
+{
+    using System.Threading.Tasks;
+    using ProcessingTools.Contracts;
+
+    public interface IDocumentWriter
+    {
+        Task<object> WriteDocument(string fileName, IDocument document);
+    }
+}
