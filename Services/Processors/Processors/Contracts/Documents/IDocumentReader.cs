a70ae7d740865aa3a769d9c825e0e52531a27ead
Bozhin Karaivanov <bozhink@gmail.com>
2017-01-06 14:01:39 +0200

    ProcessingTools.Processors.Contracts.Documents.IDocumentReader is added

---
+++ Services/Processors/Processors/Contracts/Documents/IDocumentReader.cs
@@ -0,0 +1,10 @@
+﻿namespace ProcessingTools.Processors.Contracts.Documents
+{
+    using System.Threading.Tasks;
+    using ProcessingTools.Contracts;
+
+    public interface IDocumentReader
+    {
+        Task<IDocument> ReadDocument(string fileName);
+    }
+}
