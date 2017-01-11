393d322ff6ce0505a0698f88abbc05b7d76a0cef
Bozhin Karaivanov <bozhink@gmail.com>
2017-01-11 15:28:11 +0200

    ProcessingTools.Documents.Data.Common.Contracts.Models.IJournalMeta is added.

---
+++ Data/Documents/Documents.Data.Common/Contracts/Models/IJournalMeta.cs
@@ -0,0 +1,23 @@
+﻿namespace ProcessingTools.Documents.Data.Common.Contracts.Models
+{
+    public interface IJournalMeta
+    {
+        string JournalId { get; }
+
+        string JournalTitle { get; }
+
+        string AbbreviatedJournalTitle { get; }
+
+        string IssnPPub { get; }
+
+        string IssnEPub { get; }
+
+        string PublisherName { get; }
+
+        string Permalink { get; }
+
+        string FileNamePattern { get; }
+
+        string ArchiveNamePattern { get; }
+    }
+}
