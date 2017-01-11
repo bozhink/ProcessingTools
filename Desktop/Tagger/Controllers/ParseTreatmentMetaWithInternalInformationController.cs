1ed0b07c6f56d8c2ea76825579ed44e43c473ff9
Bozhin Karaivanov <bozhink@gmail.com>
2017-01-11 11:38:45 +0200

    ProcessingTools.Tagger.Contracts.Controllers.IParseTreatmentMetaWithInternalInformationController is implemented

---
+++ Desktop/Tagger/Controllers/ParseTreatmentMetaWithInternalInformationController.cs
@@ -0,0 +1,16 @@
+﻿namespace ProcessingTools.Tagger.Controllers
+{
+    using Contracts.Controllers;
+    using Generics;
+    using ProcessingTools.Attributes;
+    using ProcessingTools.Bio.Taxonomy.Processors.Contracts.Parsers;
+
+    [Description("Parse treatment meta with internal information.")]
+    public class ParseTreatmentMetaWithInternalInformationController : GenericDocumentParserController<ITreatmentMetaParserWithInternalInformation>, IParseTreatmentMetaWithInternalInformationController
+    {
+        public ParseTreatmentMetaWithInternalInformationController(ITreatmentMetaParserWithInternalInformation parser)
+            : base(parser)
+        {
+        }
+    }
+}
