a6a4e6cbf0f658806f025d8e9f9cbb83829235c8
Bozhin Karaivanov <bozhink@gmail.com>
2017-01-11 16:23:28 +0200

    ProcessingTools.Documents.Data.Common.Contracts.Repositories.IJournalMetaRepository is added

---
+++ Data/Documents/Documents.Data.Common/Contracts/Repositories/IJournalMetaRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace ProcessingTools.Documents.Data.Common.Contracts.Repositories
+{
+    using Models;
+    using ProcessingTools.Contracts.Data.Repositories;
+
+    public interface IJournalMetaRepository : ICrudRepository<IJournalMeta>
+    {
+    }
+}
