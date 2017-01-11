e3a36a4ddc09b3c8be69ad69756b29a34bef5210
Bozhin Karaivanov <bozhink@gmail.com>
2017-01-11 16:22:21 +0200

    ProcessingTools.Data.Common.Mongo.MongoDatabaseProvider is added.

---
+++ Data/Common/Data.Common.Mongo/MongoDatabaseProvider.cs
@@ -0,0 +1,40 @@
+﻿namespace ProcessingTools.Data.Common.Mongo
+{
+    using System;
+    using Constants;
+    using Contracts;
+    using MongoDB.Bson.Serialization.Conventions;
+    using MongoDB.Driver;
+
+    public class MongoDatabaseProvider : IMongoDatabaseProvider
+    {
+        private readonly string connectionString;
+        private readonly string databaseName;
+
+        public MongoDatabaseProvider(string connectionString, string databaseName)
+        {
+            if (string.IsNullOrWhiteSpace(connectionString))
+            {
+                throw new ArgumentNullException(nameof(connectionString));
+            }
+
+            if (string.IsNullOrWhiteSpace(databaseName))
+            {
+                throw new ArgumentNullException(nameof(databaseName));
+            }
+
+            this.connectionString = connectionString;
+            this.databaseName = databaseName;
+        }
+
+        public IMongoDatabase Create()
+        {
+            var conventionPack = new ConventionPack();
+            conventionPack.Add(new CamelCaseElementNameConvention());
+            ConventionRegistry.Register(ConfigurationConstants.CamelCaseConventionPackName, conventionPack, t => true);
+
+            var client = new MongoClient(this.connectionString);
+            return client.GetDatabase(this.databaseName);
+        }
+    }
+}
