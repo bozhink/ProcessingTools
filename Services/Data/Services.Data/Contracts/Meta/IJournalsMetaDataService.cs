e83ad6219478b4f794b3f5dfc28fe495f63eaf6f
Bozhin Karaivanov <bozhink@gmail.com>
2017-01-18 11:03:39 +0200

    ProcessingTools.Services.Data.Contracts.Meta.IJournalsMetaDataService is added.

---
+++ Services/Data/Services.Data/Contracts/Meta/IJournalsMetaDataService.cs
@@ -0,0 +1,11 @@
+﻿namespace ProcessingTools.Services.Data.Contracts.Meta
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Models.Meta;
+
+    public interface IJournalsMetaDataService
+    {
+        Task<IEnumerable<IJournal>> GetAllJournalsMeta();
+    }
+}
