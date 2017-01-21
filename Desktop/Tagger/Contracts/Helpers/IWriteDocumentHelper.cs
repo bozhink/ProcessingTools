2b6e76e42baf467095eac4ad124143a83b7e9372
Bozhin Karaivanov <bozhink@gmail.com>
2017-01-21 17:50:30 +0200

    ProcessingTools.Tagger.Contracts.Helpers.IWriteDocumentHelper is added.

---
+++ Desktop/Tagger/Contracts/Helpers/IWriteDocumentHelper.cs
@@ -0,0 +1,10 @@
+﻿namespace ProcessingTools.Tagger.Contracts.Helpers
+{
+    using System.Threading.Tasks;
+    using ProcessingTools.Contracts;
+
+    public interface IWriteDocumentHelper
+    {
+        Task<object> Read(IDocument document, IProgramSettings settings);
+    }
+}
