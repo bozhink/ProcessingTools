3ce1beefe68095890e9e3671db6b2bb8d38bbe05
Bozhin Karaivanov <bozhink@gmail.com>
2017-01-21 18:48:02 +0200

    ProcessingTools.Tagger.Contracts.Helpers.IInvokeActionHelper is added.

---
+++ Desktop/Tagger/Contracts/Helpers/IInvokeActionHelper.cs
@@ -0,0 +1,19 @@
+﻿namespace ProcessingTools.Tagger.Contracts.Helpers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using System.Xml;
+    using Commands;
+
+    public interface IInvokeActionHelper
+    {
+        ICollection<Task> Tasks { get; }
+
+        Task<object> Invoke(string message, Func<Task> action);
+
+        Task<object> Invoke<TCommand>(XmlNode context) where TCommand : ITaggerCommand;
+
+        Task<object> InvokeProcessor(Type commandType, XmlNode context);
+    }
+}
