35380c3e6c8c46e96fe591b0e1ae6466caa6d966
Bozhin Karaivanov <bozhink@gmail.com>
2017-01-23 10:52:43 +0200

    ProcessingTools.ListsManager: unused ApplicationSettings are removed

--- Desktop/Tagger.ListsManager/Program.cs
+++ Desktop/Tagger.ListsManager/Program.cs
@@ -14,7 +14,6 @@
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
-            ApplicationSettings.Default.Save();
         }
     }
 }
