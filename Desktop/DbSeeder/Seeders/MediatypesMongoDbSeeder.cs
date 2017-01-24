65183aa43bb4cdf145feff49d5bfb273abb4f07e
Bozhin Karaivanov <bozhink@gmail.com>
2017-01-24 17:15:13 +0200

    ProcessingTools.DbSeeder.Seeders.MediatypesMongoDbSeeder is added.

---
+++ Desktop/DbSeeder/Seeders/MediatypesMongoDbSeeder.cs
@@ -0,0 +1,15 @@
+﻿namespace ProcessingTools.DbSeeder.Seeders
+{
+    using Abstractions.Seeders;
+    using Contracts.Seeders;
+    using ProcessingTools.Mediatypes.Data.Mongo.Contracts;
+    using ProcessingTools.Mediatypes.Data.Seed.Contracts;
+
+    public class MediatypesMongoDbSeeder : GenericDbSeeder<IMediatypesMongoDatabaseInitializer, IMediatypesMongoDatabaseSeeder>, IMediatypesMongoDbSeeder
+    {
+        public MediatypesMongoDbSeeder(IMediatypesMongoDatabaseInitializer initializer, IMediatypesMongoDatabaseSeeder seeder)
+            : base(initializer, seeder)
+        {
+        }
+    }
+}
