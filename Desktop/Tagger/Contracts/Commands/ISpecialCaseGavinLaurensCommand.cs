6623fc4d65f3a690465b3f93771c23f1d047f856
Bozhin Karaivanov <bozhink@gmail.com>
2017-01-28 12:47:23 +0200

    ProcessingTools.Tagger.Core.Commands.SpecialCaseGavinLaurensCommand is added.

---
+++ Desktop/Tagger/Contracts/Commands/ISpecialCaseGavinLaurensCommand.cs
@@ -0,0 +1,6 @@
+﻿namespace ProcessingTools.Tagger.Contracts.Commands
+{
+    public interface ISpecialCaseGavinLaurensCommand : ITaggerCommand
+    {
+    }
+}
---
+++ Desktop/Tagger/Core/Commands/SpecialCaseGavinLaurensCommand.cs
@@ -0,0 +1,14 @@
+﻿namespace ProcessingTools.Tagger.Core.Commands
+{
+    using Contracts.Commands;
+    using Generics;
+    using ProcessingTools.Special.Processors.Contracts.Processors;
+
+    public class SpecialCaseGavinLaurensCommand : GenericDocumentParserCommand<IGavinLaurensParser>, ISpecialCaseGavinLaurensCommand
+    {
+        public SpecialCaseGavinLaurensCommand(IGavinLaurensParser parser)
+            : base(parser)
+        {
+        }
+    }
+}
