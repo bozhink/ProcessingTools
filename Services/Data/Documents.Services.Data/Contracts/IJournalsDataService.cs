8fa5dde1131c2f02156e20b8ec6afd174d22d6d3
Bozhin Karaivanov <bozhink@gmail.com>
2017-02-01 12:12:16 +0200

    ProcessingTools.Documents.Services.Data.Contracts: IJournalsDataService and IPublishersDataService are removed

--- Services/Data/Documents.Services.Data/Contracts/IJournalsDataService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ProcessingTools.Documents.Services.Data.Contracts
-{
-    using Models.Journals;
-    using ProcessingTools.Services.Common.Contracts;
-
-    public interface IJournalsDataService : IMvcDataService<JournalMinimalServiceModel, JournalServiceModel, JournalDetailsServiceModel>
-    {
-    }
-}
