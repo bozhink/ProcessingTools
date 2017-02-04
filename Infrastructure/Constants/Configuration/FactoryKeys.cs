32da7624c56debc369c87ce768d255d2d34338e7
Bozhin Karaivanov <bozhink@gmail.com>
2017-02-05 00:25:00 +0200

    Constants: ContentTypes and FactoryKeys are added.

---
+++ Infrastructure/Constants/Configuration/FactoryKeys.cs
@@ -0,0 +1,24 @@
+﻿namespace ProcessingTools.Constants.Configuration
+{
+    public sealed class FactoryKeys
+    {
+        public const string AbbreviationsTransformerName = "AbbreviationsTransformer";
+        public const string CodesRemoveNonCodeNodesTransformerName = "CodesRemoveNonCodeNodesTransformer";
+        public const string ExternalLinksTransformerName = "ExternalLinksTransformer";
+        public const string FormatHtmlToXmlTransformerName = "FormatHtmlToXmlTransformer";
+        public const string FormatToNlmTransformerName = "FormatToNlmTransformer";
+        public const string FormatToSystemTransformerName = "FormatToSystemTransformer";
+        public const string FormatXmlToHtmlTransformerName = "FormatXmlToHtmlTransformer";
+        public const string GavinLaurensTransformerName = "GavinLaurensTransformer";
+        public const string NlmInitialFormatTransformerName = "NlmInitialFormatTransformer";
+        public const string ParseTreatmentMetaWithInternalInformationTransformerName = "ParseTreatmentMetaWithInternalInformationTransformer";
+        public const string ReferencesGetReferencesTransformerName = "ReferencesGetReferencesTransformer";
+        public const string ReferencesTagTemplateTransformerName = "ReferencesTagTemplateTransformer";
+        public const string RemoveTaxonNamePartsTransformerName = "RemoveTaxonNamePartsTransformer";
+        public const string SystemInitialFormatTransformerName = "SystemInitialFormatTransformer";
+        public const string TaxonTreatmentExtractMaterialsTransformerName = "TaxonTreatmentExtractMaterialsTransformer";
+        public const string TaxonTreatmentFormatTransformerName = "TaxonTreatmentFormatTransformer";
+        public const string TextContentTransformerName = "TextContentTransformer";
+        public const string ZooBankRegistrationTransformerName = "ZooBankRegistrationTransformer";
+    }
+}
