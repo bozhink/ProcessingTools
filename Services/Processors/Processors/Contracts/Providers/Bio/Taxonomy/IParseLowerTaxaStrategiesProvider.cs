cde405c82c92b9cd3cab182e07ccdef0320e968a
Bozhin Karaivanov <bozhink@gmail.com>
2017-02-11 17:04:23 +0200

    ProcessingTools.Processors.Contracts.Providers.Bio.Taxonomy.IParseLowerTaxaStrategiesProvider is added.

---
+++ Services/Processors/Processors/Contracts/Providers/Bio/Taxonomy/IParseLowerTaxaStrategiesProvider.cs
@@ -0,0 +1,9 @@
+﻿namespace ProcessingTools.Processors.Contracts.Providers.Bio.Taxonomy
+{
+    using ProcessingTools.Contracts;
+    using Strategies.Bio.Taxonomy;
+
+    public interface IParseLowerTaxaStrategiesProvider : IStrategiesProvider<IParseLowerTaxaStrategy>
+    {
+    }
+}
