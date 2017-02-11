b540b1001a282ed183ceeb5064aa18618efa927c
Bozhin Karaivanov <bozhink@gmail.com>
2017-02-11 16:45:57 +0200

    ProcessingTools.Processors.Contracts.Strategies.Bio.Taxonomy.IParseLowerTaxaStrategy

---
+++ Services/Processors/Processors/Contracts/Strategies/Bio/Taxonomy/IParseLowerTaxaStrategy.cs
@@ -0,0 +1,8 @@
+﻿namespace ProcessingTools.Processors.Contracts.Strategies.Bio.Taxonomy
+{
+    using ProcessingTools.Contracts;
+
+    public interface IParseLowerTaxaStrategy : IParseXmlContextStrategy
+    {
+    }
+}
