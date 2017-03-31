438f39ba84a896b5476383c57943bdc2f412003e
Bozhin Karaivanov <bozhink@gmail.com>
2017-03-31 23:09:20 +0300

    Added ProcessingTools.Contracts.Services.Data.ISelectableDataService

---
+++ Infrastructure/Contracts/Services/Data/ISelectableDataService.cs
@@ -0,0 +1,19 @@
+﻿namespace ProcessingTools.Contracts.Services.Data
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq.Expressions;
+    using System.Threading.Tasks;
+    using ProcessingTools.Contracts.Models;
+    using ProcessingTools.Enumerations;
+
+    public interface ISelectableDataService<TModel>
+        where TModel : IServiceModel
+    {
+        Task<long> Count(Expression<Func<TModel, bool>> filter = null);
+
+        Task<IEnumerable<TModel>> Select(int skip, int take, Expression<Func<TModel, object>> sort, SortOrder order = SortOrder.Ascending, Expression<Func<TModel, bool>> filter = null);
+
+        Task<IEnumerable<TModel>> Select(Expression<Func<TModel, bool>> filter = null);
+    }
+}
