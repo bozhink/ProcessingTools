7f03bfafc9971185b7974826b54ad4281c5b37ba
Bozhin Karaivanov <bozhink@gmail.com>
2017-04-06 23:28:19 +0300

    Added IQRCodeEncoder

---
+++ Services/Processors/Image.Processors/Contracts/Processors/IQRCodeEncoder.cs
@@ -0,0 +1,17 @@
+﻿namespace ProcessingTools.Image.Processors.Contracts.Processors
+{
+    using System.Drawing;
+    using System.Threading.Tasks;
+
+    public interface IQRCodeEncoder
+    {
+        Task<byte[]> Encode(string content, int pixelPerModule = 20);
+
+        Task<Bitmap> EncodeBitmap(string content, int pixelPerModule = 20);
+
+        Task<string> EncodeSvg(string content, int pixelPerModule = 20);
+
+        Task<string> EncodeBase64(string content, int pixelPerModule = 20);
+
+    }
+}
---
+++ Services/Processors/Image.Processors/Processors/QRCodeEncoder.cs
@@ -0,0 +1,59 @@
+﻿namespace ProcessingTools.Image.Processors.Processors
+{
+    using System;
+    using System.Drawing;
+    using System.Threading.Tasks;
+    using ProcessingTools.Image.Processors.Contracts.Processors;
+    using QRCoder;
+
+    public class QRCodeEncoder : IQRCodeEncoder
+    {
+        public Task<byte[]> Encode(string content, int pixelPerModule = 20)
+        {
+            var qrcodeData = this.GetQRCodeData(content);
+            var qrcode = new BitmapByteQRCode(qrcodeData);
+            byte[] qrcodeImage = qrcode.GetGraphic(pixelPerModule);
+
+            return Task.FromResult(qrcodeImage);
+        }
+
+        public Task<string> EncodeBase64(string content, int pixelPerModule = 20)
+        {
+            var qrcodeData = this.GetQRCodeData(content);
+            var qrcode = new Base64QRCode(qrcodeData);
+            string qrcodeImage = qrcode.GetGraphic(pixelPerModule);
+
+            return Task.FromResult(qrcodeImage);
+        }
+
+        public Task<Bitmap> EncodeBitmap(string content, int pixelPerModule = 20)
+        {
+            var qrcodeData = this.GetQRCodeData(content);
+            var qrcode = new QRCode(qrcodeData);
+            Bitmap qrcodeImage = qrcode.GetGraphic(pixelPerModule);
+
+            return Task.FromResult(qrcodeImage);
+        }
+
+        public Task<string> EncodeSvg(string content, int pixelPerModule = 20)
+        {
+            var qrcodeData = this.GetQRCodeData(content);
+            var qrcode = new SvgQRCode(qrcodeData);
+            string qrcodeImage = qrcode.GetGraphic(pixelPerModule);
+
+            return Task.FromResult(qrcodeImage);
+        }
+
+        private QRCodeData GetQRCodeData(string content)
+        {
+            if (content == null)
+            {
+                throw new ArgumentNullException(nameof(content));
+            }
+
+            var qrcodeGenerator = new QRCodeGenerator();
+            var qrcodeData = qrcodeGenerator.CreateQrCode(content, QRCodeGenerator.ECCLevel.Q, forceUtf8: true);
+            return qrcodeData;
+        }
+    }
+}
