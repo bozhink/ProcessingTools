5b9c990d4f338ab6162ff726845a05fdec08d156
Bozhin Karaivanov <bozhink@gmail.com>
2017-04-09 15:52:59 +0300

    Removed unused Common items

--- Infrastructure/Common/Dictionaries.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ProcessingTools.Common
-{
-    using System.Collections.Concurrent;
-
-    public static class Dictionaries
-    {
-        public static readonly ConcurrentDictionary<string, string> FileNames = new ConcurrentDictionary<string, string>();
-    }
-}
