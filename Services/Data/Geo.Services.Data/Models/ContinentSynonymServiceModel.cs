f79f4dc669b96cf7d82c3cc1e223ce73d16754f5
Bozhin Karaivanov <bozhink@gmail.com>
2017-04-14 21:38:28 +0300

    Removed IContinentsDataService

--- Services/Data/Geo.Services.Data/Models/ContinentSynonymServiceModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ProcessingTools.Geo.Services.Data.Models
-{
-    public class ContinentSynonymServiceModel
-    {
-        public int Id { get; set; }
-
-        public string Name { get; set; }
-    }
-}
