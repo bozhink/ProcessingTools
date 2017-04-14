bcabc0f41d1ce77b3d66fc0ea2f55ea99de6f833
Bozhin Karaivanov <bozhink@gmail.com>
2017-04-14 21:50:45 +0300

    IDataService is renamed to IDataServiceAsync

---
+++ Infrastructure/Contracts/Services/Data/IDataServiceAsync.cs
@@ -0,0 +1,15 @@
+﻿namespace ProcessingTools.Contracts.Services.Data
+{
+    using System.Threading.Tasks;
+
+    public interface IDataServiceAsync<TModel>
+    {
+        Task<object> InsertAsync(TModel model);
+
+        Task<object> DeleteAsync(object id);
+
+        Task<TModel> GetByIdAsync(object id);
+
+        Task<object> UpdateAsync(TModel model);
+    }
+}
