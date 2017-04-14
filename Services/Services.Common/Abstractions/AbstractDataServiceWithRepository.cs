babafcbca27f7132b7ae8a2c479d4c8f5a8425e4
Bozhin Karaivanov <bozhink@gmail.com>
2017-04-14 22:23:56 +0300

    Removed IRepositoryDataService

--- Services/Services.Common/Abstractions/AbstractDataServiceWithRepository.cs
+++ Services/Services.Common/Abstractions/AbstractDataServiceWithRepository.cs
@@ -1,14 +1,18 @@
 ﻿namespace ProcessingTools.Services.Common.Abstractions
 {
     using System;
+    using System.Collections.Concurrent;
+    using System.Collections.Generic;
     using System.Linq;
     using System.Linq.Expressions;
     using System.Threading.Tasks;
+    using ProcessingTools.Constants;
     using ProcessingTools.Contracts.Data.Repositories;
-    using ProcessingTools.Contracts.Services.Data;
+    using ProcessingTools.Exceptions;
     using ProcessingTools.Extensions;
+    using ProcessingTools.Extensions.Linq;
 
-    public abstract class AbstractDataServiceWithRepository<TDbModel, TServiceModel> : AbstractRepositoryDataService<TDbModel, TServiceModel>, IMultiEntryDataService<TServiceModel>, IDisposable
+    public abstract class AbstractDataServiceWithRepository<TDbModel, TServiceModel> : IDisposable
     {
         private readonly ICrudRepository<TDbModel> repository;
 
@@ -22,25 +26,124 @@
             this.repository = repository;
         }
 
-        protected override abstract Expression<Func<TDbModel, TServiceModel>> MapDbModelToServiceModel { get; }
+        protected abstract Expression<Func<TDbModel, TServiceModel>> MapDbModelToServiceModel { get; }
 
-        protected override abstract Expression<Func<TServiceModel, TDbModel>> MapServiceModelToDbModel { get; }
+        protected abstract Expression<Func<TServiceModel, TDbModel>> MapServiceModelToDbModel { get; }
 
-        protected override abstract Expression<Func<TDbModel, object>> SortExpression { get; }
+        protected abstract Expression<Func<TDbModel, object>> SortExpression { get; }
 
-        public virtual Task<object> Add(params TServiceModel[] models) => base.Add(repository: this.repository, models: models);
+        public virtual async Task<object> Add(params TServiceModel[] models)
+        {
+            if (models == null || models.Length < 1)
+            {
+                throw new ArgumentNullException(nameof(models));
+            }
 
-        public virtual Task<IQueryable<TServiceModel>> All() => base.All(repository: this.repository);
+            var entities = this.MapServiceModelsToEntities(models);
 
-        public virtual Task<IQueryable<TServiceModel>> Query(int skip, int take) => base.Query(repository: this.repository, skip: skip, take: take);
+            var tasks = entities.Select(e => this.repository.Add(e)).ToArray();
 
-        public virtual Task<object> Delete(params object[] ids) => base.Delete(repository: this.repository, ids: ids);
+            await Task.WhenAll(tasks);
 
-        public virtual Task<object> Delete(params TServiceModel[] models) => base.Delete(repository: this.repository, models: models);
+            return await this.repository.SaveChangesAsync();
+        }
 
-        public virtual Task<IQueryable<TServiceModel>> Get(params object[] ids) => base.Get(repository: this.repository, ids: ids);
+        public virtual async Task<IQueryable<TServiceModel>> All()
+        {
+            var result = await this.repository.Query
+                .Select(this.MapDbModelToServiceModel)
+                .ToListAsync();
 
-        public virtual Task<object> Update(params TServiceModel[] models) => base.Update(repository: this.repository, models: models);
+            return result.AsQueryable();
+        }
+
+        public virtual async Task<IQueryable<TServiceModel>> Query(int skip, int take)
+        {
+            if (skip < 0)
+            {
+                throw new InvalidSkipValuePagingException();
+            }
+
+            if (1 > take || take > PagingConstants.MaximalItemsPerPageAllowed)
+            {
+                throw new InvalidTakeValuePagingException();
+            }
+
+            var result = await this.repository.Query
+                .OrderBy(this.SortExpression)
+                .Skip(skip)
+                .Take(take)
+                .Select(this.MapDbModelToServiceModel)
+                .ToListAsync();
+
+            return result.AsQueryable();
+        }
+
+        public virtual async Task<object> Delete(params object[] ids)
+        {
+            if (ids == null || ids.Length < 1)
+            {
+                throw new ArgumentNullException(nameof(ids));
+            }
+
+            var tasks = ids.Select(id => this.repository.Delete(id)).ToArray();
+
+            await Task.WhenAll(tasks);
+
+            return await this.repository.SaveChangesAsync();
+        }
+
+        public virtual async Task<object> Delete(params TServiceModel[] models)
+        {
+            if (models == null || models.Length < 1)
+            {
+                throw new ArgumentNullException(nameof(models));
+            }
+
+            var entities = this.MapServiceModelsToEntities(models);
+
+            var tasks = entities.Select(e => this.repository.Delete(e)).ToArray();
+
+            await Task.WhenAll(tasks);
+
+            return await this.repository.SaveChangesAsync();
+        }
+
+        public virtual async Task<IQueryable<TServiceModel>> Get(params object[] ids)
+        {
+            if (ids == null || ids.Length < 1)
+            {
+                throw new ArgumentNullException(nameof(ids));
+            }
+
+            var mapping = this.MapDbModelToServiceModel.Compile();
+
+            var result = new ConcurrentQueue<TServiceModel>();
+
+            foreach (var id in ids)
+            {
+                var entity = await this.repository.GetById(id);
+                result.Enqueue(mapping.Invoke(entity));
+            }
+
+            return new HashSet<TServiceModel>(result).AsQueryable();
+        }
+
+        public virtual async Task<object> Update(params TServiceModel[] models)
+        {
+            if (models == null || models.Length < 1)
+            {
+                throw new ArgumentNullException(nameof(models));
+            }
+
+            var entities = this.MapServiceModelsToEntities(models);
+
+            var tasks = entities.Select(e => this.repository.Update(e)).ToArray();
+
+            await Task.WhenAll(tasks);
+
+            return await this.repository.SaveChangesAsync();
+        }
 
         public void Dispose()
         {
@@ -54,6 +157,13 @@
             {
                 this.repository.TryDispose();
             }
+        }
+
+        private IEnumerable<TDbModel> MapServiceModelsToEntities(TServiceModel[] models)
+        {
+            return models.AsQueryable()
+                .Select(this.MapServiceModelToDbModel)
+                .ToList();
         }
     }
 }
