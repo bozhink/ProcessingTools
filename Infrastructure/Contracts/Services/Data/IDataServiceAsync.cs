e482644869a3e70b47c7bb890a7c11b935544e43
Bozhin Karaivanov <bozhink@gmail.com>
2017-04-15 12:28:51 +0300

    Added IFilter

--- Infrastructure/Contracts/Services/Data/IDataServiceAsync.cs
+++ Infrastructure/Contracts/Services/Data/IDataServiceAsync.cs
@@ -1,14 +1,26 @@
 ﻿namespace ProcessingTools.Contracts.Services.Data
 {
     using System.Threading.Tasks;
+    using ProcessingTools.Enumerations;
 
-    public interface IDataServiceAsync<TModel>
+    public interface IDataServiceAsync<TModel, TFilter>
+        where TFilter : IFilter
     {
-        Task<object> InsertAsync(TModel model);
+        Task<object> DeleteAsync(TModel model);
 
         Task<object> DeleteAsync(object id);
 
         Task<TModel> GetByIdAsync(object id);
+
+        Task<object> InsertAsync(TModel model);
+
+        Task<object> SaveChangesAsync();
+
+        Task<TModel[]> SelectAsync(TFilter filter);
+
+        Task<TModel[]> SelectAsync(TFilter filter, int skip, int take, string sortColumn, SortOrder sortOrder = SortOrder.Ascending);
+
+        Task<long> SelectCountAsync(TFilter filter);
 
         Task<object> UpdateAsync(TModel model);
     }
