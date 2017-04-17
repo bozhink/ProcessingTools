fb2d236650ca003a6c9e236d8a5abc4fe7ce8433
Bozhin Karaivanov <bozhink@gmail.com>
2017-04-17 15:42:43 +0300

    Added IPageViewModel

---
+++ Web/ProcessingTools.Web.Common/ViewModels/Contracts/IPageViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ProcessingTools.Web.Common.ViewModels.Contracts
+{
+    public interface IPageViewModel
+    {
+        string PageTitle { get; }
+
+        string PageHeading { get; }
+
+        string ReturnUrl { get; }
+    }
+}
