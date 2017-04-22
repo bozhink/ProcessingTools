2f4d0f258ddd5e9bf6eaaf82a12bd3ef76bda980
Bozhin Karaivanov <bozhink@gmail.com>
2017-04-23 00:13:35 +0300

    Added EntityProvinceSynonymsDataService

---
+++ Services/Data/Geo.Services.Data.Entity/Services/EntityProvinceSynonymsDataService.cs
@@ -0,0 +1,35 @@
+﻿namespace ProcessingTools.Geo.Services.Data.Entity.Services
+{
+    using System;
+    using ProcessingTools.Contracts.Services;
+    using ProcessingTools.Contracts.Services.Data.Geo.Filters;
+    using ProcessingTools.Contracts.Services.Data.Geo.Models;
+    using ProcessingTools.Geo.Data.Entity.Contracts;
+    using ProcessingTools.Geo.Data.Entity.Models;
+    using ProcessingTools.Geo.Services.Data.Entity.Abstractions;
+    using ProcessingTools.Geo.Services.Data.Entity.Contracts.Services;
+
+    public class EntityProvinceSynonymsDataService : AbstractGeoDataSynonymsService<ProvinceSynonym, IProvinceSynonym, IProvinceSynonymsFilter>, IEntityProvinceSynonymsDataService
+    {
+        public EntityProvinceSynonymsDataService(IGeoRepository<ProvinceSynonym> repository, IEnvironment environment)
+            : base(repository, environment)
+        {
+        }
+
+        protected override Func<ProvinceSynonym, IProvinceSynonym> MapEntityToModel => s => new ProcessingTools.Geo.Services.Data.Entity.Models.ProvinceSynonym
+        {
+            Id = s.Id,
+            Name = s.Name,
+            LanguageCode = s.LanguageCode,
+            ParentId = s.ProvinceId
+        };
+
+        protected override Func<IProvinceSynonym, ProvinceSynonym> MapModelToEntity => s => new ProvinceSynonym
+        {
+            Id = s.Id,
+            Name = s.Name,
+            ProvinceId = s.ParentId,
+            LanguageCode = s.LanguageCode
+        };
+    }
+}
