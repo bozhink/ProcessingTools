429d9183ae905b9148879e5db50f2644d6a22023
Bozhin Karaivanov <bozhink@gmail.com>
2017-04-23 00:11:44 +0300

    Added EntityMunicipalitySynonymsDataService

---
+++ Services/Data/Geo.Services.Data.Entity/Services/EntityMunicipalitySynonymsDataService.cs
@@ -0,0 +1,35 @@
+﻿namespace ProcessingTools.Geo.Services.Data.Entity.Services
+{
+    using System;
+    using ProcessingTools.Contracts.Services;
+    using ProcessingTools.Contracts.Services.Data.Geo.Filters;
+    using ProcessingTools.Contracts.Services.Data.Geo.Models;
+    using ProcessingTools.Geo.Data.Entity.Contracts;
+    using ProcessingTools.Geo.Data.Entity.Models;
+    using ProcessingTools.Geo.Services.Data.Entity.Abstractions;
+    using ProcessingTools.Geo.Services.Data.Entity.Contracts.Services;
+
+    public class EntityMunicipalitySynonymsDataService : AbstractGeoDataSynonymsService<MunicipalitySynonym, IMunicipalitySynonym, IMunicipalitySynonymsFilter>, IEntityMunicipalitySynonymsDataService
+    {
+        public EntityMunicipalitySynonymsDataService(IGeoRepository<MunicipalitySynonym> repository, IEnvironment environment)
+            : base(repository, environment)
+        {
+        }
+
+        protected override Func<MunicipalitySynonym, IMunicipalitySynonym> MapEntityToModel => s => new ProcessingTools.Geo.Services.Data.Entity.Models.MunicipalitySynonym
+        {
+            Id = s.Id,
+            Name = s.Name,
+            LanguageCode = s.LanguageCode,
+            ParentId = s.MunicipalityId
+        };
+
+        protected override Func<IMunicipalitySynonym, MunicipalitySynonym> MapModelToEntity => s => new MunicipalitySynonym
+        {
+            Id = s.Id,
+            Name = s.Name,
+            MunicipalityId = s.ParentId,
+            LanguageCode = s.LanguageCode
+        };
+    }
+}
