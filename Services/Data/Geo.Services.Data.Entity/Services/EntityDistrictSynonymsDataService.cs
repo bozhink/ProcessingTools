8a27813a44b0876f07c615b929600bca0191ee13
Bozhin Karaivanov <bozhink@gmail.com>
2017-04-23 00:09:18 +0300

    Added EntityDistrictSynonymsDataService

---
+++ Services/Data/Geo.Services.Data.Entity/Services/EntityDistrictSynonymsDataService.cs
@@ -0,0 +1,35 @@
+﻿namespace ProcessingTools.Geo.Services.Data.Entity.Services
+{
+    using System;
+    using ProcessingTools.Contracts.Services;
+    using ProcessingTools.Contracts.Services.Data.Geo.Filters;
+    using ProcessingTools.Contracts.Services.Data.Geo.Models;
+    using ProcessingTools.Geo.Data.Entity.Contracts;
+    using ProcessingTools.Geo.Data.Entity.Models;
+    using ProcessingTools.Geo.Services.Data.Entity.Abstractions;
+    using ProcessingTools.Geo.Services.Data.Entity.Contracts.Services;
+
+    public class EntityDistrictSynonymsDataService : AbstractGeoDataSynonymsService<DistrictSynonym, IDistrictSynonym, IDistrictSynonymsFilter>, IEntityDistrictSynonymsDataService
+    {
+        public EntityDistrictSynonymsDataService(IGeoRepository<DistrictSynonym> repository, IEnvironment environment)
+            : base(repository, environment)
+        {
+        }
+
+        protected override Func<DistrictSynonym, IDistrictSynonym> MapEntityToModel => s => new ProcessingTools.Geo.Services.Data.Entity.Models.DistrictSynonym
+        {
+            Id = s.Id,
+            Name = s.Name,
+            LanguageCode = s.LanguageCode,
+            ParentId = s.DistrictId
+        };
+
+        protected override Func<IDistrictSynonym, DistrictSynonym> MapModelToEntity => s => new DistrictSynonym
+        {
+            Id = s.Id,
+            Name = s.Name,
+            DistrictId = s.ParentId,
+            LanguageCode = s.LanguageCode
+        };
+    }
+}
