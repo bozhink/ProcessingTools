9b18a130c2978fdecde18d5da6404644a8d8d1c4
Bozhin Karaivanov <bozhink@gmail.com>
2017-04-24 00:09:09 +0300

    Added EntityProvincesDataService

---
+++ Services/Data/Geo.Services.Data.Entity/Services/EntityProvincesDataService.cs
@@ -0,0 +1,71 @@
+﻿namespace ProcessingTools.Geo.Services.Data.Entity.Services
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+    using ProcessingTools.Contracts.Services;
+    using ProcessingTools.Contracts.Services.Data.Geo.Filters;
+    using ProcessingTools.Contracts.Services.Data.Geo.Models;
+    using ProcessingTools.Geo.Data.Entity.Contracts;
+    using ProcessingTools.Geo.Data.Entity.Models;
+    using ProcessingTools.Geo.Services.Data.Entity.Abstractions;
+    using ProcessingTools.Geo.Services.Data.Entity.Contracts.Services;
+
+    public class EntityProvincesDataService : AbstractGeoDataService<Province, IProvince, IProvincesFilter>, IEntityProvincesDataService
+    {
+        public EntityProvincesDataService(IGeoRepository<Province> repository, IEnvironment environment)
+            : base(repository, environment)
+        {
+        }
+
+        protected override Func<Province, IProvince> MapEntityToModel => m => new ProcessingTools.Geo.Services.Data.Entity.Models.Province
+        {
+            Id = m.Id,
+            Name = m.Name,
+            CountryId = m.CountryId,
+            StateId = m.StateId,
+            Synonyms = m.Synonyms
+                .Select(
+                    s => new ProcessingTools.Geo.Services.Data.Entity.Models.ProvinceSynonym
+                    {
+                        Id = s.Id,
+                        LanguageCode = s.LanguageCode,
+                        Name = s.Name,
+                        ParentId = m.Id
+                    })
+                .ToList<IProvinceSynonym>()
+        };
+
+        protected override Func<IProvince, Province> MapModelToEntity => m => new Province
+        {
+            Id = m.Id,
+            Name = m.Name,
+            CountryId = m.CountryId,
+            StateId = m.StateId
+        };
+
+        protected override IQueryable<Province> GetQuery(IProvincesFilter filter)
+        {
+            var query = this.Repository.Queryable()
+                .Include(e => e.Country)
+                .Include(e => e.Synonyms);
+
+            if (filter != null)
+            {
+                query = query.Where(
+                    c => (!filter.Id.HasValue || c.Id == filter.Id) &&
+                         (string.IsNullOrEmpty(filter.Name) || c.Name.ToLower().Contains(filter.Name.ToLower())) &&
+                         (string.IsNullOrEmpty(filter.Country) || c.Country.Name.ToLower().Contains(filter.Country.ToLower())) &&
+                         (string.IsNullOrEmpty(filter.State) || c.State.Name.ToLower().Contains(filter.State.ToLower())) &&
+                         (string.IsNullOrEmpty(filter.Region) || c.Regions.Any(s => s.Name.ToLower().Contains(filter.Region.ToLower()))) &&
+                         (string.IsNullOrEmpty(filter.District) || c.Districts.Any(s => s.Name.ToLower().Contains(filter.District.ToLower()))) &&
+                         (string.IsNullOrEmpty(filter.Municipality) || c.Municipalities.Any(s => s.Name.ToLower().Contains(filter.Municipality.ToLower()))) &&
+                         (string.IsNullOrEmpty(filter.County) || c.Counties.Any(s => s.Name.ToLower().Contains(filter.County.ToLower()))) &&
+                         (string.IsNullOrEmpty(filter.City) || c.Cities.Any(s => s.Name.ToLower().Contains(filter.City.ToLower()))) &&
+                         (string.IsNullOrEmpty(filter.Synonym) || c.Synonyms.Any(s => s.Name.ToLower().Contains(filter.Synonym.ToLower()))));
+            }
+
+            return query;
+        }
+    }
+}
