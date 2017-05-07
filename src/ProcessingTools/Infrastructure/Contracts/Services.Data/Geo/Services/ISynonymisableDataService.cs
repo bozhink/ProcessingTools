b2581278fa2c2fff712602c7bd9ed6066620d87a
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2017-05-07 14:44:17 +0300

    Updated ISynonymisableDataService

--- src/ProcessingTools/Infrastructure/Contracts/Services.Data/Geo/Services/ISynonymisableDataService.cs
+++ src/ProcessingTools/Infrastructure/Contracts/Services.Data/Geo/Services/ISynonymisableDataService.cs
@@ -1,15 +1,23 @@
 ﻿namespace ProcessingTools.Contracts.Services.Data.Geo.Services
 {
     using System.Threading.Tasks;
+    using ProcessingTools.Contracts.Services.Data.Geo.Filters;
     using ProcessingTools.Contracts.Services.Data.Geo.Models;
 
-    public interface ISynonymisableDataService<TSynonym>
+    public interface ISynonymisableDataService<TSynonym, TSynonymFilter>
         where TSynonym : ISynonym
+        where TSynonymFilter : ISynonymFilter
     {
-        Task<object> AddSynonymAsync(int modelId, TSynonym synonym);
+        Task<object> AddSynonymsAsync(int modelId, params TSynonym[] synonyms);
 
-        Task<object> RemoveSynonymAsync(int modelId, int synonymId);
+        Task<object> RemoveSynonymsAsync(int modelId, params int[] synonymIds);
 
-        Task<object> UpdateSynonymAsync(int modelId, TSynonym synonym);
+        Task<object> UpdateSynonymsAsync(int modelId, params TSynonym[] synonyms);
+
+        Task<TSynonym[]> SelectSynonymsAsync(TSynonymFilter filter);
+
+        Task<long> SelectSynonymCountAsync(TSynonymFilter filter);
+
+        Task<TSynonym> GetSynonymByIdAsync(int id);
     }
 }
