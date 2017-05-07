69c36b0d1ed9a96aff134c4bc255c4fead2b187e
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2017-05-08 01:04:02 +0300

    Removed IEntity*SynonymsDataService-s

--- src/ProcessingTools/Services/Data/Geo.Services.Data.Entity/Contracts/Services/IEntityContinentSynonymsDataService.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ProcessingTools.Geo.Services.Data.Entity.Contracts.Services
-{
-    using ProcessingTools.Contracts.Services.Data.Geo.Services;
-
-    public interface IEntityContinentSynonymsDataService : IContinentSynonymsDataService
-    {
-    }
-}
