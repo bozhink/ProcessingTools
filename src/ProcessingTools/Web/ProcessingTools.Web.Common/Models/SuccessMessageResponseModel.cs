0456984da5ad613b039ccd9113e6c4249b74851d
Bozhin Karaivanov <bozhink@gmail.com>
2017-05-21 00:18:43 +0300

    Removed SuccessMessageResponseModel

--- src/ProcessingTools/Web/ProcessingTools.Web.Common/Models/SuccessMessageResponseModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ProcessingTools.Web.Common.Models
-{
-    public class SuccessMessageResponseModel
-    {
-        public string Message { get; set; }
-
-        public bool Succeeded { get; set; }
-    }
-}
