c18d1d49587cd833d7a79ee35823798b46242226
Bozhin Karaivanov <bozhink@gmail.com>
2017-05-24 21:45:40 +0300

    CommonDataPartial is renamed to EditCommonDataPartial

--- src/ProcessingTools/Web/ProcessingTools.Web/Constants/ViewNames.cs
+++ src/ProcessingTools/Web/ProcessingTools.Web/Constants/ViewNames.cs
@@ -3,12 +3,12 @@
     public sealed class ViewNames
     {
         public const string AddressesPartial = "_Addresses";
-        public const string CommonDataPartial = "_CommonData";
         public const string DefaultLayout = "~/Views/Shared/_Layout.cshtml";
         public const string DefaultLoginPartial = "~/Views/Shared/Partials/_LoginPartial.cshtml";
         public const string DefaultNavigationBar = "~/Views/Shared/Partials/_NavigationBar.cshtml";
         public const string Edit = "Edit";
-        public const string EditButtonsPartial = "_EditButtons";
+        public const string EditButtonsPartial = "Partials/_EditButtons";
+        public const string EditCommonDataPartial = "Partials/_EditCommonData";
         public const string Error = "Error";
         public const string Lockout = "Lockout";
         public const string NavigationFootPartial = "~/Views/Shared/Partials/_NavigationFoot.cshtml";
