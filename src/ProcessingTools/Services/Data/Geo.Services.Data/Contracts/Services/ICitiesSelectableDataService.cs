240ba1ef37efd4a45a86131bfde7ca6e5d5648e5
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2017-06-18 00:37:44 +0300

    Remove ICitiesSelectableDataService and ICountriesSelectableDataService

--- src/ProcessingTools/Services/Data/Geo.Services.Data/Contracts/Services/ICitiesSelectableDataService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ProcessingTools.Geo.Services.Data.Contracts.Services
-{
-    using ProcessingTools.Contracts.Services.Data;
-    using ProcessingTools.Geo.Services.Data.Contracts.Models;
-
-    public interface ICitiesSelectableDataService : ISelectableDataService<ICityListableModel>
-    {
-    }
-}
