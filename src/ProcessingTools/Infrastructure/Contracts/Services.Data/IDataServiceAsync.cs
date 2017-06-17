e86f4fc6c18b5b32eff7292fdb51dc859fefd3d1
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2017-06-18 00:43:49 +0300

    Update ISelectableDataService

--- src/ProcessingTools/Infrastructure/Contracts/Services.Data/IDataServiceAsync.cs
+++ src/ProcessingTools/Infrastructure/Contracts/Services.Data/IDataServiceAsync.cs
@@ -2,24 +2,15 @@
 {
     using System.Threading.Tasks;
     using ProcessingTools.Contracts.Filters;
-    using ProcessingTools.Enumerations;
 
-    public interface IDataServiceAsync<TModel, TFilter>
+    public interface IDataServiceAsync<TModel, TFilter> : ISelectableDataServiceAsync<TModel, TFilter>
         where TFilter : IFilter
     {
         Task<object> DeleteAsync(TModel model);
 
         Task<object> DeleteAsync(object id);
 
-        Task<TModel> GetByIdAsync(object id);
-
         Task<object> InsertAsync(TModel model);
-
-        Task<TModel[]> SelectAsync(TFilter filter);
-
-        Task<TModel[]> SelectAsync(TFilter filter, int skip, int take, string sortColumn, SortOrder sortOrder = SortOrder.Ascending);
-
-        Task<long> SelectCountAsync(TFilter filter);
 
         Task<object> UpdateAsync(TModel model);
     }
