8f8f7236b21bcc013e28dca5c6999d1da0ce36dc
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2017-06-17 21:36:31 +0300

    Add ProcessingTools.Contracts.Data.Repositories.Geo

---
+++ src/ProcessingTools/Infrastructure/Contracts/Data/Repositories/Geo/IGeoNamesRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace ProcessingTools.Contracts.Data.Repositories.Geo
+{
+    using ProcessingTools.Contracts.Filters.Geo;
+    using ProcessingTools.Contracts.Models.Geo;
+
+    public interface IGeoNamesRepository : IRepositoryAsync<IGeoName, IGeoNamesFilter>
+    {
+    }
+}
