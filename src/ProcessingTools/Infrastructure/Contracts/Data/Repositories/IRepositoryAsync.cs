5273ca79aaeb90e21a42b32be099c753db09a0cc
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2017-06-17 21:05:52 +0300

    Add IRepositoryAsync

---
+++ src/ProcessingTools/Infrastructure/Contracts/Data/Repositories/IRepositoryAsync.cs
@@ -0,0 +1,28 @@
+﻿namespace ProcessingTools.Contracts.Data.Repositories
+{
+    using System.Threading.Tasks;
+    using ProcessingTools.Contracts.Filters;
+    using ProcessingTools.Enumerations;
+
+    public interface IRepositoryAsync<TModel, TFilter>
+        where TFilter : IFilter
+    {
+        Task<object> DeleteAsync(TModel model);
+
+        Task<object> DeleteAsync(object id);
+
+        Task<TModel> GetByIdAsync(object id);
+
+        Task<object> InsertAsync(TModel model);
+
+        Task<long> SaveChangesAsync();
+
+        Task<TModel[]> SelectAsync(TFilter filter);
+
+        Task<TModel[]> SelectAsync(TFilter filter, int skip, int take, string sortColumn, SortOrder sortOrder = SortOrder.Ascending);
+
+        Task<long> SelectCountAsync(TFilter filter);
+
+        Task<object> UpdateAsync(TModel model);
+    }
+}
