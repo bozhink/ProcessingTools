eb155e6fce7451db6c40f131e37b9d183ec1d73c
Bozhin Karaivanov <bozhink@gmail.com>
2017-06-18 15:45:51 +0300

    Update GeoEpithetsDataMiner

--- src/ProcessingTools/Services/Data.Miners/Data.Miners/Miners/Geo/GeoEpithetsDataMiner.cs
+++ src/ProcessingTools/Services/Data.Miners/Data.Miners/Miners/Geo/GeoEpithetsDataMiner.cs
@@ -1,12 +1,12 @@
 ﻿namespace ProcessingTools.Data.Miners.Miners.Geo
 {
     using ProcessingTools.Contracts.Filters;
+    using ProcessingTools.Contracts.Models.Geo;
+    using ProcessingTools.Contracts.Services.Data.Geo;
     using ProcessingTools.Data.Miners.Contracts.Miners.Geo;
     using ProcessingTools.Data.Miners.Generics;
-    using ProcessingTools.Geo.Services.Data.Contracts;
-    using ProcessingTools.Geo.Services.Data.Models;
 
-    public class GeoEpithetsDataMiner : SimpleServiceStringDataMiner<IGeoEpithetsDataService, GeoEpithetServiceModel, IFilter>, IGeoEpithetsDataMiner
+    public class GeoEpithetsDataMiner : SimpleServiceStringDataMiner<IGeoEpithetsDataService, IGeoEpithet, ITextFilter>, IGeoEpithetsDataMiner
     {
         public GeoEpithetsDataMiner(IGeoEpithetsDataService service)
             : base(service)
