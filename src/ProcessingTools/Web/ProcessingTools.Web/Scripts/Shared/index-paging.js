767e8abb6e4b926a0a805759edf31efb06b5b09e
Bozhin Karaivanov <bozhink@gmail.com>
2017-06-25 23:09:48 +0300

    Add index-paging.js

---
+++ src/ProcessingTools/Web/ProcessingTools.Web/Scripts/Shared/index-paging.js
@@ -0,0 +1,56 @@
+﻿(function (window, document) {
+    'use strict';
+
+    function redirect(anchorId) {
+        var id = anchorId || null,
+            anchorElement,
+            href;
+        if (id) {
+            anchorElement = document.getElementById(id);
+            if (anchorElement) {
+                href = anchorElement.href || null;
+                if (href) {
+                    document.location = href;
+                }
+            }
+        }
+    }
+
+    function keydownEventHandler(event) {
+        var e = event || window.event;
+
+        // Page Up
+        if (e.which === 33) {
+            e.stopPropagation();
+            e.preventDefault();
+            redirect('page-next');
+            return false;
+        }
+
+        // Page Down
+        if (e.which === 34) {
+            e.stopPropagation();
+            e.preventDefault();
+            redirect('page-prev');
+            return false;
+        }
+
+        // End
+        if (e.which === 35) {
+            e.stopPropagation();
+            e.preventDefault();
+            redirect('page-last');
+            return false;
+        }
+
+        // Home
+        if (e.which === 36) {
+            e.stopPropagation();
+            e.preventDefault();
+            redirect('page-first');
+            return false;
+        }
+    }
+
+    window.addEventListener('keydown', keydownEventHandler, false);
+}(window, document));
