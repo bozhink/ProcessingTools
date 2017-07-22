d14e1fbf8337c0648b73ef605a6b0b49b0556d32
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2017-07-22 22:28:46 +0300

    Add GeoNamesRequestModel

---
+++ src/ProcessingTools/Web/ProcessingTools.Web/Areas/Data/Models/GeoNames/GeoNamesRequestModel.cs
@@ -0,0 +1,26 @@
+﻿namespace ProcessingTools.Web.Areas.Data.Models.GeoNames
+{
+    using System;
+    using System.Linq;
+    using ProcessingTools.Contracts.Models.Geo;
+
+    public class GeoNamesRequestModel
+    {
+        public string Names { get; set; }
+
+        public IGeoName[] ToArray()
+        {
+            if (string.IsNullOrWhiteSpace(this.Names))
+            {
+                return new IGeoName[] { };
+            }
+
+            return this.Names.Split(new[] { '\n' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => new GeoNameRequestModel
+                {
+                    Name = x.Trim(new[] { '\r' })
+                })
+                .ToArray();
+        }
+    }
+}
