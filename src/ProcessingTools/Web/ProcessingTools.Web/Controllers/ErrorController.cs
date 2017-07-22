5cea836d513ba5e09fc51dfc8b5972ecabb2b9ff
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2017-07-22 22:51:11 +0300

    Add ProcessingTools.Web.Controllers.ErrorController

---
+++ src/ProcessingTools/Web/ProcessingTools.Web/Controllers/ErrorController.cs
@@ -0,0 +1,19 @@
+﻿namespace ProcessingTools.Web.Controllers
+{
+    using System;
+    using System.Web.Mvc;
+    using ProcessingTools.Web.Constants;
+
+    public class ErrorController : Controller
+    {
+        public const string ControllerName = "Error";
+        public const string AreaName = AreaNames.DefaultArea;
+        public const string IndexActionName = RouteValues.IndexActionName;
+
+        [HttpGet, ActionName(IndexActionName)]
+        public ActionResult Index(Exception model)
+        {
+            return this.View(model: model);
+        }
+    }
+}
