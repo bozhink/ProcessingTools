15715f76ec072f32407136666808ef6b13127a96
Bozhin Karaivanov <bozhink@users.noreply.github.com>
2017-08-20 23:01:34 +0300

    Add DocTypeConstants.TaxPubDtdPath

--- src/ProcessingTools/Common/Constants/Schema/DocTypeConstants.cs
+++ src/ProcessingTools/Common/Constants/Schema/DocTypeConstants.cs
@@ -1,8 +1,13 @@
 ﻿namespace ProcessingTools.Constants.Schema
 {
+    using System.Configuration;
+    using ProcessingTools.Constants.Configuration;
+
     public static class DocTypeConstants
     {
+        public const string TaxPubName = "article";
         public const string TaxPubPublicId = "-//TaxonX//DTD Taxonomic Treatment Publishing DTD v0 20100105//EN";
         public const string TaxPubSystemId = "tax-treatment-NS0.dtd";
+        public static readonly string TaxPubDtdPath = ConfigurationManager.AppSettings[AppSettingsKeys.TaxPubDtdPath];
     }
 }
