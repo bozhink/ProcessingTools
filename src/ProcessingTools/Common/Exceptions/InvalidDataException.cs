df1f9aaab004077c719234ae107ad1fd75f0aec2
Bozhin Karaivanov <bozhink@users.noreply.github.com>
2017-08-27 00:28:13 +0300

    Add InvalidDataException

---
+++ src/ProcessingTools/Common/Exceptions/InvalidDataException.cs
@@ -0,0 +1,65 @@
+﻿// <copyright file="InvalidDataException.cs" company="ProcessingTools">
+// Copyright (c) 2017 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Exceptions
+{
+    using System;
+    using System.Runtime.Serialization;
+
+    /// <summary>
+    /// Represents error that occur when data is invalid.
+    /// </summary>
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Sonar Code Smell", "S3925: Update this implementation of 'ISerializable' to conform to the recommended serialization pattern", Justification = "Not Applicable")]
+    [Serializable]
+    public class InvalidDataException : Exception
+    {
+        private const string DefaultMessage = "Invalid data.";
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InvalidDataException"/> class with default
+        /// error message.
+        /// </summary>
+        public InvalidDataException()
+            : base(DefaultMessage)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InvalidDataException"/> class with a specified
+        /// error message.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
+        public InvalidDataException(string message)
+            : base($"{DefaultMessage}{Environment.NewLine}{message}")
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InvalidDataException"/> class with a specified
+        /// error message and a reference to the inner exception that is the cause of this exception.
+        /// </summary>
+        /// <param name="message">The error message that explains the reason for the exception.</param>
+        /// <param name="innerException">The exception that is the cause of the current exception.</param>
+        public InvalidDataException(string message, Exception innerException)
+            : base($"{DefaultMessage}{Environment.NewLine}{message}", innerException)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InvalidDataException"/> class with serialized data.
+        /// </summary>
+        /// <param name="info">The object that holds the serialized object data.</param>
+        /// <param name="context">The contextual information about the source or destination.</param>
+        public InvalidDataException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+
+        /// <inheritdoc/>
+        public override void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            base.GetObjectData(info, context);
+        }
+    }
+}
