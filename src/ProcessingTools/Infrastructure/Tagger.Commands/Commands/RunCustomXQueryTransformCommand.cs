d3d7b0ba04ddc12c7531485d24690618b5bb86f0
Bozhin Karaivanov <bozhink@users.noreply.github.com>
2017-08-27 00:22:02 +0300

    Update RunCustomXQueryTransformCommand

--- src/ProcessingTools/Infrastructure/Tagger.Commands/Commands/RunCustomXQueryTransformCommand.cs
+++ src/ProcessingTools/Infrastructure/Tagger.Commands/Commands/RunCustomXQueryTransformCommand.cs
@@ -33,7 +33,7 @@
             int numberOfFileNames = settings.FileNames.Count;
             if (numberOfFileNames < 3)
             {
-                throw new ApplicationException("The name of the XQuey file should be set.");
+                throw new InvalidOperationException("The name of the XQuey file should be set.");
             }
 
             this.processor.XQueryFileFullName = settings.FileNames[2];
