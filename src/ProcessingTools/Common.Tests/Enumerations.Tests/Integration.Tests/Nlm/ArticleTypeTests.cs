273fe54073e65543729532d3748db24053e56251
Bozhin Karaivanov <bozhink@users.noreply.github.com>
2017-08-27 14:36:12 +0300

    Update ArticleTypeTests

--- src/ProcessingTools/Common.Tests/Enumerations.Tests/Integration.Tests/Nlm/ArticleTypeTests.cs
+++ src/ProcessingTools/Common.Tests/Enumerations.Tests/Integration.Tests/Nlm/ArticleTypeTests.cs
@@ -7,186 +7,184 @@
     [TestClass]
     public class ArticleTypeTests
     {
-        private const string UnexpectedValueMessage = "Unexpected Value.";
-
         [TestMethod]
         public void ArticleType_AbstractMember_ShouldHaveCorrectStringValue()
         {
-            Assert.AreEqual("abstract", ArticleType.Abstract.GetName(), "UnexpectedValueMessage");
+            Assert.AreEqual("abstract", ArticleType.Abstract.GetName());
         }
 
         [TestMethod]
         public void ArticleType_AddendumMember_ShouldHaveCorrectStringValue()
         {
-            Assert.AreEqual("addendum", ArticleType.Addendum.GetName(), "UnexpectedValueMessage");
+            Assert.AreEqual("addendum", ArticleType.Addendum.GetName());
         }
 
         [TestMethod]
         public void ArticleType_AnnouncementMember_ShouldHaveCorrectStringValue()
         {
-            Assert.AreEqual("announcement", ArticleType.Announcement.GetName(), "UnexpectedValueMessage");
+            Assert.AreEqual("announcement", ArticleType.Announcement.GetName());
         }
 
         [TestMethod]
         public void ArticleType_ArticleCommentaryMember_ShouldHaveCorrectStringValue()
         {
-            Assert.AreEqual("article-commentary", ArticleType.ArticleCommentary.GetName(), "UnexpectedValueMessage");
+            Assert.AreEqual("article-commentary", ArticleType.ArticleCommentary.GetName());
         }
 
         [TestMethod]
         public void ArticleType_BookReviewMember_ShouldHaveCorrectStringValue()
         {
-            Assert.AreEqual("book-review", ArticleType.BookReview.GetName(), "UnexpectedValueMessage");
+            Assert.AreEqual("book-review", ArticleType.BookReview.GetName());
         }
 
         [TestMethod]
         public void ArticleType_BooksReceivedMember_ShouldHaveCorrectStringValue()
         {
-            Assert.AreEqual("books-received", ArticleType.BooksReceived.GetName(), "UnexpectedValueMessage");
+            Assert.AreEqual("books-received", ArticleType.BooksReceived.GetName());
         }
 
         [TestMethod]
         public void ArticleType_BriefReportMember_ShouldHaveCorrectStringValue()
         {
-            Assert.AreEqual("brief-report", ArticleType.BriefReport.GetName(), "UnexpectedValueMessage");
+            Assert.AreEqual("brief-report", ArticleType.BriefReport.GetName());
         }
 
         [TestMethod]
         public void ArticleType_CalendarMember_ShouldHaveCorrectStringValue()
         {
-            Assert.AreEqual("calendar", ArticleType.Calendar.GetName(), "UnexpectedValueMessage");
+            Assert.AreEqual("calendar", ArticleType.Calendar.GetName());
         }
 
         [TestMethod]
         public void ArticleType_CaseReportMember_ShouldHaveCorrectStringValue()
         {
-            Assert.AreEqual("case-report", ArticleType.CaseReport.GetName(), "UnexpectedValueMessage");
+            Assert.AreEqual("case-report", ArticleType.CaseReport.GetName());
         }
 
         [TestMethod]
         public void ArticleType_CollectionMember_ShouldHaveCorrectStringValue()
         {
-            Assert.AreEqual("collection", ArticleType.Collection.GetName(), "UnexpectedValueMessage");
+            Assert.AreEqual("collection", ArticleType.Collection.GetName());
         }
 
         [TestMethod]
         public void ArticleType_CorrectionMember_ShouldHaveCorrectStringValue()
         {
-            Assert.AreEqual("correction", ArticleType.Correction.GetName(), "UnexpectedValueMessage");
+            Assert.AreEqual("correction", ArticleType.Correction.GetName());
         }
 
         [TestMethod]
         public void ArticleType_DiscussionMember_ShouldHaveCorrectStringValue()
         {
-            Assert.AreEqual("discussion", ArticleType.Discussion.GetName(), "UnexpectedValueMessage");
+            Assert.AreEqual("discussion", ArticleType.Discussion.GetName());
         }
 
         [TestMethod]
         public void ArticleType_DissertationMember_ShouldHaveCorrectStringValue()
         {
-            Assert.AreEqual("dissertation", ArticleType.Dissertation.GetName(), "UnexpectedValueMessage");
+            Assert.AreEqual("dissertation", ArticleType.Dissertation.GetName());
         }
 
         [TestMethod]
         public void ArticleType_EditorialMember_ShouldHaveCorrectStringValue()
         {
-            Assert.AreEqual("editorial", ArticleType.Editorial.GetName(), "UnexpectedValueMessage");
+            Assert.AreEqual("editorial", ArticleType.Editorial.GetName());
         }
 
         [TestMethod]
         public void ArticleType_InBriefMember_ShouldHaveCorrectStringValue()
         {
-            Assert.AreEqual("in-brief", ArticleType.InBrief.GetName(), "UnexpectedValueMessage");
+            Assert.AreEqual("in-brief", ArticleType.InBrief.GetName());
         }
 
         [TestMethod]
         public void ArticleType_IntroductionMember_ShouldHaveCorrectStringValue()
         {
-            Assert.AreEqual("introduction", ArticleType.Introduction.GetName(), "UnexpectedValueMessage");
+            Assert.AreEqual("introduction", ArticleType.Introduction.GetName());
         }
 
         [TestMethod]
         public void ArticleType_LetterMember_ShouldHaveCorrectStringValue()
         {
-            Assert.AreEqual("letter", ArticleType.Letter.GetName(), "UnexpectedValueMessage");
+            Assert.AreEqual("letter", ArticleType.Letter.GetName());
         }
 
         [TestMethod]
         public void ArticleType_MeetingReportMember_ShouldHaveCorrectStringValue()
         {
-            Assert.AreEqual("meeting-report", ArticleType.MeetingReport.GetName(), "UnexpectedValueMessage");
+            Assert.AreEqual("meeting-report", ArticleType.MeetingReport.GetName());
         }
 
         [TestMethod]
         public void ArticleType_NewsMember_ShouldHaveCorrectStringValue()
         {
-            Assert.AreEqual("news", ArticleType.News.GetName(), "UnexpectedValueMessage");
+            Assert.AreEqual("news", ArticleType.News.GetName());
         }
 
         [TestMethod]
         public void ArticleType_ObituaryMember_ShouldHaveCorrectStringValue()
         {
-            Assert.AreEqual("obituary", ArticleType.Obituary.GetName(), "UnexpectedValueMessage");
+            Assert.AreEqual("obituary", ArticleType.Obituary.GetName());
         }
 
         [TestMethod]
         public void ArticleType_OrationMember_ShouldHaveCorrectStringValue()
         {
-            Assert.AreEqual("oration", ArticleType.Oration.GetName(), "UnexpectedValueMessage");
+            Assert.AreEqual("oration", ArticleType.Oration.GetName());
         }
 
         [TestMethod]
         public void ArticleType_PartialRetractionMember_ShouldHaveCorrectStringValue()
         {
-            Assert.AreEqual("partial-retraction", ArticleType.PartialRetraction.GetName(), "UnexpectedValueMessage");
+            Assert.AreEqual("partial-retraction", ArticleType.PartialRetraction.GetName());
         }
 
         [TestMethod]
         public void ArticleType_ProductReviewMember_ShouldHaveCorrectStringValue()
         {
-            Assert.AreEqual("product-review", ArticleType.ProductReview.GetName(), "UnexpectedValueMessage");
+            Assert.AreEqual("product-review", ArticleType.ProductReview.GetName());
         }
 
         [TestMethod]
         public void ArticleType_RapidCommunicationMember_ShouldHaveCorrectStringValue()
         {
-            Assert.AreEqual("rapid-communication", ArticleType.RapidCommunication.GetName(), "UnexpectedValueMessage");
+            Assert.AreEqual("rapid-communication", ArticleType.RapidCommunication.GetName());
         }
 
         [TestMethod]
         public void ArticleType_ReplyMember_ShouldHaveCorrectStringValue()
         {
-            Assert.AreEqual("reply", ArticleType.Reply.GetName(), "UnexpectedValueMessage");
+            Assert.AreEqual("reply", ArticleType.Reply.GetName());
         }
 
         [TestMethod]
         public void ArticleType_ReprintMember_ShouldHaveCorrectStringValue()
         {
-            Assert.AreEqual("reprint", ArticleType.Reprint.GetName(), "UnexpectedValueMessage");
+            Assert.AreEqual("reprint", ArticleType.Reprint.GetName());
         }
 
         [TestMethod]
         public void ArticleType_ResearchArticleMember_ShouldHaveCorrectStringValue()
         {
-            Assert.AreEqual("research-article", ArticleType.ResearchArticle.GetName(), "UnexpectedValueMessage");
+            Assert.AreEqual("research-article", ArticleType.ResearchArticle.GetName());
         }
 
         [TestMethod]
         public void ArticleType_RetractionMember_ShouldHaveCorrectStringValue()
         {
-            Assert.AreEqual("retraction", ArticleType.Retraction.GetName(), "UnexpectedValueMessage");
+            Assert.AreEqual("retraction", ArticleType.Retraction.GetName());
         }
 
         [TestMethod]
         public void ArticleType_ReviewArticleMember_ShouldHaveCorrectStringValue()
         {
-            Assert.AreEqual("review-article", ArticleType.ReviewArticle.GetName(), "UnexpectedValueMessage");
+            Assert.AreEqual("review-article", ArticleType.ReviewArticle.GetName());
         }
 
         [TestMethod]
         public void ArticleType_TranslationMember_ShouldHaveCorrectStringValue()
         {
-            Assert.AreEqual("translation", ArticleType.Translation.GetName(), "UnexpectedValueMessage");
+            Assert.AreEqual("translation", ArticleType.Translation.GetName());
         }
     }
 }
