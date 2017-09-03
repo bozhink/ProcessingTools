7d19e011692524d964507d13665b4bd37bf9b227
Bozhin Karaivanov <bozhink@users.noreply.github.com>
2017-09-03 22:00:18 +0300

    Add project ProcessingTools.Web.Services

---
+++ src/ProcessingTools/Services/Web/ProcessingTools.Web.Services/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file="AssemblyInfo.cs" company="ProcessingTools">
+// Copyright (c) 2017 ProcessingTools. All rights reserved.
+// </copyright>
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ProcessingTools.Web.Services")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ProcessingTools.Web.Services")]
+[assembly: AssemblyCopyright("Copyright © 2017 ProcessingTools")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("6a2f0be3-c1cf-4a5f-9354-39462c6a068a")]
+[assembly: AssemblyVersion("0.0.9.0")]
+[assembly: AssemblyFileVersion("0.0.9.0")]
