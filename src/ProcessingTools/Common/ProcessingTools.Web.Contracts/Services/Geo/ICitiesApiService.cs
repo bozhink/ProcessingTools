e14aca1e75918bac5c1e0d0f2ba34de0868682dc
Bozhin Karaivanov <bozhink@users.noreply.github.com>
2017-09-03 23:58:59 +0300

    Add ICitiesApiService

---
+++ src/ProcessingTools/Common/ProcessingTools.Web.Contracts/Services/Geo/ICitiesApiService.cs
@@ -0,0 +1,28 @@
+﻿// <copyright file="ICitiesApiService.cs" company="ProcessingTools">
+// Copyright (c) 2017 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Web.Contracts.Services.Geo
+{
+    using System.Threading.Tasks;
+    using ProcessingTools.Web.Models.Geo.Cities;
+
+    /// <summary>
+    /// Web API service for managing of city objects.
+    /// </summary>
+    public interface ICitiesApiService
+    {
+        /// <summary>
+        /// Gets all city objects.
+        /// </summary>
+        /// <returns>Task of array of the response model</returns>
+        Task<CityResponseModel[]> GetAllAsync();
+
+        /// <summary>
+        /// Gets single city object by its ID.
+        /// </summary>
+        /// <param name="id">ID of the requested object</param>
+        /// <returns>Task of the response model</returns>
+        Task<CityResponseModel> GetById(int id);
+    }
+}
