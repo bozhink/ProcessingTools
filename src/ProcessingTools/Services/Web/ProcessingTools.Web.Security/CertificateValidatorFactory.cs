5ee9a27106d7ebf5c2fbad2eb4987f42a8931b66
Bozhin Karaivanov <bozhink@users.noreply.github.com>
2017-09-03 23:15:50 +0300

    Add project ProcessingTools.Web.Security

---
+++ src/ProcessingTools/Services/Web/ProcessingTools.Web.Security/CertificateValidatorFactory.cs
@@ -0,0 +1,41 @@
+﻿// <copyright file="CertificateValidatorFactory.cs" company="ProcessingTools">
+// Copyright (c) 2017 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Web.Security
+{
+    using Microsoft.Owin.Security;
+    using ProcessingTools.Web.Contracts.Security;
+
+    /// <summary>
+    /// Default implementation of <see cref="ICertificateValidatorFactory"/>
+    /// </summary>
+    public class CertificateValidatorFactory : ICertificateValidatorFactory
+    {
+        /// <inheritdoc/>
+        public ICertificateValidator Create()
+        {
+            return new CertificateSubjectKeyIdentifierValidator(
+                new[]
+                {
+                    // VeriSign Class 3 Secure Server CA - G2
+                    "A5EF0B11CEC04103A34A659048B21CE0572D7D47",
+
+                    // VeriSign Class 3 Secure Server CA - G3
+                    "0D445C165344C1827E1D20AB25F40163D8BE79A5",
+
+                    // VeriSign Class 3 Public Primary Certification Authority - G5
+                    "7FD365A7C2DDECBBF03009F34339FA02AF333133",
+
+                    // Symantec Class 3 Secure Server CA - G4
+                    "39A55D933676616E73A761DFA16A7E59CDE66FAD",
+
+                    // DigiCert SHA2 High Assurance Server C‎A
+                    "5168FF90AF0207753CCCD9656462A212B859723B",
+
+                    // DigiCert High Assurance EV Root CA
+                    "B13EC36903F8BF4701D498261A0802EF63642BC3"
+                });
+        }
+    }
+}
