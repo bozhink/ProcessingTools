953d373566c6a3db4a0bb67fdcb855a20c391dc1
Bozhin Karaivanov <bozhink@users.noreply.github.com>
2017-09-03 22:01:48 +0300

    Add project ProcessingTools.Web.Models

---
+++ src/ProcessingTools/Infrastructure/ProcessingTools.Web.Models/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file="AssemblyInfo.cs" company="ProcessingTools">
+// Copyright (c) 2017 ProcessingTools. All rights reserved.
+// </copyright>
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ProcessingTools.Web.Models")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ProcessingTools.Web.Models")]
+[assembly: AssemblyCopyright("Copyright © 2017 ProcessingTools")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("667d4391-17f1-4433-bc96-685aee89650a")]
+[assembly: AssemblyVersion("0.0.9.0")]
+[assembly: AssemblyFileVersion("0.0.9.0")]
