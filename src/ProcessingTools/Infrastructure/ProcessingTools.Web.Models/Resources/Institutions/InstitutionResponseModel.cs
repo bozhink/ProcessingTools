88690f15c9a6c59c4d5706a16bbb67501e95db98
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2017-09-05 12:51:06 +0300

    Add ProcessingTools.Web.Models.Resources.Institutions.*

---
+++ src/ProcessingTools/Infrastructure/ProcessingTools.Web.Models/Resources/Institutions/InstitutionResponseModel.cs
@@ -0,0 +1,24 @@
+﻿// <copyright file="InstitutionResponseModel.cs" company="ProcessingTools">
+// Copyright (c) 2017 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Web.Models.Resources.Institutions
+{
+    using ProcessingTools.Contracts.Models.Resources;
+
+    /// <summary>
+    /// Represents response model for the institutions API.
+    /// </summary>
+    public class InstitutionResponseModel : IInstitution
+    {
+        /// <summary>
+        /// Gets or sets the Identifier (ID) of the institution object.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Name of the institution object.
+        /// </summary>
+        public string Name { get; set; }
+    }
+}
