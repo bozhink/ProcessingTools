4cf61f97b791207c2d1e80f48cd16e2547de9b11
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2017-09-05 14:09:42 +0300

    Remove ProcessingTools.Web.Api.Models.EnvoTerms.EnvoTermResponseModel

--- src/ProcessingTools/Web/ProcessingTools.Web.Api/Controllers/EnvoTermsController.cs
+++ src/ProcessingTools/Web/ProcessingTools.Web.Api/Controllers/EnvoTermsController.cs
@@ -4,36 +4,49 @@
     using System.Linq;
     using System.Threading.Tasks;
     using System.Web.Http;
+    using AutoMapper;
     using ProcessingTools.Bio.Environments.Services.Data.Contracts;
     using ProcessingTools.Constants;
-    using ProcessingTools.Web.Api.Models.EnvoTerms;
+    using ProcessingTools.Contracts;
+    using ProcessingTools.Contracts.Models.Bio;
+    using ProcessingTools.Web.Models.Bio.EnvoTerms;
 
     public class EnvoTermsController : ApiController
     {
         private readonly IEnvoTermsDataService service;
+        private readonly ILogger logger;
+        private readonly IMapper mapper;
 
-        public EnvoTermsController(IEnvoTermsDataService service)
+        public EnvoTermsController(IEnvoTermsDataService service, ILogger logger)
         {
             this.service = service ?? throw new ArgumentNullException(nameof(service));
+            this.logger = logger;
+
+            MapperConfiguration mapperConfiguration = new MapperConfiguration(c =>
+            {
+                c.CreateMap<IEnvoTerm, EnvoTermResponseModel>();
+            });
+
+            this.mapper = mapperConfiguration.CreateMapper();
         }
 
-        public async Task<IHttpActionResult> GetEnvoTerms(int skip = PagingConstants.DefaultSkip, int take = PagingConstants.DefaultTake)
+        public async Task<IHttpActionResult> Get(int skip = PagingConstants.DefaultSkip, int take = PagingConstants.DefaultTake)
         {
             try
             {
-                var data = await this.service.Get(skip, take);
-                if (data == null)
+                var result = await this.service.Get(skip, take).ConfigureAwait(false);
+                if (result == null)
                 {
                     return this.NotFound();
                 }
 
-                var result = data.Select(AutoMapperConfig.Mapper.Map<EnvoTermResponseModel>).ToList();
-
-                return this.Ok(result);
+                var data = result.Select(this.mapper.Map<IEnvoTerm, EnvoTermResponseModel>).ToArray();
+                return this.Ok(data);
             }
             catch (Exception ex)
             {
-                return this.BadRequest(ex.ToString());
+                this.logger?.Log(exception: ex, message: string.Empty);
+                return this.InternalServerError();
             }
         }
     }
