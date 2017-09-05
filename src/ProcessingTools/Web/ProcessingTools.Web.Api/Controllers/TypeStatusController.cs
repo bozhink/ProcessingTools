ce797b6ac1a380da636a104de6847586201fee77
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2017-09-05 13:31:03 +0300

    Remove ProcessingTools.Web.Api.Models.TypeStatuses.*

--- src/ProcessingTools/Web/ProcessingTools.Web.Api/Controllers/TypeStatusController.cs
+++ src/ProcessingTools/Web/ProcessingTools.Web.Api/Controllers/TypeStatusController.cs
@@ -5,7 +5,7 @@
     using ProcessingTools.Bio.Services.Data.Models;
     using ProcessingTools.Contracts.Filters;
     using ProcessingTools.Web.Api.Abstractions;
-    using ProcessingTools.Web.Api.Models.TypeStatuses;
+    using ProcessingTools.Web.Models.Bio.TypeStatuses;
 
     public class TypeStatusController : GenericDataServiceController<ITypeStatusDataService, TypeStatusServiceModel, TypeStatusRequestModel, TypeStatusResponseModel, IFilter>
     {
