7a82777464f042392f5f6b00fcc9434aff0e6d5c
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2017-09-05 13:11:22 +0300

    Add ProcessingTools.Web.Models.Bio.MorphologicalEpithets.*

---
+++ src/ProcessingTools/Infrastructure/ProcessingTools.Web.Models/Bio/MorphologicalEpithets/MorphologicalEpithetRequestModel.cs
@@ -0,0 +1,27 @@
+﻿// <copyright file="MorphologicalEpithetRequestModel.cs" company="ProcessingTools">
+// Copyright (c) 2017 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Web.Models.Bio.MorphologicalEpithets
+{
+    using System.ComponentModel.DataAnnotations;
+    using ProcessingTools.Contracts.Models.Bio;
+
+    /// <summary>
+    /// Represents request model for the morphological epithets API.
+    /// </summary>
+    public class MorphologicalEpithetRequestModel : IMorphologicalEpithet
+    {
+        /// <summary>
+        /// Gets or sets the Identifier (ID) of the morphological epithet object.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Name of the morphological epithet object.
+        /// </summary>
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+    }
+}
---
+++ src/ProcessingTools/Infrastructure/ProcessingTools.Web.Models/Bio/MorphologicalEpithets/MorphologicalEpithetResponseModel.cs
@@ -0,0 +1,24 @@
+﻿// <copyright file="MorphologicalEpithetResponseModel.cs" company="ProcessingTools">
+// Copyright (c) 2017 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Web.Models.Bio.MorphologicalEpithets
+{
+    using ProcessingTools.Contracts.Models.Bio;
+
+    /// <summary>
+    /// Represents response model for the morphological epithets API.
+    /// </summary>
+    public class MorphologicalEpithetResponseModel : IMorphologicalEpithet
+    {
+        /// <summary>
+        /// Gets or sets the Identifier (ID) of the morphological epithet object.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Name of the morphological epithet object.
+        /// </summary>
+        public string Name { get; set; }
+    }
+}
