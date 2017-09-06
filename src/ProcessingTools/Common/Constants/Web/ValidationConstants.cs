00627d08d1d8bb444a282e956ec15dfb7bc6843c
Bozhin Karaivanov <bozhink@users.noreply.github.com>
2017-09-06 17:23:46 +0300

    Add ProcessingTools.Constants.Web.ValidationConstants

---
+++ src/ProcessingTools/Common/Constants/Web/ValidationConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace ProcessingTools.Constants.Web
+{
+    public static class ValidationConstants
+    {
+        public const int PasswordMaximalLength = 100;
+        public const int PasswordMinimalLength = 6;
+    }
+}
