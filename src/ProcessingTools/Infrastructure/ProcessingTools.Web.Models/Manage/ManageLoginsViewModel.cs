01db60e18977999fbf1a733db82f884cfeac2969
Bozhin Karaivanov <bozhink@users.noreply.github.com>
2017-09-06 18:40:15 +0300

    Add ProcessingTools.Web.Models.Manage

---
+++ src/ProcessingTools/Infrastructure/ProcessingTools.Web.Models/Manage/ManageLoginsViewModel.cs
@@ -0,0 +1,26 @@
+﻿// <copyright file="ManageLoginsViewModel.cs" company="ProcessingTools">
+// Copyright (c) 2017 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Web.Models.Manage
+{
+    using System.Collections.Generic;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.Owin.Security;
+
+    /// <summary>
+    /// Manage logins view model
+    /// </summary>
+    public class ManageLoginsViewModel
+    {
+        /// <summary>
+        /// Gets or sets information about current logins.
+        /// </summary>
+        public IList<UserLoginInfo> CurrentLogins { get; set; }
+
+        /// <summary>
+        /// Gets or sets information about other logins.
+        /// </summary>
+        public IList<AuthenticationDescription> OtherLogins { get; set; }
+    }
+}
---
+++ src/ProcessingTools/Infrastructure/ProcessingTools.Web.Models/Manage/SetPasswordViewModel.cs
@@ -0,0 +1,32 @@
+﻿// <copyright file="SetPasswordViewModel.cs" company="ProcessingTools">
+// Copyright (c) 2017 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Web.Models.Manage
+{
+    using System.ComponentModel.DataAnnotations;
+    using ProcessingTools.Constants.Web;
+
+    /// <summary>
+    /// Set password view model
+    /// </summary>
+    public class SetPasswordViewModel
+    {
+        /// <summary>
+        /// Gets or sets  the new password.
+        /// </summary>
+        [Required]
+        [StringLength(ValidationConstants.PasswordMaximalLength, ErrorMessageResourceName = nameof(Strings.NewPasswordErrorMessage), ErrorMessageResourceType = typeof(Strings), MinimumLength = ValidationConstants.PasswordMinimalLength)]
+        [DataType(DataType.Password)]
+        [Display(Name = nameof(Strings.NewPassword), ResourceType = typeof(Strings))]
+        public string NewPassword { get; set; }
+
+        /// <summary>
+        /// Gets or sets the confirmation value for the new password.
+        /// </summary>
+        [DataType(DataType.Password)]
+        [Display(Name = nameof(Strings.ConfirmPassword), ResourceType = typeof(Strings))]
+        [Compare(nameof(NewPassword), ErrorMessageResourceName = nameof(Strings.ConfirmPasswordErrorMessage), ErrorMessageResourceType = typeof(Strings))]
+        public string ConfirmPassword { get; set; }
+    }
+}
