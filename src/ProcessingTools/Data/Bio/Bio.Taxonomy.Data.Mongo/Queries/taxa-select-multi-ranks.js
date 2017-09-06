abca53cb2990c35d6cdc86ae5cf40272bbdbea96
Bozhin Karaivanov <bozhink@users.noreply.github.com>
2017-09-06 15:02:08 +0300

    Code clean-up

--- src/ProcessingTools/Data/Bio/Bio.Taxonomy.Data.Mongo/Queries/taxa-select-multi-ranks.js
+++ src/ProcessingTools/Data/Bio/Bio.Taxonomy.Data.Mongo/Queries/taxa-select-multi-ranks.js
@@ -1,14 +1,23 @@
-db.getCollection("taxa").aggregate([
-    {$project: {"_id": 0}},
-    {$unwind: "$ranks"},
-    {$group: {
-        "_id": {
-            "name": "$name",
-            "isWhiteListed": "$isWhiteListed"
-        },
-        "numberOfRanks": {$sum: 1}
-    }},
-    {$match: { "numberOfRanks": { $gt: 1 } }},
-    {$project: { "_id": "$_id.name", "numberOfRanks": "$numberOfRanks"}}
-    // /*Uncomment to view the number of different items*/,{$group: {_id: 1, n: {$sum: 1}}}
-])
+db.getCollection('taxa').aggregate([
+
+    {$project: {'_id': 0}},
+
+    {$unwind: '$ranks'},
+
+    {$group: {
+        '_id': {
+            'name': '$name',
+
+            'isWhiteListed': '$isWhiteListed'
+        },
+
+        'numberOfRanks': {$sum: 1}
+    }},
+
+    {$match: { 'numberOfRanks': { $gt: 1 } }},
+
+    {$project: { '_id': '$_id.name', 'numberOfRanks': '$numberOfRanks'}}
+
+    // /*Uncomment to view the number of different items*/,{$group: {_id: 1, n: {$sum: 1}}}
+
+]);
