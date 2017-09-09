08167f09347338b9ebc31c69b77de24eda7dd214
Bozhin Karaivanov <bozhink@users.noreply.github.com>
2017-09-10 00:18:16 +0300

    Add ProcessingTools.Web.Models.Geo.GeoEpithets

---
+++ src/ProcessingTools/Infrastructure/ProcessingTools.Web.Models/Geo/GeoEpithets/GeoEpithetPageViewModel.cs
@@ -0,0 +1,34 @@
+﻿// <copyright file="GeoEpithetPageViewModel.cs" company="ProcessingTools">
+// Copyright (c) 2017 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Web.Models.Geo.GeoEpithets
+{
+    using ProcessingTools.Contracts.ViewModels;
+
+    /// <summary>
+    /// GeoEpithet page view model
+    /// </summary>
+    public class GeoEpithetPageViewModel : IPageViewModel<GeoEpithetViewModel>
+    {
+        /// <summary>
+        /// Gets or sets model data.
+        /// </summary>
+        public GeoEpithetViewModel Model { get; set; }
+
+        /// <summary>
+        /// Gets the page heading.
+        /// </summary>
+        public string PageHeading => this.PageTitle;
+
+        /// <summary>
+        /// Gets or sets the page title.
+        /// </summary>
+        public string PageTitle { get; set; }
+
+        /// <summary>
+        /// Gets or sets the return-url.
+        /// </summary>
+        public string ReturnUrl { get; set; }
+    }
+}
---
+++ src/ProcessingTools/Infrastructure/ProcessingTools.Web.Models/Geo/GeoEpithets/GeoEpithetsViewModel.cs
@@ -0,0 +1,17 @@
+﻿// <copyright file="GeoEpithetsViewModel.cs" company="ProcessingTools">
+// Copyright (c) 2017 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Web.Models.Geo.GeoEpithets
+{
+    /// <summary>
+    /// GeoEpithets view model
+    /// </summary>
+    public class GeoEpithetsViewModel
+    {
+        /// <summary>
+        /// Gets or sets the geo-names.
+        /// </summary>
+        public string Names { get; set; }
+    }
+}
