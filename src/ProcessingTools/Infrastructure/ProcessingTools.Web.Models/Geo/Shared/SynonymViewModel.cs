25270058299a8d72dd911be634163ca2f72c21c0
Bozhin Karaivanov <bozhink@users.noreply.github.com>
2017-09-09 23:52:43 +0300

    Add ProcessingTools.Web.Models.Geo.Shared

---
+++ src/ProcessingTools/Infrastructure/ProcessingTools.Web.Models/Geo/Shared/SynonymViewModel.cs
@@ -0,0 +1,41 @@
+﻿// <copyright file="SynonymViewModel.cs" company="ProcessingTools">
+// Copyright (c) 2017 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Web.Models.Geo.Shared
+{
+    using System.ComponentModel.DataAnnotations;
+    using ProcessingTools.Constants.Data.Geo;
+
+    /// <summary>
+    /// Synonym view model
+    /// </summary>
+    public class SynonymViewModel
+    {
+        /// <summary>
+        /// Gets or sets the Identifier (ID) of the synonym object.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Name of the synonym object.
+        /// </summary>
+        [Display(Name = nameof(Strings.Name), ResourceType = typeof(Strings))]
+        [Required(
+            AllowEmptyStrings = false,
+            ErrorMessageResourceName = nameof(Strings.NameEmptyErrorMessage),
+            ErrorMessageResourceType = typeof(Strings))]
+        [StringLength(
+            maximumLength: ValidationConstants.MaximalLengthOfSynonymName,
+            MinimumLength = ValidationConstants.MinimalLengthOfSynonymName,
+            ErrorMessageResourceName = nameof(Strings.NameErrorMessage),
+            ErrorMessageResourceType = typeof(Strings))]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the language code.
+        /// </summary>
+        [Display(Name = nameof(Strings.LanguageCode), ResourceType = typeof(Strings))]
+        public string LanguageCode { get; set; }
+    }
+}
