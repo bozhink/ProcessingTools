dfb4d38b54834a4f966bad8a1d38a8e9b944008a
Bozhin Karaivanov <bozhink@users.noreply.github.com>
2017-09-22 00:14:51 +0300

    Add items in Processors.Contracts

---
+++ src/ProcessingTools/Common/ProcessingTools.Processors.Contracts/IO/IFileReader.cs
@@ -0,0 +1,28 @@
+﻿// <copyright file="IFileReader.cs" company="ProcessingTools">
+// Copyright (c) 2017 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Contracts.Files.IO
+{
+    using System.IO;
+
+    /// <summary>
+    /// File reader.
+    /// </summary>
+    public interface IFileReader
+    {
+        /// <summary>
+        /// Gets a <see cref="StreamReader"/> based on specified file name.
+        /// </summary>
+        /// <param name="fullName">Full name of the file to be read.</param>
+        /// <returns><see cref="StreamReader"/> object.</returns>
+        StreamReader GetReader(string fullName);
+
+        /// <summary>
+        /// Reads a file to <see cref="Stream"/>.
+        /// </summary>
+        /// <param name="fullName">Full name of the file to be read.</param>
+        /// <returns><see cref="Stream"/> object.</returns>
+        Stream ReadToStream(string fullName);
+    }
+}
---
+++ src/ProcessingTools/Common/ProcessingTools.Processors.Contracts/IO/IXmlFileReader.cs
@@ -0,0 +1,14 @@
+﻿namespace ProcessingTools.Contracts.Files.IO
+{
+    using System.Threading.Tasks;
+    using System.Xml;
+
+    public interface IXmlFileReader
+    {
+        XmlReaderSettings ReaderSettings { get; set; }
+
+        XmlReader GetReader(string fullName);
+
+        Task<XmlDocument> ReadXml(string fullName);
+    }
+}
