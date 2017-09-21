6decc7dce5c78d387639e2a3496fce6ebdd8af3c
Bozhin Karaivanov <bozhink@users.noreply.github.com>
2017-09-21 23:06:52 +0300

    Add ProcessingTools.Contracts.IUpdateCommand

---
+++ src/ProcessingTools/Common/ProcessingTools.Contracts/IUpdateExpression.cs
@@ -0,0 +1,39 @@
+﻿// <copyright file="IUpdateExpression.cs" company="ProcessingTools">
+// Copyright (c) 2017 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Contracts
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq.Expressions;
+
+    /// <summary>
+    /// Update expression.
+    /// </summary>
+    /// <typeparam name="T">Type of the updated field</typeparam>
+    public interface IUpdateExpression<T>
+    {
+        /// <summary>
+        /// Gets the list of update commands.
+        /// </summary>
+        IEnumerable<IUpdateCommand> UpdateCommands { get; }
+
+        /// <summary>
+        /// Execute Set command.
+        /// </summary>
+        /// <typeparam name="TField">Type of the field to be updated</typeparam>
+        /// <param name="field">Field selector</param>
+        /// <param name="value">Value to be set</param>
+        /// <returns>Update expression to be chained</returns>
+        IUpdateExpression<T> Set<TField>(Expression<Func<T, TField>> field, TField value);
+
+        /// <summary>
+        /// Execute Set command.
+        /// </summary>
+        /// <param name="fieldName">Name of the field for update</param>
+        /// <param name="value">Value to be set</param>
+        /// <returns>Update expression to be chained</returns>
+        IUpdateExpression<T> Set(string fieldName, object value);
+    }
+}
