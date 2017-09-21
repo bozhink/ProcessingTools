100784f360c0b23b5d867f20592afbb24af0ee54
Bozhin Karaivanov <bozhink@users.noreply.github.com>
2017-09-21 23:55:43 +0300

    Add ProcessingTools.Contracts.INetConnector

---
+++ src/ProcessingTools/Common/ProcessingTools.Contracts/INetConnectorFactory.cs
@@ -0,0 +1,25 @@
+﻿// <copyright file="INetConnectorFactory.cs" company="ProcessingTools">
+// Copyright (c) 2017 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Contracts
+{
+    /// <summary>
+    /// Factory for <see cref="INetConnector"/> objects.
+    /// </summary>
+    public interface INetConnectorFactory
+    {
+        /// <summary>
+        /// Creates an instance of <see cref="INetConnector"/>.
+        /// </summary>
+        /// <returns>Instance of <see cref="INetConnector"/></returns>
+        INetConnector Create();
+
+        /// <summary>
+        /// Creates an instance of <see cref="INetConnector"/> with specified base address.
+        /// </summary>
+        /// <param name="baseAddress">Base address for the URL</param>
+        /// <returns>Instance of <see cref="INetConnector"/></returns>
+        INetConnector Create(string baseAddress);
+    }
+}
