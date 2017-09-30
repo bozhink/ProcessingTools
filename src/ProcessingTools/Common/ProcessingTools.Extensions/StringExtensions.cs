7f81b4d96574a5758185a95d05ccf3f4f1d20430
Bozhin Karaivanov <bozhink@users.noreply.github.com>
2017-09-30 23:17:30 +0300

    Add StringExtensions to project ProcessingTools.Extensions

---
+++ src/ProcessingTools/Common/ProcessingTools.Extensions/StringExtensions.cs
@@ -0,0 +1,34 @@
+﻿// <copyright file="StringExtensions.cs" company="ProcessingTools">
+// Copyright (c) 2017 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Extensions
+{
+    using System.Text.RegularExpressions;
+
+    /// <summary>
+    /// Extension methods for <see cref="string"/>.
+    /// </summary>
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Returns a new string in which all occurrences of a specified string in the current instance are replaced with another specified string or by regex pattern.
+        /// </summary>
+        /// <param name="input"> The string to search for a match.</param>
+        /// <param name="pattern">The regular expression pattern to match.</param>
+        /// <param name="replacement">The replacement string.</param>
+        /// <param name="isRegex">Specifies if regex replacement has to be executed.</param>
+        /// <returns>A new string that is identical to the input string, except that the replacement string takes the place of each matched string. If pattern is not matched in the current instance, the method returns the current instance unchanged.</returns>
+        public static string Replace(this string input, string pattern, string replacement, bool isRegex)
+        {
+            if (isRegex)
+            {
+                return Regex.Replace(input: input, pattern: pattern, replacement: replacement);
+            }
+            else
+            {
+                return input.Replace(pattern, replacement);
+            }
+        }
+    }
+}
