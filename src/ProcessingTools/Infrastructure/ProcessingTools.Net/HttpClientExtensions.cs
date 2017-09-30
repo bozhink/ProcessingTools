c54766e484bdbbcc26b3f3942c2f1901cbf8f437
Bozhin Karaivanov <bozhink@users.noreply.github.com>
2017-09-30 23:50:09 +0300

    Add project ProcessingTools.Net as .NET standard.

---
+++ src/ProcessingTools/Infrastructure/ProcessingTools.Net/HttpClientExtensions.cs
@@ -0,0 +1,81 @@
+﻿// <copyright file="HttpClientExtensions.cs" company="ProcessingTools">
+// Copyright (c) 2017 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Net
+{
+    using System;
+    using System.Net.Http;
+    using System.Net.Http.Headers;
+    using ProcessingTools.Constants;
+
+    /// <summary>
+    /// Extensions for <see cref="HttpClient"/>.
+    /// </summary>
+    public static class HttpClientExtensions
+    {
+        /// <summary>
+        /// Adds CORS headers.
+        /// </summary>
+        /// <param name="client"><see cref="HttpClient"/> instance.</param>
+        /// <returns>Updated client.</returns>
+        public static HttpClient AddCorsHeader(this HttpClient client)
+        {
+            if (client == null)
+            {
+                throw new ArgumentNullException(nameof(client));
+            }
+
+            client.DefaultRequestHeaders.Add(
+                HttpConstants.CorsHeaderName,
+                HttpConstants.CorsHeaderDefaultValue);
+
+            return client;
+        }
+
+        /// <summary>
+        /// Adds accept-content-type header.
+        /// </summary>
+        /// <param name="client"><see cref="HttpClient"/> instance.</param>
+        /// <param name="contentType">Content-type to be added.</param>
+        /// <returns>Updated client.</returns>
+        public static HttpClient AddAcceptContentTypeHeader(this HttpClient client, string contentType)
+        {
+            if (client == null)
+            {
+                throw new ArgumentNullException(nameof(client));
+            }
+
+            if (!string.IsNullOrWhiteSpace(contentType))
+            {
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(contentType));
+            }
+
+            return client;
+        }
+
+        /// <summary>
+        /// Adds accept-type=xml header.
+        /// </summary>
+        /// <param name="client"><see cref="HttpClient"/> instance.</param>
+        /// <returns>Updated client.</returns>
+        public static HttpClient AddAcceptXmlHeader(this HttpClient client)
+        {
+            client.AddAcceptContentTypeHeader(ContentTypes.Xml);
+
+            return client;
+        }
+
+        /// <summary>
+        /// Adds accept-type=json header.
+        /// </summary>
+        /// <param name="client"><see cref="HttpClient"/> instance.</param>
+        /// <returns>Updated client.</returns>
+        public static HttpClient AddAcceptJsonHeader(this HttpClient client)
+        {
+            client.AddAcceptContentTypeHeader(ContentTypes.Json);
+
+            return client;
+        }
+    }
+}
---
+++ src/ProcessingTools/Infrastructure/ProcessingTools.Net/NetConnectorFactory.cs
@@ -0,0 +1,27 @@
+﻿// <copyright file="NetConnectorFactory.cs" company="ProcessingTools">
+// Copyright (c) 2017 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Net
+{
+    using ProcessingTools.Contracts;
+
+    /// <summary>
+    /// Factory for <see cref="INetConnector"/> objects.
+    /// </summary>
+    public class NetConnectorFactory : INetConnectorFactory
+    {
+        /// <summary>
+        /// Creates instance of <see cref="INetConnector"/> with default parameters.
+        /// </summary>
+        /// <returns>Instance of <see cref="INetConnector"/>.</returns>
+        public INetConnector Create() => new NetConnector();
+
+        /// <summary>
+        /// Creates instance of <see cref="INetConnector"/> with specified base address..
+        /// </summary>
+        /// <param name="baseAddress">Base address for the requested API.</param>
+        /// <returns>Instance of <see cref="INetConnector"/>.</returns>
+        public INetConnector Create(string baseAddress) => new NetConnector(baseAddress);
+    }
+}
