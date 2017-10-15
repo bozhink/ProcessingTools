4257ad2de13dac87b19ffb9c587dba6b7e43cc6b
Bozhin Karaivanov <bozhink@users.noreply.github.com>
2017-10-15 21:49:12 +0300

    Add project ProcessingTools.Clients.Tests.

--- src/ProcessingTools/Services.Tests/ProcessingTools.Clients.Tests/Properties/AssemblyInfo.cs
+++ src/ProcessingTools/Services.Tests/ProcessingTools.Clients.Tests/Properties/AssemblyInfo.cs
@@ -1,15 +1,19 @@
-﻿using System.Reflection;
+// <copyright file="AssemblyInfo.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("ProcessingTools.Bio.Taxonomy.ServiceClient.Aphia.Tests")]
+[assembly: AssemblyTitle("ProcessingTools.Clients.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ProcessingTools.Bio.Taxonomy.ServiceClient.Aphia.Tests")]
+[assembly: AssemblyProduct("ProcessingTools.Clients.Tests")]
 [assembly: AssemblyCopyright("Copyright © 2017 ProcessingTools")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: Guid("2738eafe-70c9-4d7f-b490-d8c22d42b531")]
+[assembly: Guid("1bdc50d1-be9b-46e1-853c-ed94b58fc357")]
 [assembly: AssemblyVersion("0.0.9.0")]
 [assembly: AssemblyFileVersion("0.0.9.0")]
