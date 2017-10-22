e19b91fe92fba473565919a5e8c487355e2ff3b6
Bozhin Karaivanov <bozhink@users.noreply.github.com>
2017-10-22 16:15:11 +0300

    Remove BiorepositoriesDataServiceFactory

--- src/ProcessingTools/Services/Data/Bio.Biorepositories.Services.Data/Services/BiorepositoriesInstitutionsDataService.cs
+++ src/ProcessingTools/Services/Data/Bio.Biorepositories.Services.Data/Services/BiorepositoriesInstitutionsDataService.cs
@@ -1,25 +1,56 @@
 ﻿namespace ProcessingTools.Bio.Biorepositories.Services.Data.Services
 {
     using System;
-    using System.Linq.Expressions;
+    using System.Linq;
+    using System.Threading.Tasks;
     using ProcessingTools.Bio.Biorepositories.Data.Mongo.Contracts.Repositories;
     using ProcessingTools.Bio.Biorepositories.Services.Data.Contracts;
-    using ProcessingTools.Bio.Biorepositories.Services.Data.Factories;
+    using ProcessingTools.Common.Extensions;
+    using ProcessingTools.Constants;
+    using ProcessingTools.Exceptions;
 
-    public class BiorepositoriesInstitutionsDataService : BiorepositoriesDataServiceFactory<Biorepositories.Data.Mongo.Models.Institution, Models.Institution>, IBiorepositoriesInstitutionsDataService
+    public class BiorepositoriesInstitutionsDataService : IBiorepositoriesInstitutionsDataService
     {
+        private readonly IBiorepositoriesRepositoryProvider<Biorepositories.Data.Mongo.Models.Institution> repositoryProvider;
+
         public BiorepositoriesInstitutionsDataService(IBiorepositoriesRepositoryProvider<Biorepositories.Data.Mongo.Models.Institution> repositoryProvider)
-            : base(repositoryProvider)
         {
+            this.repositoryProvider = repositoryProvider ?? throw new ArgumentNullException(nameof(repositoryProvider));
         }
 
-        protected override Expression<Func<Biorepositories.Data.Mongo.Models.Institution, bool>> Filter => i => i.InstitutionCode.Length > 1 && i.InstitutionName.Length > 1;
-
-        protected override Expression<Func<Biorepositories.Data.Mongo.Models.Institution, Models.Institution>> Project => i => new Models.Institution
+        public Task<Models.Institution[]> GetAsync(int skip, int take)
         {
-            Code = i.InstitutionCode,
-            Name = i.InstitutionName,
-            Url = i.Url
-        };
+            if (skip < 0)
+            {
+                throw new InvalidSkipValuePagingException();
+            }
+
+            if (take < 1 || take > PaginationConstants.MaximalItemsPerPageAllowed)
+            {
+                throw new InvalidTakeValuePagingException();
+            }
+
+            return Task.Run(() =>
+            {
+                var repository = this.repositoryProvider.Create();
+
+                var data = repository.Query
+                    .Where(i => i.InstitutionCode.Length > 1 && i.InstitutionName.Length > 1)
+                    .OrderBy(i => i.Id)
+                    .Skip(skip)
+                    .Take(take)
+                    .Select(i => new Models.Institution
+                    {
+                        Code = i.InstitutionCode,
+                        Name = i.InstitutionName,
+                        Url = i.Url
+                    })
+                    .ToArray();
+
+                repository.TryDispose();
+
+                return data;
+            });
+        }
     }
 }
