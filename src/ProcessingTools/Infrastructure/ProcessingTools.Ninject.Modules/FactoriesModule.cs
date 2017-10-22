d534a46f3341caedb6bd6018177ee595b60f49e9
Bozhin Karaivanov <bozhink@users.noreply.github.com>
2017-10-22 22:11:46 +0300

    Add ProcessingTools.Ninject.Modules.FactoriesModule

---
+++ src/ProcessingTools/Infrastructure/ProcessingTools.Ninject.Modules/FactoriesModule.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file="FactoriesModule.cs" company="ProcessingTools">
+// Copyright (c) 2017 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Ninject.Modules
+{
+    using global::Ninject.Modules;
+
+    /// <summary>
+    /// Bindings of factories.
+    /// </summary>
+    public class FactoriesModule : NinjectModule
+    {
+        /// <inheritdoc/>
+        public override void Load()
+        {
+        }
+    }
+}
