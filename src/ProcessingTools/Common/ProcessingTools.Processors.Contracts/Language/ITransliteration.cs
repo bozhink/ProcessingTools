f700413b2ca86baa13f9cf1c214d958f97871d47
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2017-11-13 12:41:19 +0200

    Add ProcessingTools.Processors.Contracts.Language.ITransliteration

---
+++ src/ProcessingTools/Common/ProcessingTools.Processors.Contracts/Language/ITransliteration.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file="ITransliteration.cs" company="ProcessingTools">
+// Copyright (c) 2017 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Processors.Contracts.Language
+{
+    /// <summary>
+    /// Transliteration.
+    /// </summary>
+    public interface ITransliteration
+    {
+        /// <summary>
+        /// Converts text content according to transliteration map.
+        /// </summary>
+        /// <param name="text">Text content to be processed.</param>
+        /// <returns>Processed text.</returns>
+        string ProcessText(string text);
+    }
+}
