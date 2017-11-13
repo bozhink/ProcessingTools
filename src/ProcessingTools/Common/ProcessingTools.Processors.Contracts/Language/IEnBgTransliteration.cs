5ebe353d0d2ad2f41a4054fad958bada5f4c5d5e
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2017-11-13 12:44:30 +0200

    Add ProcessingTools.Processors.Contracts.Language.IEnBgTransliteration

---
+++ src/ProcessingTools/Common/ProcessingTools.Processors.Contracts/Language/IEnBgTransliteration.cs
@@ -0,0 +1,13 @@
+﻿// <copyright file="IEnBgTransliteration.cs" company="ProcessingTools">
+// Copyright (c) 2017 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Processors.Contracts.Language
+{
+    /// <summary>
+    /// English-to-Bulgarian transliteration.
+    /// </summary>
+    public interface IEnBgTransliteration : ITransliteration
+    {
+    }
+}
