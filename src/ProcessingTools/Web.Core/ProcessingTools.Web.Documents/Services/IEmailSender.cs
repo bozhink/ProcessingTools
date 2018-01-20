aaf928c611859f8a6bca838a095d8bd4833f487e
Bozhin Karaivanov <bozhin@localhost.localdomain>
2018-01-20 21:15:56 +0200

    Add ProcessingTools/Web.Core/ProcessingTools.Web.Documents

---
+++ src/ProcessingTools/Web.Core/ProcessingTools.Web.Documents/Services/IEmailSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProcessingTools.Web.Documents.Services
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+    }
+}
