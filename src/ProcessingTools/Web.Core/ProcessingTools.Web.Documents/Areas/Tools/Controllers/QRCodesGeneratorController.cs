49a463546858c8a316f6c65c4c0aca0241dddd77
Bozhin Karaivanov <bozhink@users.noreply.github.com>
2018-01-28 21:47:13 +0200

    Add Document area to ProcessingTools.Web.Documents

--- src/ProcessingTools/Web.Core/ProcessingTools.Web.Documents/Areas/Tools/Controllers/QRCodesGeneratorController.cs
+++ src/ProcessingTools/Web.Core/ProcessingTools.Web.Documents/Areas/Tools/Controllers/QRCodesGeneratorController.cs
@@ -1,14 +1,32 @@
-﻿namespace ProcessingTools.Web.Documents.Areas.Tools.Controllers
+﻿// <copyright file="QRCodesGeneratorController.cs" company="ProcessingTools">
+// Copyright (c) 2017 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Web.Documents.Areas.Tools.Controllers
 {
     using Microsoft.AspNetCore.Mvc;
     using ProcessingTools.Web.Documents.Constants;
 
+    /// <summary>
+    /// QRCodesGenerator
+    /// </summary>
     [Area(AreaNames.Tools)]
     public class QRCodesGeneratorController : Controller
     {
+        /// <summary>
+        /// Controller name.
+        /// </summary>
         public const string ControllerName = "QRCodesGenerator";
+
+        /// <summary>
+        /// Index action name.
+        /// </summary>
         public const string IndexActionName = nameof(Index);
 
+        /// <summary>
+        /// Index
+        /// </summary>
+        /// <returns><see cref="IActionResult"/></returns>
         [ActionName(IndexActionName)]
         public IActionResult Index()
         {
