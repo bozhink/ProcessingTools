c44eaed6e47a9a391e31903ae41f66dd912d306f
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2018-02-02 12:56:01 +0200

    Add ProcessingTools.Web.Models.Shared.ApplicationContext

---
+++ src/ProcessingTools/Services/ProcessingTools.Web.Models/Shared/ApplicationContextFactory.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file="ApplicationContextFactory.cs" company="ProcessingTools">
+// Copyright (c) 2017 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Web.Models.Shared
+{
+    using ProcessingTools.Contracts;
+
+    /// <summary>
+    /// Application context factory.
+    /// </summary>
+    public class ApplicationContextFactory
+    {
+        /// <summary>
+        /// Gets or sets the application context.
+        /// </summary>
+        public IApplicationContext ApplicationContext { get; set; }
+    }
+}
