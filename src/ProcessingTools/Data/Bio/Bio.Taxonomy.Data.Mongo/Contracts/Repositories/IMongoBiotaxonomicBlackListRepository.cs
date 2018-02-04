3324cb4de46345c0a545233c24a185f1dcdc24fc
Bozhin Karaivanov <bozhink@users.noreply.github.com>
2018-02-04 18:15:13 +0200

    Update namespaces

--- src/ProcessingTools/Data/Bio/Bio.Taxonomy.Data.Mongo/Contracts/Repositories/IMongoBiotaxonomicBlackListRepository.cs
+++ src/ProcessingTools/Data/Bio/Bio.Taxonomy.Data.Mongo/Contracts/Repositories/IMongoBiotaxonomicBlackListRepository.cs
@@ -1,8 +1,8 @@
 ﻿namespace ProcessingTools.Bio.Taxonomy.Data.Mongo.Contracts.Repositories
 {
     using ProcessingTools.Contracts.Data.Repositories.Bio.Taxonomy;
-    using ProcessingTools.Models.Contracts.Bio.Taxonomy;
     using ProcessingTools.Data.Common.Mongo.Contracts.Repositories;
+    using ProcessingTools.Models.Contracts.Bio.Taxonomy;
 
     public interface IMongoBiotaxonomicBlackListRepository : IMongoCrudRepository<IBlackListEntity>, IBiotaxonomicBlackListRepository
     {
