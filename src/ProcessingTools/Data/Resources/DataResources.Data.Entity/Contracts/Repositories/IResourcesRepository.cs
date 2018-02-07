36e42209218be96e5356b4f2f5f36a03e3631386
Bozhin Karaivanov <bozhink@users.noreply.github.com>
2018-02-07 22:53:43 +0200

    Update DataResources.Data.Entity

--- src/ProcessingTools/Data/Resources/DataResources.Data.Entity/Contracts/Repositories/IResourcesRepository.cs
+++ src/ProcessingTools/Data/Resources/DataResources.Data.Entity/Contracts/Repositories/IResourcesRepository.cs
@@ -1,6 +1,6 @@
 ﻿namespace ProcessingTools.DataResources.Data.Entity.Contracts.Repositories
 {
-    using ProcessingTools.Data.Common.Entity.Repositories.Contracts;
+    using ProcessingTools.Data.Common.Entity.Contracts;
 
     public interface IResourcesRepository<T> : IEntityGenericRepository<T>
         where T : class
--- src/ProcessingTools/Data/Resources/DataResources.Data.Entity/Contracts/Repositories/IResourcesRepositoryProvider.cs
+++ src/ProcessingTools/Data/Resources/DataResources.Data.Entity/Contracts/Repositories/IResourcesRepositoryProvider.cs
@@ -1,6 +1,6 @@
 ﻿namespace ProcessingTools.DataResources.Data.Entity.Contracts.Repositories
 {
-    using ProcessingTools.Contracts.Data.Repositories;
+    using ProcessingTools.Data.Contracts;
 
     public interface IResourcesRepositoryProvider<T> : ICrudRepositoryProvider<T>
         where T : class
