746bcb7009e2926656a667fb2e036e64c28798f0
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2018-03-13 14:06:25 +0200

    Add ProcessingTools.Data.Models.History.Mongo.ObjectHistory

---
+++ src/ProcessingTools/Data/ProcessingTools.Data.Models.History.Mongo/ObjectHistory.cs
@@ -0,0 +1,45 @@
+﻿// <copyright file="ObjectHistory.cs" company="ProcessingTools">
+// Copyright (c) 2017 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Data.Models.History.Mongo
+{
+    using System;
+    using MongoDB.Bson;
+    using MongoDB.Bson.Serialization.Attributes;
+    using ProcessingTools.Common.Attributes;
+    using ProcessingTools.Models.Contracts.History;
+
+    /// <summary>
+    /// Object history entity for MongoDB.
+    /// </summary>
+    [CollectionName("objectHistory")]
+    public class ObjectHistory : IObjectHistory
+    {
+        /// <inheritdoc/>
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        /// <inheritdoc/>
+        public string Data { get; set; }
+
+        /// <inheritdoc/>
+        public string ObjectId { get; set; }
+
+        /// <inheritdoc/>
+        public string ObjectType { get; set; }
+
+        /// <inheritdoc/>
+        public string AssemblyName { get; set; }
+
+        /// <inheritdoc/>
+        public string AssemblyVersion { get; set; }
+
+        /// <inheritdoc/>
+        public string CreatedBy { get; set; }
+
+        /// <inheritdoc/>
+        public DateTime CreatedOn { get; set; }
+    }
+}
