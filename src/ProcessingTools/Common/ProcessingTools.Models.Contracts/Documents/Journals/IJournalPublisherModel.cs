ed1f084f783cdf525a9120185a926d36cac79619
Bozhin Karaivanov <bozhink@users.noreply.github.com>
2018-03-18 17:57:31 +0200

    Add IJournalPublisherModel

---
+++ src/ProcessingTools/Common/ProcessingTools.Models.Contracts/Documents/Journals/IJournalPublisherModel.cs
@@ -0,0 +1,22 @@
+﻿// <copyright file="IJournalPublisherModel.cs" company="ProcessingTools">
+// Copyright (c) 2017 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Models.Contracts.Documents.Journals
+{
+    /// <summary>
+    /// Journal publisher model.
+    /// </summary>
+    public interface IJournalPublisherModel : IStringIdentifiable
+    {
+        /// <summary>
+        /// Gets or sets the publisher's abbreviated name.
+        /// </summary>
+        string AbbreviatedName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the publisher's name.
+        /// </summary>
+        string Name { get; set; }
+    }
+}
