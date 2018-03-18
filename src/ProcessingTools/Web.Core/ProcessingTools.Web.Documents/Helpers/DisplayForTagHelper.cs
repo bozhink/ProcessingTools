6f8954ad0116a4736c4e18fdcfebbd6b8e2e5da5
Bozhin Karaivanov <bozhink@users.noreply.github.com>
2018-03-18 16:25:08 +0200

    Add ProcessingTools.Web.Documents.Helpers.DisplayForTagHelper

---
+++ src/ProcessingTools/Web.Core/ProcessingTools.Web.Documents/Helpers/DisplayForTagHelper.cs
@@ -0,0 +1,46 @@
+﻿// <copyright file="DisplayForTagHelper.cs" company="ProcessingTools">
+// Copyright (c) 2017 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Web.Documents.Helpers
+{
+    using System;
+    using Microsoft.AspNetCore.Mvc.ViewFeatures;
+    using Microsoft.AspNetCore.Razor.TagHelpers;
+
+    /// <summary>
+    /// DisplayFor tag helper.
+    /// See https://stackoverflow.com/questions/32671644/mvc6-alternative-to-html-displayfor
+    /// </summary>
+    [HtmlTargetElement("p", Attributes = DisplayForAttributeName)]
+    [HtmlTargetElement("div", Attributes = DisplayForAttributeName)]
+    [HtmlTargetElement("span", Attributes = DisplayForAttributeName)]
+    public class DisplayForTagHelper : TagHelper
+    {
+        private const string DisplayForAttributeName = "asp-display-for";
+
+        /// <summary>
+        /// Gets or sets the For model expression.
+        /// </summary>
+        [HtmlAttributeName(DisplayForAttributeName)]
+        public ModelExpression DisplayFor { get; set; }
+
+        /// <inheritdoc/>
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            if (output == null)
+            {
+                throw new ArgumentNullException(nameof(output));
+            }
+
+            var text = this.DisplayFor.ModelExplorer.GetSimpleDisplayText();
+
+            output.Content.SetContent(text);
+        }
+    }
+}
