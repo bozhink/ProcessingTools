72704268b1a7fd3d0ea52a09ca234bd0614cd0f2
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2018-03-22 12:21:40 +0200

    Add ProcessingTools.Models.Contracts.IWebModel

---
+++ src/ProcessingTools/Common/ProcessingTools.Models.Contracts/IWebModel.cs
@@ -0,0 +1,17 @@
+﻿// <copyright file="IWebModel.cs" company="ProcessingTools">
+// Copyright (c) 2017 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Models.Contracts
+{
+    /// <summary>
+    /// Web model.
+    /// </summary>
+    public interface IWebModel
+    {
+        /// <summary>
+        /// Gets or sets the return-URL.
+        /// </summary>
+        string ReturnUrl { get; set; }
+    }
+}
