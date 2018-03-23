9b2d89a7951f86e92d31568640a8ca1afe844e32
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2018-03-23 14:04:45 +0200

    Add ProcessingTools.Models.Contracts.Documents.Articles.IArticleJournalModel

---
+++ src/ProcessingTools/Common/ProcessingTools.Models.Contracts/Documents/Articles/IArticleJournalModel.cs
@@ -0,0 +1,22 @@
+﻿// <copyright file="IArticleJournalModel.cs" company="ProcessingTools">
+// Copyright (c) 2017 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Models.Contracts.Documents.Articles
+{
+    /// <summary>
+    /// Article journal model.
+    /// </summary>
+    public interface IArticleJournalModel : IStringIdentifiable
+    {
+        /// <summary>
+        /// Gets or sets the journal's abbreviated name.
+        /// </summary>
+        string AbbreviatedName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the journal's name.
+        /// </summary>
+        string Name { get; set; }
+    }
+}
