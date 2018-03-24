97ab78b4a8a08f29b6ae47d3df74db92713ab232
Bozhin Karaivanov <bozhink@users.noreply.github.com>
2018-03-25 00:15:55 +0200

    Add IArticleDetailsDataModel

---
+++ src/ProcessingTools/Data/ProcessingTools.Data.Models.Contracts/Documents/Articles/IArticleDetailsDataModel.cs
@@ -0,0 +1,13 @@
+﻿// <copyright file="IArticleDetailsDataModel.cs" company="ProcessingTools">
+// Copyright (c) 2017 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Data.Models.Contracts.Documents.Articles
+{
+    /// <summary>
+    /// Article details data model.
+    /// </summary>
+    public interface IArticleDetailsDataModel : IArticleDataModel, ProcessingTools.Models.Contracts.Documents.Articles.IArticleDetailsModel
+    {
+    }
+}
