9f2c127f36f06ef50c371e9c6e6f8eb27832dafb
Bozhin Karaivanov <bozhink@users.noreply.github.com>
2018-03-25 00:22:45 +0200

    Add ProcessingTools.Data.Models.Documents.Mongo.Article

---
+++ src/ProcessingTools/Data/ProcessingTools.Data.Models.Documents.Mongo/Article.cs
@@ -0,0 +1,93 @@
+﻿// <copyright file="Article.cs" company="ProcessingTools">
+// Copyright (c) 2017 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Data.Models.Documents.Mongo
+{
+    using System;
+    using MongoDB.Bson;
+    using MongoDB.Bson.Serialization.Attributes;
+    using ProcessingTools.Common.Attributes;
+    using ProcessingTools.Data.Models.Contracts.Documents.Articles;
+    using ProcessingTools.Models.Contracts;
+
+    /// <summary>
+    /// Article
+    /// </summary>
+    [CollectionName("articles")]
+    public class Article : IArticleDataModel, IArticleDetailsDataModel, ICreated, IModified
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Article"/> class.
+        /// </summary>
+        public Article()
+        {
+            this.ObjectId = Guid.NewGuid();
+        }
+
+        /// <inheritdoc/>
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        /// <inheritdoc/>
+        [BsonRequired]
+        [BsonRepresentation(BsonType.String)]
+        public Guid ObjectId { get; set; }
+
+        /// <inheritdoc/>
+        [BsonRequired]
+        public string Title { get; set; }
+
+        /// <inheritdoc/>
+        public string SubTitle { get; set; }
+
+        /// <inheritdoc/>
+        public string JournalId { get; set; }
+
+        /// <inheritdoc/>
+        public DateTime? PublishedOn { get; set; }
+
+        /// <inheritdoc/>
+        public DateTime? AcceptedOn { get; set; }
+
+        /// <inheritdoc/>
+        public DateTime? ReceivedOn { get; set; }
+
+        /// <inheritdoc/>
+        public string VolumeSeries { get; set; }
+
+        /// <inheritdoc/>
+        public string Volume { get; set; }
+
+        /// <inheritdoc/>
+        public string Issue { get; set; }
+
+        /// <inheritdoc/>
+        public string IssuePart { get; set; }
+
+        /// <inheritdoc/>
+        public string ELocationId { get; set; }
+
+        /// <inheritdoc/>
+        public string FirstPage { get; set; }
+
+        /// <inheritdoc/>
+        public string LastPage { get; set; }
+
+        /// <inheritdoc/>
+        public int NumberOfPages { get; set; }
+
+        /// <inheritdoc/>
+        public string CreatedBy { get; set; }
+
+        /// <inheritdoc/>
+        public DateTime CreatedOn { get; set; }
+
+        /// <inheritdoc/>
+        public string ModifiedBy { get; set; }
+
+        /// <inheritdoc/>
+        public DateTime ModifiedOn { get; set; }
+    }
+}
