54dd808cc109084cbdb48dd0d1cebfd9f65c4e73
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2018-03-26 15:04:14 +0300

    Add ProcessingTools.Services.Documents.ArticlesDataService

--- src/ProcessingTools/Data/ProcessingTools.Data.Models.Contracts/Documents/Articles/IArticleDetailsDataModel.cs
+++ src/ProcessingTools/Data/ProcessingTools.Data.Models.Contracts/Documents/Articles/IArticleDetailsDataModel.cs
@@ -9,5 +9,9 @@ namespace ProcessingTools.Data.Models.Contracts.Documents.Articles
     /// </summary>
     public interface IArticleDetailsDataModel : IArticleDataModel, ProcessingTools.Models.Contracts.Documents.Articles.IArticleDetailsModel
     {
+        /// <summary>
+        /// Gets or sets the article journal.
+        /// </summary>
+        IArticleJournalDataModel Journal { get; set; }
     }
 }
