4712460a58b329f46aabd8288ca69ce8021309f5
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2018-03-26 17:35:00 +0300

    Add document models

---
+++ src/ProcessingTools/Common/ProcessingTools.Models.Contracts/Documents/Files/IFileBaseModel.cs
@@ -0,0 +1,13 @@
+﻿// <copyright file="IFileBaseModel.cs" company="ProcessingTools">
+// Copyright (c) 2017 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Models.Contracts.Documents.Files
+{
+    /// <summary>
+    /// File base model.
+    /// </summary>
+    public interface IFileBaseModel
+    {
+    }
+}
