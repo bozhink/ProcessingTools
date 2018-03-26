2960080368e5f60bcd7b93528aab25fccea4d621
Bozhin Karaivanov <bozhink@gmail.com>
2018-03-26 22:56:32 +0300

    Add IDocumentModel.cs

---
+++ src/ProcessingTools/Common/ProcessingTools.Models.Contracts/Documents/Documents/IDocumentModel.cs
@@ -0,0 +1,18 @@
+﻿// <copyright file="IDocumentModel.cs" company="ProcessingTools">
+// Copyright (c) 2017 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Models.Contracts.Documents.Documents
+{
+    /// <summary>
+    /// Document model.
+    /// </summary>
+    public interface IDocumentModel : IDocumentBaseModel, IStringIdentifiable, ICreated, IModified
+    {
+        /// <summary>
+        /// Gets or sets a value indicating whether document is final.
+        /// </summary>
+        bool IsFinal { get; set; }
+    }
+}
+
