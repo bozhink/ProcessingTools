d5882c561b85703d17c836b941975996dd708e70
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2018-03-26 14:54:25 +0300

    Add ProcessingTools.Services.Models.Documents.Articles.*

---
+++ src/ProcessingTools/Services/ProcessingTools.Services.Models/Documents/Articles/ArticleDetailsModel.cs
@@ -0,0 +1,81 @@
+﻿// <copyright file="ArticleDetailsModel.cs" company="ProcessingTools">
+// Copyright (c) 2017 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Services.Models.Documents.Articles
+{
+    using System;
+    using ProcessingTools.Services.Models.Contracts.Documents.Articles;
+
+    /// <summary>
+    /// Article details model.
+    /// </summary>
+    public class ArticleDetailsModel : IArticleDetailsModel
+    {
+        /// <inheritdoc/>
+        public string Id { get; set; }
+
+        /// <inheritdoc/>
+        public bool IsFinalized { get; set; }
+
+        /// <inheritdoc/>
+        public bool IsDeployed { get; set; }
+
+        /// <inheritdoc/>
+        public string Title { get; set; }
+
+        /// <inheritdoc/>
+        public string SubTitle { get; set; }
+
+        /// <inheritdoc/>
+        public string JournalId { get; set; }
+
+        /// <inheritdoc/>
+        public IArticleJournalModel Journal { get; set; }
+
+        /// <inheritdoc/>
+        public DateTime? PublishedOn { get; set; }
+
+        /// <inheritdoc/>
+        public DateTime? AcceptedOn { get; set; }
+
+        /// <inheritdoc/>
+        public DateTime? ReceivedOn { get; set; }
+
+        /// <inheritdoc/>
+        public string VolumeSeries { get; set; }
+
+        /// <inheritdoc/>
+        public string Volume { get; set; }
+
+        /// <inheritdoc/>
+        public string Issue { get; set; }
+
+        /// <inheritdoc/>
+        public string IssuePart { get; set; }
+
+        /// <inheritdoc/>
+        public string ELocationId { get; set; }
+
+        /// <inheritdoc/>
+        public string FirstPage { get; set; }
+
+        /// <inheritdoc/>
+        public string LastPage { get; set; }
+
+        /// <inheritdoc/>
+        public int NumberOfPages { get; set; }
+
+        /// <inheritdoc/>
+        public string CreatedBy { get; set; }
+
+        /// <inheritdoc/>
+        public DateTime CreatedOn { get; set; }
+
+        /// <inheritdoc/>
+        public string ModifiedBy { get; set; }
+
+        /// <inheritdoc/>
+        public DateTime ModifiedOn { get; set; }
+    }
+}
---
+++ src/ProcessingTools/Services/ProcessingTools.Services.Models/Documents/Articles/ArticleModel.cs
@@ -0,0 +1,78 @@
+﻿// <copyright file="ArticleModel.cs" company="ProcessingTools">
+// Copyright (c) 2017 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Services.Models.Documents.Articles
+{
+    using System;
+    using ProcessingTools.Services.Models.Contracts.Documents.Articles;
+
+    /// <summary>
+    /// Article model.
+    /// </summary>
+    public class ArticleModel : IArticleModel
+    {
+        /// <inheritdoc/>
+        public string Id { get; set; }
+
+        /// <inheritdoc/>
+        public bool IsFinalized { get; set; }
+
+        /// <inheritdoc/>
+        public bool IsDeployed { get; set; }
+
+        /// <inheritdoc/>
+        public string Title { get; set; }
+
+        /// <inheritdoc/>
+        public string SubTitle { get; set; }
+
+        /// <inheritdoc/>
+        public string JournalId { get; set; }
+
+        /// <inheritdoc/>
+        public DateTime? PublishedOn { get; set; }
+
+        /// <inheritdoc/>
+        public DateTime? AcceptedOn { get; set; }
+
+        /// <inheritdoc/>
+        public DateTime? ReceivedOn { get; set; }
+
+        /// <inheritdoc/>
+        public string VolumeSeries { get; set; }
+
+        /// <inheritdoc/>
+        public string Volume { get; set; }
+
+        /// <inheritdoc/>
+        public string Issue { get; set; }
+
+        /// <inheritdoc/>
+        public string IssuePart { get; set; }
+
+        /// <inheritdoc/>
+        public string ELocationId { get; set; }
+
+        /// <inheritdoc/>
+        public string FirstPage { get; set; }
+
+        /// <inheritdoc/>
+        public string LastPage { get; set; }
+
+        /// <inheritdoc/>
+        public int NumberOfPages { get; set; }
+
+        /// <inheritdoc/>
+        public string CreatedBy { get; set; }
+
+        /// <inheritdoc/>
+        public DateTime CreatedOn { get; set; }
+
+        /// <inheritdoc/>
+        public string ModifiedBy { get; set; }
+
+        /// <inheritdoc/>
+        public DateTime ModifiedOn { get; set; }
+    }
+}
