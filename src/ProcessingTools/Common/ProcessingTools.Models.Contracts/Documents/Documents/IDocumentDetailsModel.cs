6801bb9b52401d4191d874bbdc559605bdac84c4
Bozhin Karaivanov <bozhink@gmail.com>
2018-03-26 22:59:48 +0300

    Add IDocumentDetailsModel.cs

---
+++ src/ProcessingTools/Common/ProcessingTools.Models.Contracts/Documents/Documents/IDocumentDetailsModel.cs
@@ -0,0 +1,18 @@
+﻿// <copyright file="IDocumentDetailsModel.cs" company="ProcessingTools">
+// Copyright (c) 2017 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Models.Contracts.Documents.Documents
+{
+    /// <summary>
+    /// Document details model.
+    /// </summary>
+    public interface IDocumentDetailsModel : IDocumentModel
+    {
+        /// <summary>
+        /// Gets or sets the number of files.
+        /// </summary>
+        long NumberOfFiles { get; set; }
+    }
+}
+
