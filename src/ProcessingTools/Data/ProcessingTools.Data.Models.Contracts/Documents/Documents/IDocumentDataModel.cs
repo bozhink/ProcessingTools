cf7a71e310c3dbd5f24ff5ff65f16c455bfab226
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2018-03-27 16:17:56 +0300

    Add data model contracts

---
+++ src/ProcessingTools/Data/ProcessingTools.Data.Models.Contracts/Documents/Documents/IDocumentDataModel.cs
@@ -0,0 +1,13 @@
+﻿// <copyright file="IDocumentDataModel.cs" company="ProcessingTools">
+// Copyright (c) 2017 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Data.Models.Contracts.Documents.Documents
+{
+    /// <summary>
+    /// Document data model.
+    /// </summary>
+    public interface IDocumentDataModel : IDataModel, ProcessingTools.Models.Contracts.Documents.Documents.IDocumentModel
+    {
+    }
+}
---
+++ src/ProcessingTools/Data/ProcessingTools.Data.Models.Contracts/Documents/Documents/IDocumentDetailsDataModel.cs
@@ -0,0 +1,13 @@
+﻿// <copyright file="IDocumentDetailsDataModel.cs" company="ProcessingTools">
+// Copyright (c) 2017 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Data.Models.Contracts.Documents.Documents
+{
+    /// <summary>
+    /// Document details data model.
+    /// </summary>
+    public interface IDocumentDetailsDataModel : IDocumentDataModel, ProcessingTools.Models.Contracts.Documents.Documents.IDocumentDetailsModel
+    {
+    }
+}
