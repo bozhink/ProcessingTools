67104928eb2ccd45c4033fe18465663307981d47
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2018-03-28 10:37:54 +0300

    Add ProcessingTools.Services.Models.Documents.Documents.DocumentFileModel

---
+++ src/ProcessingTools/Services/ProcessingTools.Services.Models/Documents/Documents/DocumentFileModel.cs
@@ -0,0 +1,47 @@
+﻿// <copyright file="DocumentFileModel.cs" company="ProcessingTools">
+// Copyright (c) 2017 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Services.Models.Documents.Documents
+{
+    using ProcessingTools.Services.Models.Contracts.Documents.Documents;
+
+    /// <summary>
+    /// Document file model.
+    /// </summary>
+    public class DocumentFileModel : IDocumentFileModel
+    {
+        /// <inheritdoc/>
+        public string DocumentId { get; set; }
+
+        /// <inheritdoc/>
+        public string Content { get; set; }
+
+        /// <inheritdoc/>
+        public string ContentType { get; set; }
+
+        /// <inheritdoc/>
+        public long ContentLength { get; set; }
+
+        /// <inheritdoc/>
+        public string FileExtension { get; set; }
+
+        /// <inheritdoc/>
+        public string FileName { get; set; }
+
+        /// <inheritdoc/>
+        public long OriginalContentLength { get; set; }
+
+        /// <inheritdoc/>
+        public string OriginalContentType { get; set; }
+
+        /// <inheritdoc/>
+        public string OriginalFileExtension { get; set; }
+
+        /// <inheritdoc/>
+        public string OriginalFileName { get; set; }
+
+        /// <inheritdoc/>
+        public string SystemFileName { get; set; }
+    }
+}
