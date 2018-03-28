1062dabb2b72cf76fb66a710f33880b507aa96f5
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2018-03-28 10:32:36 +0300

    Add ProcessingTools.Models.Contracts.Documents.Documents.IDocumentFileModel

---
+++ src/ProcessingTools/Common/ProcessingTools.Models.Contracts/Documents/Documents/IDocumentFileModel.cs
@@ -0,0 +1,13 @@
+﻿// <copyright file="IDocumentFileModel.cs" company="ProcessingTools">
+// Copyright (c) 2017 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Models.Contracts.Documents.Documents
+{
+    /// <summary>
+    /// Document file model.
+    /// </summary>
+    public interface IDocumentFileModel : IDocumentContentModel, ProcessingTools.Models.Contracts.Documents.Files.IFileBaseModel
+    {
+    }
+}
