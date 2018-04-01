e547676d2928d7780b38649bfac0379c402d5f20
Bozhin Karaivanov <bozhink@users.noreply.github.com>
2018-04-01 23:55:29 +0300

    Add ProcessingTools.Harvesters.Models.Meta.ArticleMetaModel

---
+++ src/ProcessingTools/Services/ProcessingTools.Harvesters.Models.Contracts/Meta/IArticleMetaModel.cs
@@ -0,0 +1,99 @@
+﻿// <copyright file="IArticleMetaModel.cs" company="ProcessingTools">
+// Copyright (c) 2017 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Harvesters.Models.Contracts.Meta
+{
+    using System;
+
+    /// <summary>
+    /// Article meta model.
+    /// </summary>
+    public interface IArticleMetaModel
+    {
+        /// <summary>
+        /// Gets the title.
+        /// </summary>
+        string Title { get; }
+
+        /// <summary>
+        /// Gets the subtitle.
+        /// </summary>
+        string SubTitle { get; }
+
+        /// <summary>
+        /// Gets the journal ID.
+        /// </summary>
+        string JournalId { get; }
+
+        /// <summary>
+        /// Gets the journal name.
+        /// </summary>
+        string JournalName { get; }
+
+        /// <summary>
+        /// Gets the journal abbreviated name.
+        /// </summary>
+        string JournalAbbreviatedName { get; }
+
+        /// <summary>
+        /// Gets the publisher name.
+        /// </summary>
+        string PublisherName { get; }
+
+        /// <summary>
+        /// Gets the publisher abbreviated name.
+        /// </summary>
+        string PublisherAbbreviatedName { get; }
+
+        /// <summary>
+        /// Gets the published date.
+        /// </summary>
+        DateTime? PublishedOn { get; }
+
+        /// <summary>
+        /// Gets the accepted date.
+        /// </summary>
+        DateTime? AcceptedOn { get; }
+
+        /// <summary>
+        /// Gets the received date.
+        /// </summary>
+        DateTime? ReceivedOn { get; }
+
+        /// <summary>
+        /// Gets the volume series.
+        /// </summary>
+        string VolumeSeries { get; }
+
+        /// <summary>
+        /// Gets the volume.
+        /// </summary>
+        string Volume { get; }
+
+        /// <summary>
+        /// Gets the issue.
+        /// </summary>
+        string Issue { get; }
+
+        /// <summary>
+        /// Gets the issue part.
+        /// </summary>
+        string IssuePart { get; }
+
+        /// <summary>
+        /// Gets the e-location ID.
+        /// </summary>
+        string ELocationId { get; }
+
+        /// <summary>
+        /// Gets the first page.
+        /// </summary>
+        string FirstPage { get; }
+
+        /// <summary>
+        /// Gets the last page.
+        /// </summary>
+        string LastPage { get; }
+    }
+}
---
+++ src/ProcessingTools/Services/ProcessingTools.Harvesters.Models/Meta/ArticleMetaModel.cs
@@ -0,0 +1,66 @@
+﻿// <copyright file="ArticleMetaModel.cs" company="ProcessingTools">
+// Copyright (c) 2017 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Harvesters.Models.Meta
+{
+    using System;
+    using ProcessingTools.Harvesters.Models.Contracts.Meta;
+
+    /// <summary>
+    /// Article meta model.
+    /// </summary>
+    public class ArticleMetaModel : IArticleMetaModel
+    {
+        /// <inheritdoc/>
+        public string Title { get; set; }
+
+        /// <inheritdoc/>
+        public string SubTitle { get; set; }
+
+        /// <inheritdoc/>
+        public string JournalId { get; set; }
+
+        /// <inheritdoc/>
+        public string JournalName { get; set; }
+
+        /// <inheritdoc/>
+        public string JournalAbbreviatedName { get; set; }
+
+        /// <inheritdoc/>
+        public string PublisherName { get; set; }
+
+        /// <inheritdoc/>
+        public string PublisherAbbreviatedName { get; set; }
+
+        /// <inheritdoc/>
+        public DateTime? PublishedOn { get; set; }
+
+        /// <inheritdoc/>
+        public DateTime? AcceptedOn { get; set; }
+
+        /// <inheritdoc/>
+        public DateTime? ReceivedOn { get; set; }
+
+        /// <inheritdoc/>
+        public string VolumeSeries { get; set; }
+
+        /// <inheritdoc/>
+        public string Volume { get; set; }
+
+        /// <inheritdoc/>
+        public string Issue { get; set; }
+
+        /// <inheritdoc/>
+        public string IssuePart { get; set; }
+
+        /// <inheritdoc/>
+        public string ELocationId { get; set; }
+
+        /// <inheritdoc/>
+        public string FirstPage { get; set; }
+
+        /// <inheritdoc/>
+        public string LastPage { get; set; }
+    }
+}
