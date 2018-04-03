5a9355300983e940080a2268d8c384c6186e250c
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2018-04-03 17:20:59 +0300

    Removed connected services from ProcessingTools.Clients.

--- src/ProcessingTools/Services/Data/Services.Data/Services/Bio/Taxonomy/AphiaTaxaClassificationResolver.cs
+++ src/ProcessingTools/Services/Data/Services.Data/Services/Bio/Taxonomy/AphiaTaxaClassificationResolver.cs
@@ -1,97 +1,24 @@
 ﻿namespace ProcessingTools.Services.Data.Services.Bio.Taxonomy
 {
-    using System.Collections.Generic;
-    using System.Linq;
+    using System;
     using System.Threading.Tasks;
-    using ProcessingTools.Clients.Bio.Aphia.ServiceReference;
-    using ProcessingTools.Enumerations;
-    using ProcessingTools.Extensions;
+    using ProcessingTools.Clients.Contracts.Bio.Taxonomy;
     using ProcessingTools.Models.Contracts.Bio.Taxonomy;
     using ProcessingTools.Services.Abstractions.Bio.Taxonomy;
     using ProcessingTools.Services.Contracts.Bio.Taxonomy;
-    using ProcessingTools.Services.Models.Data.Bio.Taxonomy;
 
     public class AphiaTaxaClassificationResolver : AbstractTaxaInformationResolver<ITaxonClassification>, IAphiaTaxaClassificationResolver
     {
+        private readonly IAphiaTaxonClassificationRequester requester;
+
+        public AphiaTaxaClassificationResolver(IAphiaTaxonClassificationRequester requester)
+        {
+            this.requester = requester ?? throw new ArgumentNullException(nameof(requester));
+        }
+
         protected override async Task<ITaxonClassification[]> ResolveScientificNameAsync(string scientificName)
         {
-            var aphiaRecords = await this.GetAphiaRecords(scientificName);
-
-            var result = new HashSet<ITaxonClassification>();
-
-            if (aphiaRecords != null && aphiaRecords.@return.Length > 0)
-            {
-                var records = aphiaRecords.@return
-                    .Where(s => string.Compare(s.scientificname, scientificName, true) == 0)
-                    .Select(this.MapAphiaRecordToTaxonClassification);
-
-                foreach (var record in records)
-                {
-                    result.Add(record);
-                }
-            }
-
-            return result.ToArray();
-        }
-
-        private async Task<getAphiaRecordsResponse> GetAphiaRecords(string scientificName)
-        {
-            var client = new AphiaNameServicePortTypeClient();
-
-            await client.OpenAsync().ConfigureAwait(false);
-
-            var aphiaRecords = await client.getAphiaRecordsAsync(new getAphiaRecordsRequest(scientificName, false, true, false, 0)).ConfigureAwait(false);
-
-            await client.CloseAsync().ConfigureAwait(false);
-
-            return aphiaRecords;
-        }
-
-        private ITaxonClassification MapAphiaRecordToTaxonClassification(AphiaRecord record)
-        {
-            var result = new TaxonClassification
-            {
-                Rank = record.rank.MapTaxonRankStringToTaxonRankType(),
-                ScientificName = record.scientificname,
-                Authority = record.authority,
-                CanonicalName = record.valid_name
-            };
-
-            result.Classification.Add(new TaxonRank
-            {
-                Rank = TaxonRankType.Kingdom,
-                ScientificName = record.kingdom
-            });
-
-            result.Classification.Add(new TaxonRank
-            {
-                Rank = TaxonRankType.Phylum,
-                ScientificName = record.phylum
-            });
-
-            result.Classification.Add(new TaxonRank
-            {
-                Rank = TaxonRankType.Class,
-                ScientificName = record.@class
-            });
-
-            result.Classification.Add(new TaxonRank
-            {
-                Rank = TaxonRankType.Order,
-                ScientificName = record.order
-            });
-
-            result.Classification.Add(new TaxonRank
-            {
-                Rank = TaxonRankType.Family,
-                ScientificName = record.family
-            });
-
-            result.Classification.Add(new TaxonRank
-            {
-                Rank = TaxonRankType.Genus,
-                ScientificName = record.genus
-            });
+            var result = await this.requester.ResolveScientificNameAsync(scientificName).ConfigureAwait(false);
 
             return result;
         }
---
+++ src/ProcessingTools/Services/ProcessingTools.Clients/Bio/Aphia/AphiaTaxonClassificationRequester.cs
@@ -0,0 +1,113 @@
+﻿// <copyright file="AphiaTaxonClassificationRequester.cs" company="ProcessingTools">
+// Copyright (c) 2017 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Clients.Bio.Aphia
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using ProcessingTools.Clients.Bio.Aphia.ServiceReference;
+    using ProcessingTools.Clients.Contracts.Bio.Taxonomy;
+    using ProcessingTools.Clients.Models.Bio.Taxonomy.Aphia;
+    using ProcessingTools.Enumerations;
+    using ProcessingTools.Extensions;
+    using ProcessingTools.Models.Contracts.Bio.Taxonomy;
+
+    /// <summary>
+    /// Aphia taxon classification requester.
+    /// </summary>
+    public class AphiaTaxonClassificationRequester : IAphiaTaxonClassificationRequester
+    {
+        /// <inheritdoc/>
+        public async Task<ITaxonClassification[]> ResolveScientificNameAsync(string scientificName)
+        {
+            if (string.IsNullOrWhiteSpace(scientificName))
+            {
+                throw new ArgumentNullException(nameof(scientificName));
+            }
+
+            var aphiaRecords = await this.GetAphiaRecords(scientificName);
+
+            var result = new HashSet<ITaxonClassification>();
+
+            if (aphiaRecords != null && aphiaRecords.@return.Length > 0)
+            {
+                var records = aphiaRecords.@return
+                    .Where(s => string.Compare(s.scientificname, scientificName, true) == 0)
+                    .Select(this.MapAphiaRecordToTaxonClassification);
+
+                foreach (var record in records)
+                {
+                    result.Add(record);
+                }
+            }
+
+            return result.ToArray();
+        }
+
+        private async Task<getAphiaRecordsResponse> GetAphiaRecords(string scientificName)
+        {
+            var client = new AphiaNameServicePortTypeClient();
+
+            await client.OpenAsync().ConfigureAwait(false);
+
+            var aphiaRecords = await client.getAphiaRecordsAsync(new getAphiaRecordsRequest(scientificName, false, true, false, 0)).ConfigureAwait(false);
+
+            await client.CloseAsync().ConfigureAwait(false);
+
+            return aphiaRecords;
+        }
+
+        private ITaxonClassification MapAphiaRecordToTaxonClassification(AphiaRecord record)
+        {
+            var result = new TaxonClassification
+            {
+                Rank = record.rank.MapTaxonRankStringToTaxonRankType(),
+                ScientificName = record.scientificname,
+                Authority = record.authority,
+                CanonicalName = record.valid_name,
+                Classification = new HashSet<ITaxonRank>()
+            };
+
+            result.Classification.Add(new TaxonRank
+            {
+                Rank = TaxonRankType.Kingdom,
+                ScientificName = record.kingdom
+            });
+
+            result.Classification.Add(new TaxonRank
+            {
+                Rank = TaxonRankType.Phylum,
+                ScientificName = record.phylum
+            });
+
+            result.Classification.Add(new TaxonRank
+            {
+                Rank = TaxonRankType.Class,
+                ScientificName = record.@class
+            });
+
+            result.Classification.Add(new TaxonRank
+            {
+                Rank = TaxonRankType.Order,
+                ScientificName = record.order
+            });
+
+            result.Classification.Add(new TaxonRank
+            {
+                Rank = TaxonRankType.Family,
+                ScientificName = record.family
+            });
+
+            result.Classification.Add(new TaxonRank
+            {
+                Rank = TaxonRankType.Genus,
+                ScientificName = record.genus
+            });
+
+            return result;
+        }
+    }
+}
