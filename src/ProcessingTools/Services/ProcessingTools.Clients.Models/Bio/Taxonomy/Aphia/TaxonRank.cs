3fc17e8679d377623bf18c317c199e24d2506835
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2018-04-03 17:12:46 +0300

    Update ProcessingTools.Clients.Models

---
+++ src/ProcessingTools/Services/ProcessingTools.Clients.Models/Bio/Taxonomy/Aphia/TaxonRank.cs
@@ -0,0 +1,21 @@
+﻿// <copyright file="TaxonRank.cs" company="ProcessingTools">
+// Copyright (c) 2017 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Clients.Models.Bio.Taxonomy.Aphia
+{
+    using ProcessingTools.Enumerations;
+    using ProcessingTools.Models.Contracts.Bio.Taxonomy;
+
+    /// <summary>
+    /// Taxon rank.
+    /// </summary>
+    public class TaxonRank : ITaxonRank
+    {
+        /// <inheritdoc/>
+        public string ScientificName { get; set; }
+
+        /// <inheritdoc/>
+        public TaxonRankType Rank { get; set; }
+    }
+}
