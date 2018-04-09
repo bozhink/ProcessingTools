666f2b407208894afee254a5a60d409cbee06af4
Bozhin Karaivanov <bozhink@users.noreply.github.com>
2018-04-09 13:23:43 +0300

    Add ProcessingTools.Models.Contracts.IO

---
+++ src/ProcessingTools/Common/ProcessingTools.Models.Contracts/IO/ISystemFileMetadata.cs
@@ -0,0 +1,17 @@
+﻿// <copyright file="ISystemFileMetadata.cs" company="ProcessingTools">
+// Copyright (c) 2017 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Models.Contracts.IO
+{
+    /// <summary>
+    /// System file metadata.
+    /// </summary>
+    internal interface ISystemFileMetadata
+    {
+        /// <summary>
+        /// Gets the system file name.
+        /// </summary>
+        string SystemFileName { get; }
+    }
+}
