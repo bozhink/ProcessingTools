c399752d5eb57512b80b3599622e4e1e41f881de
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2018-04-17 15:51:40 +0300

    Add style models

---
+++ src/ProcessingTools/Common/ProcessingTools.Models.Contracts/Layout/Styles/Floats/IFloatObjectDetailsTagStyleModel.cs
@@ -0,0 +1,13 @@
+﻿// <copyright file="IFloatObjectDetailsTagStyleModel.cs" company="ProcessingTools">
+// Copyright (c) 2017 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Models.Contracts.Layout.Styles.Floats
+{
+    /// <summary>
+    /// Float object details tag style model.
+    /// </summary>
+    internal interface IFloatObjectDetailsTagStyleModel : IFloatObjectTagStyleModel
+    {
+    }
+}
