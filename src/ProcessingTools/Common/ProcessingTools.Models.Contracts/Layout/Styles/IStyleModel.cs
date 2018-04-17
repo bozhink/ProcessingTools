937fe4e5085523d4d94155e1720fb88459150456
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2018-04-17 15:27:37 +0300

    Add IStyleModel

---
+++ src/ProcessingTools/Common/ProcessingTools.Models.Contracts/Layout/Styles/IStyleModel.cs
@@ -0,0 +1,13 @@
+﻿// <copyright file="IStyleModel.cs" company="ProcessingTools">
+// Copyright (c) 2017 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Models.Contracts.Layout.Styles
+{
+    /// <summary>
+    /// Style model.
+    /// </summary>
+    public interface IStyleModel
+    {
+    }
+}
---
+++ src/ProcessingTools/Common/ProcessingTools.Models.Contracts/Layout/Styles/ITagStyleModel.cs
@@ -0,0 +1,13 @@
+﻿// <copyright file="ITagStyleModel.cs" company="ProcessingTools">
+// Copyright (c) 2017 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Models.Contracts.Layout.Styles
+{
+    /// <summary>
+    /// Tag style model.
+    /// </summary>
+    public interface ITagStyleModel : IStyleModel
+    {
+    }
+}
