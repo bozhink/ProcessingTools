aef6e25b14ee77186cd8368484ca13e53d7eaec2
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2018-04-17 16:26:36 +0300

    Add IFloatObjectTagStylesDataAccessObject

---
+++ src/ProcessingTools/Data/ProcessingTools.Data.Contracts/Layout/Styles/IFloatObjectTagStylesDataAccessObject.cs
@@ -0,0 +1,16 @@
+﻿// <copyright file="IFloatObjectTagStylesDataAccessObject.cs" company="ProcessingTools">
+// Copyright (c) 2017 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Data.Contracts.Layout.Styles
+{
+    using ProcessingTools.Data.Models.Contracts.Layout.Styles.Floats;
+    using ProcessingTools.Models.Contracts.Layout.Styles.Floats;
+
+    /// <summary>
+    /// Float object tag styles data access object.
+    /// </summary>
+    public interface IFloatObjectTagStylesDataAccessObject : IDataAccessObject<IFloatObjectTagStyleDataModel, IFloatObjectDetailsTagStyleDataModel, IFloatObjectInsertTagStyleModel, IFloatObjectUpdateTagStyleModel>
+    {
+    }
+}
