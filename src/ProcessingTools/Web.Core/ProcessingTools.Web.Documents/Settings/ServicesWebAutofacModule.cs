d03a16dc6570df3b2a379b0320b4618c220482f5
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2018-04-26 13:35:15 +0300

    Update ProcessingTools.Web.Documents.Settings.ServicesWebAutofacModule

--- src/ProcessingTools/Web.Core/ProcessingTools.Web.Documents/Settings/ServicesWebAutofacModule.cs
+++ src/ProcessingTools/Web.Core/ProcessingTools.Web.Documents/Settings/ServicesWebAutofacModule.cs
@@ -8,7 +8,9 @@ namespace ProcessingTools.Web.Documents.Settings
     using ProcessingTools.Web.Services.Admin;
     using ProcessingTools.Web.Services.Contracts.Admin;
     using ProcessingTools.Web.Services.Contracts.Documents;
+    using ProcessingTools.Web.Services.Contracts.Layout.Styles;
     using ProcessingTools.Web.Services.Documents;
+    using ProcessingTools.Web.Services.Layout.Styles;
 
     /// <summary>
     /// Autofac bindings for ProcessingTools.Web.Services.*
@@ -22,6 +24,8 @@ namespace ProcessingTools.Web.Documents.Settings
             builder.RegisterType<JournalsService>().As<IJournalsService>().InstancePerDependency();
             builder.RegisterType<ArticlesService>().As<IArticlesService>().InstancePerDependency();
             builder.RegisterType<DocumentsService>().As<IDocumentsService>().InstancePerDependency();
+
+            builder.RegisterType<FloatObjectTagStylesService>().As<IFloatObjectTagStylesService>().InstancePerDependency();
 
             builder.RegisterType<DatabasesService>().As<IDatabasesService>().InstancePerDependency();
         }
