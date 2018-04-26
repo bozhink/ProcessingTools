70d0993c78310e77303684f1c43be952842be0ec
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2018-04-26 15:07:32 +0300

    Add FloatObjectTagStylesController

--- src/ProcessingTools/Web.Core/ProcessingTools.Web.Documents/Constants/AreaNames.cs
+++ src/ProcessingTools/Web.Core/ProcessingTools.Web.Documents/Constants/AreaNames.cs
@@ -10,19 +10,24 @@ namespace ProcessingTools.Web.Documents.Constants
     public static class AreaNames
     {
         /// <summary>
-        /// Default area.
-        /// </summary>
-        public const string Default = "";
-
-        /// <summary>
         /// Admin area.
         /// </summary>
         public const string Admin = nameof(Admin);
 
         /// <summary>
+        /// Default area.
+        /// </summary>
+        public const string Default = "";
+
+        /// <summary>
         /// Documents area.
         /// </summary>
         public const string Documents = nameof(Documents);
+
+        /// <summary>
+        /// Layout area.
+        /// </summary>
+        public const string Layout = nameof(Layout);
 
         /// <summary>
         /// Tools area.
