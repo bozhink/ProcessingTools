de056a05848b072857a2064867a341f2df5c900e
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2018-04-27 13:38:38 +0300

    Add ProcessingTools.Data.Contracts.Layout.Styles.IFloatObjectParseStylesDataAccessObject

---
+++ src/ProcessingTools/Data/ProcessingTools.Data.Contracts/Layout/Styles/IFloatObjectParseStylesDataAccessObject.cs
@@ -0,0 +1,16 @@
+﻿// <copyright file="IFloatObjectParseStylesDataAccessObject.cs" company="ProcessingTools">
+// Copyright (c) 2017 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Data.Contracts.Layout.Styles
+{
+    using ProcessingTools.Data.Models.Contracts.Layout.Styles.Floats;
+    using ProcessingTools.Models.Contracts.Layout.Styles.Floats;
+
+    /// <summary>
+    /// Float object parse styles data access object.
+    /// </summary>
+    public interface IFloatObjectParseStylesDataAccessObject : IDataAccessObject<IFloatObjectParseStyleDataModel, IFloatObjectDetailsParseStyleDataModel, IFloatObjectInsertParseStyleModel, IFloatObjectUpdateParseStyleModel>
+    {
+    }
+}
