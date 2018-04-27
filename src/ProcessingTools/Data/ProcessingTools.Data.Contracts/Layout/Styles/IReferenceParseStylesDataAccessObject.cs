f65477c7f2aa974527fe567ef418c9602396664a
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2018-04-27 14:14:34 +0300

    Add contracts for reference styles data access objects

---
+++ src/ProcessingTools/Data/ProcessingTools.Data.Contracts/Layout/Styles/IReferenceParseStylesDataAccessObject.cs
@@ -0,0 +1,16 @@
+﻿// <copyright file="IReferenceParseStylesDataAccessObject.cs" company="ProcessingTools">
+// Copyright (c) 2017 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Data.Contracts.Layout.Styles
+{
+    using ProcessingTools.Data.Models.Contracts.Layout.Styles.References;
+    using ProcessingTools.Models.Contracts.Layout.Styles.References;
+
+    /// <summary>
+    /// Reference parse styles data access object.
+    /// </summary>
+    public interface IReferenceParseStylesDataAccessObject : IDataAccessObject<IReferenceParseStyleDataModel, IReferenceDetailsParseStyleDataModel, IReferenceInsertParseStyleModel, IReferenceUpdateParseStyleModel>
+    {
+    }
+}
