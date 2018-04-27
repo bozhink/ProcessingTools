5418e9c6f00ba12cbb2bd313c8482e60d82a0821
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2018-04-27 13:26:28 +0300

    Add ProcessingTools.Models.Contracts.Layout.Styles.Floats.*ParseModels

--- src/ProcessingTools/Common/ProcessingTools.Models.Contracts/Layout/Styles/Floats/IFloatObjectBaseParseStyleModel.cs
+++ src/ProcessingTools/Common/ProcessingTools.Models.Contracts/Layout/Styles/Floats/IFloatObjectBaseParseStyleModel.cs
@@ -7,7 +7,11 @@ namespace ProcessingTools.Models.Contracts.Layout.Styles.Floats
     /// <summary>
     /// Float object base parse style model.
     /// </summary>
-    public interface IFloatObjectBaseParseStyleModel
+    public interface IFloatObjectBaseParseStyleModel : IFloatObjectStyleModel, IParseStyleModel
     {
+        /// <summary>
+        /// Gets the script content.
+        /// </summary>
+        string Script { get; }
     }
 }
