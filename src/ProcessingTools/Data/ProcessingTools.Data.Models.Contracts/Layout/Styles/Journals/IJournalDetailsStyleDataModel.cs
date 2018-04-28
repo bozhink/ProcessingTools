7f71d99461f09554f1e4b3d5416eaf90f495be24
Bozhin Karaivanov <bozhink@gmail.com>
2018-04-28 22:15:01 +0300

    Add ProcessingTools.Data.Models.Contracts.Layout.Styles.Journals.*

---
+++ src/ProcessingTools/Data/ProcessingTools.Data.Models.Contracts/Layout/Styles/Journals/IJournalDetailsStyleDataModel.cs
@@ -0,0 +1,13 @@
+// <copyright file="IJournalDetailsStyleDataModel.cs" company="ProcessingTools">
+// Copyright (c) 2017 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Data.Models.Contracts.Layout.Styles.Journals
+{
+    /// <summary>
+    /// Journal details style data model.
+    /// </summary>
+    public interface IJournalDetailsStyleDataModel : IJournalStyleDataModel, ProcessingTools.Models.Contracts.Layout.Styles.Journals.IJournalDetailsStyleModel
+    {
+    }
+}
---
+++ src/ProcessingTools/Data/ProcessingTools.Data.Models.Contracts/Layout/Styles/Journals/IJournalStyleDataModel.cs
@@ -0,0 +1,13 @@
+// <copyright file="IJournalStyleDataModel.cs" company="ProcessingTools">
+// Copyright (c) 2017 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Data.Models.Contracts.Layout.Styles.Journals
+{
+    /// <summary>
+    /// Journal style data model.
+    /// </summary>
+    public interface IJournalStyleDataModel : IDataModel, ProcessingTools.Models.Contracts.Layout.Styles.Journals.IJournalStyleModel
+    {
+    }
+}
