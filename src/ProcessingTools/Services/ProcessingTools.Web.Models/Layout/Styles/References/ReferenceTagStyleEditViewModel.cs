9012036923a76ce142d3406e1831771a982b6833
Bozhin Karaivanov <bozhink@gmail.com>
2018-04-28 12:17:50 +0300

    Add ProcessingTools.Web.Models.Layout.Styles.References.ReferenceTagStyle* models

---
+++ src/ProcessingTools/Services/ProcessingTools.Web.Models/Layout/Styles/References/ReferenceTagStyleEditViewModel.cs
@@ -0,0 +1,110 @@
+﻿// <copyright file="ReferenceTagStyleEditViewModel.cs" company="ProcessingTools">
+// Copyright (c) 2017 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Web.Models.Layout.Styles.References
+{
+    using System;
+    using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations;
+    using ProcessingTools.Web.Models.Shared;
+
+    /// <summary>
+    /// Reference tag style edit view model.
+    /// </summary>
+    public class ReferenceTagStyleEditViewModel : ProcessingTools.Models.Contracts.IWebModel
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ReferenceTagStyleEditViewModel"/> class.
+        /// </summary>
+        /// <param name="userContext">The user context.</param>
+        public ReferenceTagStyleEditViewModel(UserContext userContext)
+        {
+            this.UserContext = userContext ?? throw new ArgumentNullException(nameof(userContext));
+        }
+
+        /// <summary>
+        /// Gets or sets the page heading.
+        /// </summary>
+        [Display(Name = "Edit Reference Tag Style")]
+        public string PageHeading { get; set; }
+
+        /// <summary>
+        /// Gets the user context.
+        /// </summary>
+        public UserContext UserContext { get; }
+
+        /// <summary>
+        /// Gets or sets the object ID.
+        /// </summary>
+        [Required(AllowEmptyStrings = false)]
+        [Display(Name = "ID")]
+        public string Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the style.
+        /// </summary>
+        [Required(AllowEmptyStrings = false)]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the description of the style.
+        /// </summary>
+        [Required(AllowEmptyStrings = false)]
+        [Display(Name = "Description")]
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Gets or sets the script content.
+        /// </summary>
+        [Required(AllowEmptyStrings = false)]
+        [Display(Name = "Script")]
+        public string Script { get; set; }
+
+        /// <summary>
+        /// Gets or sets the XPath for selection of the XML objects which represent the reference.
+        /// </summary>
+        [Required(AllowEmptyStrings = false)]
+        [Display(Name = "Reference XPath")]
+        public string ReferenceXPath { get; set; }
+
+        /// <summary>
+        /// Gets or sets the target XPath.
+        /// </summary>
+        [Required(AllowEmptyStrings = false)]
+        [Display(Name = "Target XPath")]
+        public string TargetXPath { get; set; }
+
+        /// <summary>
+        /// Gets or sets created by.
+        /// </summary>
+        [ReadOnly(true)]
+        [Display(Name = "Created by")]
+        public string CreatedBy { get; set; }
+
+        /// <summary>
+        /// Gets or sets created on.
+        /// </summary>
+        [ReadOnly(true)]
+        [Display(Name = "Created on")]
+        public DateTime CreatedOn { get; set; }
+
+        /// <summary>
+        /// Gets or sets modified by.
+        /// </summary>
+        [ReadOnly(true)]
+        [Display(Name = "Modified by")]
+        public string ModifiedBy { get; set; }
+
+        /// <summary>
+        /// Gets or sets modified on.
+        /// </summary>
+        [ReadOnly(true)]
+        [Display(Name = "Modified on")]
+        public DateTime ModifiedOn { get; set; }
+
+        /// <inheritdoc/>
+        public string ReturnUrl { get; set; }
+    }
+}
---
+++ src/ProcessingTools/Services/ProcessingTools.Web.Models/Layout/Styles/References/ReferenceTagStylesIndexViewModel.cs
@@ -0,0 +1,45 @@
+﻿// <copyright file="ReferenceTagStylesIndexViewModel.cs" company="ProcessingTools">
+// Copyright (c) 2017 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Web.Models.Layout.Styles.References
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using ProcessingTools.Web.Models.Shared;
+
+    /// <summary>
+    /// Reference tag styles index view model.
+    /// </summary>
+    public class ReferenceTagStylesIndexViewModel : GridViewModel<ReferenceTagStyleIndexViewModel>, ProcessingTools.Models.Contracts.IWebModel
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ReferenceTagStylesIndexViewModel"/> class.
+        /// </summary>
+        /// <param name="userContext">The user context.</param>
+        /// <param name="numberOfItems">The number of grid items.</param>
+        /// <param name="numberOfItemsPerPage">The number of items per page.</param>
+        /// <param name="currentPage">The value number of the current page.</param>
+        /// <param name="items">The grid items.</param>
+        public ReferenceTagStylesIndexViewModel(UserContext userContext, long numberOfItems, long numberOfItemsPerPage, long currentPage, IEnumerable<ReferenceTagStyleIndexViewModel> items)
+            : base(numberOfItems, numberOfItemsPerPage, currentPage, items)
+        {
+            this.UserContext = userContext ?? throw new ArgumentNullException(nameof(userContext));
+        }
+
+        /// <summary>
+        /// Gets or sets the page heading.
+        /// </summary>
+        [Display(Name = "Reference Tag Styles")]
+        public string PageHeading { get; set; }
+
+        /// <summary>
+        /// Gets the user context.
+        /// </summary>
+        public UserContext UserContext { get; }
+
+        /// <inheritdoc/>
+        public string ReturnUrl { get; set; }
+    }
+}
