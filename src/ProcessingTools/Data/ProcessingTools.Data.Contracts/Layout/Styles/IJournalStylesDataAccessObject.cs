4e6027aba1d33d21b6b5aa492cb68ddeccf8bd38
Bozhin Karaivanov <bozhink@gmail.com>
2018-04-28 22:32:44 +0300

    Add IJournalStylesDataAccessObject

---
+++ src/ProcessingTools/Data/ProcessingTools.Data.Contracts/Layout/Styles/IJournalStylesDataAccessObject.cs
@@ -0,0 +1,16 @@
+// <copyright file="IJournalStylesDataAccessObject.cs" company="ProcessingTools">
+// Copyright (c) 2017 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Data.Contracts.Layout.Styles
+{
+    using ProcessingTools.Data.Models.Contracts.Layout.Styles.Journals;
+    using ProcessingTools.Models.Contracts.Layout.Styles.Journals;
+
+    /// <summary>
+    /// Journal styles data access object.
+    /// </summary>
+    public interface IJournalStylesDataAccessObject : IDataAccessObject<IJournalStyleDataModel, IJournalDetailsStyleDataModel, IJournalInsertStyleModel, IJournalUpdateStyleModel>
+    {
+    }
+}
