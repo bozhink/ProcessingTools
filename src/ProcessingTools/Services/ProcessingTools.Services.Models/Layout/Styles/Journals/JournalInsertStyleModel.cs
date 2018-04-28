f4da48658c217176d2c5d1d174a9528a39716ad0
Bozhin Karaivanov <bozhink@gmail.com>
2018-04-29 00:17:01 +0300

    Add ProcessingTools.Services.Models.Layout.Styles.Journals.*

---
+++ src/ProcessingTools/Services/ProcessingTools.Services.Models/Layout/Styles/Journals/JournalInsertStyleModel.cs
@@ -0,0 +1,32 @@
+// <copyright file="JournalInsertStyleModel.cs" company="ProcessingTools">
+// Copyright (c) 2017 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Services.Models.Layout.Styles.Journals
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Journal insert style model.
+    /// </summary>
+    public class JournalInsertStyleModel : ProcessingTools.Services.Models.Contracts.Layout.Styles.Journals.IJournalInsertStyleModel
+    {
+        /// <inheritdoc/>
+        public string Name { get; set; }
+
+        /// <inheritdoc/>
+        public string Description { get; set; }
+
+        /// <inheritdoc/>
+        public IList<string> FloatObjectParseStyleIds { get; set; } = new List<string>();
+
+        /// <inheritdoc/>
+        public IList<string> FloatObjectTagStyleIds { get; set; } = new List<string>();
+
+        /// <inheritdoc/>
+        public IList<string> ReferenceParseStyleIds { get; set; } = new List<string>();
+
+        /// <inheritdoc/>
+        public IList<string> ReferenceTagStyleIds { get; set; } = new List<string>();
+    }
+}
