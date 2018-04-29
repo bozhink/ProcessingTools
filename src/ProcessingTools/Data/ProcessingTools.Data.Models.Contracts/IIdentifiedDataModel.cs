cf32caf834c8564c8a6ffc3bbe6662beb922ad29
Bozhin Karaivanov <bozhink@gmail.com>
2018-04-29 13:18:39 +0300

    Add IIdentifiedStyleDataModel

---
+++ src/ProcessingTools/Data/ProcessingTools.Data.Models.Contracts/IIdentifiedDataModel.cs
@@ -0,0 +1,20 @@
+// <copyright file="IIdentifiedDataModel.cs" company="ProcessingTools">
+// Copyright (c) 2017 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Data.Models.Contracts
+{
+    using System;
+    using ProcessingTools.Models.Contracts;
+
+    /// <summary>
+    /// Identified data model.
+    /// </summary>
+    public interface IIdentifiedDataModel : IStringIdentifiable
+    {
+        /// <summary>
+        /// Gets the object ID.
+        /// </summary>
+        Guid ObjectId { get; }
+    }
+}
