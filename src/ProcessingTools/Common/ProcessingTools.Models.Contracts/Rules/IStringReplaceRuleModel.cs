a898066a5b437dd7ca7d4d3b1c5dd6dee70e6ecf
Bozhin Karaivanov <bozhink@users.noreply.github.com>
2018-05-01 13:45:31 +0300

    Add IXmlReplaceRuleSetModel

---
+++ src/ProcessingTools/Common/ProcessingTools.Models.Contracts/Rules/IStringReplaceRuleModel.cs
@@ -0,0 +1,22 @@
+﻿// <copyright file="IStringReplaceRuleModel.cs" company="ProcessingTools">
+// Copyright (c) 2017 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Models.Contracts.Rules
+{
+    /// <summary>
+    /// String replace rule model.
+    /// </summary>
+    public interface IStringReplaceRuleModel : IRuleModel
+    {
+        /// <summary>
+        /// Gets the replace pattern.
+        /// </summary>
+        string Pattern { get; }
+
+        /// <summary>
+        /// Gets the replace replacement.
+        /// </summary>
+        string Replacement { get; }
+    }
+}
