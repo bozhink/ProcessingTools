6ad11d971f5aaefac0a18d8c3f1484f423ab788d
Bozhin Karaivanov <bozhink@users.noreply.github.com>
2018-05-01 14:03:54 +0300

    Add XmlReplaceRuleSetModel

---
+++ src/ProcessingTools/Services/ProcessingTools.Services.Models/Rules/StringReplaceRuleModel.cs
@@ -0,0 +1,24 @@
+﻿// <copyright file="StringReplaceRuleModel.cs" company="ProcessingTools">
+// Copyright (c) 2017 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Services.Models.Rules
+{
+    using ProcessingTools.Models.Contracts.Rules;
+
+    /// <summary>
+    /// String replace rule model.
+    /// </summary>
+    public class StringReplaceRuleModel : IStringReplaceRuleModel
+    {
+        /// <summary>
+        /// Gets or sets the replace pattern.
+        /// </summary>
+        public string Pattern { get; set; }
+
+        /// <summary>
+        /// Gets or sets the replace replacement.
+        /// </summary>
+        public string Replacement { get; set; }
+    }
+}
