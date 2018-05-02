e60f551dd2f8c39f33e9fe52159893ea3b493b8b
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2018-05-02 13:38:44 +0300

    Add ReferenceParseStyleRuleSetsProvider

---
+++ src/ProcessingTools/Services/ProcessingTools.Services.Contracts/Layout/Styles/IReferenceParseStyleRuleSetsProvider.cs
@@ -0,0 +1,20 @@
+﻿// <copyright file="IReferenceParseStyleRuleSetsProvider.cs" company="ProcessingTools">
+// Copyright (c) 2017 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Services.Contracts.Layout.Styles
+{
+    using System.Collections.Generic;
+    using ProcessingTools.Models.Contracts.Rules;
+
+    /// <summary>
+    /// Reference parse style rule sets provider.
+    /// </summary>
+    public interface IReferenceParseStyleRuleSetsProvider
+    {
+        /// <summary>
+        /// Gets or sets the rule sets.
+        /// </summary>
+        IEnumerable<IXmlReplaceRuleSetModel> RuleSets { get; set; }
+    }
+}
