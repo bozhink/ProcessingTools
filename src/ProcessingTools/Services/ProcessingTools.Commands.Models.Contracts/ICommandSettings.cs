68c951aff910fdd5ac2560bf3182d69b653288b4
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2018-05-03 10:47:16 +0300

    Moved ICommandSettings

--- src/ProcessingTools/Services/ProcessingTools.Commands.Models.Contracts/ICommandSettings.cs
+++ src/ProcessingTools/Services/ProcessingTools.Commands.Models.Contracts/ICommandSettings.cs
@@ -2,7 +2,7 @@
 // Copyright (c) 2017 ProcessingTools. All rights reserved.
 // </copyright>
 
-namespace ProcessingTools.Contracts.Commands
+namespace ProcessingTools.Commands.Models.Contracts
 {
     using System.Collections.Generic;
 
---
+++ src/ProcessingTools/Services/ProcessingTools.Commands.Models/CommandSettings.cs
@@ -0,0 +1,30 @@
+﻿// <copyright file="CommandSettings.cs" company="ProcessingTools">
+// Copyright (c) 2017 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Commands.Models
+{
+    using System.Collections.Generic;
+    using ProcessingTools.Commands.Models.Contracts;
+
+    /// <summary>
+    /// Command settings.
+    /// </summary>
+    public class CommandSettings : ICommandSettings
+    {
+        /// <inheritdoc/>
+        public bool ExtractHigherTaxa { get; set; } = false;
+
+        /// <inheritdoc/>
+        public bool ExtractLowerTaxa { get; set; } = false;
+
+        /// <inheritdoc/>
+        public bool ExtractTaxa { get; set; } = false;
+
+        /// <inheritdoc/>
+        public IList<string> FileNames { get; set; } = new List<string>();
+
+        /// <inheritdoc/>
+        public string OutputFileName { get; set; }
+    }
+}
