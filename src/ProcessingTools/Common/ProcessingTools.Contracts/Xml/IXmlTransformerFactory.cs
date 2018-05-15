058c9ce20d7a6de8c0c862c1c85e066aa3850bcb
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2018-05-15 15:21:19 +0300

    Add IXmlTransformerFactory.

---
+++ src/ProcessingTools/Common/ProcessingTools.Contracts/Xml/IXmlTransformerFactory.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file="IXmlTransformerFactory.cs" company="ProcessingTools">
+// Copyright (c) 2017 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Contracts.Xml
+{
+    /// <summary>
+    /// <see cref="IXmlTransformer"/> factory.
+    /// </summary>
+    public interface IXmlTransformerFactory
+    {
+        /// <summary>
+        /// Creates <see cref="IXmlTransformer"/> from source script.
+        /// </summary>
+        /// <param name="source">Source script to build the transformer.</param>
+        /// <returns>Instance of <see cref="IXmlTransformer"/>.</returns>
+        IXmlTransformer CreateXmlTransformerFromSourceScript(string source);
+    }
+}
