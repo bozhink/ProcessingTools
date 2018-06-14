7e63ae1e83304e21e203b3dae9c7e0ec3ab7a8ab
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2018-06-14 14:18:02 +0300

    ProcessingTools.Web.Document: client-side code is moved to ClientApp

--- src/ProcessingTools/Web.Core/ProcessingTools.Web.Documents/gulpfile.js
+++ src/ProcessingTools/Web.Core/ProcessingTools.Web.Documents/gulpfile.js
@@ -1,39 +1,70 @@
 "use strict";
 
-// See https://docs.microsoft.com/en-us/aspnet/core/client-side/bundling-and-minification?tabs=netcore-cli%2Caspnetcore2x
+const APPS_RELATIVE_PATH = "apps";
 
-const
-    srcPath = "ClientApp/src",
-    distPath = "wwwroot/build/dist",
-    compilePath = "wwwroot/build/compiled",
-    testsPath = "ClientApp/tests",
-    paths = {
-        templates: "templates",
-        less: "less",
-        css: "css",
-        js: "js",
-        apps: "js/apps",
-        typescript: "typescript",
-        tsdefinitions: "tsdefinitions"
-    };
+const JS_SRC_PATH = "ClientApp/code/js";
+const JS_DIST_PATH = "wwwroot/build/dist/js";
 
-var gulp = require("gulp"),
-    gulpUtil = require("gulp-util"),
-    concat = require("gulp-concat"),
-    less = require("gulp-less"),
-    cssmin = require("gulp-cssmin"),
-    htmlmin = require("gulp-htmlmin"),
-    uglify = require("gulp-uglify"),
-    merge = require("merge-stream"),
-    del = require("del"),
-    mocha = require("gulp-mocha"),
-    ts = require("gulp-typescript"),
-    tsProject = ts.createProject("./tsconfig.json"),
-    bundleconfig = require("./bundleconfig.json"),
-    path = require("path"),
-    webpackStream = require("webpack-stream"),
-    webpack = require("webpack"),
-    WebpackDevServer = require("webpack-dev-server");
+const TS_SRC_PATH = "ClientApp/code/ts";
+const TS_DIST_PATH = "wwwroot/build/dist/js";
+
+const CSS_SRC_PATH = "ClientApp/styles/css";
+const CSS_DIST_PATH = "wwwroot/build/dist/css";
+
+const LESS_SRC_PATH = "ClientApp/styles/less";
+const LESS_DIST_PATH = "wwwroot/build/dist/css";
+
+const SASS_SRC_PATH = "ClientApp/styles/sass";
+const SASS_DIST_PATH = "wwwroot/build/dist/css";
+
+const TEMPLATES_SRC_PATH = "ClientApp/templates";
+const TEMPLATES_DIST_PATH = "wwwroot/build/dist/templates";
+
+const TESTS_PATH = "ClientApp/tests";
+
+var gulp = require("gulp");
+var gulpUtil = require("gulp-util");
+var concat = require("gulp-concat");
+var less = require("gulp-less");
+var cssmin = require("gulp-cssmin");
+var htmlmin = require("gulp-htmlmin");
+var uglify = require("gulp-uglify");
+var merge = require("merge-stream");
+var del = require("del");
+var mocha = require("gulp-mocha");
+var ts = require("gulp-typescript");
+var tsconfig = ts.createProject("./tsconfig.json");
+var bundleconfig = require("./bundleconfig.json");
+var path = require("path");
+var webpackStream = require("webpack-stream");
+var webpack = require("webpack");
+var WebpackDevServer = require("webpack-dev-server");
+
+function getBundles(regexPattern) {
+    return bundleconfig.filter(function (bundle) {
+        return regexPattern.test(bundle.outputFileName);
+    });
+}
+
+function JsAppFactory() {
+    this.createBuild = function (srcFileName, distFileName, uglify) {
+        return function () {
+            var stream = gulp.src(path.join(JS_SRC_PATH, APPS_RELATIVE_PATH, srcFileName))
+                .pipe(webpackStream())
+                .pipe(concat(distFileName));
+
+            if (uglify) {
+                stream = stream.pipe(uglify());
+            }
+
+            stream = stream.pipe(gulp.dest(path.join(JS_DIST_PATH, APPS_RELATIVE_PATH)));
+
+            return stream;
+        }
+    }
+}
+
+var jsAppFactory = new JsAppFactory();
 
 var regex = {
     css: /\.css$/,
@@ -41,12 +72,17 @@ var regex = {
     js: /\.js$/
 };
 
+/**
+ * min
+ */
+
 gulp.task("min", ["min:js", "min:css", "min:html"]);
-gulp.task("clean", ["clean:bundle", "clean:build"]);
 
 gulp.task("min:js", function () {
     var tasks = getBundles(regex.js).map(function (bundle) {
-        return gulp.src(bundle.inputFiles, { base: "." })
+        return gulp.src(bundle.inputFiles, {
+                base: "."
+            })
             .pipe(concat(bundle.outputFileName))
             .pipe(uglify())
             .pipe(gulp.dest("."));
@@ -82,40 +118,11 @@ gulp.task("min:html", function () {
     return merge(tasks);
 });
 
-// See https://wildermuth.com/2017/11/19/ASP-NET-Core-2-0-and-the-End-of-Bower
-// Dependency Dirs
-var deps = {
-    "jquery": {
-        "/**": ""
-    },
-    "jquery-validation": {
-        "/**": ""
-    },
-    "jquery-validation-unobtrusive": {
-        "/**": ""
-    },
-    "bootstrap": {
-        "/**": ""
-    },
-    // ...
-};
+/**
+ * clean
+ */
 
-gulp.task("copyScripts", function () {
-
-    var streams = [];
-
-    for (var prop in deps) {
-        console.log("Prepping Scripts for: " + prop);
-        for (var itemProp in deps[prop]) {
-            var src = gulp.src("node_modules/" + prop + "/" + itemProp);
-            var dest = gulp.dest("wwwroot/external/" + prop + "/" + deps[prop][itemProp]);
-            streams.push(src.pipe(dest));
-        }
-    }
-
-    return merge(streams);
-
-});
+gulp.task("clean", ["clean:bundle", "clean:build"]);
 
 gulp.task("clean:bundle", function () {
     var files = bundleconfig.map(function (bundle) {
@@ -126,7 +133,14 @@ gulp.task("clean:bundle", function () {
 });
 
 gulp.task("clean:build", function () {
-    return del([compilePath, distPath, tsProject.config.compilerOptions.outDir.toString()]);
+    return del([
+        JS_DIST_PATH,
+        TS_DIST_PATH,
+        CSS_DIST_PATH,
+        LESS_DIST_PATH,
+        SASS_DIST_PATH,
+        tsconfig.config.compilerOptions.outDir.toString()
+    ]);
 });
 
 gulp.task("watch", function () {
@@ -143,36 +157,30 @@ gulp.task("watch", function () {
     });
 });
 
-function getBundles(regexPattern) {
-    return bundleconfig.filter(function (bundle) {
-        return regexPattern.test(bundle.outputFileName);
-    });
-}
-
 /**
  * Copy all templates to the distribution directory
  */
 gulp.task("copy-templates", function () {
-    return gulp.src(path.join(srcPath, paths.templates, "**/*"))
-        .pipe(gulp.dest(path.join(distPath, paths.templates)));
+    return gulp.src(path.join(TEMPLATES_SRC_PATH, "**/*"))
+        .pipe(gulp.dest(path.join(TEMPLATES_DIST_PATH)));
 });
 
 /**
  * Copy JavaScript files to the distribution directory
  */
 gulp.task("copy-js", function () {
-    return gulp.src(path.join(srcPath, paths.js, "**/*.js"))
-        .pipe(gulp.dest(path.join(distPath, paths.js)));
+    return gulp.src(path.join(JS_SRC_PATH, "**/*.js"))
+        .pipe(gulp.dest(path.join(JS_DIST_PATH)));
 });
 
 /**
  * Compile and minify LESS files
  */
 gulp.task("compile-less", function () {
-    return gulp.src(path.join(srcPath, paths.less, "**/*.less"))
+    return gulp.src(path.join(LESS_SRC_PATH, "**/*.less"))
         .pipe(less())
         .pipe(cssmin())
-        .pipe(gulp.dest(path.join(distPath, paths.css)));
+        .pipe(gulp.dest(path.join(LESS_DIST_PATH)));
 });
 
 gulp.task("webpack", function (callback) {
@@ -193,14 +201,14 @@ gulp.task("webpack", function (callback) {
     });
 });
 
-gulp.task("webpack-dev-server", function (callback){
+gulp.task("webpack-dev-server", function (callback) {
     var compiler = webpack({
         // configuration
     });
 
     new WebpackDevServer(compiler, {
         // server and middleware options
-    }).listen(9090, "localhost", function (error){
+    }).listen(9090, "localhost", function (error) {
         if (error) {
             throw new gulpUtil.PluginError("webpack-dev-server", error);
         }
@@ -212,56 +220,32 @@ gulp.task("webpack-dev-server", function (callback){
         //     callback();
         // }
     })
-})
+});
 
 /**
- * Compile apps
+ * Build apps.
  */
-gulp.task("build-bio-data-app", function () {
-    return gulp
-        .src([
-            path.join(srcPath, paths.apps, "bio-data-app.js")
-        ])
-        .pipe(webpackStream())
-        .pipe(concat("bio-data-app.min.js"))
-        //.pipe(uglify())
-        .pipe(gulp.dest(path.join(distPath, paths.apps)));
-});
 
-gulp.task("build-document-edit", function () {
-    return gulp.src([
-            path.join(srcPath, paths.apps, "document-edit.js")
-        ])
-        .pipe(webpackStream())
-        .pipe(concat("document-edit.min.js"))
-        //.pipe(uglify())
-        .pipe(gulp.dest(path.join(distPath, paths.apps)));
-});
+gulp.task("build-apps", [
+    "build-bio-data-app",
+    "build-document-edit",
+    "build-document-preview",
+    "build-files-index"
+]);
 
-gulp.task("build-document-preview", function () {
-    return gulp.src([
-            path.join(srcPath, paths.apps, "document-preview.js")
-        ])
-        .pipe(webpackStream())
-        .pipe(concat("document-preview.min.js"))
-        //.pipe(uglify())
-        .pipe(gulp.dest(path.join(distPath, paths.apps)));
-});
+gulp.task("build-bio-data-app", jsAppFactory.createBuild("bio-data-app.js", "bio-data-app.min.js", false));
+gulp.task("build-document-edit", jsAppFactory.createBuild("document-edit.js", "document-edit.min.js", false));
+gulp.task("build-document-preview", jsAppFactory.createBuild("document-preview.js", "document-preview.min.js", false));
+gulp.task("build-files-index", jsAppFactory.createBuild("files-index.js", "files-index.min.js", false));
 
-gulp.task("build-files-index", function () {
-    return gulp.src([
-            path.join(srcPath, paths.apps, "files-index.js")
-        ])
-        .pipe(webpackStream())
-        .pipe(concat("files-index.min.js"))
-        .pipe(uglify())
-        .pipe(gulp.dest(path.join(distPath, paths.apps)));
-});
+/**
+ * Build Typescript.
+ */
 
-gulp.task("build-typescript", function (){
-    return tsProject.src()
-        .pipe(tsProject())
-        .js.pipe(gulp.dest(tsProject.config.compilerOptions.outDir.toString()));
+gulp.task("build-typescript", function () {
+    return tsconfig.src()
+        .pipe(tsconfig())
+        .js.pipe(gulp.dest(tsconfig.config.compilerOptions.outDir.toString()));
 });
 
 /**
@@ -269,24 +253,18 @@ gulp.task("build-typescript", function (){
  */
 gulp.task("build", [
     "build-typescript",
+    "build-apps",
     "compile-less",
     "copy-js",
     "copy-templates",
-    "build-document-edit",
-    "build-document-preview",
-    "build-files-index",
-    "build-bio-data-app",
-    "min"]);
-
-gulp.task("watch", function () {
-    gulp.watch([srcPath], ["build"]);
-});
+    "min"
+]);
 
 /**
  * Run tests
  */
 gulp.task("test", function () {
-    return gulp.src(path.join(testsPath, "**/*.js"))
+    return gulp.src(path.join(TESTS_PATH, "**/*.js"))
         .pipe(mocha())
         .on("error", function () {
             this.emit("end");
@@ -294,7 +272,7 @@ gulp.task("test", function () {
 });
 
 gulp.task("watch-tests", function () {
-    gulp.watch(path.join(testsPath, "**/*.js"), ["test"]);
+    gulp.watch(path.join(TESTS_PATH, "**/*.js"), ["test"]);
 });
 
 /**
