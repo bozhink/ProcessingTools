d73b5aa03e90ffa121c3beeb347c2500bada9bc3
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2018-07-02 17:05:07 +0300

    Update json-requester.ts

--- src/ProcessingTools/Web.Core/ProcessingTools.Web.Documents/ClientApp/code/ts/services/http/json-requester.ts
+++ src/ProcessingTools/Web.Core/ProcessingTools.Web.Documents/ClientApp/code/ts/services/http/json-requester.ts
@@ -1,7 +1,7 @@
 import { HttpMethod, IRequesterBase, IRequestModel } from "../../contracts/http/requester-base";
 import { ContentType, IJsonRequester } from "../../contracts/http/json-requester";
 
-export class JsonRequester implements IJsonRequester<any> {
+export class JsonRequester implements IJsonRequester<any>, IRequesterBase<any> {
     private jQuery: JQueryStatic;
 
     public constructor(jQuery: JQueryStatic) {
@@ -9,7 +9,7 @@ export class JsonRequester implements IJsonRequester<any> {
     }
 
     public send(method: (string | HttpMethod), url: string, options?: IRequestModel): Promise<any> {
-        var headers: any, data: any;
+        var headers: any, data: any, promise: Promise<any>, $: JQueryStatic = this.jQuery;
 
         if (!url) {
             throw "URL is required";
@@ -24,13 +24,23 @@ export class JsonRequester implements IJsonRequester<any> {
         headers = options.headers || {};
         data = options.data || {};
 
-        return this.jQuery.ajax({
-            url: url,
-            method: method,
-            contentType: ContentType,
-            headers: headers,
-            data: JSON.stringify(data)
+        promise = new Promise(function (resolve: (value?: any) => void, reject: (reason?: any) => void): void {
+            $.ajax({
+                url: url,
+                method: method,
+                contentType: ContentType,
+                headers: headers,
+                data: JSON.stringify(data),
+                success: function (response: any): any {
+                    resolve(response);
+                },
+                error: function (error: any): any {
+                    reject(error);
+                }
+            });
         });
+
+        return promise;
     }
 
     public delete(url: string, options?: IRequestModel): Promise<any> {
