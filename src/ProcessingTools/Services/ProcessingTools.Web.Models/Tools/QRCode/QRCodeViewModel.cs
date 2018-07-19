5cf8fdf46ce55f6f70b26e061cec6f0b8217dbcf
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2018-07-19 14:18:08 +0300

    Add ProcessingTools.Web.Models.Tools.QRCode.QRCodeViewModel

--- src/ProcessingTools/Services/ProcessingTools.Web.Models/Tools/QRCode/QRCodeViewModel.cs
+++ src/ProcessingTools/Services/ProcessingTools.Web.Models/Tools/QRCode/QRCodeViewModel.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="QRCodeRequestModel.cs" company="ProcessingTools">
+﻿// <copyright file="QRCodeViewModel.cs" company="ProcessingTools">
 // Copyright (c) 2018 ProcessingTools. All rights reserved.
 // </copyright>
 
@@ -9,15 +9,24 @@ namespace ProcessingTools.Web.Models.Tools.QRCode
     using ValidationConstants = ProcessingTools.Constants.ValidationConstants;
 
     /// <summary>
-    /// QR code request model
+    /// QR code view model.
     /// </summary>
-    public class QRCodeRequestModel
+    public class QRCodeViewModel
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="QRCodeViewModel"/> class.
+        /// </summary>
+        public QRCodeViewModel()
+        {
+            this.PixelPerModule = ImagingConstants.DefaultQRCodePixelsPerModule;
+        }
+
         /// <summary>
         /// Gets or sets the number of pixels per module.
         /// </summary>
         [Required]
         [Range(ImagingConstants.MinimalQRCodePixelsPerModule, ImagingConstants.MaximalQRCodePixelsPerModule)]
+        [Display(Name = "Pixels per module", Description = "Pixels per module")]
         public int PixelPerModule { get; set; }
 
         /// <summary>
@@ -25,6 +34,12 @@ namespace ProcessingTools.Web.Models.Tools.QRCode
         /// </summary>
         [Required(AllowEmptyStrings = false)]
         [MaxLength(ValidationConstants.DefaultMaximalLengthOfContent)]
+        [Display(Name = "Text to encode", Description = "Text to encode")]
         public string Content { get; set; }
+
+        /// <summary>
+        /// Gets or sets the encoded content as image object.
+        /// </summary>
+        public object Image { get; set; }
     }
 }
