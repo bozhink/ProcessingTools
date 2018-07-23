d9ddd2df859dab5c3fe1596370b11f37362a69ac
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2018-07-23 17:53:58 +0300

    Add ProcessingTools.Web.Services.Contracts.Geo.Coordinates.ICoordinatesParseService

---
+++ src/ProcessingTools/Services/ProcessingTools.Web.Services.Contracts/Geo/Coordinates/ICoordinatesParseService.cs
@@ -0,0 +1,22 @@
+﻿// <copyright file="ICoordinatesParseService.cs" company="ProcessingTools">
+// Copyright (c) 2018 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Web.Services.Contracts.Geo.Coordinates
+{
+    using System.Threading.Tasks;
+    using ProcessingTools.Web.Models.Tools.Coordinates;
+
+    /// <summary>
+    /// Coordinates parse service.
+    /// </summary>
+    public interface ICoordinatesParseService
+    {
+        /// <summary>
+        /// Parses coordinates string.
+        /// </summary>
+        /// <param name="model">Request model.</param>
+        /// <returns>Calculated coordinates.</returns>
+        Task<CoordinatesResponseViewModel> ParseCoordinatesStringAsync(CoordinatesRequestModel model);
+    }
+}
