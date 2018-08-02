f301452371496c986d25be925f7a9bd09481cf1b
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2018-08-02 16:44:14 +0300

    Add IDeployDataAccessObject

---
+++ src/ProcessingTools/Data/ProcessingTools.Data.Contracts/Deployment/IDeployDataAccessObject.cs
@@ -0,0 +1,22 @@
+﻿// <copyright file="IDeployDataAccessObject.cs" company="ProcessingTools">
+// Copyright (c) 2018 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Data.Contracts.Deployment
+{
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Deploy data access object.
+    /// </summary>
+    /// <typeparam name="TI">Type of the insert data model.</typeparam>
+    public interface IDeployDataAccessObject<TI> : IDataAccessObject
+    {
+        /// <summary>
+        /// Inserts item to the data store.
+        /// </summary>
+        /// <param name="model">Item to be inserted.</param>
+        /// <returns>Task</returns>
+        Task<object> InsertAsync(TI model);
+    }
+}
