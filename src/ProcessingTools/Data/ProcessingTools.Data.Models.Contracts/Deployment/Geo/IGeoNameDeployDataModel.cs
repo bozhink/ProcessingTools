1cbc4b0f88b3d8ba08acdfaaef35c58c76be19df
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2018-08-02 16:44:07 +0300

    Add Deployment data models

---
+++ src/ProcessingTools/Data/ProcessingTools.Data.Models.Contracts/Deployment/Geo/IGeoNameDeployDataModel.cs
@@ -0,0 +1,22 @@
+﻿// <copyright file="IGeoNameDeployDataModel.cs" company="ProcessingTools">
+// Copyright (c) 2018 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Data.Models.Contracts.Deployment.Geo
+{
+    /// <summary>
+    /// Geo name deploy data model.
+    /// </summary>
+    public interface IGeoNameDeployDataModel : IDeployDataModel
+    {
+        /// <summary>
+        /// Gets the value of the geo-name as text.
+        /// </summary>
+        string NameText { get; }
+
+        /// <summary>
+        /// Gets the name of the geo-name as verbatim XML.
+        /// </summary>
+        string NameXml { get; }
+    }
+}
---
+++ src/ProcessingTools/Data/ProcessingTools.Data.Models.Contracts/Deployment/IDeployDataModel.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file="IDeployDataModel.cs" company="ProcessingTools">
+// Copyright (c) 2018 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Data.Models.Contracts.Deployment
+{
+    using ProcessingTools.Models.Contracts;
+
+    /// <summary>
+    /// Deploy data model.
+    /// </summary>
+    public interface IDeployDataModel : IIdentifiedDataModel, ICreated
+    {
+        /// <summary>
+        /// Gets the identifier of the source article.
+        /// </summary>
+        string ArticleId { get; }
+    }
+}
