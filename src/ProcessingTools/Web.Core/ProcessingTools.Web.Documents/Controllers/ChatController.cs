225f01c1b3bbf736fbec011563d5efe005fdd988
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2018-08-03 14:49:46 +0300

    Update Startup.cs

---
+++ src/ProcessingTools/Web.Core/ProcessingTools.Web.Documents/Controllers/ChatController.cs
@@ -0,0 +1,36 @@
+﻿// <copyright file="ChatController.cs" company="ProcessingTools">
+// Copyright (c) 2018 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Web.Documents.Controllers
+{
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    /// <summary>
+    /// Chat
+    /// </summary>
+    [Authorize]
+    public class ChatController : Controller
+    {
+        /// <summary>
+        /// Controller name.
+        /// </summary>
+        public const string ControllerName = "Chat";
+
+        /// <summary>
+        /// Index action name.
+        /// </summary>
+        public const string IndexActionName = nameof(Index);
+
+        /// <summary>
+        /// /Chat
+        /// </summary>
+        /// <returns><see cref="IActionResult"/></returns>
+        [ActionName(IndexActionName)]
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
