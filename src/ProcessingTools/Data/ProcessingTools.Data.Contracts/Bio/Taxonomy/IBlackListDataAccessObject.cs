7666d44222ff80e1806187db57368b6b9c17d2aa
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2018-08-09 09:01:46 +0300

    Add ProcessingTools.Data.Contracts.Bio.Taxonomy.IBlackListDataAccessObject.

---
+++ src/ProcessingTools/Data/ProcessingTools.Data.Contracts/Bio/Taxonomy/IBlackListDataAccessObject.cs
@@ -0,0 +1,13 @@
+﻿// <copyright file="IBlackListDataAccessObject.cs" company="ProcessingTools">
+// Copyright (c) 2018 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Data.Contracts.Bio.Taxonomy
+{
+    /// <summary>
+    /// Taxonomic black list data access object.
+    /// </summary>
+    public interface IBlackListDataAccessObject : IStringListDataAccessObject
+    {
+    }
+}
