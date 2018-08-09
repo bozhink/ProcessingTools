21ce361d1fb94e67124b3a128ea20567a023f534
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2018-08-09 09:04:58 +0300

    Add ProcessingTools.Data.Contracts.Bio.Taxonomy.IWhiteListDataAccessObject

---
+++ src/ProcessingTools/Data/ProcessingTools.Data.Contracts/Bio/Taxonomy/IWhiteListDataAccessObject.cs
@@ -0,0 +1,13 @@
+﻿// <copyright file="IWhiteListDataAccessObject.cs" company="ProcessingTools">
+// Copyright (c) 2018 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Data.Contracts.Bio.Taxonomy
+{
+    /// <summary>
+    /// Taxonomic white list data access object.
+    /// </summary>
+    public interface IWhiteListDataAccessObject : IStringListDataAccessObject
+    {
+    }
+}
