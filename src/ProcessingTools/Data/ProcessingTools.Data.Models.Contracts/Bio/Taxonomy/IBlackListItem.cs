c3ef347467e3a0b1df952b40300f10c2d296d98f
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2018-08-09 09:45:13 +0300

    Add ProcessingTools.Bio.Taxonomy.Data.Mongo.Models.*

---
+++ src/ProcessingTools/Data/ProcessingTools.Data.Models.Contracts/Bio/Taxonomy/IBlackListItem.cs
@@ -0,0 +1,17 @@
+﻿// <copyright file="IBlackListItem.cs" company="ProcessingTools">
+// Copyright (c) 2018 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Data.Models.Contracts.Bio.Taxonomy
+{
+    /// <summary>
+    /// Taxonomic black list item.
+    /// </summary>
+    public interface IBlackListItem
+    {
+        /// <summary>
+        /// Gets content.
+        /// </summary>
+        string Content { get; }
+    }
+}
---
+++ src/ProcessingTools/Data/ProcessingTools.Data.Models.Contracts/Bio/Taxonomy/IWhiteListItem.cs
@@ -0,0 +1,17 @@
+﻿// <copyright file="IWhiteListItem.cs" company="ProcessingTools">
+// Copyright (c) 2018 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Data.Models.Contracts.Bio.Taxonomy
+{
+    /// <summary>
+    /// Taxonomic white list item.
+    /// </summary>
+    public interface IWhiteListItem
+    {
+        /// <summary>
+        /// Gets content.
+        /// </summary>
+        string Content { get; }
+    }
+}
