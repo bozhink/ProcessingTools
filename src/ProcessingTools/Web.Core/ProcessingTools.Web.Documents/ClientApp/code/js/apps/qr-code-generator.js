9f77f598296f2ebdc7094cc1af3c039e04aa769e
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2018-08-27 17:21:15 +0300

    Update Tool pages

---
+++ src/ProcessingTools/Web.Core/ProcessingTools.Web.Documents/ClientApp/code/js/apps/qr-code-generator.js
@@ -0,0 +1,12 @@
+﻿$("#btn-generate").on("click", function () {
+    var element = document.getElementById("qrCode");
+    if (element) {
+        element.innerHTML = "";
+        new QRCode(element,
+            {
+                text: $("#text-content").val(),
+                width: 300,
+                height: 300
+            });
+    }
+});
