9fe3ba9fcd5547e22897061c77be386e8264554b
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2018-08-28 14:40:32 +0300

    Add project SentimentAnalysis

---
+++ src/ML/ML.Net.Samples/SentimentAnalysis/Program.cs
@@ -0,0 +1,23 @@
+﻿// <copyright file="Program.cs" company="ProcessingTools">
+// Copyright (c) 2018 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace SentimentAnalysis
+{
+    using System;
+
+    /// <summary>
+    /// Main Program.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// Program entry point.
+        /// </summary>
+        /// <param name="args">Command line parameters.</param>
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
