51e064d0f39a3c8f070058f6f6999fd56baab3f6
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2018-08-28 12:24:13 +0300

    Add IDecodeService

---
+++ src/ProcessingTools/Services/ProcessingTools.Services.Contracts/Tools/IDecodeService.cs
@@ -0,0 +1,28 @@
+﻿// <copyright file="IDecodeService.cs" company="ProcessingTools">
+// Copyright (c) 2018 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Services.Contracts.Tools
+{
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Decode service.
+    /// </summary>
+    public interface IDecodeService
+    {
+        /// <summary>
+        /// Decodes Base64 string to string.
+        /// </summary>
+        /// <param name="source">Base64 encoded string source to be decoded.</param>
+        /// <returns>Decoded source as string.</returns>
+        Task<string> DecodeBase64Async(string source);
+
+        /// <summary>
+        /// Decodes Base64 URL string to string.
+        /// </summary>
+        /// <param name="source">Base64 URL encoded string source to be decoded.</param>
+        /// <returns>Decoded source as string.</returns>
+        Task<string> DecodeBase64UrlAsync(string source);
+    }
+}
---
+++ src/ProcessingTools/Services/ProcessingTools.Services/Tools/DecodeService.cs
@@ -0,0 +1,52 @@
+﻿namespace ProcessingTools.Services.Tools
+{
+    using System;
+    using System.Text;
+    using System.Threading.Tasks;
+    using ProcessingTools.Services.Contracts.Tools;
+
+    /// <summary>
+    /// Decode service.
+    /// </summary>
+    public class DecodeService : IDecodeService
+    {
+        private readonly Encoding encoding;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DecodeService"/> class.
+        /// </summary>
+        /// <param name="encoding">Character encoding</param>
+        public DecodeService(Encoding encoding)
+        {
+            this.encoding = encoding ?? throw new ArgumentNullException(nameof(encoding));
+        }
+
+        /// <inheritdoc/>
+        public Task<string> DecodeBase64Async(string source)
+        {
+            string result = null;
+
+            if (!string.IsNullOrEmpty(source))
+            {
+                byte[] bytes = Convert.FromBase64String(source);
+                result = this.encoding.GetString(bytes);
+            }
+
+            return Task.FromResult(result);
+        }
+
+        /// <inheritdoc/>
+        public Task<string> DecodeBase64UrlAsync(string source)
+        {
+            string result = null;
+
+            if (!string.IsNullOrEmpty(source))
+            {
+                byte[] bytes = ProcessingTools.Security.Utils.FromBase64Url(source);
+                result = this.encoding.GetString(bytes);
+            }
+
+            return Task.FromResult(result);
+        }
+    }
+}
