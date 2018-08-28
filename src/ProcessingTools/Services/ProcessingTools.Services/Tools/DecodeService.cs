db9795816a210ebb0d23a4e8302747f6d0341da9
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2018-08-28 13:34:39 +0300

    Update ProcessingTools.Services.Tools.DecodeService: add whitespace trim

--- src/ProcessingTools/Services/ProcessingTools.Services/Tools/DecodeService.cs
+++ src/ProcessingTools/Services/ProcessingTools.Services/Tools/DecodeService.cs
@@ -32,7 +32,7 @@ namespace ProcessingTools.Services.Tools
 
             if (!string.IsNullOrEmpty(source))
             {
-                byte[] bytes = Convert.FromBase64String(source);
+                byte[] bytes = Convert.FromBase64String(source.Trim(new[] { ' ', '\t', '\r', '\n' }));
                 result = this.encoding.GetString(bytes);
             }
 
@@ -46,7 +46,7 @@ namespace ProcessingTools.Services.Tools
 
             if (!string.IsNullOrEmpty(source))
             {
-                byte[] bytes = ProcessingTools.Security.Utils.FromBase64Url(source);
+                byte[] bytes = ProcessingTools.Security.Utils.FromBase64Url(source.Trim(new[] { ' ', '\t', '\r', '\n' }));
                 result = this.encoding.GetString(bytes);
             }
 
