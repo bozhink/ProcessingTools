80ab1cb1f9a95988567fe7f01dd42dc0f9bdc762
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2018-09-14 10:19:57 +0300

    Add DocumentFullModel

---
+++ src/ProcessingTools/Infrastructure/ProcessingTools.Models.Contracts/Documents/Meta/IDocumentFullModel.cs
@@ -0,0 +1,37 @@
+﻿// <copyright file="IDocumentFullModel.cs" company="ProcessingTools">
+// Copyright (c) 2018 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Models.Contracts.Documents.Meta
+{
+    using ProcessingTools.Models.Contracts.Documents.Articles;
+    using ProcessingTools.Models.Contracts.Documents.Documents;
+    using ProcessingTools.Models.Contracts.Documents.Journals;
+    using ProcessingTools.Models.Contracts.Documents.Publishers;
+
+    /// <summary>
+    /// Document full model.
+    /// </summary>
+    public interface IDocumentFullModel
+    {
+        /// <summary>
+        /// Gets the document.
+        /// </summary>
+        IDocumentModel Document { get; }
+
+        /// <summary>
+        /// Gets the article.
+        /// </summary>
+        IArticleModel Article { get; }
+
+        /// <summary>
+        /// Gets the journal.
+        /// </summary>
+        IJournalModel Journal { get; }
+
+        /// <summary>
+        /// Gets the publisher.
+        /// </summary>
+        IPublisherModel Publisher { get; }
+    }
+}
