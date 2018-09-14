e5b30cc5d476519ccb58e32368756417de82b032
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2018-09-14 11:04:44 +0300

    Update IDocumentFullModel

--- src/ProcessingTools/Infrastructure/ProcessingTools.Models.Contracts/Documents/Meta/IDocumentFullModel.cs
+++ src/ProcessingTools/Infrastructure/ProcessingTools.Models.Contracts/Documents/Meta/IDocumentFullModel.cs
@@ -4,34 +4,16 @@
 
 namespace ProcessingTools.Models.Contracts.Documents.Meta
 {
-    using ProcessingTools.Models.Contracts.Documents.Articles;
     using ProcessingTools.Models.Contracts.Documents.Documents;
-    using ProcessingTools.Models.Contracts.Documents.Journals;
-    using ProcessingTools.Models.Contracts.Documents.Publishers;
 
     /// <summary>
     /// Document full model.
     /// </summary>
-    public interface IDocumentFullModel
+    public interface IDocumentFullModel : IArticleFullModel
     {
         /// <summary>
         /// Gets the document.
         /// </summary>
         IDocumentModel Document { get; }
-
-        /// <summary>
-        /// Gets the article.
-        /// </summary>
-        IArticleModel Article { get; }
-
-        /// <summary>
-        /// Gets the journal.
-        /// </summary>
-        IJournalModel Journal { get; }
-
-        /// <summary>
-        /// Gets the publisher.
-        /// </summary>
-        IPublisherModel Publisher { get; }
     }
 }
