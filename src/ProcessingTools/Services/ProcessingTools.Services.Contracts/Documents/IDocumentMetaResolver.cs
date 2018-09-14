59965a4655aaa2636f00c1111fc50987ac3fd45e
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2018-09-14 10:25:12 +0300

    Add DocumentMetaResolver

---
+++ src/ProcessingTools/Services/ProcessingTools.Services.Contracts/Documents/IDocumentMetaResolver.cs
@@ -0,0 +1,22 @@
+﻿// <copyright file="IDocumentMetaResolver.cs" company="ProcessingTools">
+// Copyright (c) 2018 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Services.Contracts.Documents
+{
+    using System.Threading.Tasks;
+    using ProcessingTools.Models.Contracts.Documents.Meta;
+
+    /// <summary>
+    /// Resolver of document meta-data.
+    /// </summary>
+    public interface IDocumentMetaResolver
+    {
+        /// <summary>
+        /// Gets full document meta information by specified document ID.
+        /// </summary>
+        /// <param name="documentId">ID of the document to be retrieved.</param>
+        /// <returns>Task of document model.</returns>
+        Task<IDocumentFullModel> GetDocumentAsync(object documentId);
+    }
+}
---
+++ src/ProcessingTools/Services/ProcessingTools.Services/Documents/DocumentMetaResolver.cs
@@ -0,0 +1,70 @@
+﻿namespace ProcessingTools.Services.Documents
+{
+    using System;
+    using System.Threading.Tasks;
+    using ProcessingTools.Models.Contracts.Documents.Meta;
+    using ProcessingTools.Services.Contracts.Documents;
+    using ProcessingTools.Services.Models.Documents.Meta;
+
+    /// <summary>
+    /// Resolver of document meta-data.
+    /// </summary>
+    public class DocumentMetaResolver : IDocumentMetaResolver
+    {
+        private readonly IDocumentsDataService documentsDataService;
+        private readonly IArticlesDataService articlesDataService;
+        private readonly IJournalsDataService journalsDataService;
+        private readonly IPublishersDataService publishersDataService;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DocumentMetaResolver"/> class.
+        /// </summary>
+        /// <param name="documentsDataService">Documents data service.</param>
+        /// <param name="articlesDataService">Articles data service.</param>
+        /// <param name="journalsDataService">Journals data service.</param>
+        /// <param name="publishersDataService">Publishers data service.</param>
+        public DocumentMetaResolver(IDocumentsDataService documentsDataService, IArticlesDataService articlesDataService, IJournalsDataService journalsDataService, IPublishersDataService publishersDataService)
+        {
+            this.documentsDataService = documentsDataService ?? throw new ArgumentNullException(nameof(documentsDataService));
+            this.articlesDataService = articlesDataService ?? throw new ArgumentNullException(nameof(articlesDataService));
+            this.journalsDataService = journalsDataService ?? throw new ArgumentNullException(nameof(journalsDataService));
+            this.publishersDataService = publishersDataService ?? throw new ArgumentNullException(nameof(publishersDataService));
+        }
+
+        /// <inheritdoc/>
+        public async Task<IDocumentFullModel> GetDocumentAsync(object documentId)
+        {
+            var document = await this.documentsDataService.GetByIdAsync(documentId).ConfigureAwait(false);
+            if (document == null)
+            {
+                return null;
+            }
+
+            var article = await this.articlesDataService.GetByIdAsync(document.ArticleId).ConfigureAwait(false);
+            if (article == null)
+            {
+                return null;
+            }
+
+            var journal = await this.journalsDataService.GetByIdAsync(article.JournalId).ConfigureAwait(false);
+            if (journal == null)
+            {
+                return null;
+            }
+
+            var publisher = await this.publishersDataService.GetByIdAsync(journal.PublisherId).ConfigureAwait(false);
+            if (publisher == null)
+            {
+                return null;
+            }
+
+            return new DocumentFullModel
+            {
+                Document = document,
+                Article = article,
+                Journal = journal,
+                Publisher = publisher
+            };
+        }
+    }
+}
