28eb863bd6965d764968a73afc9ca4bfa31f6b93
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2018-09-14 10:34:24 +0300

    Add ArticleFullModel

---
+++ src/ProcessingTools/Infrastructure/ProcessingTools.Models.Contracts/Documents/Meta/IArticleFullModel.cs
@@ -0,0 +1,38 @@
+﻿// <copyright file="IArticleFullModel.cs" company="ProcessingTools">
+// Copyright (c) 2018 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Models.Contracts.Documents.Meta
+{
+    using System.Collections.Generic;
+    using ProcessingTools.Models.Contracts.Documents.Articles;
+    using ProcessingTools.Models.Contracts.Documents.Documents;
+    using ProcessingTools.Models.Contracts.Documents.Journals;
+    using ProcessingTools.Models.Contracts.Documents.Publishers;
+
+    /// <summary>
+    /// Article full model.
+    /// </summary>
+    public interface IArticleFullModel
+    {
+        /// <summary>
+        /// Gets the article.
+        /// </summary>
+        IArticleModel Article { get; }
+
+        /// <summary>
+        /// Gets the journal.
+        /// </summary>
+        IJournalModel Journal { get; }
+
+        /// <summary>
+        /// Gets the publisher.
+        /// </summary>
+        IPublisherModel Publisher { get; }
+
+        /// <summary>
+        /// Gets documents of the article.
+        /// </summary>
+        IEnumerable<IDocumentModel> Documents { get; }
+    }
+}
---
+++ src/ProcessingTools/Services/ProcessingTools.Services.Models/Documents/Meta/ArticleFullModel.cs
@@ -0,0 +1,39 @@
+﻿// <copyright file="ArticleFullModel.cs" company="ProcessingTools">
+// Copyright (c) 2018 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Services.Models.Documents.Meta
+{
+    using System.Collections.Generic;
+    using ProcessingTools.Models.Contracts.Documents.Articles;
+    using ProcessingTools.Models.Contracts.Documents.Documents;
+    using ProcessingTools.Models.Contracts.Documents.Journals;
+    using ProcessingTools.Models.Contracts.Documents.Meta;
+    using ProcessingTools.Models.Contracts.Documents.Publishers;
+
+    /// <summary>
+    /// Article full model.
+    /// </summary>
+    public class ArticleFullModel : IArticleFullModel
+    {
+        /// <summary>
+        /// Gets or sets the article.
+        /// </summary>
+        public IArticleModel Article { get; set; }
+
+        /// <summary>
+        /// Gets or sets the journal.
+        /// </summary>
+        public IJournalModel Journal { get; set; }
+
+        /// <summary>
+        /// Gets or sets the publisher.
+        /// </summary>
+        public IPublisherModel Publisher { get; set; }
+
+        /// <summary>
+        /// Gets or sets the documents of the article.
+        /// </summary>
+        public IEnumerable<IDocumentModel> Documents { get; set; }
+    }
+}
