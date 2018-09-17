9b6d8c6c8bba9030b453c1b9e5f510a32db8ea38
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2018-09-17 11:48:18 +0300

    Remove project ProcessingTools.Tests.Library

--- src/ProcessingTools/Infrastructure.Tests/Tagger.Commands.Tests/Unit.Tests/Commands/ParseCoordinatesCommandUnitTests.cs
+++ src/ProcessingTools/Infrastructure.Tests/Tagger.Commands.Tests/Unit.Tests/Commands/ParseCoordinatesCommandUnitTests.cs
@@ -7,10 +7,10 @@
     using NUnit.Framework;
     using ProcessingTools.Commands.Models.Contracts;
     using ProcessingTools.Commands.Tagger;
+    using ProcessingTools.Common.Code.Tests;
     using ProcessingTools.Common.Constants.Configuration;
     using ProcessingTools.Contracts;
     using ProcessingTools.Processors.Contracts.Geo.Coordinates;
-    using ProcessingTools.Tests.Library;
 
     [TestFixture(Author = "Bozhin Karaivanov", Category = "Unit", TestOf = typeof(ParseCoordinatesCommand))]
     public class ParseCoordinatesCommandUnitTests
--- src/ProcessingTools/Infrastructure.Tests/Tagger.Commands.Tests/Unit.Tests/Commands/ParseTreatmentMetaWithCatalogueOfLifeCommandUnitTests.cs
+++ src/ProcessingTools/Infrastructure.Tests/Tagger.Commands.Tests/Unit.Tests/Commands/ParseTreatmentMetaWithCatalogueOfLifeCommandUnitTests.cs
@@ -6,11 +6,11 @@
     using NUnit.Framework;
     using ProcessingTools.Commands.Models.Contracts;
     using ProcessingTools.Commands.Tagger;
+    using ProcessingTools.Common.Code.Tests;
     using ProcessingTools.Common.Constants.Configuration;
     using ProcessingTools.Contracts;
     using ProcessingTools.Processors.Contracts.Bio.Taxonomy;
     using ProcessingTools.Services.Contracts.Bio.Taxonomy;
-    using ProcessingTools.Tests.Library;
 
     [TestFixture(Author = "Bozhin Karaivanov", Category = "Unit", TestOf = typeof(ParseTreatmentMetaWithCatalogueOfLifeCommand))]
     public class ParseTreatmentMetaWithCatalogueOfLifeCommandUnitTests
