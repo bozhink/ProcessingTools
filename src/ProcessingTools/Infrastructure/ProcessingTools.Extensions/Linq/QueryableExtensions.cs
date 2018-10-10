c53bdd0cc454456e66602bcf528f504923e8eb8f
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2018-10-10 15:58:55 +0300

    Update QueryableExtensions

--- src/ProcessingTools/Infrastructure/ProcessingTools.Extensions/Linq/QueryableExtensions.cs
+++ src/ProcessingTools/Infrastructure/ProcessingTools.Extensions/Linq/QueryableExtensions.cs
@@ -6,6 +6,8 @@ namespace ProcessingTools.Extensions.Linq
 {
     using System;
     using System.Linq;
+    using System.Linq.Expressions;
+    using System.Reflection;
     using ProcessingTools.Common.Enumerations;
     using ProcessingTools.Extensions.Linq.Expressions;
 
@@ -87,6 +89,120 @@ namespace ProcessingTools.Extensions.Linq
                 default:
                     return source.OrderBy(keySelector: propertyName.ToExpressionFromPropertyName<TSource, object>());
             }
+        }
+
+        /// <summary>
+        /// Order by sort expression.
+        /// </summary>
+        /// <typeparam name="T">Type of the source items.</typeparam>
+        /// <param name="source">A sequence of values to order.</param>
+        /// <param name="sortExpression">Sort expression to be applied. It must be in the format "&lt;columnName&gt;[ DESCENDING]"</param>
+        /// <param name="defaultSort">Default sort column name.</param>
+        /// <returns>An <see cref="IOrderedQueryable{T}"/> whose elements are sorted according to the sort expression.</returns>
+        public static IOrderedQueryable<T> OrderBySortExpression<T>(this IQueryable<T> source, string sortExpression, string defaultSort = "ID")
+        {
+            string orderMethod = nameof(Queryable.OrderBy);
+
+            if (string.IsNullOrEmpty(sortExpression))
+            {
+                sortExpression = defaultSort;
+            }
+
+            string[] sortExp = null;
+
+            if (sortExpression.Contains(","))
+            {
+                sortExp = sortExpression.Split(',');
+            }
+            else
+            {
+                sortExp = new string[] { sortExpression };
+            }
+
+            object result = source;
+
+            for (int i = 0; i < sortExp.Length; i++)
+            {
+                string[] split = sortExp[i].Split(' ');
+                string sort = split[0];
+                bool descending = split.Length > 1 && split[1].ToUpperInvariant().StartsWith("DESCENDING");
+                if (i == 0)
+                {
+                    orderMethod = descending ? nameof(Queryable.OrderByDescending) : nameof(Queryable.OrderBy);
+                }
+                else
+                {
+                    orderMethod = descending ? nameof(Queryable.ThenByDescending) : nameof(Queryable.ThenBy);
+                }
+
+                ParameterExpression arg = Expression.Parameter(typeof(T), "x");
+                Expression expr = arg;
+                Type type = typeof(T);
+
+                // Split for nested properties
+                string[] properties = sort.ToUpperInvariant().Split('.');
+
+                foreach (string property in properties)
+                {
+                    PropertyInfo propertyInfo = type.GetProperties().FirstOrDefault(t => t.Name.ToUpperInvariant() == property);
+                    expr = Expression.Property(expr, propertyInfo);
+                    type = propertyInfo.PropertyType;
+                }
+
+                // Construct lambda
+                Type delegateType = typeof(Func<,>).MakeGenericType(typeof(T), type);
+                LambdaExpression lambda = Expression.Lambda(delegateType, expr, arg);
+
+                var method = typeof(Queryable).GetMethods()
+                    .Single(
+                        m =>
+                            m.Name == orderMethod &&
+                            m.IsGenericMethodDefinition &&
+                            m.GetGenericArguments().Length == 2 &&
+                            m.GetParameters().Length == 2)
+                    .MakeGenericMethod(typeof(T), type);
+
+                result = method.Invoke(null, new object[] { result, lambda });
+            }
+
+            return (IOrderedQueryable<T>)result;
+        }
+
+        /// <summary>
+        /// Applies simple filter on a sequence of values.
+        /// </summary>
+        /// <typeparam name="T">Type of the source items.</typeparam>
+        /// <param name="source">A sequence of values to be filtered.</param>
+        /// <param name="filterExpression">Filter expression to be applied. It must be of the form &lt;propertyNam&gt;=&lt;value&gt;.</param>
+        /// <returns>An <see cref="IQueryable{T}"/> whose elements are filtered according to the filter expression.</returns>
+        public static IQueryable<T> SimpleFilter<T>(this IQueryable<T> source, string filterExpression)
+        {
+            string[] filterExpressionValues = filterExpression?.Split('=') ?? new string[] { };
+            if (filterExpressionValues.Length > 1)
+            {
+                string columnName = filterExpressionValues[0].ToUpperInvariant();
+                string value = filterExpressionValues[1];
+
+                PropertyInfo property = typeof(T).GetProperties().FirstOrDefault(w => w.Name.ToUpperInvariant() == columnName);
+                if (property != null)
+                {
+                    ParameterExpression parameter = Expression.Parameter(typeof(T), "x");
+                    Expression leftExpression = Expression.Property(parameter, property);
+                    Expression rightExpression = Expression.Constant(value);
+                    var experssion = Expression.Equal(leftExpression, rightExpression);
+                    var lambda = Expression.Lambda<Func<T, bool>>(experssion, parameter);
+
+                    var method = typeof(Queryable).GetMethods()
+                        .Single(m => m.Name == nameof(Queryable.Where) && m.IsGenericMethodDefinition)
+                        .MakeGenericMethod(typeof(T));
+
+                    var result = method.Invoke(null, new object[] { source, lambda });
+
+                    return (IQueryable<T>)result;
+                }
+            }
+
+            return source;
         }
     }
 }
