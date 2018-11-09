bfc840abdeba3d6274ccd8754e59755354c5857e
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2018-11-09 14:57:57 +0200

    Add ProcessingTools.Services.Files.MediatypesDataService

---
+++ src/ProcessingTools/Services/ProcessingTools.Services.Models/Files/Mediatypes/MediatypeMetaModel.cs
@@ -0,0 +1,23 @@
+﻿// <copyright file="MediatypeMetaModel.cs" company="ProcessingTools">
+// Copyright (c) 2018 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Services.Models.Files.Mediatypes
+{
+    using ProcessingTools.Services.Models.Contracts.Files.Mediatypes;
+
+    /// <summary>
+    /// Mediatype meta model.
+    /// </summary>
+    public class MediatypeMetaModel : IMediatypeMetaModel
+    {
+        /// <inheritdoc/>
+        public string Extension { get; set; }
+
+        /// <inheritdoc/>
+        public string MimeType { get; set; }
+
+        /// <inheritdoc/>
+        public string MimeSubtype { get; set; }
+    }
+}
---
+++ src/ProcessingTools/Services/ProcessingTools.Services.Models/Files/Mediatypes/MediatypeModel.cs
@@ -0,0 +1,42 @@
+﻿// <copyright file="MediatypeModel.cs" company="ProcessingTools">
+// Copyright (c) 2018 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Services.Models.Files.Mediatypes
+{
+    using System;
+    using ProcessingTools.Services.Models.Contracts.Files.Mediatypes;
+
+    /// <summary>
+    /// Mediatype model.
+    /// </summary>
+    public class MediatypeModel : IMediatypeModel
+    {
+        /// <inheritdoc/>
+        public string Id { get; set; }
+
+        /// <inheritdoc/>
+        public string Extension { get; set; }
+
+        /// <inheritdoc/>
+        public string MimeType { get; set; }
+
+        /// <inheritdoc/>
+        public string MimeSubtype { get; set; }
+
+        /// <inheritdoc/>
+        public string Description { get; set; }
+
+        /// <inheritdoc/>
+        public string CreatedBy { get; set; }
+
+        /// <inheritdoc/>
+        public DateTime CreatedOn { get; set; }
+
+        /// <inheritdoc/>
+        public string ModifiedBy { get; set; }
+
+        /// <inheritdoc/>
+        public DateTime ModifiedOn { get; set; }
+    }
+}
