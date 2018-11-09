273f33a503dfbc282baa2edf3b92d0801193495a
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2018-11-09 14:24:11 +0200

    Add ProcessingTools.Data.Contracts.Files.IMediatypesDataAccessObject

---
+++ src/ProcessingTools/Data/ProcessingTools.Data.Contracts/Files/IFilesDatabaseInitializer.cs
@@ -0,0 +1,13 @@
+﻿// <copyright file="IFilesDatabaseInitializer.cs" company="ProcessingTools">
+// Copyright (c) 2018 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Data.Contracts.Files
+{
+    /// <summary>
+    /// Files database initializer.
+    /// </summary>
+    public interface IFilesDatabaseInitializer : IDatabaseInitializer
+    {
+    }
+}
---
+++ src/ProcessingTools/Data/ProcessingTools.Data.Models.Contracts/Files/Mediatypes/IMediatypeDataModel.cs
@@ -0,0 +1,13 @@
+﻿// <copyright file="IMediatypeDataModel.cs" company="ProcessingTools">
+// Copyright (c) 2018 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Data.Models.Contracts.Files.Mediatypes
+{
+    /// <summary>
+    /// Mediatype data model.
+    /// </summary>
+    public interface IMediatypeDataModel : IDataModel, ProcessingTools.Models.Contracts.Files.Mediatypes.IMediatypeModel
+    {
+    }
+}
