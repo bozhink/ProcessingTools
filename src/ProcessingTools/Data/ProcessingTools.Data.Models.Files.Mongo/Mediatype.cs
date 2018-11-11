07a54416ae6447322d66c67b85c6cd865795f43c
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2018-11-11 18:09:58 +0200

    Add ProcessingTools.Data.Models.Files.Mongo.Mediatype

---
+++ src/ProcessingTools/Data/ProcessingTools.Data.Models.Files.Mongo/Mediatype.cs
@@ -0,0 +1,68 @@
+﻿// <copyright file="Mediatype.cs" company="ProcessingTools">
+// Copyright (c) 2018 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Data.Models.Files.Mongo
+{
+    using System;
+    using MongoDB.Bson;
+    using MongoDB.Bson.Serialization.Attributes;
+    using ProcessingTools.Common.Attributes;
+    using ProcessingTools.Data.Models.Contracts.Files.Mediatypes;
+
+    /// <summary>
+    /// Mediatype
+    /// </summary>
+    [CollectionName("mediatypes")]
+    public class Mediatype : IMediatypeDetailsDataModel
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Mediatype"/> class.
+        /// </summary>
+        public Mediatype()
+        {
+            this.ObjectId = Guid.NewGuid();
+        }
+
+        /// <inheritdoc/>
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        /// <inheritdoc/>
+        [BsonRequired]
+        [BsonRepresentation(BsonType.String)]
+        public Guid ObjectId { get; set; }
+
+        /// <inheritdoc/>
+        [BsonRequired]
+        public string Extension { get; set; }
+
+        /// <inheritdoc/>
+        [BsonRequired]
+        public string MimeType { get; set; }
+
+        /// <inheritdoc/>
+        [BsonRequired]
+        public string MimeSubtype { get; set; }
+
+        /// <inheritdoc/>
+        public string Description { get; set; }
+
+        /// <inheritdoc/>
+        public string CreatedBy { get; set; }
+
+        /// <inheritdoc/>
+        public DateTime CreatedOn { get; set; }
+
+        /// <inheritdoc/>
+        public string ModifiedBy { get; set; }
+
+        /// <inheritdoc/>
+        public DateTime ModifiedOn { get; set; }
+
+        /// <inheritdoc/>
+        [BsonIgnore]
+        public string ContentType => $"{this.MimeType}/{this.MimeSubtype}";
+    }
+}
