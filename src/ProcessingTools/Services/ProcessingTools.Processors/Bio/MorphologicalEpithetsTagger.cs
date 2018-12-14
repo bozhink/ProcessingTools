88d4cd455b4728ee0146194dc8a2efc7989a3a06
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2018-12-14 12:32:09 +0200

    Update processors

--- src/ProcessingTools/Services/ProcessingTools.Processors/Bio/MorphologicalEpithetsTagger.cs
+++ src/ProcessingTools/Services/ProcessingTools.Processors/Bio/MorphologicalEpithetsTagger.cs
@@ -1,4 +1,8 @@
-﻿namespace ProcessingTools.Processors.Processors.Bio
+﻿// <copyright file="MorphologicalEpithetsTagger.cs" company="ProcessingTools">
+// Copyright (c) 2018 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Processors.Bio
 {
     using ProcessingTools.Data.Miners.Contracts.Bio;
     using ProcessingTools.Processors.Abstractions;
@@ -6,8 +10,17 @@
     using ProcessingTools.Processors.Contracts.Bio;
     using ProcessingTools.Processors.Models.Contracts;
 
+    /// <summary>
+    /// Morphological epithets tagger.
+    /// </summary>
     public class MorphologicalEpithetsTagger : StringDataMinerTagger<IMorphologicalEpithetsDataMiner, IMorphologicalEpithetTagModelProvider>, IMorphologicalEpithetsTagger
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MorphologicalEpithetsTagger"/> class.
+        /// </summary>
+        /// <param name="evaluator">Data miner evaluator.</param>
+        /// <param name="tagger">String tagger.</param>
+        /// <param name="tagModelProvider">Tag model provider.</param>
         public MorphologicalEpithetsTagger(IStringDataMinerEvaluator<IMorphologicalEpithetsDataMiner> evaluator, IStringTagger tagger, IMorphologicalEpithetTagModelProvider tagModelProvider)
             : base(evaluator, tagger, tagModelProvider)
         {
--- src/ProcessingTools/Services/ProcessingTools.Processors/Bio/SpecimenCountTagger.cs
+++ src/ProcessingTools/Services/ProcessingTools.Processors/Bio/SpecimenCountTagger.cs
@@ -1,4 +1,8 @@
-﻿namespace ProcessingTools.Processors.Processors.Bio
+﻿// <copyright file="SpecimenCountTagger.cs" company="ProcessingTools">
+// Copyright (c) 2018 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Processors.Bio
 {
     using ProcessingTools.Data.Miners.Contracts.Bio;
     using ProcessingTools.Processors.Abstractions;
@@ -6,8 +10,17 @@
     using ProcessingTools.Processors.Contracts.Bio;
     using ProcessingTools.Processors.Models.Contracts;
 
+    /// <summary>
+    /// Specimen count tagger.
+    /// </summary>
     public class SpecimenCountTagger : StringDataMinerTagger<ISpecimenCountDataMiner, ISpecimenCountTagModelProvider>, ISpecimenCountTagger
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SpecimenCountTagger"/> class.
+        /// </summary>
+        /// <param name="evaluator">Data miner evaluator.</param>
+        /// <param name="tagger">String tagger.</param>
+        /// <param name="tagModelProvider">Tag model provider.</param>
         public SpecimenCountTagger(IStringDataMinerEvaluator<ISpecimenCountDataMiner> evaluator, IStringTagger tagger, ISpecimenCountTagModelProvider tagModelProvider)
             : base(evaluator, tagger, tagModelProvider)
         {
