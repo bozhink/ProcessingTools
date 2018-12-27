8115d527fd50a10a368d9fd47e88a712c0801b2b
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2018-12-27 21:23:43 +0200

    Add ProcessingTools.Web.Models.Bio.Taxonomy

---
+++ src/ProcessingTools/Services/ProcessingTools.Web.Models/Bio/Taxonomy/BlackList/ItemsRequestModel.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file="ItemsRequestModel.cs" company="ProcessingTools">
+// Copyright (c) 2018 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Web.Models.Bio.Taxonomy.BlackList
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Blacklist items request model.
+    /// </summary>
+    public class ItemsRequestModel
+    {
+        /// <summary>
+        /// Gets or sets the blacklist items.
+        /// </summary>
+        public IList<ItemRequestModel> Items { get; set; } = new List<ItemRequestModel>();
+    }
+}
---
+++ src/ProcessingTools/Services/ProcessingTools.Web.Models/Bio/Taxonomy/TaxonRanks/TaxonRankResponseModel.cs
@@ -0,0 +1,22 @@
+﻿// <copyright file="TaxonRankResponseModel.cs" company="ProcessingTools">
+// Copyright (c) 2018 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Web.Models.Bio.Taxonomy.TaxonRanks
+{
+    /// <summary>
+    /// Taxon rank response model.
+    /// </summary>
+    public class TaxonRankResponseModel
+    {
+        /// <summary>
+        /// Gets or sets the taxon name.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the taxon rank.
+        /// </summary>
+        public string Rank { get; set; }
+    }
+}
