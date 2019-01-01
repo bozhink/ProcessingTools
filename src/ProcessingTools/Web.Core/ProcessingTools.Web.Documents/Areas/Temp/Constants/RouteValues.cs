8f26bb6fdc43365a13c5006f246a22dedadee7d6
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2019-01-01 16:54:23 +0200

    Remove project ProcessingTools.Web (break)

---
+++ src/ProcessingTools/Web.Core/ProcessingTools.Web.Documents/Areas/Temp/Constants/RouteValues.cs
@@ -0,0 +1,9 @@
+﻿namespace ProcessingTools.Web.Documents.Areas.Temp.Constants
+{
+    public static class RouteValues
+    {
+        public const string DefaultContollerName = "Home";
+        public const string HelpActionName = "Help";
+        public const string IndexActionName = "Index";
+    }
+}
---
+++ src/ProcessingTools/Web.Core/ProcessingTools.Web.Documents/Areas/Temp/Controllers/BaseMvcController.cs
@@ -0,0 +1,17 @@
+﻿namespace ProcessingTools.Web.Abstractions.Controllers
+{
+    using System.Web.Mvc;
+
+    public abstract class BaseMvcController : Controller
+    {
+        protected string UserId => "fake-user-id";
+
+        protected void AddErrors(params string[] errors)
+        {
+            foreach (var error in errors)
+            {
+                this.ModelState.AddModelError("Error", error);
+            }
+        }
+    }
+}
