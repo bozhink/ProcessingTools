750af3f20bfd64e1cb587ff71f0ed7fef3b44079
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2019-03-08 17:19:19 +0200

    Add IUnzipFilesAction

---
+++ src/ProcessingTools/Infrastructure/ProcessingTools.Tasks.Models.Contracts/Actions/IUnzipFilesAction.cs
@@ -0,0 +1,27 @@
+﻿// <copyright file="IUnzipFilesAction.cs" company="ProcessingTools">
+// Copyright (c) 2019 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Tasks.Models.Contracts.Actions
+{
+    /// <summary>
+    /// Unzip files action.
+    /// </summary>
+    public interface IUnzipFilesAction : IFileOverwritable
+    {
+        /// <summary>
+        /// Gets or sets the source directory of archive files to be decompressed.
+        /// </summary>
+        string SourceDirectory { get; set; }
+
+        /// <summary>
+        /// Gets or sets the wildcard to filter the archive files to be decompressed in the source directory.
+        /// </summary>
+        string FilterWildcard { get; set; }
+
+        /// <summary>
+        /// Gets or sets the destination directory where archive files have to be decompressed.
+        /// </summary>
+        string DestinationDirectory { get; set; }
+    }
+}
