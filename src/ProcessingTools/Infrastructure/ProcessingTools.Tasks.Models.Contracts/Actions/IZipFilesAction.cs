d08c519edf34feb197458f2223ac742920b2ffdb
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2019-03-08 17:19:29 +0200

    Add IZipFilesAction

---
+++ src/ProcessingTools/Infrastructure/ProcessingTools.Tasks.Models.Contracts/Actions/IZipFilesAction.cs
@@ -0,0 +1,27 @@
+﻿// <copyright file="IZipFilesAction.cs" company="ProcessingTools">
+// Copyright (c) 2019 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Tasks.Models.Contracts.Actions
+{
+    /// <summary>
+    /// Zip files action.
+    /// </summary>
+    public interface IZipFilesAction : IFileOverwritable
+    {
+        /// <summary>
+        /// Gets or sets the source directory of files to be archived.
+        /// </summary>
+        string SourceDirectory { get; set; }
+
+        /// <summary>
+        /// Gets or sets the wildcard to filter the files to be archived in the source directory.
+        /// </summary>
+        string FilterWildcard { get; set; }
+
+        /// <summary>
+        /// Gets or sets the destination directory where archive file have to be stored.
+        /// </summary>
+        string DestinationDirectory { get; set; }
+    }
+}
