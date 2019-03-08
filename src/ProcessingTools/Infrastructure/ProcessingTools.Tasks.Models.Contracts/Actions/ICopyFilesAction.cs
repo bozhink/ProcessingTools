78737ad51584804eed05de35c517800900659eb8
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2019-03-08 16:28:49 +0200

    Add ICopyFilesAction

---
+++ src/ProcessingTools/Infrastructure/ProcessingTools.Tasks.Models.Contracts/Actions/ICopyFilesAction.cs
@@ -0,0 +1,27 @@
+﻿// <copyright file="ICopyFilesAction.cs" company="ProcessingTools">
+// Copyright (c) 2019 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Tasks.Models.Contracts.Actions
+{
+    /// <summary>
+    /// Copy files action.
+    /// </summary>
+    public interface ICopyFilesAction : IFileOverwritable
+    {
+        /// <summary>
+        /// Gets or sets the source directory of files to be copied.
+        /// </summary>
+        string SourceDirectory { get; set; }
+
+        /// <summary>
+        /// Gets or sets the wildcard to filter the files to be copied in the source directory.
+        /// </summary>
+        string FilterWildcard { get; set; }
+
+        /// <summary>
+        /// Gets or sets the destination directory where files have to be copied.
+        /// </summary>
+        string DestinationDirectory { get; set; }
+    }
+}
