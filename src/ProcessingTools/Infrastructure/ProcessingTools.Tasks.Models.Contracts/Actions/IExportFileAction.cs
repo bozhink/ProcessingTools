464ce0b25fc117108c136750954ad736f5ffcb93
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2019-03-08 16:31:44 +0200

    Add IExportFileAction

---
+++ src/ProcessingTools/Infrastructure/ProcessingTools.Tasks.Models.Contracts/Actions/IExportFileAction.cs
@@ -0,0 +1,22 @@
+﻿// <copyright file="IExportFileAction.cs" company="ProcessingTools">
+// Copyright (c) 2019 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Tasks.Models.Contracts.Actions
+{
+    /// <summary>
+    /// Export file action.
+    /// </summary>
+    public interface IExportFileAction : IFileOverwritable
+    {
+        /// <summary>
+        /// Gets or sets the destination directory where the file have to be exported.
+        /// </summary>
+        string DestinationDirectory { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the exported file.
+        /// </summary>
+        string FileName { get; set; }
+    }
+}
