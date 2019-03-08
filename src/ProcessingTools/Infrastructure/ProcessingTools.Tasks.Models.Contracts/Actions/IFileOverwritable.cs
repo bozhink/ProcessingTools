b2f6c393dd69a2f86c36319ec94902dbcdfcc596
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2019-03-08 16:30:19 +0200

    Add IFileOverwritable

---
+++ src/ProcessingTools/Infrastructure/ProcessingTools.Tasks.Models.Contracts/Actions/IFileOverwritable.cs
@@ -0,0 +1,17 @@
+﻿// <copyright file="IFileOverwritable.cs" company="ProcessingTools">
+// Copyright (c) 2019 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Tasks.Models.Contracts.Actions
+{
+    /// <summary>
+    /// File overwritable.
+    /// </summary>
+    public interface IFileOverwritable
+    {
+        /// <summary>
+        /// Gets or sets a value indicating whether files have to be overwritten.
+        /// </summary>
+        bool OverwriteFile { get; set; }
+    }
+}
