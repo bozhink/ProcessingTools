b554f92ea599e885ef5859fa4827632c61464af2
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2019-03-08 16:29:02 +0200

    Add IDeleteFilesAction

---
+++ src/ProcessingTools/Infrastructure/ProcessingTools.Tasks.Models.Contracts/Actions/IDeleteFilesAction.cs
@@ -0,0 +1,22 @@
+﻿// <copyright file="IDeleteFilesAction.cs" company="ProcessingTools">
+// Copyright (c) 2019 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Tasks.Models.Contracts.Actions
+{
+    /// <summary>
+    /// Delete files action.
+    /// </summary>
+    public interface IDeleteFilesAction
+    {
+        /// <summary>
+        /// Gets or sets the source directory of files to be deleted.
+        /// </summary>
+        string SourceDirectory { get; set; }
+
+        /// <summary>
+        /// Gets or sets the wildcard to filter the files to be deleted in the source directory.
+        /// </summary>
+        string FilterWildcard { get; set; }
+    }
+}
