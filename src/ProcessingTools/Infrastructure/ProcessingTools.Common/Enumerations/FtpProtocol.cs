4c16a5d5d374e2d351d71417b62706cf995a8fd6
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2019-03-08 16:23:42 +0200

    Add FtpProtocol

---
+++ src/ProcessingTools/Infrastructure/ProcessingTools.Common/Enumerations/FtpProtocol.cs
@@ -0,0 +1,27 @@
+﻿// <copyright file="FtpProtocol.cs" company="ProcessingTools">
+// Copyright (c) 2019 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Common.Enumerations
+{
+    /// <summary>
+    /// FTP protocol.
+    /// </summary>
+    public enum FtpProtocol
+    {
+        /// <summary>
+        /// FTP
+        /// </summary>
+        Ftp,
+
+        /// <summary>
+        /// SFTP
+        /// </summary>
+        Sftp,
+
+        /// <summary>
+        /// FTPS
+        /// </summary>
+        Ftps,
+    }
+}
