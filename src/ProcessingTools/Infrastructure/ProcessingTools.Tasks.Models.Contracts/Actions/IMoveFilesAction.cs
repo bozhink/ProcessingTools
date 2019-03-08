85dcc5208fab5d882d40c7ac3b4dbf197ae9342d
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2019-03-08 16:29:20 +0200

    Add IMoveFilesAction

---
+++ src/ProcessingTools/Infrastructure/ProcessingTools.Tasks.Models.Contracts/Actions/IMoveFilesAction.cs
@@ -0,0 +1,27 @@
+﻿// <copyright file="IMoveFilesAction.cs" company="ProcessingTools">
+// Copyright (c) 2019 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Tasks.Models.Contracts.Actions
+{
+    /// <summary>
+    /// Move files action.
+    /// </summary>
+    public interface IMoveFilesAction : IFileOverwritable
+    {
+        /// <summary>
+        /// Gets or sets the source directory of files to be moved.
+        /// </summary>
+        string SourceDirectory { get; set; }
+
+        /// <summary>
+        /// Gets or sets the wildcard to filter the files to be moved in the source directory.
+        /// </summary>
+        string FilterWildcard { get; set; }
+
+        /// <summary>
+        /// Gets or sets the destination directory where files have to be moved.
+        /// </summary>
+        string DestinationDirectory { get; set; }
+    }
+}
