e930122b84285f67192d93c36c46f02bcd761f67
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2019-03-19 13:02:28 +0200

    Add project ProcessingTools.PowerShell

---
+++ src/ProcessingTools/Desktop/ProcessingTools.PowerShell/Program.cs
@@ -0,0 +1,22 @@
+﻿// <copyright file="Program.cs" company="ProcessingTools">
+// Copyright (c) 2019 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.PowerShell
+{
+    using System;
+
+    /// <summary>
+    /// Main program.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// Main method.
+        /// </summary>
+        public static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
