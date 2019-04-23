49ff72b91989d35f2929a14d12af2c5978b87754
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2019-04-23 10:22:35 +0300

    Add ProcessingTools.Security.SHA256Hasher

---
+++ src/ProcessingTools/Infrastructure/ProcessingTools.Security/SHA256Hasher.cs
@@ -0,0 +1,27 @@
+﻿// <copyright file="SHA256Hasher.cs" company="ProcessingTools">
+// Copyright (c) 2019 ProcessingTools. All rights reserved.
+// </copyright>
+
+#pragma warning disable S101 // Types should be named in PascalCase
+
+namespace ProcessingTools.Security
+{
+    using System.Security.Cryptography;
+    using ProcessingTools.Common.Constants;
+
+    /// <summary>
+    /// SHA256 hasher
+    /// </summary>
+    public class SHA256Hasher : BaseHasher
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SHA256Hasher"/> class.
+        /// </summary>
+        public SHA256Hasher()
+            : base(SHA256.Create(), Defaults.Encoding)
+        {
+        }
+    }
+}
+
+#pragma warning restore S101 // Types should be named in PascalCase
