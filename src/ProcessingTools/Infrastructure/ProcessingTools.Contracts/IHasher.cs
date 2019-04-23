83e09e07ac67aa5001578181dfadb183accc6330
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2019-04-23 09:55:08 +0300

    Add IHasher

---
+++ src/ProcessingTools/Infrastructure/ProcessingTools.Contracts/IHasher.cs
@@ -0,0 +1,26 @@
+﻿// <copyright file="IHasher.cs" company="ProcessingTools">
+// Copyright (c) 2019 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Contracts
+{
+    /// <summary>
+    /// Hasher: hashing utility.
+    /// </summary>
+    public interface IHasher
+    {
+        /// <summary>
+        /// Compute the hash of source string in HEX format.
+        /// </summary>
+        /// <param name="source">Source string to be evaluated.</param>
+        /// <returns>Hash string in HEX format.</returns>
+        string ComputeHash(string source);
+
+        /// <summary>
+        /// Compute the hash of source string in Base64 format.
+        /// </summary>
+        /// <param name="source">Source string to be evaluated.</param>
+        /// <returns>Hash string in Base64 format.</returns>
+        string ComputeHashBase64(string source);
+    }
+}
