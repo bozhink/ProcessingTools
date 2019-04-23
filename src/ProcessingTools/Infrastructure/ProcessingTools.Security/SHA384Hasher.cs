458789f8ceb9e89a2e8df5e8244b325f738523ad
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2019-04-23 10:27:30 +0300

    Add SHA384Hasher

---
+++ src/ProcessingTools/Infrastructure/ProcessingTools.Security/SHA384Hasher.cs
@@ -0,0 +1,27 @@
+﻿// <copyright file="SHA384Hasher.cs" company="ProcessingTools">
+// Copyright (c) 2019 ProcessingTools. All rights reserved.
+// </copyright>
+
+#pragma warning disable S101 // Types should be named in PascalCase
+
+namespace ProcessingTools.Security
+{
+    using System.Security.Cryptography;
+    using ProcessingTools.Common.Constants;
+
+    /// <summary>
+    /// SHA384 hasher.
+    /// </summary>
+    public class SHA384Hasher : BaseHasher
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SHA384Hasher"/> class.
+        /// </summary>
+        public SHA384Hasher()
+            : base(SHA384.Create(), Defaults.Encoding)
+        {
+        }
+    }
+}
+
+#pragma warning restore S101 // Types should be named in PascalCase
