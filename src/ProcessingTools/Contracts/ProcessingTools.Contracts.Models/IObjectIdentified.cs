68ba2a365f0369c4ded7bef54d62d20c8b109551
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2019-06-13 13:39:58 +0300

    Add IObjectIdentified

---
+++ src/ProcessingTools/Contracts/ProcessingTools.Contracts.Models/IObjectIdentified.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file="IObjectIdentified.cs" company="ProcessingTools">
+// Copyright (c) 2019 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Contracts.Models
+{
+    using System;
+
+    /// <summary>
+    /// Model with object-ID.
+    /// </summary>
+    public interface IObjectIdentified
+    {
+        /// <summary>
+        /// Gets the object ID.
+        /// </summary>
+        Guid ObjectId { get; }
+    }
+}
