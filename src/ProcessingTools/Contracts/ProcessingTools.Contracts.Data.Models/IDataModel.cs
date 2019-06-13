4c08cca4cbc2ed6fe009585050eadbb9197f9660
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2019-06-13 13:43:07 +0300

    Add ProcessingTools.Contracts.Data.Models.IDataModel

---
+++ src/ProcessingTools/Contracts/ProcessingTools.Contracts.Data.Models/IDataModel.cs
@@ -0,0 +1,15 @@
+﻿// <copyright file="IDataModel.cs" company="ProcessingTools">
+// Copyright (c) 2019 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Contracts.Data.Models
+{
+    using ProcessingTools.Contracts.Models;
+
+    /// <summary>
+    /// Data model.
+    /// </summary>
+    public interface IDataModel : IStringIdentified, IObjectIdentified, ICreated, IModified
+    {
+    }
+}
