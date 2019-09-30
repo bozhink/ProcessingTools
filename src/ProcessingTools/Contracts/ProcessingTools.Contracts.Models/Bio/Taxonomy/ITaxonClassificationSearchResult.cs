97b09c3d52c8425af49735ef494deb693d72de46
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2019-09-30 12:26:39 +0300

    Add ITaxonClassificationSearchResult

---
+++ src/ProcessingTools/Contracts/ProcessingTools.Contracts.Models/Bio/Taxonomy/ITaxonClassificationSearchResult.cs
@@ -0,0 +1,13 @@
+﻿// <copyright file="ITaxonClassificationSearchResult.cs" company="ProcessingTools">
+// Copyright (c) 2019 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Contracts.Models.Bio.Taxonomy
+{
+    /// <summary>
+    /// Taxon classification search result.
+    /// </summary>
+    public interface ITaxonClassificationSearchResult : ITaxonClassification, ISearchResult
+    {
+    }
+}
