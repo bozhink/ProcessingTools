9e55d018cbabf4be4b4425d1a55345e2d2fe7830
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2019-09-30 12:27:15 +0300

    Add TaxonClassificationSearchResult

---
+++ src/ProcessingTools/Services/ProcessingTools.Services.Models/Data/Bio/Taxonomy/TaxonClassificationSearchResult.cs
@@ -0,0 +1,17 @@
+﻿// <copyright file="TaxonClassificationSearchResult.cs" company="ProcessingTools">
+// Copyright (c) 2019 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Services.Models.Data.Bio.Taxonomy
+{
+    using ProcessingTools.Contracts.Models.Bio.Taxonomy;
+
+    /// <summary>
+    /// Taxon classification search result.
+    /// </summary>
+    public class TaxonClassificationSearchResult : TaxonClassification, ITaxonClassificationSearchResult
+    {
+        /// <inheritdoc/>
+        public string SearchKey { get; set; }
+    }
+}
