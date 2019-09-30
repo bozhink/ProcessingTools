8967cc5e5becd72c6d6442892202d4dd3bb04a8a
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2019-09-30 12:27:29 +0300

    Add TaxonRankSearchResult

---
+++ src/ProcessingTools/Services/ProcessingTools.Services.Models/Data/Bio/Taxonomy/TaxonRankSearchResult.cs
@@ -0,0 +1,17 @@
+﻿// <copyright file="TaxonRankSearchResult.cs" company="ProcessingTools">
+// Copyright (c) 2019 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Services.Models.Data.Bio.Taxonomy
+{
+    using ProcessingTools.Contracts.Models.Bio.Taxonomy;
+
+    /// <summary>
+    /// Taxon rank search result.
+    /// </summary>
+    public class TaxonRankSearchResult : TaxonRank, ITaxonRankSearchResult
+    {
+        /// <inheritdoc/>
+        public string SearchKey { get; set; }
+    }
+}
