68668291382059a81010724edbca78fe4b06237d
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2019-10-01 12:32:22 +0300

    Add project ProcessingTools.Services.Integration.Tests

---
+++ src/ProcessingTools/Services.Tests/ProcessingTools.Services.Integration.Tests/AssemblySetup.cs
@@ -0,0 +1,27 @@
+﻿// <copyright file="AssemblySetup.cs" company="ProcessingTools">
+// Copyright (c) 2019 ProcessingTools. All rights reserved.
+// </copyright>
+
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("DynamicProxyGenAssembly2")]
+
+namespace ProcessingTools.Services.Integration.Tests
+{
+    using System.IO;
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Assembly setup.
+    /// </summary>
+    [SetUpFixture]
+    public static class AssemblySetup
+    {
+        /// <summary>
+        /// One-time set-up.
+        /// </summary>
+        [OneTimeSetUp]
+        public static void OneTimeSetUp()
+        {
+            Directory.SetCurrentDirectory(Path.GetDirectoryName(typeof(AssemblySetup).Assembly.Location));
+        }
+    }
+}
