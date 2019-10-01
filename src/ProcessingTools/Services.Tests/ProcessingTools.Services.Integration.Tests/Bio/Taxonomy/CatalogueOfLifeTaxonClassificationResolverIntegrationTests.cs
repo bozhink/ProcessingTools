8c350655a4cf0e10eef170591f18f0694c98c005
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2019-10-01 12:33:25 +0300

    Update services integration tests

--- src/ProcessingTools/Services.Tests/ProcessingTools.Services.Integration.Tests/Bio/Taxonomy/CatalogueOfLifeTaxonClassificationResolverIntegrationTests.cs
+++ src/ProcessingTools/Services.Tests/ProcessingTools.Services.Integration.Tests/Bio/Taxonomy/CatalogueOfLifeTaxonClassificationResolverIntegrationTests.cs
@@ -1,8 +1,8 @@
-﻿// <copyright file="CatalogueOfLifeTaxonClassificationResolverTests.cs" company="ProcessingTools">
+﻿// <copyright file="CatalogueOfLifeTaxonClassificationResolverIntegrationTests.cs" company="ProcessingTools">
 // Copyright (c) 2019 ProcessingTools. All rights reserved.
 // </copyright>
 
-namespace ProcessingTools.Services.Tests.Integration.Tests.Bio.Taxonomy
+namespace ProcessingTools.Services.Integration.Tests.Bio.Taxonomy
 {
     using System;
     using System.Linq;
@@ -10,14 +10,21 @@ namespace ProcessingTools.Services.Tests.Integration.Tests.Bio.Taxonomy
     using Moq;
     using NUnit.Framework;
     using ProcessingTools.Clients.Bio.Taxonomy.CatalogueOfLife;
+    using ProcessingTools.Clients.Models.Bio.Taxonomy.CatalogueOfLife;
     using ProcessingTools.Common.Enumerations;
     using ProcessingTools.Services.Bio.Taxonomy;
-    using ProcessingTools.Services.Net;
+    using ProcessingTools.Services.Serialization;
 
+    /// <summary>
+    /// <see cref="CatalogueOfLifeTaxonClassificationResolver"/> integration tests.
+    /// </summary>
     [TestFixture(Author = "Bozhin Karaivanov", Category = "Integration", TestOf = typeof(CatalogueOfLifeTaxonClassificationResolver))]
-    public class CatalogueOfLifeTaxonClassificationResolverTests
+    public class CatalogueOfLifeTaxonClassificationResolverIntegrationTests
     {
-        [Test(Author = "Bozhin Karaivanov", TestOf = typeof(CatalogueOfLifeTaxonClassificationResolver))]
+        /// <summary>
+        /// <see cref="CatalogueOfLifeTaxonClassificationResolver"/> default constructor should work.
+        /// </summary>
+        [Test(Author = "Bozhin Karaivanov", TestOf = typeof(CatalogueOfLifeTaxonClassificationResolver), Description = "CatalogueOfLifeTaxonClassificationResolver  default constructor should work.")]
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Reliability", "CA2000:Dispose objects before losing scope", Justification = "HttpClient")]
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Code Quality", "IDE0067:Dispose objects before losing scope", Justification = "HttpClient")]
         public void CatalogueOfLifeTaxonClassificationResolver_DefaultConstructor_ShouldWork()
@@ -27,14 +34,19 @@ namespace ProcessingTools.Services.Tests.Integration.Tests.Bio.Taxonomy
             _ = factoryMock.Setup(f => f.CreateClient())
                 .Returns(new HttpClient { BaseAddress = new Uri("http://www.catalogueoflife.org") });
 
-            var requester = new CatalogueOfLifeWebserviceClient(factoryMock.Object);
+            var deserializer = new XmlDeserializer<CatalogueOfLifeApiServiceXmlResponseModel>();
+
+            var requester = new CatalogueOfLifeWebserviceClient(factoryMock.Object, deserializer);
             var service = new CatalogueOfLifeTaxonClassificationResolver(requester);
 
             // Assert
             Assert.IsNotNull(service);
         }
 
-        [Test(Author = "Bozhin Karaivanov", TestOf = typeof(CatalogueOfLifeTaxonClassificationResolver))]
+        /// <summary>
+        /// <see cref="CatalogueOfLifeTaxonClassificationResolver"/> resolve should work.
+        /// </summary>
+        [Test(Author = "Bozhin Karaivanov", TestOf = typeof(CatalogueOfLifeTaxonClassificationResolver), Description = "CatalogueOfLifeTaxonClassificationResolver resolve should work.")]
         [MaxTime(100000)]
         [Ignore(reason: "Integration test")]
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Reliability", "CA2000:Dispose objects before losing scope", Justification = "HttpClient")]
@@ -49,7 +61,9 @@ namespace ProcessingTools.Services.Tests.Integration.Tests.Bio.Taxonomy
             _ = factoryMock.Setup(f => f.CreateClient())
                 .Returns(new HttpClient { BaseAddress = new Uri("http://www.catalogueoflife.org") });
 
-            var requester = new CatalogueOfLifeWebserviceClient(factoryMock.Object);
+            var deserializer = new XmlDeserializer<CatalogueOfLifeApiServiceXmlResponseModel>();
+
+            var requester = new CatalogueOfLifeWebserviceClient(factoryMock.Object, deserializer);
             var service = new CatalogueOfLifeTaxonClassificationResolver(requester);
 
             // Act
---
+++ src/ProcessingTools/Services.Tests/ProcessingTools.Services.Integration.Tests/Bio/Taxonomy/GbifTaxonClassificationResolverIntegrationTests.cs
@@ -0,0 +1,79 @@
+﻿// <copyright file="GbifTaxonClassificationResolverIntegrationTests.cs" company="ProcessingTools">
+// Copyright (c) 2019 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Services.Integration.Tests.Bio.Taxonomy
+{
+    using System;
+    using System.Linq;
+    using System.Net.Http;
+    using Moq;
+    using NUnit.Framework;
+    using ProcessingTools.Clients.Bio.Taxonomy.Gbif;
+    using ProcessingTools.Clients.Models.Bio.Taxonomy.Gbif.Json;
+    using ProcessingTools.Common.Enumerations;
+    using ProcessingTools.Services.Bio.Taxonomy;
+    using ProcessingTools.Services.Serialization;
+
+    /// <summary>
+    /// <see cref="GbifTaxonClassificationResolver"/> integration tests.
+    /// </summary>
+    [TestFixture(Author = "Bozhin Karaivanov", Category = "Integration", TestOf = typeof(GbifTaxonClassificationResolver))]
+    public class GbifTaxonClassificationResolverIntegrationTests
+    {
+        /// <summary>
+        /// <see cref="GbifTaxonClassificationResolver"/> default constructor should work.
+        /// </summary>
+        [Test(Author = "Bozhin Karaivanov", TestOf = typeof(GbifTaxonClassificationResolver), Description = "GbifTaxonClassificationResolver default constructor should work.")]
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Reliability", "CA2000:Dispose objects before losing scope", Justification = "HttpClient")]
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Code Quality", "IDE0067:Dispose objects before losing scope", Justification = "HttpClient")]
+        public void GbifTaxonClassificationResolver_DefaultConstructor_ShouldWork()
+        {
+            // Arrange
+            var factoryMock = new Mock<IHttpClientFactory>();
+            _ = factoryMock.Setup(f => f.CreateClient())
+                .Returns(new HttpClient { BaseAddress = new Uri("http://api.gbif.org") });
+
+            var deserializer = new NewtonsoftJsonDeserializer<GbifApiV09ResponseModel>();
+
+            var requester = new GbifApiV09Client(factoryMock.Object, deserializer);
+            var service = new GbifTaxonClassificationResolver(requester);
+
+            // Assert
+            Assert.IsNotNull(service);
+        }
+
+        /// <summary>
+        /// <see cref="GbifTaxonClassificationResolver"/> resolve should work.
+        /// </summary>
+        [Test(Author = "Bozhin Karaivanov", TestOf = typeof(GbifTaxonClassificationResolver), Description = "GbifTaxonClassificationResolver resolve should work.")]
+        [MaxTime(100000)]
+        [Ignore(reason: "Integration test")]
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Reliability", "CA2000:Dispose objects before losing scope", Justification = "HttpClient")]
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Code Quality", "IDE0067:Dispose objects before losing scope", Justification = "HttpClient")]
+        public void GbifTaxonClassificationResolver_Resolve_ShouldWork()
+        {
+            // Arrange
+            const string CanonicalName = "Coleoptera";
+            const TaxonRankType Rank = TaxonRankType.Order;
+
+            var factoryMock = new Mock<IHttpClientFactory>();
+            _ = factoryMock.Setup(f => f.CreateClient())
+                .Returns(new HttpClient { BaseAddress = new Uri("http://api.gbif.org") });
+
+            var deserializer = new NewtonsoftJsonDeserializer<GbifApiV09ResponseModel>();
+
+            var requester = new GbifApiV09Client(factoryMock.Object, deserializer);
+            var service = new GbifTaxonClassificationResolver(requester);
+
+            // Act
+            var response = service.ResolveAsync(new[] { CanonicalName }).Result;
+
+            var defaultClassification = response.FirstOrDefault();
+
+            // Assert
+            Assert.AreEqual(CanonicalName, defaultClassification.CanonicalName);
+            Assert.AreEqual(Rank, defaultClassification.Rank);
+        }
+    }
+}
