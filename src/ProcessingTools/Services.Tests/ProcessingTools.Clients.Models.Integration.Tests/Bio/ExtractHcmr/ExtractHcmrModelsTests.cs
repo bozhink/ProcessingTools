50a03340f1a7c5fe62b5a043e4b375fab86fad69
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2019-10-04 13:22:51 +0300

    Update Client models integration tests

---
+++ src/ProcessingTools/Services.Tests/ProcessingTools.Clients.Models.Integration.Tests/Bio/ExtractHcmr/ExtractHcmrModelsTests.cs
@@ -0,0 +1,69 @@
+﻿// <copyright file="ExtractHcmrModelsTests.cs" company="ProcessingTools">
+// Copyright (c) 2019 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Clients.Models.Integration.Tests.Bio.ExtractHcmr
+{
+    using System.IO;
+    using System.Linq;
+    using System.Xml;
+    using System.Xml.Serialization;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using ProcessingTools.Clients.Models.Bio.ExtractHcmr.Xml;
+
+    /// <summary>
+    /// EXTRACT HCMR models tests.
+    /// </summary>
+    [TestClass]
+    public class ExtractHcmrModelsTests
+    {
+        /// <summary>
+        /// <see cref="ExtractHcmrResponseModel"/> deserialization tests.
+        /// </summary>
+        [TestMethod]
+        public void ExtractHcmrResponseModel_Deserialization_Tests()
+        {
+            // Arrange
+            XmlSerializer serializer = new XmlSerializer(typeof(ExtractHcmrResponseModel));
+            ExtractHcmrResponseModel response = null;
+
+            string fileName = @"data-files/extract-hcmr-sample.xml";
+
+            // Act
+            using (var stream = new FileStream(fileName, FileMode.Open))
+            {
+                using (var reader = XmlReader.Create(stream))
+                {
+                    response = serializer.Deserialize(reader) as ExtractHcmrResponseModel;
+                }
+            }
+
+            // Assert
+            Assert.IsNotNull(response);
+
+            Assert.IsNotNull(response.Items);
+
+            const int NumberOfReponseItems = 3;
+
+            Assert.AreEqual(NumberOfReponseItems, response.Items.Length);
+
+            var volcanoItem = response.Items.FirstOrDefault(i => i.Name == "Volcano");
+
+            Assert.IsNotNull(volcanoItem);
+
+            Assert.AreEqual("ENVO:00000247", volcanoItem.Entities.FirstOrDefault().Identifier);
+
+            var zetaproteobacteriaItem = response.Items.FirstOrDefault(i => i.Name == "Zetaproteobacteria");
+
+            Assert.IsNotNull(zetaproteobacteriaItem);
+
+            Assert.AreEqual("580370", zetaproteobacteriaItem.Entities.FirstOrDefault().Identifier);
+
+            var sedimentsItem = response.Items.FirstOrDefault(i => i.Name == "sediments");
+
+            Assert.IsNotNull(sedimentsItem);
+
+            Assert.AreEqual("ENVO:00002007", sedimentsItem.Entities.FirstOrDefault().Identifier);
+        }
+    }
+}
---
+++ src/ProcessingTools/Services.Tests/ProcessingTools.Clients.Models.Integration.Tests/Bio/Taxonomy/GlobalNamesResolver/GlobalNamesResolverXmlModelsTests.cs
@@ -0,0 +1,59 @@
+﻿// <copyright file="GlobalNamesResolverXmlModelsTests.cs" company="ProcessingTools">
+// Copyright (c) 2019 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Clients.Models.Integration.Tests.Bio.Taxonomy.GlobalNamesResolver
+{
+    using System.IO;
+    using System.Xml;
+    using System.Xml.Serialization;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using ProcessingTools.Clients.Models.Bio.Taxonomy.GlobalNamesResolver.Xml;
+
+    /// <summary>
+    /// Global Names Resolver XML models tests.
+    /// </summary>
+    [TestClass]
+    public class GlobalNamesResolverXmlModelsTests
+    {
+        /// <summary>
+        /// Global Names Resolver XML models deserialization of first sample file should work.
+        /// </summary>
+        [TestMethod]
+        public void GlobalNamesResolver_XmlModels_DeserializationOfFirstSampleFile_ShouldWork()
+        {
+            // Arrange
+            XmlSerializer serializer = new XmlSerializer(typeof(Hash));
+            Hash hash = null;
+
+            string fileName = @"data-files/gnr-sample-1.xml";
+
+            // Act
+            using (var stream = new FileStream(fileName, FileMode.Open))
+            {
+                using (var reader = XmlReader.Create(stream))
+                {
+                    hash = serializer.Deserialize(reader) as Hash;
+                }
+            }
+
+            // Assert
+            Assert.IsNotNull(hash);
+
+            Assert.IsNotNull(hash.Id);
+            Assert.AreEqual("hiij5yf4b0jb", hash.Id.Value);
+
+            Assert.IsNotNull(hash.Url);
+            Assert.AreEqual("http://resolver.globalnames.org/name_resolvers/hiij5yf4b0jb.xml", hash.Url.Value);
+
+            Assert.IsNotNull(hash.DataSources);
+            Assert.AreEqual("array", hash.DataSources.Type);
+
+            Assert.IsNotNull(hash.Data);
+            Assert.AreEqual("array", hash.Data.Type);
+
+            Assert.IsNotNull(hash.Data.Datum);
+            Assert.AreEqual(42, hash.Data.Datum.Length);
+        }
+    }
+}
