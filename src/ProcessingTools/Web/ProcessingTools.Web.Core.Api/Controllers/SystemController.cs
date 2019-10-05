24439f957905b7ee11615d6ed319b6468876ee4c
Bozhin Karaivanov <bozhin.karaivanov@gmail.com>
2019-10-05 22:15:24 +0300

    Update API SystemController to return all versions

--- src/ProcessingTools/Web/ProcessingTools.Web.Core.Api/Controllers/SystemController.cs
+++ src/ProcessingTools/Web/ProcessingTools.Web.Core.Api/Controllers/SystemController.cs
@@ -4,6 +4,7 @@
 
 namespace ProcessingTools.Web.Core.Api.Controllers
 {
+    using System.Linq;
     using Microsoft.AspNetCore.Mvc;
 
     /// <summary>
@@ -14,13 +15,38 @@ namespace ProcessingTools.Web.Core.Api.Controllers
     public class SystemController : ControllerBase
     {
         /// <summary>
-        /// Gets the version of current assembly.
+        /// Gets the version of entry assembly.
         /// </summary>
-        /// <returns></returns>
+        /// <returns>Version of the entry assembly as string.</returns>
         [HttpGet("version")]
         public string GetVersion()
         {
             return typeof(Startup).Assembly.GetName().Version.ToString();
+        }
+
+        /// <summary>
+        /// Gets the versions of all assemblies.
+        /// </summary>
+        /// <returns>Versions of all assemblies.</returns>
+        [HttpGet("versions")]
+        public IActionResult GetVersions()
+        {
+            var assemblies = System.AppDomain.CurrentDomain.GetAssemblies();
+
+            return this.Ok(assemblies.ToDictionary(a => a.FullName, a => a.GetName().Version.ToString()));
+        }
+
+        /// <summary>
+        /// Gets the status of the system.
+        /// </summary>
+        /// <returns>Status of the system.</returns>
+        [HttpGet("status")]
+        public IActionResult GetStatus()
+        {
+            return this.Ok(new
+            {
+                Status = 0,
+            });
         }
     }
 }
