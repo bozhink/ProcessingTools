0adebfb59bfa2c8f2e1bc016faba04b69c967506
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2019-11-06 14:37:52 +0200

    Add configuration constants

--- src/ProcessingTools/Infrastructure/ProcessingTools.Common/Constants/ConfigurationConstants.cs
+++ src/ProcessingTools/Infrastructure/ProcessingTools.Common/Constants/ConfigurationConstants.cs
@@ -10,6 +10,66 @@ namespace ProcessingTools.Common.Constants
     public static class ConfigurationConstants
     {
         /// <summary>
+        /// Abbreviations XQuery file path.
+        /// </summary>
+        public const string AbbreviationsXQueryFilePath = "StaticFiles:AbbreviationsXQueryFilePath";
+
+        /// <summary>
+        /// Facebook application ID.
+        /// </summary>
+        public const string AuthenticationFacebookAppId = "Authentication:Facebook:AppId";
+
+        /// <summary>
+        /// Facebook application secret.
+        /// </summary>
+        public const string AuthenticationFacebookAppSecret = "Authentication:Facebook:AppSecret";
+
+        /// <summary>
+        /// Google client ID.
+        /// </summary>
+        public const string AuthenticationGoogleClientId = "Authentication:Google:ClientId";
+
+        /// <summary>
+        /// Google client secret.
+        /// </summary>
+        public const string AuthenticationGoogleClientSecret = "Authentication:Google:ClientSecret";
+
+        /// <summary>
+        /// Microsoft application ID.
+        /// </summary>
+        public const string AuthenticationMicrosoftApplicationId = "Authentication:Microsoft:ApplicationId";
+
+        /// <summary>
+        /// Microsoft application password.
+        /// </summary>
+        public const string AuthenticationMicrosoftPassword = "Authentication:Microsoft:Password";
+
+        /// <summary>
+        /// Twitter consumer key.
+        /// </summary>
+        public const string AuthenticationTwitterConsumerKey = "Authentication:Twitter:ConsumerKey";
+
+        /// <summary>
+        /// Twitter consumer secret.
+        /// </summary>
+        public const string AuthenticationTwitterConsumerSecret = "Authentication:Twitter:ConsumerSecret";
+
+        /// <summary>
+        /// Biotaxonomy database MongoDB connection string name.
+        /// </summary>
+        public const string BiotaxonomyDatabaseMongoDBConnectionStringName = "BiotaxonomyDatabaseMongoDB";
+
+        /// <summary>
+        /// Biotaxonomy MongoDB database name.
+        /// </summary>
+        public const string BiotaxonomyMongoDBDatabaseName = "DatabaseNames:BiotaxonomyMongoDB";
+
+        /// <summary>
+        /// Codes remove non-code nodes XSL file path.
+        /// </summary>
+        public const string CodesRemoveNonCodeNodesXslFilePath = "StaticFiles:CodesRemoveNonCodeNodesXslFilePath";
+
+        /// <summary>
         /// DefaultConnection connection string name.
         /// </summary>
         public const string DefaultConnectionConnectionStringName = "DefaultConnection";
@@ -20,49 +80,34 @@ namespace ProcessingTools.Common.Constants
         public const string DocumentsDatabaseMongoDBConnectionStringName = "DocumentsDatabaseMongoDB";
 
         /// <summary>
-        /// Files database MongoDB connection string name.
-        /// </summary>
-        public const string FilesDatabaseMongoDBConnectionStringName = "FilesDatabaseMongoDB";
-
-        /// <summary>
-        /// Layout database MongoDB connection string name.
-        /// </summary>
-        public const string LayoutDatabaseMongoDBConnectionStringName = "LayoutDatabaseMongoDB";
-
-        /// <summary>
-        /// Biotaxonomy database MongoDB connection string name.
-        /// </summary>
-        public const string BiotaxonomyDatabaseMongoDBConnectionStringName = "BiotaxonomyDatabaseMongoDB";
-
-        /// <summary>
-        /// History database MongoDB connection string name.
-        /// </summary>
-        public const string HistoryDatabaseMongoDBConnectionStringName = "HistoryDatabaseMongoDB";
-
-        /// <summary>
         /// Documents MongoDB database name.
         /// </summary>
         public const string DocumentsMongoDBDatabaseName = "DatabaseNames:DocumentsMongoDB";
 
         /// <summary>
+        /// External links XSL file path.
+        /// </summary>
+        public const string ExternalLinksXslFilePath = "StaticFiles:ExternalLinksXslFilePath";
+
+        /// <summary>
+        /// Catalogue of Life webservice base address.
+        /// </summary>
+        public const string ExternalServicesCatalogueOfLifeWebserviceBaseAddress = "ExternalServices:CatalogueOfLife:WebserviceBaseAddress";
+
+        /// <summary>
+        /// GBIF API v0.9 base address.
+        /// </summary>
+        public const string ExternalServicesGbifApi09BaseAddress = "ExternalServices:GBIF:Api09BaseAddress";
+
+        /// <summary>
+        /// Files database MongoDB connection string name.
+        /// </summary>
+        public const string FilesDatabaseMongoDBConnectionStringName = "FilesDatabaseMongoDB";
+
+        /// <summary>
         /// Files MongoDB database name.
         /// </summary>
         public const string FilesMongoDBDatabaseName = "DatabaseNames:FilesMongoDB";
-
-        /// <summary>
-        /// Layout MongoDB database name.
-        /// </summary>
-        public const string LayoutMongoDBDatabaseName = "DatabaseNames:LayoutMongoDB";
-
-        /// <summary>
-        /// Biotaxonomy MongoDB database name.
-        /// </summary>
-        public const string BiotaxonomyMongoDBDatabaseName = "DatabaseNames:BiotaxonomyMongoDB";
-
-        /// <summary>
-        /// History MongoDB database name.
-        /// </summary>
-        public const string HistoryMongoDBDatabaseName = "DatabaseNames:HistoryMongoDB";
 
         /// <summary>
         /// Files root directory.
@@ -73,26 +118,6 @@ namespace ProcessingTools.Common.Constants
         /// Format HTML-to-XML XSL file path.
         /// </summary>
         public const string FormatHtmlToXmlXslFilePath = "StaticFiles:FormatHtmlToXmlXslFilePath";
-
-        /// <summary>
-        /// Format XML-to-HTML XSL file path.
-        /// </summary>
-        public const string FormatXmlToHtmlXslFilePath = "StaticFiles:FormatXmlToHtmlXslFilePath";
-
-        /// <summary>
-        /// Abbreviations XQuery file path.
-        /// </summary>
-        public const string AbbreviationsXQueryFilePath = "StaticFiles:AbbreviationsXQueryFilePath";
-
-        /// <summary>
-        /// Codes remove non-code nodes XSL file path.
-        /// </summary>
-        public const string CodesRemoveNonCodeNodesXslFilePath = "StaticFiles:CodesRemoveNonCodeNodesXslFilePath";
-
-        /// <summary>
-        /// External links XSL file path.
-        /// </summary>
-        public const string ExternalLinksXslFilePath = "StaticFiles:ExternalLinksXslFilePath";
 
         /// <summary>
         /// Format NLM to system XSL file path.
@@ -110,9 +135,84 @@ namespace ProcessingTools.Common.Constants
         public const string FormatTaxonTreatmentsXslFilePath = "StaticFiles:FormatTaxonTreatmentsXslFilePath";
 
         /// <summary>
+        /// Format XML-to-HTML XSL file path.
+        /// </summary>
+        public const string FormatXmlToHtmlXslFilePath = "StaticFiles:FormatXmlToHtmlXslFilePath";
+
+        /// <summary>
         /// Gavin-Laurens XSL file path.
         /// </summary>
         public const string GavinLaurensXslFilePath = "StaticFiles:GavinLaurensXslFilePath";
+
+        /// <summary>
+        /// History database MongoDB connection string name.
+        /// </summary>
+        public const string HistoryDatabaseMongoDBConnectionStringName = "HistoryDatabaseMongoDB";
+
+        /// <summary>
+        /// History MongoDB database name.
+        /// </summary>
+        public const string HistoryMongoDBDatabaseName = "DatabaseNames:HistoryMongoDB";
+
+        /// <summary>
+        /// HttpServer:Endpoints section name.
+        /// </summary>
+        public const string HttpServerEndpointsSectionName = "HttpServer:Endpoints";
+
+        /// <summary>
+        /// Kestrel section name.
+        /// </summary>
+        public const string KestrelSectionName = "Kestrel";
+
+        /// <summary>
+        /// Layout database MongoDB connection string name.
+        /// </summary>
+        public const string LayoutDatabaseMongoDBConnectionStringName = "LayoutDatabaseMongoDB";
+
+        /// <summary>
+        /// Layout MongoDB database name.
+        /// </summary>
+        public const string LayoutMongoDBDatabaseName = "DatabaseNames:LayoutMongoDB";
+
+        /// <summary>
+        /// Logging section name.
+        /// </summary>
+        public const string LoggingSectionName = "Logging";
+
+        /// <summary>
+        /// Message queue exchange name.
+        /// </summary>
+        public const string MessageQueueExchangeName = "MessageQueue:ExchangeName";
+
+        /// <summary>
+        /// Message queue host name.
+        /// </summary>
+        public const string MessageQueueHostName = "MessageQueue:HostName";
+
+        /// <summary>
+        /// Message queue password.
+        /// </summary>
+        public const string MessageQueuePassword = "MessageQueue:Password";
+
+        /// <summary>
+        /// Message queue port.
+        /// </summary>
+        public const string MessageQueuePort = "MessageQueue:Port";
+
+        /// <summary>
+        /// Message queue name.
+        /// </summary>
+        public const string MessageQueueQueueName = "MessageQueue:QueueName";
+
+        /// <summary>
+        /// Message queue user name.
+        /// </summary>
+        public const string MessageQueueUserName = "MessageQueue:UserName";
+
+        /// <summary>
+        /// Message queue virtual host.
+        /// </summary>
+        public const string MessageQueueVirtualHost = "MessageQueue:VirtualHost";
 
         /// <summary>
         /// NLM initial format XSL file path.
@@ -155,54 +255,9 @@ namespace ProcessingTools.Common.Constants
         public const string TextContentXslFilePath = "StaticFiles:TextContentXslFilePath";
 
         /// <summary>
-        /// ZooBank registration NLM XSL file path.
+        /// Users database MSSQL connection string name.
         /// </summary>
-        public const string ZooBankRegistrationNlmXslFilePath = "StaticFiles:ZooBankRegistrationNlmXslFilePath";
-
-        /// <summary>
-        /// Facebook application ID.
-        /// </summary>
-        public const string AuthenticationFacebookAppId = "Authentication:Facebook:AppId";
-
-        /// <summary>
-        /// Facebook application secret.
-        /// </summary>
-        public const string AuthenticationFacebookAppSecret = "Authentication:Facebook:AppSecret";
-
-        /// <summary>
-        /// Twitter consumer key.
-        /// </summary>
-        public const string AuthenticationTwitterConsumerKey = "Authentication:Twitter:ConsumerKey";
-
-        /// <summary>
-        /// Twitter consumer secret.
-        /// </summary>
-        public const string AuthenticationTwitterConsumerSecret = "Authentication:Twitter:ConsumerSecret";
-
-        /// <summary>
-        /// Google client ID.
-        /// </summary>
-        public const string AuthenticationGoogleClientId = "Authentication:Google:ClientId";
-
-        /// <summary>
-        /// Google client secret.
-        /// </summary>
-        public const string AuthenticationGoogleClientSecret = "Authentication:Google:ClientSecret";
-
-        /// <summary>
-        /// Microsoft application ID.
-        /// </summary>
-        public const string AuthenticationMicrosoftApplicationId = "Authentication:Microsoft:ApplicationId";
-
-        /// <summary>
-        /// Microsoft application password.
-        /// </summary>
-        public const string AuthenticationMicrosoftPassword = "Authentication:Microsoft:Password";
-
-        /// <summary>
-        /// Users database type.
-        /// </summary>
-        public const string UsersDatabaseType = "DatabaseTypes:UsersDatabase";
+        public const string UsersDatabaseMSSQLConnectionStringName = "UsersDatabaseMSSQL";
 
         /// <summary>
         /// Users database SQLite connection string name.
@@ -210,53 +265,13 @@ namespace ProcessingTools.Common.Constants
         public const string UsersDatabaseSQLiteConnectionStringName = "UsersDatabaseSQLite";
 
         /// <summary>
-        /// Users database MSSQL connection string name.
+        /// Users database type.
         /// </summary>
-        public const string UsersDatabaseMSSQLConnectionStringName = "UsersDatabaseMSSQL";
+        public const string UsersDatabaseType = "DatabaseTypes:UsersDatabase";
 
         /// <summary>
-        /// Message queue host name.
+        /// ZooBank registration NLM XSL file path.
         /// </summary>
-        public const string MessageQueueHostName = "MessageQueue:HostName";
-
-        /// <summary>
-        /// Message queue port.
-        /// </summary>
-        public const string MessageQueuePort = "MessageQueue:Port";
-
-        /// <summary>
-        /// Message queue virtual host.
-        /// </summary>
-        public const string MessageQueueVirtualHost = "MessageQueue:VirtualHost";
-
-        /// <summary>
-        /// Message queue user name.
-        /// </summary>
-        public const string MessageQueueUserName = "MessageQueue:UserName";
-
-        /// <summary>
-        /// Message queue password.
-        /// </summary>
-        public const string MessageQueuePassword = "MessageQueue:Password";
-
-        /// <summary>
-        /// Message queue name.
-        /// </summary>
-        public const string MessageQueueQueueName = "MessageQueue:QueueName";
-
-        /// <summary>
-        /// Message queue exchange name.
-        /// </summary>
-        public const string MessageQueueExchangeName = "MessageQueue:ExchangeName";
-
-        /// <summary>
-        /// Catalogue of Life webservice base address.
-        /// </summary>
-        public const string ExternalServicesCatalogueOfLifeWebserviceBaseAddress = "ExternalServices:CatalogueOfLife:WebserviceBaseAddress";
-
-        /// <summary>
-        /// GBIF API v0.9 base address.
-        /// </summary>
-        public const string ExternalServicesGbifApi09BaseAddress = "ExternalServices:GBIF:Api09BaseAddress";
+        public const string ZooBankRegistrationNlmXslFilePath = "StaticFiles:ZooBankRegistrationNlmXslFilePath";
     }
 }
