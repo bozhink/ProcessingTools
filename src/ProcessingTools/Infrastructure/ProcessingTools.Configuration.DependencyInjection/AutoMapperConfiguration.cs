244b94b2aa72321ee926f7fe54e1064c11ac88e9
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2019-11-06 14:39:10 +0200

    Add ProcessingTools.Configuration.DependencyInjection.AutoMapperConfiguration

---
+++ src/ProcessingTools/Infrastructure/ProcessingTools.Configuration.DependencyInjection/AutoMapperConfiguration.cs
@@ -0,0 +1,37 @@
+﻿// <copyright file="AutoMapperConfiguration.cs" company="ProcessingTools">
+// Copyright (c) 2019 ProcessingTools. All rights reserved.
+// </copyright>
+
+namespace ProcessingTools.Configuration.DependencyInjection
+{
+    using System;
+    using Microsoft.Extensions.DependencyInjection;
+
+    /// <summary>
+    /// <see cref="global::AutoMapper"/> configuration.
+    /// </summary>
+    public static class AutoMapperConfiguration
+    {
+        /// <summary>
+        /// Add configurations of <see cref="global::AutoMapper"/>.
+        /// </summary>
+        /// <param name="services">Instance of <see cref="IServiceCollection"/> to be configured.</param>
+        /// <returns>Updated instance of <see cref="IServiceCollection"/>.</returns>
+        public static IServiceCollection ConfigureAutoMapper(this IServiceCollection services)
+        {
+            if (services is null)
+            {
+                throw new ArgumentNullException(nameof(services));
+            }
+
+            var mapperConfiguration = new global::AutoMapper.MapperConfiguration(c =>
+            {
+                c.AddMaps(typeof(ProcessingTools.Configuration.AutoMapper.AssemblySetup).Assembly);
+            });
+
+            services.AddSingleton<global::AutoMapper.IMapper>(mapperConfiguration.CreateMapper());
+
+            return services;
+        }
+    }
+}
