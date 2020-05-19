6cbd4d5f38c046c528238d784d6dc8aeb9503878
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2020-05-19 13:46:10 +0300

    Update packages

--- src/ProcessingTools/Desktop/ProcessingTools.NlmArchiveConsoleManager/Models/FileReplacementModel.cs
+++ src/ProcessingTools/Desktop/ProcessingTools.NlmArchiveConsoleManager/Models/FileReplacementModel.cs
@@ -6,8 +6,17 @@ namespace ProcessingTools.NlmArchiveConsoleManager.Models
 {
     using ProcessingTools.NlmArchiveConsoleManager.Contracts.Models;
 
+    /// <summary>
+    /// File replacement model.
+    /// </summary>
     public class FileReplacementModel : IFileReplacementModel
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FileReplacementModel"/> class.
+        /// </summary>
+        /// <param name="destination">Destination of the files.</param>
+        /// <param name="originalFileName">Original file name.</param>
+        /// <param name="source">Source folder of the file.</param>
         public FileReplacementModel(string destination, string originalFileName, string source)
         {
             this.Destination = destination;
@@ -15,10 +24,13 @@ namespace ProcessingTools.NlmArchiveConsoleManager.Models
             this.Source = source;
         }
 
+        /// <inheritdoc/>
         public string Destination { get; private set; }
 
+        /// <inheritdoc/>
         public string OriginalFileName { get; private set; }
 
+        /// <inheritdoc/>
         public string Source { get; private set; }
     }
 }
